7b60ad27f5f709c72cbe444d1819abf8de5159f7
Yulong Wu <yulong@radixdlt.com>
2021-07-27 16:35:06 +0100

    Minor cleanup

--- scrypto-derive/src/utils.rs
+++ scrypto-derive/src/utils.rs
@@ -3,6 +3,7 @@ use std::io;
 use std::path::Path;
 use std::process::Command;
 
+#[allow(dead_code)]
 pub fn write_and_fmt<P: AsRef<Path>, S: ToString>(path: P, code: S) -> io::Result<()> {
     fs::write(&path, code.to_string())?;
 
@@ -11,14 +12,25 @@ pub fn write_and_fmt<P: AsRef<Path>, S: ToString>(path: P, code: S) -> io::Resul
     Ok(())
 }
 
+#[allow(unused_variables)]
 pub fn print_compiled_code<S: ToString>(kind: &str, code: S) {
-    if cfg!(trace) {
-        let file = "/tmp/code.rs";
-        let result = write_and_fmt(file, code);
+    #[cfg(feature = "trace")]
+    {
+        let mut path = std::env::temp_dir();
+        path.push(format!("{}.rs", uuid::Uuid::new_v4()));
 
+        let result = write_and_fmt(path.clone(), code);
         if result.is_ok() {
-            let formatted = fs::read_to_string(file).expect("Unable to open formatted code");
-            println!("{}\n{}:\n----\n{}----\n", "=".repeat(40), kind, formatted);
+            let formatted =
+                fs::read_to_string(path.clone()).expect("Unable to open formatted code");
+            println!(
+                "{}\n{}:\n{}\n{}\n",
+                "=".repeat(kind.len()),
+                kind,
+                "=".repeat(kind.len()),
+                formatted
+            );
+            fs::remove_file(path).unwrap();
         }
     }
 }
