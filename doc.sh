7e3c871f21a0884aaf1b955d44be92643f808248
Yulong Wu <35706111+iamyulong@users.noreply.github.com>
2021-08-13 17:43:24 +0100

    Update doc.sh

--- doc.sh
+++ doc.sh
@@ -7,3 +7,4 @@ cd "$(dirname "$0")"
 
 cd radix-engine;
 cargo doc --no-deps --package scrypto --package sbor --package radix-engine;
+(xdg-open ./target/doc/scrypto/index.html || open ./target/doc/scrypto/index.html);
