460c372642091588ba7e26c8435d6eb396dbfe57
Yulong Wu <yulong@radixdlt.com>
2021-09-01 12:00:18 +0100

    No serde for SBOR value

--- sbor/src/parse.rs
+++ sbor/src/parse.rs
@@ -1,73 +1,53 @@
-#[cfg(any(feature = "serde_std", feature = "serde_alloc"))]
-use serde::{Deserialize, Serialize};
-
-use crate::sbor::{Decode, Encode};
-
 use crate::rust::boxed::Box;
 use crate::rust::string::String;
-use crate::rust::string::ToString;
 use crate::*;
 
-// For enum, we use internally tagged representation for readability.
-// See: https://serde.rs/enum-representations.html
-
-/// Represents a SBOR value.
-#[cfg_attr(
-    any(feature = "serde_std", feature = "serde_alloc"),
-    derive(Serialize, Deserialize),
-    serde(tag = "type")
-)]
-#[derive(Debug, Clone, PartialEq, Eq, Decode, Encode)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Value {
     Unit,
-    Bool { value: bool },
-    I8 { value: i8 },
-    I16 { value: i16 },
-    I32 { value: i32 },
-    I64 { value: i64 },
-    I128 { value: String }, // i128/u128 is not supported by serde_json
-    U8 { value: u8 },
-    U16 { value: u16 },
-    U32 { value: u32 },
-    U64 { value: u64 },
-    U128 { value: String },
-    String { value: String },
+    Bool(bool),
+    I8(i8),
+    I16(i16),
+    I32(i32),
+    I64(i64),
+    I128(i128),
+    U8(u8),
+    U16(u16),
+    U32(u32),
+    U64(u64),
+    U128(u128),
+    String(String),
 
-    Option { value: Option<Box<Value>> },
+    Option(Option<Box<Value>>),
 
-    Box { value: Box<Value> },
+    Box(Box<Value>),
 
-    Array { elements: Vec<Value> },
+    Array(Vec<Value>),
 
-    Tuple { elements: Vec<Value> },
+    Tuple(Vec<Value>),
 
-    Struct { fields: Fields },
+    Struct(Fields),
 
-    Enum { index: u8, fields: Fields },
+    Enum(u8, Fields),
 
-    Vec { elements: Vec<Value> },
+    Vec(Vec<Value>),
 
-    TreeSet { elements: Vec<Value> },
+    TreeSet(Vec<Value>),
 
-    TreeMap { elements: Vec<(Value, Value)> },
+    TreeMap(Vec<(Value, Value)>),
 
-    HashSet { elements: Vec<Value> },
+    HashSet(Vec<Value>),
 
-    HashMap { elements: Vec<(Value, Value)> },
+    HashMap(Vec<(Value, Value)>),
 
-    Custom { ty: u8, data: Vec<u8> },
+    Custom(u8, Vec<u8>),
 }
 
-#[cfg_attr(
-    any(feature = "serde_std", feature = "serde_alloc"),
-    derive(Serialize, Deserialize),
-    serde(tag = "type")
-)]
-#[derive(Debug, Clone, PartialEq, Eq, Decode, Encode)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Fields {
-    Named { named: Vec<Value> },
+    Named(Vec<Value>),
 
-    Unnamed { unnamed: Vec<Value> },
+    Unnamed(Vec<Value>),
 
     Unit,
 }
@@ -88,57 +68,29 @@ fn traverse(ty_known: Option<u8>, dec: &mut Decoder) -> Result<Value, DecodeErro
 
     match ty {
         constants::TYPE_UNIT => Ok(Value::Unit),
-        constants::TYPE_BOOL => Ok(Value::Bool {
-            value: <bool>::decode_value(dec)?,
-        }),
-        constants::TYPE_I8 => Ok(Value::I8 {
-            value: <i8>::decode_value(dec)?,
-        }),
-        constants::TYPE_I16 => Ok(Value::I16 {
-            value: <i16>::decode_value(dec)?,
-        }),
-        constants::TYPE_I32 => Ok(Value::I32 {
-            value: <i32>::decode_value(dec)?,
-        }),
-        constants::TYPE_I64 => Ok(Value::I64 {
-            value: <i64>::decode_value(dec)?,
-        }),
-        constants::TYPE_I128 => Ok(Value::I128 {
-            value: <i128>::decode_value(dec)?.to_string(),
-        }),
-        constants::TYPE_U8 => Ok(Value::U8 {
-            value: <u8>::decode_value(dec)?,
-        }),
-        constants::TYPE_U16 => Ok(Value::U16 {
-            value: <u16>::decode_value(dec)?,
-        }),
-        constants::TYPE_U32 => Ok(Value::U32 {
-            value: <u32>::decode_value(dec)?,
-        }),
-        constants::TYPE_U64 => Ok(Value::U64 {
-            value: <u64>::decode_value(dec)?,
-        }),
-        constants::TYPE_U128 => Ok(Value::U128 {
-            value: <u128>::decode_value(dec)?.to_string(),
-        }),
-        constants::TYPE_STRING => Ok(Value::String {
-            value: <String>::decode_value(dec)?,
-        }),
+        constants::TYPE_BOOL => Ok(Value::Bool(<bool>::decode_value(dec)?)),
+        constants::TYPE_I8 => Ok(Value::I8(<i8>::decode_value(dec)?)),
+        constants::TYPE_I16 => Ok(Value::I16(<i16>::decode_value(dec)?)),
+        constants::TYPE_I32 => Ok(Value::I32(<i32>::decode_value(dec)?)),
+        constants::TYPE_I64 => Ok(Value::I64(<i64>::decode_value(dec)?)),
+        constants::TYPE_I128 => Ok(Value::I128(<i128>::decode_value(dec)?)),
+        constants::TYPE_U8 => Ok(Value::U8(<u8>::decode_value(dec)?)),
+        constants::TYPE_U16 => Ok(Value::U16(<u16>::decode_value(dec)?)),
+        constants::TYPE_U32 => Ok(Value::U32(<u32>::decode_value(dec)?)),
+        constants::TYPE_U64 => Ok(Value::U64(<u64>::decode_value(dec)?)),
+        constants::TYPE_U128 => Ok(Value::U128(<u128>::decode_value(dec)?)),
+        constants::TYPE_STRING => Ok(Value::String(<String>::decode_value(dec)?)),
         constants::TYPE_OPTION => {
             // index
             let index = dec.read_u8()?;
             // optional value
             match index {
-                0 => Ok(Value::Option { value: None }),
-                1 => Ok(Value::Option {
-                    value: Some(Box::new(traverse(None, dec)?)),
-                }),
+                0 => Ok(Value::Option(None)),
+                1 => Ok(Value::Option(Some(Box::new(traverse(None, dec)?)))),
                 _ => Err(DecodeError::InvalidIndex(index)),
             }
         }
-        constants::TYPE_BOX => Ok(Value::Box {
-            value: Box::new(traverse(None, dec)?),
-        }),
+        constants::TYPE_BOX => Ok(Value::Box(Box::new(traverse(None, dec)?))),
         constants::TYPE_ARRAY => {
             // element type
             let ele_ty = dec.read_type()?;
@@ -149,7 +101,7 @@ fn traverse(ty_known: Option<u8>, dec: &mut Decoder) -> Result<Value, DecodeErro
             for _ in 0..len {
                 elements.push(traverse(Some(ele_ty), dec)?);
             }
-            Ok(Value::Array { elements })
+            Ok(Value::Array(elements))
         }
         constants::TYPE_TUPLE => {
             //length
@@ -159,19 +111,19 @@ fn traverse(ty_known: Option<u8>, dec: &mut Decoder) -> Result<Value, DecodeErro
             for _ in 0..len {
                 elements.push(traverse(None, dec)?);
             }
-            Ok(Value::Tuple { elements })
+            Ok(Value::Tuple(elements))
         }
         constants::TYPE_STRUCT => {
             // fields
             let fields = traverse_fields(dec)?;
-            Ok(Value::Struct { fields })
+            Ok(Value::Struct(fields))
         }
         constants::TYPE_ENUM => {
             // index
             let index = dec.read_u8()?;
             // fields
             let fields = traverse_fields(dec)?;
-            Ok(Value::Enum { index, fields })
+            Ok(Value::Enum(index, fields))
         }
         constants::TYPE_VEC => {
             // element type
@@ -183,7 +135,7 @@ fn traverse(ty_known: Option<u8>, dec: &mut Decoder) -> Result<Value, DecodeErro
             for _ in 0..len {
                 elements.push(traverse(Some(ele_ty), dec)?);
             }
-            Ok(Value::Vec { elements })
+            Ok(Value::Vec(elements))
         }
         constants::TYPE_TREE_SET | constants::TYPE_HASH_SET => {
             // element type
@@ -196,9 +148,9 @@ fn traverse(ty_known: Option<u8>, dec: &mut Decoder) -> Result<Value, DecodeErro
                 elements.push(traverse(Some(ele_ty), dec)?);
             }
             if ty == constants::TYPE_TREE_SET {
-                Ok(Value::TreeSet { elements })
+                Ok(Value::TreeSet(elements))
             } else {
-                Ok(Value::HashSet { elements })
+                Ok(Value::HashSet(elements))
             }
         }
         constants::TYPE_TREE_MAP | constants::TYPE_HASH_MAP => {
@@ -214,21 +166,17 @@ fn traverse(ty_known: Option<u8>, dec: &mut Decoder) -> Result<Value, DecodeErro
                 elements.push((traverse(Some(key_ty), dec)?, traverse(Some(value_ty), dec)?));
             }
             if ty == constants::TYPE_TREE_MAP {
-                Ok(Value::TreeMap { elements })
+                Ok(Value::TreeMap(elements))
             } else {
-                Ok(Value::HashMap { elements })
+                Ok(Value::HashMap(elements))
             }
         }
-        // scrypto types
         _ => {
             if ty >= constants::CUSTOM_TYPE_START {
                 // length
                 let len = dec.read_len()?;
                 let slice = dec.read_bytes(len)?;
-                Ok(Value::Custom {
-                    ty,
-                    data: slice.to_vec(),
-                })
+                Ok(Value::Custom(ty, slice.to_vec()))
             } else {
                 Err(DecodeError::InvalidType {
                     expected: 0xff,
@@ -250,7 +198,7 @@ fn traverse_fields(dec: &mut Decoder) -> Result<Fields, DecodeError> {
             for _ in 0..len {
                 named.push(traverse(None, dec)?);
             }
-            Ok(Fields::Named { named })
+            Ok(Fields::Named(named))
         }
         constants::TYPE_FIELDS_UNNAMED => {
             //length
@@ -260,7 +208,7 @@ fn traverse_fields(dec: &mut Decoder) -> Result<Fields, DecodeError> {
             for _ in 0..len {
                 unnamed.push(traverse(None, dec)?);
             }
-            Ok(Fields::Unnamed { unnamed })
+            Ok(Fields::Unnamed(unnamed))
         }
         constants::TYPE_FIELDS_UNIT => Ok(Fields::Unit),
         _ => Err(DecodeError::InvalidType {
@@ -364,83 +312,34 @@ mod tests {
         let value = parse_any(&bytes).unwrap();
 
         assert_eq!(
-            Value::Struct {
-                fields: Fields::Named {
-                    named: vec![
-                        Value::Unit,
-                        Value::Bool { value: true },
-                        Value::I8 { value: 1 },
-                        Value::I16 { value: 2 },
-                        Value::I32 { value: 3 },
-                        Value::I64 { value: 4 },
-                        Value::I128 {
-                            value: 5u128.to_string()
-                        },
-                        Value::U8 { value: 6 },
-                        Value::U16 { value: 7 },
-                        Value::U32 { value: 8 },
-                        Value::U64 { value: 9 },
-                        Value::U128 {
-                            value: 10u128.to_string()
-                        },
-                        Value::String {
-                            value: String::from("abc")
-                        },
-                        Value::Option {
-                            value: Some(Box::new(Value::U32 { value: 1 }))
-                        },
-                        Value::Box {
-                            value: Box::new(Value::U32 { value: 1 })
-                        },
-                        Value::Array {
-                            elements: vec![
-                                Value::U32 { value: 1 },
-                                Value::U32 { value: 2 },
-                                Value::U32 { value: 3 },
-                            ]
-                        },
-                        Value::Tuple {
-                            elements: vec![Value::U32 { value: 1 }, Value::U32 { value: 2 },]
-                        },
-                        Value::Struct {
-                            fields: Fields::Named {
-                                named: vec![Value::U32 { value: 1 }]
-                            }
-                        },
-                        Value::Enum {
-                            index: 0,
-                            fields: Fields::Named {
-                                named: vec![Value::U32 { value: 1 }]
-                            }
-                        },
-                        Value::Enum {
-                            index: 1,
-                            fields: Fields::Unnamed {
-                                unnamed: vec![Value::U32 { value: 2 }]
-                            }
-                        },
-                        Value::Enum {
-                            index: 2,
-                            fields: Fields::Unit
-                        },
-                        Value::Vec {
-                            elements: vec![Value::U32 { value: 1 }, Value::U32 { value: 2 },]
-                        },
-                        Value::TreeSet {
-                            elements: vec![Value::U32 { value: 1 }]
-                        },
-                        Value::HashSet {
-                            elements: vec![Value::U32 { value: 2 }]
-                        },
-                        Value::TreeMap {
-                            elements: vec![(Value::U32 { value: 1 }, Value::U32 { value: 2 }),]
-                        },
-                        Value::HashMap {
-                            elements: vec![(Value::U32 { value: 1 }, Value::U32 { value: 2 }),]
-                        }
-                    ]
-                }
-            },
+            Value::Struct(Fields::Named(vec![
+                Value::Unit,
+                Value::Bool(true),
+                Value::I8(1),
+                Value::I16(2),
+                Value::I32(3),
+                Value::I64(4),
+                Value::I128(5),
+                Value::U8(6),
+                Value::U16(7),
+                Value::U32(8),
+                Value::U64(9),
+                Value::U128(10),
+                Value::String(String::from("abc")),
+                Value::Option(Some(Box::new(Value::U32(1)))),
+                Value::Box(Box::new(Value::U32(1))),
+                Value::Array(vec![Value::U32(1), Value::U32(2), Value::U32(3),]),
+                Value::Tuple(vec![Value::U32(1), Value::U32(2),]),
+                Value::Struct(Fields::Named(vec![Value::U32(1)])),
+                Value::Enum(0, Fields::Named(vec![Value::U32(1)])),
+                Value::Enum(1, Fields::Unnamed(vec![Value::U32(2)])),
+                Value::Enum(2, Fields::Unit),
+                Value::Vec(vec![Value::U32(1), Value::U32(2),]),
+                Value::TreeSet(vec![Value::U32(1)]),
+                Value::HashSet(vec![Value::U32(2)]),
+                Value::TreeMap(vec![(Value::U32(1), Value::U32(2)),]),
+                Value::HashMap(vec![(Value::U32(1), Value::U32(2)),])
+            ])),
             value
         );
     }
@@ -450,12 +349,6 @@ mod tests {
         let bytes: Vec<u8> = vec![0x80, 0x02, 0x00, 0x00, 0x00, 0x01, 0x02];
         let value = parse_any(&bytes).unwrap();
 
-        assert_eq!(
-            Value::Custom {
-                ty: 0x80,
-                data: vec![1, 2]
-            },
-            value
-        );
+        assert_eq!(Value::Custom(0x80, vec![1, 2]), value);
     }
 }
