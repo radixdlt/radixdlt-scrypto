1ded767c1b64821a258143384ea5d19867323259
Yulong Wu <yulong@radixdlt.com>
2021-09-27 11:49:27 +0100

    Fix colored output for windows

--- simulator/src/bin/rev2.rs
+++ simulator/src/bin/rev2.rs
@@ -1,5 +1,9 @@
+#[cfg(windows)]
+use colored::*;
 use simulator::rev2;
 
 pub fn main() -> Result<(), rev2::Error> {
+    #[cfg(windows)]
+    control::set_virtual_terminal(true).unwrap();
     rev2::run(std::env::args())
 }
