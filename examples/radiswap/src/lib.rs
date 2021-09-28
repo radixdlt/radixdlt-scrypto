ce3907708f6c1be588df24ffdee80df200ef8762
Yulong Wu <yulong@radixdlt.com>
2021-09-28 16:01:36 +0100

    Clean up radiswap

--- examples/radiswap/src/lib.rs
+++ examples/radiswap/src/lib.rs
@@ -4,18 +4,19 @@ blueprint! {
     struct Radiswap {
         /// The resource definition of the LP token.
         lp_token_def: ResourceDef,
-        /// The reserve of token A.
+        /// The reserve for token A.
         a_pool: Vault,
-        /// The reserve of token B.
+        /// The reserve for token B.
         b_pool: Vault,
-        /// The commission to apply for every swap.
-        fee_in_thousandth: Amount,
-        /// The precision used when calculating shares.
-        precision: Amount,
+        /// The fee to apply for every swap, like `3` for a 0.3% fee.
+        fee_in_thousandths: u32,
+        /// The scale to use when adding/removing liquidity. When this value is
+        /// set to 5, the min liquidity to add is `0.001%` of the pools.
+        scale: u32,
     }
 
     impl Radiswap {
-        /// Creates a swap pool for token A and B and returns the Radiswap component address
+        /// Creates a Radiswap component for token pair A and B and returns its address
         /// and the initial LP tokens.
         pub fn new(
             a_tokens: Bucket,
@@ -24,14 +25,16 @@ blueprint! {
             lp_symbol: String,
             lp_name: String,
             lp_url: String,
-            fee_in_thousandth: Amount,
-            precision: Amount,
+            fee_in_thousandths: u32,
+            scale: u32,
         ) -> (Address, Bucket) {
-            // Make sure we were provided with an initial supply of each token
+            // Check arguments
             assert!(
                 a_tokens.amount() > 0.into() && b_tokens.amount() > 0.into(),
                 "You must pass in an initial supply of each token"
             );
+            assert!(fee_in_thousandths <= 1000, "Invalid fee in thousandths");
+            assert!(scale >= 1 && scale <= 9, "Invalid scale");
 
             // Instantiate our LP token and mint an initial supply of them
             let lp_token_def = ResourceBuilder::new()
@@ -46,12 +49,12 @@ blueprint! {
                 lp_token_def,
                 a_pool: Vault::with_bucket(a_tokens),
                 b_pool: Vault::with_bucket(b_tokens),
-                fee_in_thousandth,
-                precision,
+                fee_in_thousandths,
+                scale,
             }
             .instantiate();
 
-            // Return the new Uniswap component, as well as the initial supply of LP tokens
+            // Return the new Radiswap component, as well as the initial supply of LP tokens
             (radiswap, lp_tokens)
         }
 
@@ -63,27 +66,24 @@ blueprint! {
         /// Adds liquidity to this pool and return the LP tokens representing pool shares
         /// along with any remainder.
         pub fn add_liquidity(&self, a_tokens: Bucket, b_tokens: Bucket) -> (Bucket, Bucket) {
-            let a_share = self.precision * a_tokens.amount() / self.a_pool.amount();
-            let b_share = self.precision * b_tokens.amount() / self.b_pool.amount();
+            let scale = Amount::from(10u32.pow(self.scale));
+            let a_share = scale * a_tokens.amount() / self.a_pool.amount();
+            let b_share = scale * b_tokens.amount() / self.b_pool.amount();
 
-            let actual_share;
-            let remainder;
-            if a_share <= b_share {
+            let (actual_share, remainder) = if a_share <= b_share {
                 // We will claim all input token A's, and only the correct amount of token B
-                actual_share = a_share;
                 self.a_pool.put(a_tokens);
-                self.b_pool.put(b_tokens.take(actual_share * self.b_pool.amount() / self.precision));
-                remainder = b_tokens;
+                self.b_pool.put(b_tokens.take(a_share * self.b_pool.amount() / scale));
+                (a_share, b_tokens)
             } else {
                 // We will claim all input token B's, and only the correct amount of token A
-                actual_share = b_share;
                 self.b_pool.put(b_tokens);
-                self.a_pool.put(a_tokens.take(actual_share * self.a_pool.amount() / self.precision));
-                remainder = a_tokens;
-            }
+                self.a_pool.put(a_tokens.take(b_share * self.a_pool.amount() / scale));
+                (b_share, a_tokens)
+            };
 
             // Mint LP tokens according to the share the provider is contributing
-            let lp_tokens = self.lp_token_def.mint(actual_share * self.lp_token_supply() / self.precision);
+            let lp_tokens = self.lp_token_def.mint(actual_share * self.lp_token_supply() / scale);
 
             // Return the LP tokens along with any remainder
             (lp_tokens, remainder)
@@ -96,10 +96,13 @@ blueprint! {
                 "Wrong token type passed in"
             );
 
+            // Calculate the share based on the input LP tokens.
+            let scale = Amount::from(10u32.pow(self.scale));
+            let share = scale * lp_tokens.amount() / self.lp_token_supply();
+
             // Withdraw the correct amounts of tokens A and B from reserves
-            let share = self.precision * lp_tokens.amount() / self.lp_token_supply();
-            let a_withdrawn = self.a_pool.take(share * self.a_pool.amount() / self.precision);
-            let b_withdrawn = self.b_pool.take(share * self.b_pool.amount() / self.precision);
+            let a_withdrawn = self.a_pool.take(share * self.a_pool.amount() / scale);
+            let b_withdrawn = self.b_pool.take(share * self.b_pool.amount() / scale);
 
             // Burn the LP tokens received
             ResourceDef::burn(lp_tokens);
@@ -111,15 +114,13 @@ blueprint! {
         /// Swaps token A for B, or vice versa.
         pub fn swap(&self, input_tokens: Bucket) -> Bucket {
             // Calculate the swap fee
-            let fee_amount = self.fee_in_thousandth * input_tokens.amount() / 1000.into();
-
-            // Calculate the constant product of the formula
-            let product = self.a_pool.amount() * self.b_pool.amount();
+            let fee_amount = Amount::from(self.fee_in_thousandths) * input_tokens.amount() / 1000.into();
 
             if input_tokens.resource() == self.a_pool.resource() {
                 // Calculate how much of token B we will return
                 let b_amount = self.b_pool.amount()
-                    - product / (input_tokens.amount() - fee_amount + self.a_pool.amount());
+                    - self.a_pool.amount() * self.b_pool.amount()
+                        / (input_tokens.amount() - fee_amount + self.a_pool.amount());
 
                 // Put the input tokens into our pool
                 self.a_pool.put(input_tokens);
@@ -129,7 +130,8 @@ blueprint! {
             } else if input_tokens.resource() == self.b_pool.resource() {
                 // Calculate how much of token A we will return
                 let a_amount = self.a_pool.amount()
-                    - product / (input_tokens.amount() - fee_amount + self.b_pool.amount());
+                    - self.a_pool.amount() * self.b_pool.amount()
+                        / (input_tokens.amount() - fee_amount + self.b_pool.amount());
 
                 // Put the input tokens into our pool
                 self.b_pool.put(input_tokens);
