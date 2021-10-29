20ec01f693c19b21772a0e9bd24230c2c2b5093d
Russell Harvey <russell@radixdlt.com>
2021-10-29 15:04:59 -0700

    Simplify FlatAdmin

--- examples/flat-admin/src/lib.rs
+++ examples/flat-admin/src/lib.rs
@@ -3,8 +3,7 @@ use scrypto::prelude::*;
 blueprint! {
     struct FlatAdmin {
         admin_mint_auth: Vault,
-        admin_badge_def: ResourceDef,
-        admin_badge_address: Address
+        admin_badge: ResourceDef
     }
 
     impl FlatAdmin {
@@ -17,22 +16,26 @@ blueprint! {
             let first_admin_badge = admin_badge_def.mint(1, mint_auth.borrow());
             let component = Self {
                 admin_mint_auth: Vault::with_bucket(mint_auth),
-                admin_badge_def: first_admin_badge.resource_def(),
-                admin_badge_address: first_admin_badge.resource_def().address()
+                admin_badge: first_admin_badge.resource_def()
             }
             .instantiate();
             (component, first_admin_badge)
         }
 
         // Any existing admin may create another admin token
-        #[auth(admin_badge_address)]
+        #[auth(admin_badge)]
         pub fn create_additional_admin(&self) -> Bucket {
-            self.admin_mint_auth.authorize(|badge| { self.admin_badge_def.mint(1, badge) })            
+            self.admin_mint_auth.authorize(|badge| { self.admin_badge.mint(1, badge) })            
         }
 
+        // Not needed at the moment when anyone can burn, leaving here as a TODO for when burning is restricted
         pub fn destroy_admin_badge(&self, to_destroy: Bucket) {
-            scrypto_assert!(to_destroy.resource_def().address() == self.admin_badge_address, "Can not destroy the contents of this bucket");
+            scrypto_assert!(to_destroy.resource_def().address() == self.admin_badge.address(), "Can not destroy the contents of this bucket!");
             to_destroy.burn();
+        }
+
+        pub fn get_admin_badge_address(&self) -> Address {
+            self.admin_badge.address()
         }
     }
 }
