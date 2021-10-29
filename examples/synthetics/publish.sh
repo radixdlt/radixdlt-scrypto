f5dd236f0657c5f22027828d7d67f1643759f83e
David Edey <david+github@radixdlt.com>
2021-10-29 13:33:21 +0100

    Updated synthetics to stake to personal vault

--- examples/synthetics/publish.sh
+++ examples/synthetics/publish.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 set -e
+
 cd "$(dirname "$0")/../"
+./reset_simulator.sh
 
 # Copies from reset_simulator.sh output
 
+acc1_address='02526629b90e1142492e934fbe807b446935407064db3ea2fcf856'
 acc1_pub_key='04005feceb66ffc86f38d952786c6d696c79c2dbc239dd4e91b46729d73a27fb57e9'
 acc1_mint_auth='03d1f50010e4102d88aacc347711491f852c515134a9ecf67ba17c'
+po_cp='0203672369abe1ac2f25e2a44ec60f8257172aac525030331cf2ea'
+btc='03c29248a0d4c7d4da7b323adfeb4b4fbe811868eb637725ebb7c1'
 
+rev2 set-default-account $acc1_address
 
 # SYNTHETICS
 
@@ -15,8 +21,24 @@ acc1_mint_auth='03d1f50010e4102d88aacc347711491f852c515134a9ecf67ba17c'
 snx=`rev2 new-resource-mutable $acc1_mint_auth --name "Synthetics Collateral Token" --symbol SNX --description "A token which is used in the synthetics component for collateral" | tee /dev/tty | awk '/ResourceDef:/ {print $NF}'`
 rev2 mint 117921786 $snx $acc1_mint_auth --signers $acc1_pub_key
 
+
 # Publish synthetics blueprint
-snythetics_blueprint=`rev2 publish ./synthetics | tee /dev/tty | awk '/Package:/ {print $NF}'`
-snythetics_pool_creation_output=`rev2 call-function $snythetics_bp SyntheticPool new $po_cp $snx 4000000000 | tee /dev/tty | awk '/Component:|ResourceDef:/ {print $NF}'`
-snythetics_pool_update_auth=`echo $snythetics_pool_creation_output | cut -d " " -f1`
-snythetics_pool_component=`echo $snythetics_pool_creation_output | cut -d " " -f2`
+synthetics_blueprint=`rev2 publish ./synthetics | tee /dev/tty | awk '/Package:/ {print $NF}'`
+synthetics_pool_component=`rev2 call-function $synthetics_blueprint SyntheticPool new $po_cp $snx 4000000000 | tee /dev/tty | awk '/Component:|ResourceDef:/ {print $NF}'`
+
+# Stake some SNX tokens! (from the default account)
+amount_to_stake=10
+vault_badge=`rev2 call-method $synthetics_pool_component stake_to_new_vault "$amount_to_stake,$snx" --signers $acc1_pub_key | tee /dev/tty | awk '/ResourceDef:/ {print $NF}'`
+
+echo "Vault badge resource def: $vault_badge"
+
+# Top up our account
+additional_amount_to_stake=21
+rev2 call-method $synthetics_pool_component stake_to_existing_vault "1,$vault_badge" "$additional_amount_to_stake,$snx" --signers $acc1_pub_key --trace
+
+echo
+echo "================================="
+echo "SNX Resource Def Address: $snx"
+echo "Synthetics Blueprint Address: $synthetics_blueprint"
+echo "Synthetics Component Address: $synthetics_pool_component"
+echo "Vault badge resource def: $vault_badge"
--- examples/synthetics/src/synthetic_pool.rs
+++ examples/synthetics/src/synthetic_pool.rs
@@ -72,8 +72,9 @@ blueprint! {
         // Parameters
         oracle: PriceOracle,
         minting_collateralization_ratio_billionths: u128, // Suggested ratio of 4, ie 4000000000
+        collateral_resource_definition: ResourceDef,
         // State
-        collateral_vault: Vault,
+        staked_collateral_vault_map: LazyMap<Address, Vault>,
         synthetic_token_resource_definitions_by_exchange_ticker_code: LazyMap<(String, String), ResourceDef>,
         synthetic_token_resource_definitions_to_exchange_ticker_code: LazyMap<ResourceDef, (String, String)>,
         // Oracle State
@@ -88,11 +89,12 @@ blueprint! {
             minting_collateralization_ratio_billionths: u128
         ) -> Component {
             let oracle: PriceOracle = oracle_address.into();
-            let collateral_token_resource_definition: ResourceDef = collateral_token_address.into();
+            let collateral_resource_definition: ResourceDef = collateral_token_address.into();
             let synthetic_pool = Self {
                 oracle,
                 minting_collateralization_ratio_billionths,
-                collateral_vault: Vault::new(collateral_token_resource_definition),
+                collateral_resource_definition,
+                staked_collateral_vault_map: LazyMap::new(),
                 synthetic_token_resource_definitions_by_exchange_ticker_code: LazyMap::new(),
                 synthetic_token_resource_definitions_to_exchange_ticker_code: LazyMap::new(),
                 off_ledger_exchange_ticker_code_prices_in_billionths_of_base: LazyMap::new(),
@@ -102,6 +104,58 @@ blueprint! {
             synthetic_pool
         }
 
+        pub fn stake_to_new_vault(&self, collateral: Bucket) -> Bucket {
+
+            scrypto_assert!(
+                collateral.resource_def() == self.collateral_resource_definition,
+                "You need to provide {} ({}) as collateral, but you provided {} ({})",
+                self.collateral_resource_definition.metadata().get("symbol").unwrap_or(&"UNKNOWN_SYMBOL".to_string()),
+                self.collateral_resource_definition.address().to_string(),
+                collateral.resource_def().metadata().get("symbol").unwrap_or(&"UNKNOWN_SYMBOL".to_string()),
+                collateral.resource_def().address().to_string()
+            );
+
+            let vault_owner_badge = ResourceBuilder::new()
+                .metadata("name", "Vault Badge")
+                .create_fixed(1);
+
+            let vault_owner_badge_address = vault_owner_badge.resource_def().address();
+
+            self.staked_collateral_vault_map.insert(vault_owner_badge_address, Vault::with_bucket(collateral));
+
+            vault_owner_badge
+        }
+
+        pub fn stake_to_existing_vault(&self, vault_owner_badge: BucketRef, collateral: Bucket) {
+            scrypto_assert!(
+                vault_owner_badge.is_empty(),
+                "Your badge bucket doesn't contain a badge"
+            );
+
+            scrypto_assert!(
+                collateral.resource_def() == self.collateral_resource_definition,
+                "You need to provide {} ({}) as collateral, but you provided {} ({})",
+                self.collateral_resource_definition.metadata().get("symbol").unwrap_or(&"UNKNOWN_SYMBOL".to_string()),
+                self.collateral_resource_definition.address().to_string(),
+                collateral.resource_def().metadata().get("symbol").unwrap_or(&"UNKNOWN_SYMBOL".to_string()),
+                collateral.resource_def().address().to_string()
+            );
+
+            let vault_owner_badge_address = vault_owner_badge.resource_def().address();
+            vault_owner_badge.drop();
+
+            let vault_map_contents = self.staked_collateral_vault_map.get(&vault_owner_badge_address);
+
+            scrypto_assert!(
+                vault_map_contents.is_some(),
+                "The provided badge does not correspond to an active vault"
+            );
+
+            let vault = vault_map_contents.unwrap();
+
+            vault.put(collateral);
+        }
+/*
         pub fn mint_synthetic(&self, exchange: String, ticker_code: String, amount_in_billionths_to_mint: u128, collateral: Bucket) -> Bucket {
 
             // Assert collateral is of right type(!)
@@ -135,6 +189,7 @@ blueprint! {
             // Notes: We could store a last interest paid timestamp; do we store the date of issuance into the pool share tokens somehow??
             // How do we store interest into the 
         }
+        */
 
         // SLIGHT HACK - WE ADD AN OFF-LEDGER PRICE ORACLE TO THIS COMPONENT - BUT IT COULD BE SEPARATE
 
@@ -148,11 +203,11 @@ blueprint! {
             self.off_ledger_exchange_ticker_code_prices_in_billionths_of_base.insert((exchange, ticker_code, quote_token_definition), price_in_billionths);
         }
 
-        pub fn get_unix_timestamp(&self, timestamp_in_seconds: u128) -> u128 {
+        pub fn get_unix_timestamp(&self) -> u128 {
             self.unix_timestamp_oracle
         }
 
-        pub fn update_unix_timestamp(&self, timestamp_in_seconds: u128) {
+        pub fn update_unix_timestamp(&mut self, timestamp_in_seconds: u128) {
             self.unix_timestamp_oracle = timestamp_in_seconds;
         }
     }
