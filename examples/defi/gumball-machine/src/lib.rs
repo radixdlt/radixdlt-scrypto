ba8f88375a4f3cb5d32f28e887777001b657fdc3
Russell Harvey <russell@radixdlt.com>
2021-11-08 19:34:31 -0800

    Update README for g umball machine, minor code adjustments

--- examples/defi/gumball-machine/src/lib.rs
+++ examples/defi/gumball-machine/src/lib.rs
@@ -26,7 +26,11 @@ blueprint! {
             .instantiate()
         }
 
-        pub fn get_gumball(&mut self, payment: Bucket) -> (Bucket, Bucket) {
+        pub fn get_price(&self) -> u32 {
+            self.price
+        }
+
+        pub fn buy_gumball(&self, payment: Bucket) -> (Bucket, Bucket) {
             // take our price in XRD out of the payment
             // if the caller has sent too few, or sent something other than XRD, they'll get a runtime error
             let our_share = payment.take(self.price);
