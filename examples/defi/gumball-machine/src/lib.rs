07ee39c096d7e1dd6a9f039bd26dc3ef3c9da1f2
Russell Harvey <russell@radixdlt.com>
2021-11-08 18:02:48 -0800

    Rejigger gumball machine example code

--- examples/defi/gumball-machine/src/lib.rs
+++ examples/defi/gumball-machine/src/lib.rs
@@ -4,31 +4,40 @@ blueprint! {
     struct GumballMachine {
         gumballs: Vault,
         collected_xrd: Vault,
+        price: u32
     }
 
     impl GumballMachine {
-        pub fn new() -> Component {
-            let bucket = ResourceBuilder::new()
+        // given a price in XRD, creates a ready-to-use gumball machine
+        pub fn new(price: u32) -> Component {
+            // create a new Gumball resource, with a fixed quantity of 100
+            let bucket_of_gumballs = ResourceBuilder::new()
                 .metadata("name", "Gumball")
-                .metadata("symbol", "gum")
-                .metadata("description", "The best gumball in the world.")
-                .new_token_fixed(1000);
+                .metadata("symbol", "GUM")
+                .metadata("description", "A delicious gumball")
+                .new_token_fixed(100);
+
+            // populate a GumballMachine struct and instantiate a new component
             Self {
-                gumballs: Vault::with_bucket(bucket),
-                collected_xrd: Vault::new(RADIX_TOKEN)
+                gumballs: Vault::with_bucket(bucket_of_gumballs),
+                collected_xrd: Vault::new(RADIX_TOKEN),
+                price: price
             }
             .instantiate()
         }
 
-        pub fn get_gumball(&mut self, payment: Bucket) -> Bucket {
-            // make sure they sent us exactly 1 XRD
-            scrypto_assert!(payment.amount() == 1.into(), "Wrong amount of XRD sent");
+        pub fn get_gumball(&mut self, payment: Bucket) -> (Bucket, Bucket) {
+            // take our price in XRD out of the payment
+            // if the caller has sent too few, or sent something other than XRD, they'll get a runtime error
+            let our_share = payment.take(self.price);
+            self.collected_xrd.put(our_share);
+            
+            // we could have simplified the above into a single line, like so:
+            // self.collected_xrd.put(payment.take(self.price));
 
-            // take ownership of their XRD
-            self.collected_xrd.put(payment);
-
-            // give them back a gumball
-            self.gumballs.take(1)
+            // return a tuple containing a gumball, plus whatever change is left on the input payment (if any)
+            // if we're out of gumballs to give, we'll see a runtime error when we try to grab one
+            (self.gumballs.take(1), payment)
         }
     }
 }
