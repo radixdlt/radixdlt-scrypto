b9d0039488f48476f1146d9b3e9c8e85986ea173
Yulong Wu <yulong@radixdlt.com>
2021-11-09 09:46:31 +0000

    Update scripts

--- examples/defi/demo.sh
+++ examples/defi/demo.sh
@@ -20,48 +20,54 @@ out=`resim new-account | tee /dev/tty | awk '/Component:|Public key:/ {print $NF
 acc4_address=`echo $out | cut -d " " -f1`
 acc4_pub_key=`echo $out | cut -d " " -f2`
 
-# generate acc1_mint_auth token
-acc1_mint_auth=`resim new-badge-fixed --name Acc1_mint_authToken 1 | tee /dev/tty | awk '/ResourceDef:/ {print $NF}'`
+# generate acc1_minter_badge token
+acc1_minter_badge=`resim new-badge-fixed --name MinterBadge 1 | tee /dev/tty | awk '/ResourceDef:/ {print $NF}'`
 
 # mint btc
-btc=`resim new-token-mutable $acc1_mint_auth --name Bitcoin --symbol BTC --description "Bitcoin is a decentralized digital currency, without a central bank or single administrator, that can be sent from user to user on the peer-to-peer bitcoin network without the need for intermediaries." | tee /dev/tty | awk '/ResourceDef:/ {print $NF}'`
-resim mint 18843462 $btc $acc1_mint_auth --signers $acc1_pub_key
+btc=`resim new-token-mutable $acc1_minter_badge --name Bitcoin --symbol BTC --description "Bitcoin is a decentralized digital currency, without a central bank or single administrator, that can be sent from user to user on the peer-to-peer bitcoin network without the need for intermediaries." | tee /dev/tty | awk '/ResourceDef:/ {print $NF}'`
+resim mint 18843462 $btc $acc1_minter_badge
 
 # mint ethereum
-eth=`resim new-token-mutable $acc1_mint_auth --name Ethereum --symbol ETH --description "Ethereum is a decentralized, open-source blockchain with smart contract functionality." | tee /dev/tty | awk '/ResourceDef:/ {print $NF}'`
-resim mint 117921786 $eth $acc1_mint_auth --signers $acc1_pub_key
+eth=`resim new-token-mutable $acc1_minter_badge --name Ethereum --symbol ETH --description "Ethereum is a decentralized, open-source blockchain with smart contract functionality." | tee /dev/tty | awk '/ResourceDef:/ {print $NF}'`
+resim mint 117921786 $eth $acc1_minter_badge
 
 # mint USD
-usd=`resim new-token-mutable $acc1_mint_auth --name "US Dollar" --symbol USD --description "The United States dollar is the official currency of the United States and its territories." | tee /dev/tty | awk '/ResourceDef:/ {print $NF}'`
-resim mint 19677000000000 $usd $acc1_mint_auth --signers $acc1_pub_key
+usd=`resim new-token-mutable $acc1_minter_badge --name "US Dollar" --symbol USD --description "The United States dollar is the official currency of the United States and its territories." | tee /dev/tty | awk '/ResourceDef:/ {print $NF}'`
+resim mint 19677000000000 $usd $acc1_minter_badge
 
 # mint GBP
-gbp=`resim new-token-mutable $acc1_mint_auth --name "Pound sterling" --symbol GBP --description "The pound sterling, known in some contexts simply as the pound or sterling, is the official currency of the United Kingdom, Jersey, Guernsey, the Isle of Man, Gibraltar, South Georgia and the South Sandwich Islands, the British Antarctic Territory, and Tristan da Cunha." | tee /dev/tty | awk '/ResourceDef:/ {print $NF}'`
-resim mint 2896859000000 $gbp $acc1_mint_auth --signers $acc1_pub_key
+gbp=`resim new-token-mutable $acc1_minter_badge --name "Pound sterling" --symbol GBP --description "The pound sterling, known in some contexts simply as the pound or sterling, is the official currency of the United Kingdom, Jersey, Guernsey, the Isle of Man, Gibraltar, South Georgia and the South Sandwich Islands, the British Antarctic Territory, and Tristan da Cunha." | tee /dev/tty | awk '/ResourceDef:/ {print $NF}'`
+resim mint 2896859000000 $gbp $acc1_minter_badge
 
-# publish radiswap blueprint
-rs_bp=`resim publish ./radiswap | tee /dev/tty | awk '/Package:/ {print $NF}'`
+# publish PriceOracle
+price_oracle_package=`resim publish ./price-oracle | tee /dev/tty | awk '/Package:/ {print $NF}'`
+out=`resim call-function $price_oracle_package PriceOracle new 18 1 | tee /dev/tty | awk '/Component:|ResourceDef:/ {print $NF}'`
+price_oracle_update_auth=`echo $out | cut -d " " -f1`
+price_oracle_component=`echo $out | cut -d " " -f2`
 
-# publish price oracle blueprint
-po_bp=`resim publish ./price-oracle | tee /dev/tty | awk '/Package:/ {print $NF}'`
-out=`resim call-function $po_bp PriceOracle new 18 1 | tee /dev/tty | awk '/Component:|ResourceDef:/ {print $NF}'`
-po_update_auth=`echo $out | cut -d " " -f1`
-po_cp=`echo $out | cut -d " " -f2`
+resim call-method $price_oracle_component update_price $btc $usd 57523 1,$price_oracle_update_auth
+resim call-method $price_oracle_component update_price $eth $usd 3763 1,$price_oracle_update_auth
+resim call-method $price_oracle_component update_price $btc $gbp 41950 1,$price_oracle_update_auth
+resim call-method $price_oracle_component update_price $eth $gbp 2746 1,$price_oracle_update_auth
+resim call-method $price_oracle_component update_price $btc $eth 15 1,$price_oracle_update_auth
+resim call-method $price_oracle_component get_price $btc $eth
+resim call-method $price_oracle_component get_price $eth $btc
 
-resim call-method $po_cp update_price $btc $usd 57523 1,$po_update_auth --signers $acc1_pub_key
-resim call-method $po_cp update_price $eth $usd 3763 1,$po_update_auth --signers $acc1_pub_key
-resim call-method $po_cp update_price $btc $gbp 41950 1,$po_update_auth --signers $acc1_pub_key
-resim call-method $po_cp update_price $eth $gbp 2746 1,$po_update_auth --signers $acc1_pub_key
-resim call-method $po_cp update_price $btc $eth 15 1,$po_update_auth --signers $acc1_pub_key
-resim call-method $po_cp get_price $btc $eth
-resim call-method $po_cp get_price $eth $btc
+# publish Radiswap
+radiswap_package=`resim publish ./radiswap | tee /dev/tty | awk '/Package:/ {print $NF}'`
+
+# publish AutoLend
+auto_lend_package=`resim publish ./auto-lend | tee /dev/tty | awk '/Package:/ {print $NF}'`
+
+# publish Synthetics
+synthetics_package=`resim publish ./synthetics | tee /dev/tty | awk '/Package:/ {print $NF}'`
 
 # Summary
 echo "===================================================================================="
 echo "Please assume a fixed number of decimal places for all resources: 18"
 echo "Account 1 address: $acc1_address"
 echo "Account 1 public key: $acc1_pub_key"
-echo "Account 1 mint auth: $acc1_mint_auth"
+echo "Account 1 mint auth: $acc1_minter_badge"
 echo "Account 2 address: $acc2_address"
 echo "Account 2 public key: $acc2_pub_key"
 echo "Account 3 address: $acc3_address"
@@ -72,8 +78,10 @@ echo "BTC: $btc"
 echo "ETH: $eth"
 echo "USD: $usd"
 echo "GBP: $gbp"
-echo "Radix swap blueprint: $rs_bp Radiswap"
-echo "Price oracle blueprint: $po_bp PriceOracle"
-echo "Price oracle component: $po_cp"
-echo "Price oracle update auth: $po_update_auth"
+echo "Price Oracle blueprint: $price_oracle_package PriceOracle"
+echo "Price Oracle component: $price_oracle_component"
+echo "Price Oracle update auth: $price_oracle_update_auth"
+echo "Radixswap blueprint: $radiswap_package Radiswap"
+echo "Synthetics blueprint: $auto_lend_package SyntheticPool"
+echo "xPerpFutures blueprint: $synthetics_package ClearingHouse"
 echo "===================================================================================="
--- examples/defi/synthetics/publish-and-test.sh
+++ examples/defi/synthetics/publish-and-test.sh
@@ -5,13 +5,13 @@ set -e
 cd "$(dirname "$0")/../"
 ./demo.sh
 
-# Copies from reset_simulator.sh output
+# Copies from ../demo.sh output
 
 acc1_address='02526629b90e1142492e934fbe807b446935407064db3ea2fcf856'
 acc1_pub_key='04005feceb66ffc86f38d952786c6d696c79c2dbc239dd4e91b46729d73a27fb57e9'
-acc1_mint_auth='03d1f50010e4102d88aacc347711491f852c515134a9ecf67ba17c'
-po_cp='0203672369abe1ac2f25e2a44ec60f8257172aac525030331cf2ea'
-po_update_auth='03b6fe12281eb607ec48a4599f01a328db4836c1e3510b639d761f'
+acc1_minter_badge='03d1f50010e4102d88aacc347711491f852c515134a9ecf67ba17c'
+price_oracle_component='02ccd9ea66c5e785eef48f950e42b3248ae37be4d0aa9612b0894c'
+price_oracle_update_auth='03024420d4c8749579abc13133bf07b0a4fc307aa0172f595a0245'
 btc='03c29248a0d4c7d4da7b323adfeb4b4fbe811868eb637725ebb7c1'
 usd='03806c33ab58c922240ce20a5b697546cc84aaecdf1b460a42c425'
 
@@ -20,20 +20,20 @@ resim set-default-account $acc1_address $acc1_pub_key
 ## SYNTHETICS - PREPARATION
 
 # mint SNX
-snx=`resim new-token-mutable $acc1_mint_auth --name "Synthetics Token" --symbol SNX --description "A token which is used in the synthetics component for collateral" | tee /dev/tty | awk '/ResourceDef:/ {print $NF}'`
-resim mint 114841533.01 $snx $acc1_mint_auth
+snx=`resim new-token-mutable $acc1_minter_badge --name "Synthetics Token" --symbol SNX --description "A token which is used in the synthetics component for collateral" | tee /dev/tty | awk '/ResourceDef:/ {print $NF}'`
+resim mint 114841533.01 $snx $acc1_minter_badge
 
 # Publish synthetics blueprint
 synthetics_blueprint=`resim publish ./synthetics | tee /dev/tty | awk '/Package:/ {print $NF}'`
 
 # Publish SyntheticsPool with collat ratio of 4, using collateral of SNX and base price of USD
-synthetics_component=`resim call-function $synthetics_blueprint SyntheticPool new $po_cp $snx $usd 4 | tee /dev/tty | awk '/Component:/ {print $NF}'`
+synthetics_component=`resim call-function $synthetics_blueprint SyntheticPool new $price_oracle_component $snx $usd 4 | tee /dev/tty | awk '/Component:/ {print $NF}'`
 
 # One SNX is $10.40
-resim call-method $po_cp update_price $snx $usd 10.40  1,$po_update_auth
+resim call-method $price_oracle_component update_price $snx $usd 10.40  1,$price_oracle_update_auth
 
 # One BTC is $66050.98
-resim call-method $po_cp update_price $btc $usd 66050.98  1,$po_update_auth
+resim call-method $price_oracle_component update_price $btc $usd 66050.98  1,$price_oracle_update_auth
 
 ## SYNTHETICS - TESTING
 
@@ -60,7 +60,7 @@ resim call-method $synthetics_component mint 1,$user1 0.01 "BTC"
 resim call-method $synthetics_component get_user_summary $user1
 #read -n 1 -p "Press any key to continue!"
 
-# Burn 0.01 sBTC
+# Burn 0.005 sBTC
 resim call-method $synthetics_component burn 1,$user1 0.005,$sbtc
 resim call-method $synthetics_component get_user_summary $user1
 #read -n 1 -p "Press any key to continue!"
