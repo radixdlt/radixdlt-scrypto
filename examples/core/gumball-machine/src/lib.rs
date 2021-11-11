d2ae39693718b8b186e07225d4985bbe98d05d35
Yulong Wu <yulong@radixdlt.com>
2021-11-11 03:38:16 +0000

    Remove unnecessary clone

--- examples/core/gumball-machine/src/lib.rs
+++ examples/core/gumball-machine/src/lib.rs
@@ -27,17 +27,17 @@ blueprint! {
         }
 
         pub fn get_price(&self) -> Decimal {
-            self.price.clone()
+            self.price
         }
 
         pub fn buy_gumball(&mut self, payment: Bucket) -> (Bucket, Bucket) {
             // take our price in XRD out of the payment
             // if the caller has sent too few, or sent something other than XRD, they'll get a runtime error
-            let our_share = payment.take(self.price.clone());
+            let our_share = payment.take(self.price);
             self.collected_xrd.put(our_share);
             
             // we could have simplified the above into a single line, like so:
-            // self.collected_xrd.put(payment.take(self.price.clone()));
+            // self.collected_xrd.put(payment.take(self.price));
 
             // return a tuple containing a gumball, plus whatever change is left on the input payment (if any)
             // if we're out of gumballs to give, we'll see a runtime error when we try to grab one
