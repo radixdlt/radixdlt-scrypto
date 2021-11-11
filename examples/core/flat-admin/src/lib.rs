6fcdc6b0647665bb195deb3e2e2650957fd30aff
Russell Harvey <russell@radixdlt.com>
2021-11-11 15:12:10 -0800

    Update flat-admin example and README

--- examples/core/flat-admin/src/lib.rs
+++ examples/core/flat-admin/src/lib.rs
@@ -8,23 +8,33 @@ blueprint! {
 
     impl FlatAdmin {
         pub fn new(badge_name: String) -> (Component, Bucket) {
-            // Instantiate the component, create the first admin badge, and return both
+        
+            // Create a badge for internal use which will hold mint/burn authority for the admin badge we will soon create
             let admin_mint_badge = ResourceBuilder::new().new_badge_fixed(1);
+
+            // Create the ResourceDef for a mutable supply admin badge
             let admin_badge_def = ResourceBuilder::new()
                 .metadata("name", badge_name)
                 .new_badge_mutable(admin_mint_badge.resource_def());
+
+            // Using our minting authority badge, mint a single admin badge
             let first_admin_badge = admin_badge_def.mint(1, admin_mint_badge.borrow());
+
+            // Initialize our component, placing the minting authority badge within its vault, where it will remain forever
             let component = Self {
                 admin_mint_badge: Vault::with_bucket(admin_mint_badge),
-                admin_badge: first_admin_badge.resource_def(),
+                admin_badge: admin_badge_def
             }
             .instantiate();
+
+            // Return the instantiated component and the admin badge we just minted
             (component, first_admin_badge)
         }
 
         // Any existing admin may create another admin token
         #[auth(admin_badge)]
         pub fn create_additional_admin(&self) -> Bucket {
+            // The "authorize" method provides a convenient shortcut to make use of the mint authority badge within our vault without removing it
             self.admin_mint_badge
                 .authorize(|badge| self.admin_badge.mint(1, badge))
         }
