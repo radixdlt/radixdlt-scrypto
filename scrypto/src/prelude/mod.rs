651bf049a42f9b7d14195c37bf1e48bb43d07109
Yulong Wu <yulong@radixdlt.com>
2021-12-03 11:04:48 +0000

    Add NftData derive

--- scrypto/src/prelude/mod.rs
+++ scrypto/src/prelude/mod.rs
@@ -4,7 +4,9 @@ pub use crate::kernel::{call_kernel, LogLevel, NewSupply, ResourceType};
 pub use crate::resource::*;
 pub use crate::types::*;
 pub use crate::utils::*;
-pub use crate::{args, auth, blueprint, debug, error, import, include_code, info, trace, warn};
+pub use crate::{
+    args, auth, blueprint, debug, error, import, include_code, info, trace, warn, NftData,
+};
 
 pub use crate::rust::borrow::ToOwned;
 pub use crate::rust::collections::{BTreeMap, BTreeSet, HashMap, HashSet};
