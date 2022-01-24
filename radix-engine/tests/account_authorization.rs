a55cd81a307bedd7d72cac20a5ac9c56e063f265
Joshua Primero <jprimero@gmail.com>
2022-01-24 12:52:33 -0600

    Add failing test

--- radix-engine/tests/account_authorization.rs
+++ radix-engine/tests/account_authorization.rs
@@ -2,44 +2,79 @@ use radix_engine::ledger::*;
 use radix_engine::transaction::*;
 use scrypto::prelude::*;
 
+fn fungible_amount() -> ResourceAmount {
+    ResourceAmount::Fungible {
+        amount: Decimal(100),
+        resource_address: RADIX_TOKEN
+    }
+}
+
 #[test]
-fn withdrawing_from_account_i_own_should_succeed() {
+fn i_can_withdraw_from_my_account() {
+    // Arrange
     let mut ledger = InMemoryLedger::with_bootstrap();
     let mut executor = TransactionExecutor::new(&mut ledger, 0, 0);
     let key = executor.new_public_key();
     let account = executor.new_account(key);
     let other_key = executor.new_public_key();
     let other_account = executor.new_account(other_key);
-    let resource = ResourceAmount::Fungible {
-        amount: Decimal(100),
-        resource_address: RADIX_TOKEN
-    };
     let transaction = TransactionBuilder::new(&executor)
-        .withdraw_from_account(&resource, account)
+        .withdraw_from_account(&fungible_amount(), account)
         .deposit_all_buckets(other_account)
-        .drop_all_bucket_refs()
         .build(vec![key]).unwrap();
+
+    // Act
     let result = executor.run(transaction, false);
+
+    // Assert
     assert!(result.unwrap().success);
 }
 
 #[test]
-fn withdrawing_from_account_i_dont_own_should_fail() {
+fn i_cant_withdraw_from_another_account() {
+    // Arrange
     let mut ledger = InMemoryLedger::with_bootstrap();
     let mut executor = TransactionExecutor::new(&mut ledger, 0, 0);
     let key = executor.new_public_key();
     let account = executor.new_account(key);
     let other_key = executor.new_public_key();
     let other_account = executor.new_account(other_key);
-    let resource = ResourceAmount::Fungible {
-        amount: Decimal(100),
-        resource_address: RADIX_TOKEN
-    };
     let transaction = TransactionBuilder::new(&executor)
-        .withdraw_from_account(&resource, account)
+        .withdraw_from_account(&fungible_amount(), account)
         .deposit_all_buckets(other_account)
-        .drop_all_bucket_refs()
         .build(vec![other_key]).unwrap();
+
+    // Act
     let result = executor.run(transaction, false);
+
+    // Assert
     assert!(!result.unwrap().success);
 }
+
+#[test]
+fn account_to_bucket_to_account() {
+    // Arrange
+    let mut ledger = InMemoryLedger::with_bootstrap();
+    let mut executor = TransactionExecutor::new(&mut ledger, 0, 0);
+    let key = executor.new_public_key();
+    let account = executor.new_account(key);
+    let amount = fungible_amount();
+    let transaction = TransactionBuilder::new(&executor)
+        .withdraw_from_account(&amount, account)
+        .declare_bucket(|builder, bid| {
+            builder.take_from_context(amount.amount(), RADIX_TOKEN, bid);
+            builder.add_instruction(Instruction::CallMethod {
+                component_address: account,
+                method: "deposit".to_owned(),
+                args: vec![SmartValue::from(bid)]
+            })
+        })
+        .build(vec![key]).unwrap();
+
+    // Act
+    let result = executor.run(transaction, false);
+
+    // Assert
+    assert!(result.unwrap().success);
+}
+
