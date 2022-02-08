a0569cfdc733f4f5a84d4b411a44c570ca156b76
Joshua Primero <josh@radixdlt.com>
2022-02-07 21:58:44 -0600

    Add failing dangling map test

---
+++ radix-engine/tests/lazy_map.rs
@@ -0,0 +1,38 @@
+use std::fs;
+use std::process::Command;
+
+use radix_engine::ledger::*;
+use radix_engine::transaction::*;
+use scrypto::prelude::*;
+
+pub fn compile(name: &str) -> Vec<u8> {
+    Command::new("cargo")
+        .current_dir(format!("./tests/{}", name))
+        .args(["build", "--target", "wasm32-unknown-unknown", "--release"])
+        .status()
+        .unwrap();
+    fs::read(format!(
+        "./tests/{}/target/wasm32-unknown-unknown/release/{}.wasm",
+        name,
+        name.replace("-", "_")
+    ))
+        .unwrap()
+}
+
+#[test]
+fn dangling_lazy_map_should_fail() {
+    // Arrange
+    let mut ledger = InMemorySubstateStore::with_bootstrap();
+    let mut executor = TransactionExecutor::new(&mut ledger, false);
+    let package = executor.publish_package(&compile("lazy_map")).unwrap();
+
+    // Act
+    let transaction = TransactionBuilder::new(&executor)
+        .call_function(package, "LazyMapTest", "dangling_lazy_map", vec![], None)
+        .build(vec![])
+        .unwrap();
+    let receipt = executor.run(transaction).unwrap();
+
+    // Assert
+    assert!(!receipt.result.is_ok());
+}
--- radix-engine/tests/lazy_map/src/lazy_map.rs
+++ radix-engine/tests/lazy_map/src/lazy_map.rs
@@ -4,7 +4,7 @@ blueprint! {
     struct LazyMapTest {}
 
     impl LazyMapTest {
-        pub fn test_lazy_map() -> Option<String> {
+        pub fn dangling_lazy_map() -> Option<String> {
             let s = LazyMap::new();
             s.insert("hello".to_owned(), "world".to_owned());
             s.get(&"hello".to_owned())
