d3778c709831f57a007ae66f83160eea0f118f47
Joshua Primero <josh@radixdlt.com>
2022-03-13 00:26:28 -0500

    Move authorize into resource system

--- scrypto/src/component/mod.rs
+++ scrypto/src/component/mod.rs
@@ -6,4 +6,4 @@ mod system;
 pub use component::{Component, ComponentId, ComponentState, ParseComponentIdError};
 pub use lazy_map::{LazyMap, ParseLazyMapError};
 pub use package::{Package, PackageId, ParsePackageIdError};
-pub use system::{component_system, init_component_system, authorize, ComponentSystem};
+pub use system::{component_system, init_component_system, ComponentSystem};
