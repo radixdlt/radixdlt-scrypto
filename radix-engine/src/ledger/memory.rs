e28595ed10b95f7b53c2cc114c2283fb3d0cbf62
Joshua Primero <jprimero@gmail.com>
2022-04-26 15:43:43 -0500

    Use bytes in memory ledger

--- radix-engine/src/ledger/memory.rs
+++ radix-engine/src/ledger/memory.rs
@@ -1,5 +1,5 @@
 use sbor::Encode;
-use scrypto::buffer::scrypto_encode;
+use scrypto::buffer::{scrypto_decode, scrypto_encode};
 use scrypto::rust::collections::HashMap;
 use scrypto::rust::vec::Vec;
 
@@ -9,7 +9,7 @@ use crate::ledger::*;
 /// An in-memory ledger stores all substates in host memory.
 #[derive(Debug, Clone)]
 pub struct InMemorySubstateStore {
-    substates: HashMap<Vec<u8>, Substate>,
+    substates: HashMap<Vec<u8>, Vec<u8>>,
     current_epoch: u64,
     nonce: u64,
 }
@@ -38,15 +38,15 @@ impl Default for InMemorySubstateStore {
 
 impl ReadableSubstateStore for InMemorySubstateStore {
     fn get_substate<T: Encode>(&self, address: &T) -> Option<Substate> {
-        self.substates.get(&scrypto_encode(address)).cloned()
+        self.substates.get(&scrypto_encode(address))
+            .map(|bytes| scrypto_decode(bytes).unwrap())
     }
 
     fn get_child_substate<T: Encode>(&self, address: &T, key: &[u8]) -> Option<Substate> {
         let mut id = scrypto_encode(address);
         id.extend(key.to_vec());
-        self.substates.get(&id).cloned()
+        self.substates.get(&id).map(|bytes| scrypto_decode(bytes).unwrap())
     }
-
 
     fn get_epoch(&self) -> u64 {
         self.current_epoch
@@ -59,13 +59,13 @@ impl ReadableSubstateStore for InMemorySubstateStore {
 
 impl WriteableSubstateStore for InMemorySubstateStore {
     fn put_substate(&mut self, address: &[u8], substate: Substate) {
-        self.substates.insert(address.to_vec(), substate);
+        self.substates.insert(address.to_vec(), scrypto_encode(&substate));
     }
 
     fn put_child_substate(&mut self, address: &[u8], key: &[u8], substate: Substate) {
         let mut id = address.to_vec();
         id.extend(key.to_vec());
-        self.substates.insert(id, substate);
+        self.substates.insert(id, scrypto_encode(&substate));
     }
 
     fn set_epoch(&mut self, epoch: u64) {
