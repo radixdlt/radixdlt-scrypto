277e8cf8cd510b79577b1585895cd1dd1777f869
Yulong Wu <yulong@radixdlt.com>
2022-05-06 11:48:42 +0100

    Add support of `--format` and `--check` to scrypto fmt, update doc

--- check.sh
+++ check.sh
@@ -15,9 +15,9 @@ cd "$(dirname "$0")"
 (cd simulator; cargo fmt --check --quiet)
 (cd transaction-manifest; cargo fmt --check --quiet)
 
-(cd assets/account; cargo fmt --check --quiet)
-(cd assets/system; cargo fmt --check --quiet)
-(cd examples; find . -maxdepth 1 -type d \( ! -name . \) -print0 | xargs -0 -n1 -I '{}' cargo fmt --manifest-path {}/Cargo.toml --check --quiet)
-(cd radix-engine/tests; find . -maxdepth 1 -type d \( ! -name . \) -print0 | xargs -0 -n1 -I '{}' cargo fmt --manifest-path {}/Cargo.toml --check --quiet)
+(cd assets/account; scrypto fmt --check --quiet)
+(cd assets/system; scrypto fmt --check --quiet)
+(cd examples; find . -maxdepth 1 -type d \( ! -name . \) -print0 | xargs -0 -n1 -I '{}' scrypto fmt --path {} --check --quiet)
+(cd radix-engine/tests; find . -maxdepth 1 -type d \( ! -name . \) -print0 | xargs -0 -n1 -I '{}' scrypto fmt --path {} --check --quiet)
 
 echo "Code format check passed!"
--- format.sh
+++ format.sh
@@ -16,9 +16,9 @@ cd "$(dirname "$0")"
 (cd simulator; cargo fmt)
 (cd transaction-manifest; cargo fmt)
 
-(cd assets/account; cargo fmt)
-(cd assets/system; cargo fmt)
-(cd examples; find . -maxdepth 1 -type d \( ! -name . \) -print0 | xargs -0 -n1 -I '{}' cargo fmt --manifest-path {}/Cargo.toml)
-(cd radix-engine/tests; find . -maxdepth 1 -type d \( ! -name . \) -print0 | xargs -0 -n1 -I '{}' cargo fmt --manifest-path {}/Cargo.toml)
+(cd assets/account; scrypto fmt)
+(cd assets/system; scrypto fmt)
+(cd examples; find . -maxdepth 1 -type d \( ! -name . \) -print0 | xargs -0 -n1 -I '{}' scrypto fmt --path {})
+(cd radix-engine/tests; find . -maxdepth 1 -type d \( ! -name . \) -print0 | xargs -0 -n1 -I '{}' scrypto fmt --path {})
 
 echo "All packages have been formatted."
