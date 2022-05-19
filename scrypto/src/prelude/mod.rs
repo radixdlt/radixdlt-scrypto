66c7f258c74b2139ab17a3dac050378b74d1fa70
David Edey <david@rdx.works>
2022-05-19 17:16:19 +0100

    Added blueprint_bridge and component_bridge macros

--- scrypto/src/prelude/mod.rs
+++ scrypto/src/prelude/mod.rs
@@ -10,6 +10,7 @@ pub use crate::{
     access_and_or, access_rule_node, args, blueprint, borrow_component, borrow_package,
     borrow_resource_manager, compile_package, debug, dec, error, import, include_package, info,
     resource_list, rule, trace, warn, Decode, Describe, Encode, NonFungibleData, TypeId,
+    blueprint_bridge, component_bridge,
 };
 
 pub use sbor::rust::borrow::ToOwned;
