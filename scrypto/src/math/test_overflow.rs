0fc25083e29ec8128158d9fd4275362d27d61ea2
Robert Horvath <rob@nyar.eu>
2022-05-24 15:34:21 +0200

    check for tests

--- scrypto/src/math/test_overflow.rs
+++ scrypto/src/math/test_overflow.rs
@@ -1,77 +1,77 @@
 // We are testing if builtin types panic on overflow. They should as this creates a safe math
 // environment for Scrypto
+// Whenever these tests fail, Cargo.toml file shold be doulblechecked for following lines:
+// [profile.release]
+// overflow-checks = true
+
+#![allow(arithmetic_overflow)]
+
 #[cfg(test)]
 mod tests {
     macro_rules! overflow {
-        ($t:ty, $b:literal) => (
-            paste::paste! {
+        ( $( $t:ty, $b:literal ),* ) => {
+            $(
+                paste::paste! {
                     #[test]
                     #[should_panic]
                     fn [<overflow_test_add_$t>]() {
-                        let a = 1$t;
+                        let a: $t = 1;
                         let b = <$t>::MAX;
-                        let c = a + b;         
+                        let _c = a + b;         
                     }
 
 
                     #[test]
                     #[should_panic]
                     fn [<overflow_test_mul_$t>]() {
-                        let a = 2$t;
+                        let a: $t = 2;
                         let b = <$t>::MAX;
-                        let c = a * b;         
+                        let _c = a * b;         
                     }
 
                     #[test]
                     #[should_panic]
-                    fn [<overflow_test_div_$t>]() {
-                        let a = 0$t;
+                    fn [<overflow_test_pow_$t>]() {
+                        let a: $t = 2;
                         let b = <$t>::MAX;
-                        let c = b / a;         
-                    }
-
-                    #[test]
-                    #[should_panic]
-                    fn [<overflow_test_powi_$t>]() {
-                        let a = 2$t;
-                        let b = <$t>::MAX;
-                        let c = b.powi(a);         
+                        let _c = b.pow(a.try_into().unwrap());         
                     }
 
                     #[test]
                     #[should_panic]
                     fn [<overflow_test_shl_$t>]() {
-                        let a = $b$t + 1;
-                        let b = <$t>::MAX;
-                        let c = b.shl(a);         
+                        let a: $t = $b + 1;
+                        let b: $t = <$t>::MAX;
+                        let _c = b << a;         
                     }
 
                     #[test]
                     #[should_panic]
-                    fn [<overflow_test_shl_$t>]() {
-                        let a = $b$t + 1;
-                        let b = <$t>::MAX;
-                        let c = b.shr(a);         
+                    fn [<overflow_test_shr_$t>]() {
+                        let a: $t = $b + 1;
+                        let b: $t = <$t>::MAX;
+                        let _c = b >> a;         
                     }
-            }
-        )
+                }
+            )*
+        };
     }
     macro_rules! overflow_signed {
-        ($($t:ty)*) => (
+        ($( $t:ty ),*) => {
             $(
                 paste::paste! {
                     #[test]
                     #[should_panic]
                     fn [<overflow_test_sub_$t>]() {
-                        let a = -1$t;
+                        let a: $t = -1;
                         let b = <$t>::MAX;
-                        let c = a - b;         
+                        let _c = b - a;         
                     }
                 }
             )*
-        )
+        };
     }
 
-    overflow!{ i8, 8 }
-    overflow_signed! { i8 i16 i32 i64 i128 }
+    overflow!{ i8, 8, i16, 16, i32, 32, i64, 64, i128, 128, u8, 8, u16, 16, u32, 32, u64, 64, u128, 128 }
+    overflow_signed!{i8, i16, i32, i64, i128}
 }
