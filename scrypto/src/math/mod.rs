a63d093091c2318039cbd8efb32eebd9a4591fde
Robert Horvath <rob@nyar.eu>
2022-05-25 12:50:39 +0200

    commit overflow changes

--- scrypto/src/math/mod.rs
+++ scrypto/src/math/mod.rs
@@ -1,5 +1,5 @@
 mod decimal;
-mod integer;
+mod integers;
 
 pub use decimal::*;
-pub use integer::*;
+pub use integers::*;
--- test.sh
+++ test.sh
@@ -10,6 +10,7 @@ echo "Testing with std..."
 (cd sbor-derive; cargo test)
 (cd sbor-tests; cargo test)
 (cd scrypto; cargo test)
+(cd scrypto; cargo test --release) # test overflow check in release version
 (cd scrypto-derive; cargo test)
 (cd scrypto-tests; cargo test)
 (cd radix-engine; cargo test)
@@ -19,6 +20,7 @@ echo "Testing with no_std..."
 (cd sbor; cargo test --no-default-features --features alloc)
 (cd sbor-tests; cargo test --no-default-features --features alloc)
 (cd scrypto; cargo test --no-default-features --features alloc)
+(cd scrypto; cargo test --no-default-features --features alloc --release)
 (cd scrypto-abi; cargo test --no-default-features --features alloc)
 (cd scrypto-tests; cargo test --no-default-features --features alloc)
 
