600aa5a91222b7c1c13430368a3eeb992b8e4c71
Yulong Wu <yulong@radixdlt.com>
2022-05-26 10:59:29 +0100

    Add grow memory tests

--- radix-engine/tests/metering.rs
+++ radix-engine/tests/metering.rs
@@ -101,3 +101,49 @@ fn test_recursion_stack_overflow() {
     // Assert
     assert_invoke_error!(receipt.result, InvokeError::WasmError { .. })
 }
+
+#[test]
+fn test_grow_memory() {
+    // Arrange
+    let mut substate_store = InMemorySubstateStore::with_bootstrap();
+    let mut executor = TransactionExecutor::new(&mut substate_store, true);
+
+    // Act
+    let code = wat2wasm(&include_str!("wasm/memory.wat").replace("${n}", "99999"));
+    let package_address = executor
+        .publish_package(code)
+        .expect("Failed to publish package");
+    let transaction = TransactionBuilder::new()
+        .call_function(package_address, "Test", call_data!(f()))
+        .build(executor.get_nonce([]))
+        .sign([]);
+    let receipt = executor
+        .validate_and_execute(&transaction)
+        .expect("Failed to execute transaction");
+
+    // Assert
+    receipt.result.expect("It should work")
+}
+
+#[test]
+fn test_grow_memory_out_of_tbd() {
+    // Arrange
+    let mut substate_store = InMemorySubstateStore::with_bootstrap();
+    let mut executor = TransactionExecutor::new(&mut substate_store, true);
+
+    // Act
+    let code = wat2wasm(&include_str!("wasm/memory.wat").replace("${n}", "100000"));
+    let package_address = executor
+        .publish_package(code)
+        .expect("Failed to publish package");
+    let transaction = TransactionBuilder::new()
+        .call_function(package_address, "Test", call_data!(f()))
+        .build(executor.get_nonce([]))
+        .sign([]);
+    let receipt = executor
+        .validate_and_execute(&transaction)
+        .expect("Failed to execute transaction");
+
+    // Assert
+    assert_invoke_error!(receipt.result, InvokeError::OutOfTbd { .. })
+}
