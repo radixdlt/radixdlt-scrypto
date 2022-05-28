a78ea3db3bf62e4912bd7da9ab9782ea89bbfb6a
Yulong Wu <yulong@radixdlt.com>
2022-05-28 10:00:36 +0100

    Add notes about Box<dyn WasmRuntime>

--- radix-engine/src/wasm/traits.rs
+++ radix-engine/src/wasm/traits.rs
@@ -18,7 +18,10 @@ pub trait WasmInstance {
     /// Invokes an export defined in this module.
     ///
     /// The export must have a signature of `f(u32) -> u32` where both arguments and return
-    ///  are pointers to Scrypto buffer.
+    /// are pointers to a Scrypto buffer.
+    ///
+    /// Note that trait objects are "fat pointer" (16 bytes). We wrap it with a `Box` so
+    /// to be able to store them in `usize`.
     fn invoke_export<'r>(
         &mut self,
         name: &str,
