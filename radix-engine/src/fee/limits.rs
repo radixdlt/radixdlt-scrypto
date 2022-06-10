32b453955f29f15284e4b2148f2ffbe826f355e8
Yulong Wu <yulong@radixdlt.com>
2022-06-10 10:34:34 +0100

    Tweak parameters to fix tests

--- radix-engine/src/fee/limits.rs
+++ radix-engine/src/fee/limits.rs
@@ -2,13 +2,13 @@
 
 /// The amount of system loan granted to a transaction to boostrap execution.
 /// TODO: implement system loan
-pub const SYSTEM_LOAN_AMOUNT: u32 = 5_000_000;
+pub const SYSTEM_LOAN_AMOUNT: u32 = 10_000_000;
 
 /// The maximum number of cost units that a transaction can consume.
-pub const MAX_TRANSACTION_COST: u32 = 5_000_000;
+pub const MAX_TRANSACTION_COST: u32 = 10_000_000;
 
 /// The maximum number of cost units to be used when extracting blueprint ABIs.
-pub const MAX_EXTRACT_ABI_COST: u32 = 5_000_000;
+pub const MAX_EXTRACT_ABI_COST: u32 = 10_000_000;
 
 /// The maximum number of recursive function/method calls.
 /// TODO: track call depth
