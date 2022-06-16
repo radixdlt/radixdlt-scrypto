f5a7c7434a8464c625bf2b0e08851a19e90ae612
Yulong Wu <yulong@radixdlt.com>
2022-06-16 18:06:59 +0100

    Check function local types

--- radix-engine/src/wasm/prepare.rs
+++ radix-engine/src/wasm/prepare.rs
@@ -36,79 +36,90 @@ impl WasmModule {
 
     pub fn ensure_no_floating_point(self) -> Result<Self, PrepareError> {
         if let Some(code) = self.module.code_section() {
-            for op in code.bodies().iter().flat_map(|body| body.code().elements()) {
-                match op {
-                    F32Load(_, _)
-                    | F64Load(_, _)
-                    | F32Store(_, _)
-                    | F64Store(_, _)
-                    | F32Const(_)
-                    | F64Const(_)
-                    | F32Eq
-                    | F32Ne
-                    | F32Lt
-                    | F32Gt
-                    | F32Le
-                    | F32Ge
-                    | F64Eq
-                    | F64Ne
-                    | F64Lt
-                    | F64Gt
-                    | F64Le
-                    | F64Ge
-                    | F32Abs
-                    | F32Neg
-                    | F32Ceil
-                    | F32Floor
-                    | F32Trunc
-                    | F32Nearest
-                    | F32Sqrt
-                    | F32Add
-                    | F32Sub
-                    | F32Mul
-                    | F32Div
-                    | F32Min
-                    | F32Max
-                    | F32Copysign
-                    | F64Abs
-                    | F64Neg
-                    | F64Ceil
-                    | F64Floor
-                    | F64Trunc
-                    | F64Nearest
-                    | F64Sqrt
-                    | F64Add
-                    | F64Sub
-                    | F64Mul
-                    | F64Div
-                    | F64Min
-                    | F64Max
-                    | F64Copysign
-                    | F32ConvertSI32
-                    | F32ConvertUI32
-                    | F32ConvertSI64
-                    | F32ConvertUI64
-                    | F32DemoteF64
-                    | F64ConvertSI32
-                    | F64ConvertUI32
-                    | F64ConvertSI64
-                    | F64ConvertUI64
-                    | F64PromoteF32
-                    | F32ReinterpretI32
-                    | F64ReinterpretI64
-                    | I32TruncSF32
-                    | I32TruncUF32
-                    | I32TruncSF64
-                    | I32TruncUF64
-                    | I64TruncSF32
-                    | I64TruncUF32
-                    | I64TruncSF64
-                    | I64TruncUF64
-                    | I32ReinterpretF32
-                    | I64ReinterpretF64 => {
-                        return Err(PrepareError::FloatingPointNotAllowed);
+            for func_body in code.bodies() {
+                for local in func_body.locals() {
+                    match local.value_type() {
+                        ValueType::F32 | ValueType::F64 => {
+                            return Err(PrepareError::FloatingPointNotAllowed)
+                        }
+                        _ => {}
                     }
-                    _ => {}
+                }
+
+                for op in func_body.code().elements() {
+                    match op {
+                        F32Load(_, _)
+                        | F64Load(_, _)
+                        | F32Store(_, _)
+                        | F64Store(_, _)
+                        | F32Const(_)
+                        | F64Const(_)
+                        | F32Eq
+                        | F32Ne
+                        | F32Lt
+                        | F32Gt
+                        | F32Le
+                        | F32Ge
+                        | F64Eq
+                        | F64Ne
+                        | F64Lt
+                        | F64Gt
+                        | F64Le
+                        | F64Ge
+                        | F32Abs
+                        | F32Neg
+                        | F32Ceil
+                        | F32Floor
+                        | F32Trunc
+                        | F32Nearest
+                        | F32Sqrt
+                        | F32Add
+                        | F32Sub
+                        | F32Mul
+                        | F32Div
+                        | F32Min
+                        | F32Max
+                        | F32Copysign
+                        | F64Abs
+                        | F64Neg
+                        | F64Ceil
+                        | F64Floor
+                        | F64Trunc
+                        | F64Nearest
+                        | F64Sqrt
+                        | F64Add
+                        | F64Sub
+                        | F64Mul
+                        | F64Div
+                        | F64Min
+                        | F64Max
+                        | F64Copysign
+                        | F32ConvertSI32
+                        | F32ConvertUI32
+                        | F32ConvertSI64
+                        | F32ConvertUI64
+                        | F32DemoteF64
+                        | F64ConvertSI32
+                        | F64ConvertUI32
+                        | F64ConvertSI64
+                        | F64ConvertUI64
+                        | F64PromoteF32
+                        | F32ReinterpretI32
+                        | F64ReinterpretI64
+                        | I32TruncSF32
+                        | I32TruncUF32
+                        | I32TruncSF64
+                        | I32TruncUF64
+                        | I64TruncSF32
+                        | I64TruncUF32
+                        | I64TruncSF64
+                        | I64TruncUF64
+                        | I32ReinterpretF32
+                        | I64ReinterpretF64 => {
+                            return Err(PrepareError::FloatingPointNotAllowed);
+                        }
+                        _ => {}
+                    }
                 }
             }
         }
@@ -204,6 +215,7 @@ impl WasmModule {
     pub fn ensure_table_limit(self) -> Result<Self, PrepareError> {
         if let Some(section) = self.module.table_section() {
             if section.entries().len() > 1 {
+                // Sanity check MVP rule
                 return Err(PrepareError::InvalidTable(
                     InvalidTable::MoreThanOneTableEntries,
                 ));
