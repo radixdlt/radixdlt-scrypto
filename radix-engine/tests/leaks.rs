13757e6ef08cfaefefd305e0bba4d87e92a251a3
Joshua Primero <jprimero@gmail.com>
2022-06-22 11:11:48 -0500

    Add leaky component unit test

---
+++ radix-engine/tests/leaks.rs
@@ -0,0 +1,29 @@
+#[rustfmt::skip]
+pub mod test_runner;
+
+use crate::test_runner::TestRunner;
+use radix_engine::engine::{ResourceFailure, RuntimeError};
+use scrypto::prelude::*;
+use scrypto::to_struct;
+use transaction::builder::ManifestBuilder;
+
+#[test]
+fn leaky_component_should_cause_error() {
+    // Arrange
+    let mut test_runner = TestRunner::new(true);
+    let package_address = test_runner.extract_and_publish_package("leaks");
+
+    // Act
+    let manifest = ManifestBuilder::new()
+        .call_function(
+            package_address,
+            "Leaks",
+            "leaky_component",
+            to_struct!(),
+        )
+        .build();
+    let receipt = test_runner.execute_manifest(manifest, vec![]);
+
+    // Assert
+    receipt.expect_err(|e| matches!(e, RuntimeError::ResourceCheckFailure(ResourceFailure::Component)));
+}
---
+++ radix-engine/tests/leaks/src/lib.rs
@@ -0,0 +1,11 @@
+use scrypto::prelude::*;
+
+blueprint! {
+    struct Leaks {}
+
+    impl Leaks {
+        pub fn leaky_component() {
+            Self {}.instantiate()
+        }
+    }
+}
