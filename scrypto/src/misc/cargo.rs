7d521d967f38735b4ff40217ce5cd6a7175e277f
Yulong Wu <yulong@radixdlt.com>
2022-06-24 17:25:28 +0100

    Fix tests

--- scrypto/src/misc/cargo.rs
+++ scrypto/src/misc/cargo.rs
@@ -4,11 +4,15 @@ use std::process::Command;
 
 // Pattern matching to find the crate name. Not a sound solution but saves from
 // heavy serialization frameworks.
-fn extract_crate_name(content: &str) -> Result<String, ()> {
-    let start = content.find("name").ok_or(())?;
-    let start = content[start + 1..].find("\"").ok_or(())?;
-    let end = content[start + 1..].find("\"").ok_or(())?;
-    Ok(content[start + 1..end].replace("-", "_"))
+fn extract_crate_name(mut content: &str) -> Result<String, ()> {
+    let idx = content.find("name").ok_or(())?;
+    content = &content[idx + 4..];
+
+    let idx = content.find('"').ok_or(())?;
+    content = &content[idx + 1..];
+
+    let end = content.find('"').ok_or(())?;
+    Ok(content[..end].to_string())
 }
 
 /// Compiles a Scrypto package.
@@ -28,7 +32,9 @@ pub fn compile_package<P: AsRef<Path>>(package_dir: P) -> Vec<u8> {
     cargo.push("Cargo.toml");
     let wasm_name = if cargo.exists() {
         let content = fs::read_to_string(cargo).expect("Failed to read the Cargo.toml file");
-        extract_crate_name(&content).expect("Failed to extract crate name from the Cargo.toml file")
+        extract_crate_name(&content)
+            .expect("Failed to extract crate name from the Cargo.toml file")
+            .replace("-", "_")
     } else {
         // file name
         package_dir
@@ -51,4 +57,25 @@ pub fn compile_package<P: AsRef<Path>>(package_dir: P) -> Vec<u8> {
 
     // return
     fs::read(path).unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_extract_crate_name() {
+        assert_eq!(
+            "hello-world",
+            extract_crate_name(
+                r#"
+                [package]
+                name = "hello-world"
+                version = "0.1.0"
+                edition = "2021"
+                "#
+            )
+            .unwrap()
+        )
+    }
 }
