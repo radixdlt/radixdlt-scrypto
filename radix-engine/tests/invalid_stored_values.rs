5e0814233e6dfee099b84d1f2881041140550089
Joshua Primero <jprimero@gmail.com>
2022-07-01 15:22:14 -0500

    Add test for invalid stored bucket in owned component

--- radix-engine/tests/invalid_stored_values.rs
+++ radix-engine/tests/invalid_stored_values.rs
@@ -8,7 +8,7 @@ use scrypto::to_struct;
 use transaction::builder::ManifestBuilder;
 
 #[test]
-fn stored_bucket_should_fail() {
+fn stored_bucket_in_committed_component_should_fail() {
     // Arrange
     let mut test_runner = TestRunner::new(true);
     let package_address = test_runner.extract_and_publish_package("stored_values");
@@ -19,6 +19,27 @@ fn stored_bucket_should_fail() {
             package_address,
             "InvalidInitStoredBucket",
             "create",
+            to_struct!(),
+        )
+        .build();
+    let receipt = test_runner.execute_manifest(manifest, vec![]);
+
+    // Assert
+    receipt.expect_err(|e| matches!(e, RuntimeError::ValueNotAllowed));
+}
+
+#[test]
+fn stored_bucket_in_owned_component_should_fail() {
+    // Arrange
+    let mut test_runner = TestRunner::new(true);
+    let package_address = test_runner.extract_and_publish_package("stored_values");
+
+    // Act
+    let manifest = ManifestBuilder::new()
+        .call_function(
+            package_address,
+            "InvalidStoredBucketInOwnedComponent",
+            "create_bucket_in_owned_component",
             to_struct!(),
         )
         .build();
--- radix-engine/tests/stored_values/src/lib.rs
+++ radix-engine/tests/stored_values/src/lib.rs
@@ -19,3 +19,28 @@ blueprint! {
         }
     }
 }
+
+blueprint! {
+    struct InvalidStoredBucketInOwnedComponent {
+        bucket: Option<Bucket>
+    }
+
+    impl InvalidStoredBucketInOwnedComponent {
+        pub fn put_bucket(&mut self, bucket: Bucket) {
+            self.bucket = Option::Some(bucket);
+        }
+
+        pub fn create_bucket_in_owned_component() -> ComponentAddress {
+            let bucket = ResourceBuilder::new_fungible()
+                .divisibility(DIVISIBILITY_NONE)
+                .restrict_withdraw(rule!(allow_all), LOCKED)
+                .initial_supply(Decimal::from(5));
+
+            let component = InvalidStoredBucketInOwnedComponent {
+                bucket: Option::None,
+            }.instantiate();
+            component.put_bucket(bucket);
+            component.globalize()
+        }
+    }
+}
