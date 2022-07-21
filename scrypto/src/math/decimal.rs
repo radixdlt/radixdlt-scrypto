806cb5e3f8a16b10b5fe31f4be60ba7e86d7c853
Robert Horvath <rob@nyar.eu>
2022-07-21 15:59:00 +0200

    dec!() macro to doc

--- scrypto/src/math/decimal.rs
+++ scrypto/src/math/decimal.rs
@@ -246,8 +246,8 @@ macro_rules! decimals {
                 /// ```ignore
                 /// use scrypto::prelude::*;
                 ///
-                /// let a = dec!(1);
-                /// let b = dec!("1.1");
+                /// let a = $dec_macro!(1);
+                /// let b = $dec_macro!("1.1");
                 /// ```
                 #[macro_export]
                 macro_rules! $dec_macro {
