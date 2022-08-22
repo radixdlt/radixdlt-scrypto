11d3be268ff5b33a0903001932a519d007d22a78
David Edey <david.edey@rdx.works>
2022-08-22 11:22:49 +0100

    fix: Run cargo format

--- scrypto/src/address/hrpset.rs
+++ scrypto/src/address/hrpset.rs
@@ -1,7 +1,7 @@
 use crate::address::entity::EntityType;
 use crate::core::{Network, NetworkDefinition};
-use crate::sbor::rust::string::String;
 use crate::sbor::rust::format;
+use crate::sbor::rust::string::String;
 
 /// Represents an HRP set (typically corresponds to a network).
 #[derive(Debug, Clone)]
