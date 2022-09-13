bade75f2b5907c39c73edc884c02bc73fbdf8eaa
0xOmarA <OmarAbdulla7@hotmail.com>
2022-09-13 09:56:15 +0300

    Derive `Hash` for a few types

--- scrypto/src/crypto/ed25519.rs
+++ scrypto/src/crypto/ed25519.rs
@@ -13,7 +13,7 @@ use crate::misc::copy_u8_array;
 pub struct Ed25519PublicKey(pub [u8; Self::LENGTH]);
 
 /// Represents an ED25519 signature.
-#[derive(Clone, Copy, PartialEq, Eq)]
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
 pub struct Ed25519Signature(pub [u8; Self::LENGTH]);
 
 /// Represents an error ocurred when validating a signature.
