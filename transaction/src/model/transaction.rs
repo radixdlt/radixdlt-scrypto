2d924eeed6dbafcfe6ca8ab4afdf8ba65a0dd291
0xOmarA <OmarAbdulla7@hotmail.com>
2022-09-14 14:03:27 +0300

    Add a serde feature to the transaction crate

--- transaction/src/model/transaction.rs
+++ transaction/src/model/transaction.rs
@@ -9,6 +9,11 @@ use crate::model::Instruction;
 
 // TODO: add versioning of transaction schema
 
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize, serde::Deserialize),
+    serde(tag = "type")
+)]
 #[derive(Debug, Clone, TypeId, Encode, Decode, PartialEq, Eq)]
 pub struct TransactionHeader {
     pub version: u8,
