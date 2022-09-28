4af0788bbc1781063123420df1c2375b7e64cf5d
Yulong Wu <yulong.wu@rdx.works>
2022-09-28 17:30:01 +0100

    Fix metering tests

--- radix-engine/tests/metering.rs
+++ radix-engine/tests/metering.rs
@@ -156,13 +156,13 @@ fn test_basic_transfer() {
         + 2000 /* create_node */
         + 1248 /* decode_manifest */
         + 1000 /* drop_node */
-        + 598092 /* instantiate_wasm */
+        + 596181 /* instantiate_wasm */
         + 2205 /* invoke_function */
         + 2215 /* invoke_method */
         + 5000 /* read_substate */
         + 1000 /* run_function */
         + 5200 /* run_method */
-        + 275043 /* run_wasm */
+        + 275033 /* run_wasm */
         + 416 /* verify_manifest */
         + 3750 /* verify_signatures */
         + 3000, /* write_substate */
@@ -196,5 +196,5 @@ fn test_publish_large_package() {
     receipt.expect_commit_success();
 
     // Assert
-    assert_eq!(4413787, receipt.execution.fee_summary.cost_unit_consumed);
+    assert_eq!(4408469, receipt.execution.fee_summary.cost_unit_consumed);
 }
