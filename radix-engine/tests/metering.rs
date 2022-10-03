9b41f4eebbc7c974479b0b46401d801afd3a8364
Yulong Wu <yulong.wu@rdx.works>
2022-10-03 15:19:04 +0100

    Disable WASM instantiation cost

--- radix-engine/tests/metering.rs
+++ radix-engine/tests/metering.rs
@@ -152,7 +152,7 @@ fn test_basic_transfer() {
         + 2000 /* create_node */
         + 1248 /* decode_manifest */
         + 1000 /* drop_node */
-        + 578328 /* instantiate_wasm */
+        + 0 /* instantiate_wasm */
         + 2205 /* invoke_function */
         + 2215 /* invoke_method */
         + 5000 /* read_substate */
@@ -193,5 +193,5 @@ fn test_publish_large_package() {
     receipt.expect_commit_success();
 
     // Assert
-    assert_eq!(4401348, receipt.execution.fee_summary.cost_unit_consumed);
+    assert_eq!(4271555, receipt.execution.fee_summary.cost_unit_consumed);
 }
