42b4c23ce06dae1a832fe0b92811c493b251488c
Yulong Wu <yulong.wu@rdx.works>
2022-10-03 20:22:01 +0100

    Change default parameters

--- radix-engine/src/constants.rs
+++ radix-engine/src/constants.rs
@@ -3,13 +3,13 @@
 pub const DEFAULT_SYSTEM_LOAN: u32 = 1_500_000;
 
 /// The default max cost unit limit for a transaction, used by transaction validator.
-pub const DEFAULT_MAX_COST_UNIT_LIMIT: u32 = 10_000_000;
+pub const DEFAULT_MAX_COST_UNIT_LIMIT: u32 = 100_000_000;
 
 /// The default cost unit limit for a transaction.
-pub const DEFAULT_COST_UNIT_LIMIT: u32 = 10_000_000;
+pub const DEFAULT_COST_UNIT_LIMIT: u32 = 100_000_000;
 
 /// The default cost unit price.
-pub const DEFAULT_COST_UNIT_PRICE: &'static str = "0.000001";
+pub const DEFAULT_COST_UNIT_PRICE: &'static str = "0.0000001";
 
 /// The default max call depth.
 pub const DEFAULT_MAX_CALL_DEPTH: usize = 16;
