b87684f654e6ffa39baaf7bfd56ac339f83149af
Yulong Wu <yulong.wu@rdx.works>
2022-10-20 12:02:15 +0100

    Fix tests

--- transaction/src/signing/ecdsa_secp256k1.rs
+++ transaction/src/signing/ecdsa_secp256k1.rs
@@ -70,7 +70,7 @@ mod tests {
 
     #[test]
     fn test_non_fungible_address_codec() {
-        let expected = "00b91737ee8a4de59d49dad40de5560e5754466ac84cf5432ea95d300721000000031b84c5567b126440995d3ed5aaba0565d71e1834604819ff9c17f5e9d5dd078f";
+        let expected = "00922306a6d13af63866b688c9103719d04b14c060bc4899a613b5300721000000031b84c5567b126440995d3ed5aaba0565d71e1834604819ff9c17f5e9d5dd078f";
         let private_key = EcdsaSecp256k1PrivateKey::from_bytes(&[1u8; 32]).unwrap();
         let public_key = private_key.public_key();
         let auth_address = NonFungibleAddress::new(
