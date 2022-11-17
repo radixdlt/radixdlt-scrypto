529324dc59bf17f1f386d0deb5f9a66bc0fa91d7
Joshua Primero <josh.primero@rdx.works>
2022-11-17 09:55:35 -0600

    Use dec and pdec macro in radix-engine-lib

--- scrypto/src/prelude/mod.rs
+++ scrypto/src/prelude/mod.rs
@@ -7,9 +7,9 @@ pub use crate::resource::non_fungible::ScryptoNonFungibleId;
 pub use crate::resource::*;
 pub use crate::{
     access_and_or, access_rule_node, args_from_bytes_vec, args_from_value_vec, blueprint,
-    borrow_component, borrow_package, borrow_resource_manager, debug, dec, error,
-    external_blueprint, external_component, i, import, include_abi, include_code, info, pdec,
-    resource_list, rule, scrypto, this_package, trace, warn, Describe, NonFungibleData,
+    borrow_component, borrow_package, borrow_resource_manager, debug, error, external_blueprint,
+    external_component, import, include_abi, include_code, info, resource_list, rule, scrypto,
+    this_package, trace, warn, Describe, NonFungibleData,
 };
 pub use num_traits::{
     cast::FromPrimitive, cast::ToPrimitive, identities::One, identities::Zero, pow::Pow,
@@ -22,6 +22,7 @@ pub use radix_engine_lib::math::integer::{
 };
 pub use radix_engine_lib::math::*;
 pub use radix_engine_lib::model::*;
+pub use radix_engine_lib::{dec, i, pdec};
 pub use sbor::rust::borrow::ToOwned;
 pub use sbor::rust::collections::{BTreeMap, BTreeSet, HashMap, HashSet};
 pub use sbor::rust::str::FromStr;
