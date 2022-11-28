702b899fd32e2b22f82be993fe204301a463c56a
0xOmarA <OmarAbdulla7@hotmail.com>
2022-11-28 21:57:27 +0300

    Updated the `StatefulAccessRules` model

--- scrypto/src/component/stateful_access_rules.rs
+++ scrypto/src/component/stateful_access_rules.rs
@@ -1,25 +1,32 @@
 use radix_engine_derive::scrypto;
-use radix_engine_interface::model::{AccessRules, AccessRule, ComponentAddress};
+use radix_engine_interface::api::types::{ComponentId, GlobalAddress, RENodeId};
+use radix_engine_interface::model::{AccessRule, AccessRules, ComponentAddress};
 
+// TODO: Should `Decode` be removed so that `StatefulAccessRules` can not be passed between 
+// components?
 #[derive(Debug, Clone, PartialEq, Eq)]
 #[scrypto(TypeId, Encode, Decode, Describe)]
 pub struct StatefulAccessRules {
-    component: ComponentAddress,
+    component: ComponentIdentifier,
     access_rules: AccessRules,
     index: usize,
 }
 
 impl StatefulAccessRules {
-    pub fn new(component: ComponentAddress, access_rules: AccessRules, index: usize) -> Self {
+    pub fn new<T: Into<ComponentIdentifier>>(
+        component: T,
+        access_rules: AccessRules,
+        index: usize,
+    ) -> Self {
         Self {
-            component,
+            component: component.into(),
             access_rules,
             index,
         }
     }
 
-    pub fn component_address(&self) -> ComponentAddress {
-        self.component
+    pub fn component_address(&self) -> ComponentIdentifier {
+        self.component.clone()
     }
 
     pub fn access_rules(&self) -> AccessRules {
@@ -44,5 +51,33 @@ impl StatefulAccessRules {
 
     pub fn lock_default(&mut self) {
         todo!();
+    }
+
+    fn _component_re_node(&self) -> RENodeId {
+        match self.component {
+            ComponentIdentifier::NodeId(node_id) => RENodeId::Component(node_id),
+            ComponentIdentifier::ComponentAddress(component_address) => {
+                RENodeId::Global(GlobalAddress::Component(component_address))
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+#[scrypto(TypeId, Encode, Decode, Describe)]
+pub enum ComponentIdentifier {
+    NodeId(ComponentId),
+    ComponentAddress(ComponentAddress),
+}
+
+impl From<ComponentId> for ComponentIdentifier {
+    fn from(value: ComponentId) -> Self {
+        ComponentIdentifier::NodeId(value)
+    }
+}
+
+impl From<ComponentAddress> for ComponentIdentifier {
+    fn from(value: ComponentAddress) -> Self {
+        ComponentIdentifier::ComponentAddress(value)
     }
 }
