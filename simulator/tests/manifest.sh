7c6fbc387f0d9dea43cae603d6c3b619302e564f
Yulong Wu <yulong.wu@rdx.works>
2022-12-07 17:00:49 +0000

    Fix tests

--- simulator/tests/manifest.sh
+++ simulator/tests/manifest.sh
@@ -11,7 +11,7 @@ $resim reset
 
 export account=`$resim new-account | awk '/Account component address:/ {print $NF}'`
 export owner_badge=`$resim new-simple-badge --name 'OwnerBadge' | awk '/NFAddress:/ {print $NF}'`
-export package=`$resim publish ../examples/hello-world $owner_badge | awk '/Package:/ {print $NF}'`
+export package=`$resim publish ../examples/hello-world --owner-badge $owner_badge | awk '/Package:/ {print $NF}'`
 
 export xrd=resource_sim1qzkcyv5dwq3r6kawy6pxpvcythx8rh8ntum6ws62p95sqjjpwr
 
