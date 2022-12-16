e8a28e4fd5f5cb8193480a04dc3fff18e1e65d32
Joshua Primero <josh.primero@rdx.works>
2022-12-16 15:45:43 -0500

    Add failing import test

---
+++ radix-engine/tests/import.rs
@@ -0,0 +1,14 @@
+use radix_engine::ledger::TypedInMemorySubstateStore;
+use scrypto_unit::*;
+use transaction::builder::ManifestBuilder;
+
+// TODO: Test should be moved to a higher level package (e.g. scrypto-tests)
+#[test]
+fn can_compile_and_publish_blueprint_with_import() {
+    // Arrange
+    let mut store = TypedInMemorySubstateStore::with_bootstrap();
+    let mut test_runner = TestRunner::new(true, &mut store);
+
+    // Act
+    test_runner.compile_and_publish("./tests/blueprints/import");
+}
