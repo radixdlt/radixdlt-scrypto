395d804866ce8e44df4e68e1944cc2f1cb90b229
Lukasz Rubaszewski <lukasz.rubaszewski@rdx.works>
2022-12-19 23:09:54 +0100

    add I256/I512 integer benchmarks

--- radix-engine-interface/benches/math.rs
+++ radix-engine-interface/benches/math.rs
@@ -1,8 +1,10 @@
 use criterion::{criterion_group, criterion_main};
 mod macros;
 mod decimal;
+mod integer;
 
 use decimal::*;
+use integer::*;
 
 criterion_group!(
     bench_math,
@@ -14,5 +16,23 @@ criterion_group!(
     bench_decimal_pow,
     bench_decimal_from_string,
     bench_decimal_to_string,
+
+    bench_i256_add,
+    bench_i256_sub,
+    bench_i256_mul,
+    bench_i256_div,
+    bench_i256_root,
+    bench_i256_pow,
+    bench_i256_from_string,
+    bench_i256_to_string,
+
+    bench_i512_add,
+    bench_i512_sub,
+    bench_i512_mul,
+    bench_i512_div,
+    bench_i512_root,
+    bench_i512_pow,
+    bench_i512_from_string,
+    bench_i512_to_string
 );
 criterion_main!(bench_math);
