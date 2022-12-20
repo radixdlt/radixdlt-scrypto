2f026e7d5b542ce3c261156f903c53f0a99f7578
Lukasz Rubaszewski <lukasz.rubaszewski@rdx.works>
2022-12-20 08:28:48 +0100

    add bnum_integer initial version
    This is an alternative implementation for large integer arithmetic.
    It is based on 'bnum' crate, which is supposed to behave like Rust's
    primitive integer types. Integers are stored as an array of digits
    (primitive unsigned integer) of fixed length, which allows to keep them
    on the stack since they are fixed size.
    That should result of much better performance.
    
    Types defined:
    - signed: BnumI256, BnumI512.
    - usigned: BnumU256, BnumU512.

--- radix-engine-interface/src/math/mod.rs
+++ radix-engine-interface/src/math/mod.rs
@@ -1,12 +1,16 @@
+pub mod bnum_integer;
 pub mod decimal;
 pub mod integer;
 pub mod precise_decimal;
 pub mod rounding_mode;
 
 pub use decimal::*;
+
 pub use integer::basic::*;
 pub use integer::bits::*;
 pub use integer::convert::*;
 pub use integer::*;
+
+pub use bnum_integer::*;
 pub use precise_decimal::*;
 pub use rounding_mode::*;
