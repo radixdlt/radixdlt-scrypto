ce8bd2b3b6d165b24cc876fac19088be3110a3e3
Lukasz Rubaszewski <lukasz.rubaszewski@rdx.works>
2022-12-21 16:17:31 +0100

    align integer benchmarks

--- radix-engine-interface/benches/integer.rs
+++ radix-engine-interface/benches/integer.rs
@@ -6,81 +6,8 @@ use num_traits::Pow;
 use num_bigint::BigInt;
 use rug::{Integer, ops::Pow as RugPow};
 
-use crate::{bench_ops,process_op};
+use crate::{ops_fn,bench_ops,process_op};
 use crate::macros::QUICK;
-
-macro_rules! ops_fn {
-    ($t:ty, $root_fn:ident) => {
-        paste::item! {
-            fn [< $t:lower _add >](a: &$t, b: &$t) {
-                let _ = a + b;
-            }
-
-            fn [< $t:lower _sub >](a: &$t, b: &$t) {
-                let _ = a - b;
-            }
-
-            fn [< $t:lower _mul >](a: &$t, b: &$t) {
-                let _ = a * b;
-            }
-
-            fn [< $t:lower _div >](a: &$t, b: &$t) {
-                let _ = a / b;
-            }
-
-            fn [< $t:lower _root >](a: &$t, n: &u32) {
-                let _ = a.$root_fn(*n);
-            }
-
-            fn [< $t:lower _pow >](a: &$t, exp: &u32) {
-                let _ = a.pow(*exp);
-            }
-
-            fn [< $t:lower _to_string >](a: &$t, _: &str) {
-                let _ = a.to_string();
-            }
-
-            fn [< $t:lower _from_string >](s: &str, _: &str) {
-                let _ = <$t>::from_str(s).unwrap();
-            }
-        }
-    };
-    ($t:ty, $root_fn:ident, "clone") => {
-        paste::item! {
-            fn [< $t:lower _add >](a: &$t, b: &$t) {
-                let _ = a.clone() + b.clone();
-            }
-
-            fn [< $t:lower _sub >](a: &$t, b: &$t) {
-                let _ = a.clone() - b.clone();
-            }
-
-            fn [< $t:lower _mul >](a: &$t, b: &$t) {
-                let _ = a.clone() * b.clone();
-            }
-
-            fn [< $t:lower _div >](a: &$t, b: &$t) {
-                let _ = a.clone() / b.clone();
-            }
-
-            fn [< $t:lower _root >](a: &$t, n: &u32) {
-                let _ = a.clone().$root_fn(*n);
-            }
-
-            fn [< $t:lower _pow >](a: &$t, exp: &u32) {
-                let _ = a.pow(*exp);
-            }
-
-            fn [< $t:lower _to_string >](a: &$t, _: &str) {
-                let _ = a.to_string();
-            }
-
-            fn [< $t:lower _from_string >](s: &str, _: &str) {
-                let _ = <$t>::from_str(s).unwrap();
-            }
-        }
-    };
-}
 
 const ADD_OPERANDS: [(&str, &str); 4] = [
     ("278960446186580977117854925043439539266349000000000000000000000000000000000", "278960446186580977117854925043439539266349000000000000000000000000000000000"),
@@ -138,7 +65,7 @@ const FROM_STRING_OPERANDS: [&str; 4] = [
     "9",
 ];
 
-ops_fn!(I256, nth_root);
+ops_fn!(I256, nth_root, pow, u32);
 bench_ops!(I256, "add");
 bench_ops!(I256, "sub");
 bench_ops!(I256, "mul");
@@ -148,7 +75,7 @@ bench_ops!(I256, "pow", u32);
 bench_ops!(I256, "to_string");
 bench_ops!(I256, "from_string");
 
-ops_fn!(I512, nth_root);
+ops_fn!(I512, nth_root, pow, u32);
 bench_ops!(I512, "add");
 bench_ops!(I512, "sub");
 bench_ops!(I512, "mul");
@@ -158,7 +85,7 @@ bench_ops!(I512, "pow", u32);
 bench_ops!(I512, "to_string");
 bench_ops!(I512, "from_string");
 
-ops_fn!(BigInt, nth_root);
+ops_fn!(BigInt, nth_root, pow, u32);
 bench_ops!(BigInt, "add");
 bench_ops!(BigInt, "sub");
 bench_ops!(BigInt, "mul");
@@ -168,7 +95,7 @@ bench_ops!(BigInt, "pow", u32);
 bench_ops!(BigInt, "to_string");
 bench_ops!(BigInt, "from_string");
 
-ops_fn!(Integer, root, "clone");
+ops_fn!(Integer, root, pow, u32, "clone");
 bench_ops!(Integer, "add");
 bench_ops!(Integer, "sub");
 bench_ops!(Integer, "mul");
