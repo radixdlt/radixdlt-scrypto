e0534330b44be92ae70897eb744e171d84c766a7
Lukasz Rubaszewski <lukasz.rubaszewski@rdx.works>
2022-12-23 14:29:19 +0100

    fix code formatting

--- radix-engine-interface/benches/math.rs
+++ radix-engine-interface/benches/math.rs
@@ -1,12 +1,12 @@
 use criterion::{criterion_group, criterion_main};
-mod macros;
 mod decimal;
-mod precise_decimal;
 mod integer;
+mod macros;
+mod precise_decimal;
 
 use decimal::*;
-use precise_decimal::*;
 use integer::*;
+use precise_decimal::*;
 
 criterion_group!(
     bench_math,
@@ -18,7 +18,6 @@ criterion_group!(
     bench_decimal_pow,
     bench_decimal_from_string,
     bench_decimal_to_string,
-
     bench_precisedecimal_add,
     bench_precisedecimal_sub,
     bench_precisedecimal_mul,
@@ -27,7 +26,6 @@ criterion_group!(
     bench_precisedecimal_pow,
     bench_precisedecimal_from_string,
     bench_precisedecimal_to_string,
-
     bench_i256_add,
     bench_i256_sub,
     bench_i256_mul,
@@ -36,7 +34,6 @@ criterion_group!(
     bench_i256_pow,
     bench_i256_from_string,
     bench_i256_to_string,
-
     bench_i512_add,
     bench_i512_sub,
     bench_i512_mul,
@@ -45,7 +42,6 @@ criterion_group!(
     bench_i512_pow,
     bench_i512_from_string,
     bench_i512_to_string,
-
     bench_bigint_add,
     bench_bigint_sub,
     bench_bigint_mul,
@@ -54,7 +50,6 @@ criterion_group!(
     bench_bigint_pow,
     bench_bigint_from_string,
     bench_bigint_to_string,
-
     bench_integer_add,
     bench_integer_sub,
     bench_integer_mul,
@@ -63,7 +58,6 @@ criterion_group!(
     bench_integer_pow,
     bench_integer_from_string,
     bench_integer_to_string,
-
     bench_bnumbint256_add,
     bench_bnumbint256_sub,
     bench_bnumbint256_mul,
@@ -72,12 +66,11 @@ criterion_group!(
     bench_bnumbint256_pow,
     bench_bnumbint256_from_string,
     bench_bnumbint256_to_string,
-
     bench_ethnumi256_add,
     bench_ethnumi256_sub,
     bench_ethnumi256_mul,
     bench_ethnumi256_div,
-//    bench_ethnumi256_root,
+    //    bench_ethnumi256_root,
     bench_ethnumi256_pow,
     bench_ethnumi256_from_string,
     bench_ethnumi256_to_string,
