0d4c58af959faf8560fcb91bc1ce30bbbb70694e
Joshua Primero <josh.primero@rdx.works>
2023-01-03 09:44:37 -0600

    Separate epoch_manager and validator logic

--- radix-engine/src/model/epoch_manager/epoch_manager.rs
+++ radix-engine/src/model/epoch_manager/epoch_manager.rs
@@ -3,9 +3,8 @@ use crate::engine::{
     RENode, ResolvedActor, ResolverApi, RuntimeError, SystemApi,
 };
 use crate::model::{
-    AccessRulesChainSubstate, EpochManagerSubstate, GlobalAddressSubstate, HardAuthRule,
-    HardProofRule, HardResourceOrNonFungible, MethodAuthorization, Validator, ValidatorSetSubstate,
-    ValidatorSubstate,
+    AccessRulesChainSubstate, GlobalAddressSubstate, HardAuthRule, HardProofRule,
+    HardResourceOrNonFungible, MethodAuthorization, ValidatorSubstate,
 };
 use crate::types::*;
 use crate::wasm::WasmEngine;
@@ -17,6 +16,29 @@ use radix_engine_interface::api::types::{
 use radix_engine_interface::model::*;
 use radix_engine_interface::modules::auth::AuthAddresses;
 use radix_engine_interface::rule;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+#[scrypto(TypeId, Encode, Decode)]
+pub struct EpochManagerSubstate {
+    pub address: SystemAddress, // TODO: Does it make sense for this to be stored here?
+    pub epoch: u64,
+    pub round: u64,
+    pub rounds_per_epoch: u64,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Ord, PartialOrd)]
+#[scrypto(TypeId, Encode, Decode)]
+pub struct Validator {
+    pub key: EcdsaSecp256k1PublicKey,
+    pub stake: Decimal,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+#[scrypto(TypeId, Encode, Decode)]
+pub struct ValidatorSetSubstate {
+    pub validator_set: BTreeMap<SystemAddress, Validator>,
+    pub epoch: u64,
+}
 
 #[derive(Debug, Clone, Eq, PartialEq, TypeId, Encode, Decode)]
 pub enum EpochManagerError {
@@ -482,260 +504,5 @@ impl EpochManager {
                 AuthAddresses::system_role(),
             )),
         ))]
-    }
-}
-
-pub struct ValidatorRegisterExecutable(RENodeId);
-
-impl<W: WasmEngine> ExecutableInvocation<W> for ValidatorRegisterInvocation {
-    type Exec = ValidatorRegisterExecutable;
-
-    fn resolve<D: ResolverApi<W>>(
-        self,
-        deref: &mut D,
-    ) -> Result<(ResolvedActor, CallFrameUpdate, Self::Exec), RuntimeError>
-    where
-        Self: Sized,
-    {
-        let mut call_frame_update = CallFrameUpdate::empty();
-        let receiver = RENodeId::Global(GlobalAddress::System(self.receiver));
-        let resolved_receiver = deref_and_update(receiver, &mut call_frame_update, deref)?;
-
-        let actor = ResolvedActor::method(
-            NativeFn::Validator(ValidatorFn::Register),
-            resolved_receiver,
-        );
-        let executor = ValidatorRegisterExecutable(resolved_receiver.receiver);
-        Ok((actor, call_frame_update, executor))
-    }
-}
-
-impl Executor for ValidatorRegisterExecutable {
-    type Output = ();
-
-    fn execute<Y>(self, api: &mut Y) -> Result<((), CallFrameUpdate), RuntimeError>
-    where
-        Y: SystemApi + EngineApi<RuntimeError> + InvokableModel<RuntimeError>,
-    {
-        let offset = SubstateOffset::Validator(ValidatorOffset::Validator);
-        let (receiver, validator_address, key, stake_vault_id) = {
-            let handle = api.lock_substate(self.0, offset.clone(), LockFlags::MUTABLE)?;
-            let mut substate = api.get_ref_mut(handle)?;
-            let validator = substate.validator();
-
-            if validator.is_registered {
-                return Ok(((), CallFrameUpdate::empty()));
-            }
-
-            validator.is_registered = true;
-
-            (
-                validator.manager,
-                validator.address,
-                validator.key,
-                validator.stake_vault_id,
-            )
-        };
-        let stake_vault = Vault(stake_vault_id);
-        let stake_amount = stake_vault.sys_amount(api)?;
-
-        if stake_amount.is_positive() {
-            let invocation = EpochManagerUpdateValidatorInvocation {
-                receiver,
-                validator_address,
-                update: UpdateValidator::Register(key, stake_amount),
-            };
-            api.invoke(invocation)?;
-        }
-
-        Ok(((), CallFrameUpdate::empty()))
-    }
-}
-
-pub struct ValidatorUnregisterExecutable(RENodeId);
-
-impl<W: WasmEngine> ExecutableInvocation<W> for ValidatorUnregisterInvocation {
-    type Exec = ValidatorUnregisterExecutable;
-
-    fn resolve<D: ResolverApi<W>>(
-        self,
-        deref: &mut D,
-    ) -> Result<(ResolvedActor, CallFrameUpdate, Self::Exec), RuntimeError>
-    where
-        Self: Sized,
-    {
-        let mut call_frame_update = CallFrameUpdate::empty();
-        let receiver = RENodeId::Global(GlobalAddress::System(self.receiver));
-        let resolved_receiver = deref_and_update(receiver, &mut call_frame_update, deref)?;
-        let actor = ResolvedActor::method(
-            NativeFn::Validator(ValidatorFn::Unregister),
-            resolved_receiver,
-        );
-        let executor = ValidatorUnregisterExecutable(resolved_receiver.receiver);
-        Ok((actor, call_frame_update, executor))
-    }
-}
-
-impl Executor for ValidatorUnregisterExecutable {
-    type Output = ();
-
-    fn execute<Y>(self, api: &mut Y) -> Result<((), CallFrameUpdate), RuntimeError>
-    where
-        Y: SystemApi + InvokableModel<RuntimeError>,
-    {
-        let offset = SubstateOffset::Validator(ValidatorOffset::Validator);
-        let handle = api.lock_substate(self.0, offset.clone(), LockFlags::MUTABLE)?;
-        let mut substate = api.get_ref_mut(handle)?;
-        let validator = substate.validator();
-        if !validator.is_registered {
-            return Ok(((), CallFrameUpdate::empty()));
-        }
-        validator.is_registered = false;
-
-        let invocation = EpochManagerUpdateValidatorInvocation {
-            receiver: validator.manager,
-            validator_address: validator.address,
-            update: UpdateValidator::Unregister,
-        };
-
-        api.invoke(invocation)?;
-
-        Ok(((), CallFrameUpdate::empty()))
-    }
-}
-
-pub struct ValidatorStakeExecutable(RENodeId, Bucket);
-
-impl<W: WasmEngine> ExecutableInvocation<W> for ValidatorStakeInvocation {
-    type Exec = ValidatorStakeExecutable;
-
-    fn resolve<D: ResolverApi<W>>(
-        self,
-        deref: &mut D,
-    ) -> Result<(ResolvedActor, CallFrameUpdate, Self::Exec), RuntimeError>
-    where
-        Self: Sized,
-    {
-        let mut call_frame_update = CallFrameUpdate::empty();
-        let receiver = RENodeId::Global(GlobalAddress::System(self.receiver));
-        let resolved_receiver = deref_and_update(receiver, &mut call_frame_update, deref)?;
-        call_frame_update
-            .nodes_to_move
-            .push(RENodeId::Bucket(self.stake.0));
-
-        let actor =
-            ResolvedActor::method(NativeFn::Validator(ValidatorFn::Stake), resolved_receiver);
-        let executor = ValidatorStakeExecutable(resolved_receiver.receiver, self.stake);
-        Ok((actor, call_frame_update, executor))
-    }
-}
-
-impl Executor for ValidatorStakeExecutable {
-    type Output = ();
-
-    fn execute<Y>(self, api: &mut Y) -> Result<((), CallFrameUpdate), RuntimeError>
-    where
-        Y: SystemApi + EngineApi<RuntimeError> + InvokableModel<RuntimeError>,
-    {
-        let (is_registered, receiver, validator_address, key, stake_amount) = {
-            let offset = SubstateOffset::Validator(ValidatorOffset::Validator);
-            let handle = api.lock_substate(self.0, offset, LockFlags::read_only())?;
-            let substate = api.get_ref(handle)?;
-            let validator = substate.validator();
-            let validator_address = validator.address;
-            let manager = validator.manager;
-            let key = validator.key;
-            let is_registered = validator.is_registered;
-            let mut stake_vault = Vault(validator.stake_vault_id);
-            stake_vault.sys_put(self.1, api)?;
-            let amount = stake_vault.sys_amount(api)?;
-            (is_registered, manager, validator_address, key, amount)
-        };
-
-        if is_registered {
-            let invocation = EpochManagerUpdateValidatorInvocation {
-                receiver,
-                validator_address,
-                update: UpdateValidator::Register(key, stake_amount),
-            };
-
-            api.invoke(invocation)?;
-        }
-
-        Ok(((), CallFrameUpdate::empty()))
-    }
-}
-
-pub struct ValidatorUnstakeExecutable(RENodeId, Decimal);
-
-impl<W: WasmEngine> ExecutableInvocation<W> for ValidatorUnstakeInvocation {
-    type Exec = ValidatorUnstakeExecutable;
-
-    fn resolve<D: ResolverApi<W>>(
-        self,
-        deref: &mut D,
-    ) -> Result<(ResolvedActor, CallFrameUpdate, Self::Exec), RuntimeError>
-    where
-        Self: Sized,
-    {
-        let mut call_frame_update = CallFrameUpdate::empty();
-        let receiver = RENodeId::Global(GlobalAddress::System(self.receiver));
-        let resolved_receiver = deref_and_update(receiver, &mut call_frame_update, deref)?;
-
-        let actor =
-            ResolvedActor::method(NativeFn::Validator(ValidatorFn::Unstake), resolved_receiver);
-        let executor = ValidatorUnstakeExecutable(resolved_receiver.receiver, self.amount);
-        Ok((actor, call_frame_update, executor))
-    }
-}
-
-impl Executor for ValidatorUnstakeExecutable {
-    type Output = Bucket;
-
-    fn execute<Y>(self, api: &mut Y) -> Result<(Bucket, CallFrameUpdate), RuntimeError>
-    where
-        Y: SystemApi + EngineApi<RuntimeError> + InvokableModel<RuntimeError>,
-    {
-        let (is_registered, receiver, validator_address, key, unstake_bucket, stake_amount) = {
-            let offset = SubstateOffset::Validator(ValidatorOffset::Validator);
-            let handle = api.lock_substate(self.0, offset, LockFlags::read_only())?;
-            let substate = api.get_ref(handle)?;
-            let validator = substate.validator();
-            let validator_address = validator.address;
-            let manager = validator.manager;
-            let key = validator.key;
-            let is_registered = validator.is_registered;
-
-            let mut stake_vault = Vault(validator.stake_vault_id);
-            let bucket = stake_vault.sys_take(self.1, api)?;
-            let amount = stake_vault.sys_amount(api)?;
-
-            (
-                is_registered,
-                manager,
-                validator_address,
-                key,
-                bucket,
-                amount,
-            )
-        };
-
-        if is_registered {
-            let update = if stake_amount.is_zero() {
-                UpdateValidator::Unregister
-            } else {
-                UpdateValidator::Register(key, stake_amount)
-            };
-
-            let invocation = EpochManagerUpdateValidatorInvocation {
-                receiver,
-                validator_address,
-                update,
-            };
-            api.invoke(invocation)?;
-        }
-
-        let update = CallFrameUpdate::move_node(RENodeId::Bucket(unstake_bucket.0));
-        Ok((unstake_bucket, update))
     }
 }
