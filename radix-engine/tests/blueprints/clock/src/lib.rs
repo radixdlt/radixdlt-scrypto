cfebe5a5c9f0c9900e5b5109226aa0cf4bc68dc8
Yulong Wu <yulong.wu@rdx.works>
2023-01-24 13:57:14 +0000

    Fix more

--- radix-engine/tests/blueprints/clock/src/lib.rs
+++ radix-engine/tests/blueprints/clock/src/lib.rs
@@ -1,4 +1,4 @@
-use crate::radix_engine_interface::api::blueprints::clock::ClockSetCurrentTimeInvocation;
+use crate::radix_engine_interface::api::blueprints::clock::*;
 use radix_engine_interface::api::Invokable;
 use radix_engine_interface::time::*;
 use scrypto::engine::scrypto_env::*;
