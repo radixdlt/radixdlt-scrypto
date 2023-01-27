68b607f3a217eab7833e26812a1f7c2e029105b9
David Edey <david.edey@rdx.works>
2023-01-27 18:29:53 +0000

    fix: Run ./format.sh

--- radix-engine/src/model/fee/module.rs
+++ radix-engine/src/model/fee/module.rs
@@ -167,8 +167,12 @@ impl<R: FeeReserve> BaseModule<R> for CostingModule {
         // We add a little to make up for billing overhead, and then multiply by a large enough factor
         // to ensure spin loops end within a fraction of a second.
         // These values will be tweaked, alongside the whole fee table.
-        let amount_to_consume = units.checked_add(20).and_then(|a| a.checked_mul(20))
-            .ok_or(ModuleError::CostingError(CostingError::FeeReserveError(FeeReserveError::Overflow)))?;
+        let amount_to_consume = units
+            .checked_add(20)
+            .and_then(|a| a.checked_mul(20))
+            .ok_or(ModuleError::CostingError(CostingError::FeeReserveError(
+                FeeReserveError::Overflow,
+            )))?;
         track
             .fee_reserve()
             .consume_execution(amount_to_consume, "run_wasm")
