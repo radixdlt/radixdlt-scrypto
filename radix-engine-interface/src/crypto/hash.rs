cf9b9d2d62ee993dca91ba0cc8b83acb8b82ea56
Yulong Wu <yulong.wu@rdx.works>
2023-01-27 10:27:47 +0000

    Use blake3

--- radix-engine-interface/src/crypto/hash.rs
+++ radix-engine-interface/src/crypto/hash.rs
@@ -44,7 +44,7 @@ pub fn hash<T: AsRef<[u8]>>(data: T) -> Hash {
     // TODO: replace with whatever hash algorithm we eventually agrees on
     // The point here is to have a single "main" hashing function in the code base
 
-    crate::crypto::sha256(data)
+    Hash(blake3::hash(data.as_ref()).as_bytes().clone())
 }
 
 //========
