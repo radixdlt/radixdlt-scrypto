985f6c9c58129e471ac534a9c6c479cd401c9c9b
Yulong Wu <yulong.wu@rdx.works>
2023-01-28 11:21:12 +0000

    Dedicated component/package instantiation API

--- radix-engine-interface/src/api/component_api.rs
+++ radix-engine-interface/src/api/component_api.rs
@@ -1,8 +1,22 @@
 use crate::api::types::*;
+use crate::blueprints::resource::AccessRules;
 use crate::data::IndexedScryptoValue;
+use sbor::rust::collections::*;
 use sbor::rust::vec::Vec;
 
 pub trait ClientComponentApi<E> {
+    // TODO: refine the interface
+    fn instantiate_component(
+        &mut self,
+        blueprint_ident: &str,
+        app_states: BTreeMap<u8, Vec<u8>>,
+        access_rules: AccessRules,
+        royalty_config: RoyaltyConfig,
+        metadata: BTreeMap<String, String>,
+    ) -> Result<ComponentId, E>;
+
+    fn globalize_component(&mut self, component_id: ComponentId) -> Result<ComponentAddress, E>;
+
     fn call_method(
         &mut self,
         receiver: ScryptoReceiver,
--- radix-engine-interface/src/api/package_api.rs
+++ radix-engine-interface/src/api/package_api.rs
@@ -1,10 +1,20 @@
 use crate::abi::BlueprintAbi;
 use crate::api::types::*;
+use crate::blueprints::resource::AccessRules;
 use crate::data::IndexedScryptoValue;
 use sbor::rust::collections::BTreeMap;
 use sbor::rust::vec::Vec;
 
-pub trait ClientPackageApi<E> {
+pub trait ClientPackageApi<E> { 
+    fn instantiate_package(
+        &mut self,
+        code: Vec<u8>,
+        abi: BTreeMap<String, BlueprintAbi>,
+        access_rules: AccessRules,
+        royalty_config: BTreeMap<String, RoyaltyConfig>,
+        metadata: BTreeMap<String, String>,
+    ) -> Result<PackageAddress, E>;
+
     fn call_function(
         &mut self,
         package_address: PackageAddress,
