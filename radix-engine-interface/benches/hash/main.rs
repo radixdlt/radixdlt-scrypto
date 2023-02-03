393d7ea4581551dfd7b34d7e47b9e2ad1cc1e994
Lukasz Rubaszewski <lukasz.rubaszewski@rdx.works>
2023-02-03 20:02:46 +0100

    Add SHA3, Poseidon, Pedersen hash benchmarks

--- radix-engine-interface/benches/hash/main.rs
+++ radix-engine-interface/benches/hash/main.rs
@@ -1,39 +1,89 @@
+use ::sha2::Sha512_256;
 use blake2::{digest::consts::U32, Blake2b, Digest};
 use blake3;
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
-use radix_engine_interface::crypto::{sha256, sha256_twice};
+use radix_engine_interface::crypto::{sha256, sha256_twice, sha3};
+use webb_pedersen_hash;
+
+use dusk_bls12_381::BlsScalar;
+use dusk_bytes::Serializable;
+use dusk_poseidon::sponge;
 
 const KB: u32 = 1024;
 const MB: u32 = 1024 * KB;
 
+fn sha2_512_256<T: AsRef<[u8]>>(data: T) -> [u8; 32] {
+    let mut hasher = Sha512_256::new();
+    hasher.update(data);
+    hasher.finalize().into()
+}
+
+fn blake2b_hash<T: AsRef<[u8]>>(data: T) -> [u8; 32] {
+    let mut hasher = Blake2b::<U32>::new();
+    hasher.update(data);
+    hasher.finalize().into()
+}
+
+fn poseidon_hash<T: AsRef<[u8]>>(data: T) -> [u8; 32] {
+    let mut vec_scalar = Vec::<BlsScalar>::new();
+
+    for i in (0..data.as_ref().len()).step_by(32) {
+        let scalar = BlsScalar::from_bytes(data.as_ref()[i..i + 32].try_into().unwrap()).unwrap();
+        vec_scalar.push(scalar);
+    }
+    sponge::hash(&vec_scalar).to_bytes()
+}
+
 fn bench_hash(c: &mut Criterion) {
-    let sizes = [(500, "500B"), (MB, "1MB")];
+    let sizes = [(512, "512B"), (MB, "1MB")];
     let mut group = c.benchmark_group("hash");
     for size in sizes {
         let data = vec![0_u8; size.0 as usize];
 
         group.throughput(Throughput::Bytes(size.0 as u64));
 
-        group.bench_with_input(BenchmarkId::new("sha256", size.1), &data[..], |b, d| {
+        // SHA2-256
+        group.bench_with_input(BenchmarkId::new("SHA2-256", size.1), &data[..], |b, d| {
             b.iter(|| sha256(d))
         });
 
+        // SHA2-256_twice
         group.bench_with_input(
-            BenchmarkId::new("sha256_twice", size.1),
+            BenchmarkId::new("SHA2-256_twice", size.1),
             &data[..],
             |b, d| b.iter(|| sha256_twice(d)),
         );
 
-        group.bench_with_input(BenchmarkId::new("blake2b", size.1), &data[..], |b, d| {
-            b.iter(|| {
-                let mut hasher = Blake2b::<U32>::new();
-                hasher.update(d);
-                hasher.finalize()
-            })
+        // SHA2-512/256
+        group.bench_with_input(
+            BenchmarkId::new("SHA2-512_256", size.1),
+            &data[..],
+            |b, d| b.iter(|| sha2_512_256(d)),
+        );
+
+        // SHA3-256
+        group.bench_with_input(BenchmarkId::new("SHA3-256", size.1), &data[..], |b, d| {
+            b.iter(|| sha3(d))
         });
 
-        group.bench_with_input(BenchmarkId::new("blake3", size.1), &data[..], |b, d| {
+        // Blake2b
+        group.bench_with_input(BenchmarkId::new("Blake2b", size.1), &data[..], |b, d| {
+            b.iter(|| blake2b_hash(d))
+        });
+
+        // Blake3
+        group.bench_with_input(BenchmarkId::new("Blake3", size.1), &data[..], |b, d| {
             b.iter(|| blake3::hash(d))
+        });
+
+        // Pedersen Hash
+        group.bench_with_input(BenchmarkId::new("Pedersen", size.1), &data[..], |b, d| {
+            b.iter(|| webb_pedersen_hash::hash(d))
+        });
+
+        // Poseidon Hash
+        group.bench_with_input(BenchmarkId::new("Poseidon", size.1), &data[..], |b, d| {
+            b.iter(|| poseidon_hash(d))
         });
     }
 }
