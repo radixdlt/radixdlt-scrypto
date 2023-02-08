fb1e1d94976d676b2100b09b55fff4abcea7828e
Lukasz Rubaszewski <lukasz.rubaszewski@rdx.works>
2023-02-08 21:21:16 +0100

    switch to double SHA2-256 as a default hash

--- radix-engine-interface/src/crypto/hash.rs
+++ radix-engine-interface/src/crypto/hash.rs
@@ -44,7 +44,8 @@ pub fn hash<T: AsRef<[u8]>>(data: T) -> Hash {
     // TODO: replace with whatever hash algorithm we eventually agrees on
     // The point here is to have a single "main" hashing function in the code base
 
-    crate::crypto::sha256(data)
+//    crate::crypto::sha256(data)
+    crate::crypto::sha256_twice(data)
 }
 
 //========
