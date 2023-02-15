6feea43056a9add7673b601f1c2f8d150aa9f3dc
Lukasz Rubaszewski <lukasz.rubaszewski@rdx.works>
2023-02-15 12:38:13 +0100

    Remove no longer needed sha2 and sha3 deps

--- radix-engine-interface/src/crypto/mod.rs
+++ radix-engine-interface/src/crypto/mod.rs
@@ -3,13 +3,9 @@ mod ecdsa_secp256k1;
 mod eddsa_ed25519;
 mod hash;
 mod public_key;
-mod sha2;
-mod sha3;
 
 pub use self::blake2b::blake2b_256_hash;
 pub use self::ecdsa_secp256k1::*;
 pub use self::eddsa_ed25519::*;
 pub use self::hash::*;
 pub use self::public_key::*;
-pub use self::sha2::{sha256, sha256_twice};
-pub use self::sha3::sha3;
