33ac48c40b9252f0c2bf70f6c18b60ffcaddf2e4
Michał Strug <michal.strug@rdx.works>
2023-02-17 15:29:47 +0100

    Added counting of substate reads

--- radix-engine/src/system/kernel_modules/transaction_limits/module.rs
+++ radix-engine/src/system/kernel_modules/transaction_limits/module.rs
@@ -7,8 +7,9 @@ use crate::{
     },
     types::Vec,
 };
-
-use radix_engine_interface::*;
+use radix_engine_interface::{
+    api::types::LockHandle, ScryptoCategorize, ScryptoDecode, ScryptoEncode,
+};
 
 #[derive(Debug, Clone, PartialEq, Eq, ScryptoCategorize, ScryptoEncode, ScryptoDecode)]
 pub enum TransactionLimitsError {
@@ -20,6 +21,15 @@ pub enum TransactionLimitsError {
     MaxWasmInstanceMemoryExceeded(usize),
 }
 
+/// Representatino of data which needs to be limited for each call frame.
+#[derive(Default)]
+struct CallFrameInfo {
+    /// Consumed WASM memory.
+    wasm_memory_usage: usize,
+    /// Substate store read count.
+    substate_store_read: usize,
+}
+
 /// Tracks and verifies transaction limits during transactino execution,
 /// if exceeded breaks execution with appropriate error.
 pub struct TransactionLimitsModule {
@@ -27,8 +37,8 @@ pub struct TransactionLimitsModule {
     max_wasm_memory: usize,
     /// Maximum WASM memory which can be consumed during transaction execution.
     max_wasm_memory_per_call_frame: usize,
-    /// Consumed WASM memory for each invocation call.
-    wasm_memory_usage_stack: Vec<usize>,
+    /// Internal stack of data for each call frame.
+    call_frames_stack: Vec<CallFrameInfo>,
 }
 
 impl TransactionLimitsModule {
@@ -36,29 +46,35 @@ impl TransactionLimitsModule {
         TransactionLimitsModule {
             max_wasm_memory,
             max_wasm_memory_per_call_frame: max_wasm_instance_memory,
-            wasm_memory_usage_stack: Vec::with_capacity(8),
+            call_frames_stack: Vec::with_capacity(8),
         }
     }
 
     /// Checks if maximum WASM memory limit for one instance was exceeded and then
     /// checks if memory limit for all instances was exceeded.
-    fn validate(&self) -> Result<(), RuntimeError> {
+    fn validate_wasm_memory(&self) -> Result<(), RuntimeError> {
         // check last (current) call frame
-        let current_instance_memory = *self
-            .wasm_memory_usage_stack
+        let current_call_frame = self
+            .call_frames_stack
             .last()
-            .expect("Wasm memory usage stack should not be empty.");
-        if current_instance_memory > self.max_wasm_memory_per_call_frame {
+            .expect("Call frames stack (Wasm memory) should not be empty.");
+        if current_call_frame.wasm_memory_usage > self.max_wasm_memory_per_call_frame {
             return Err(RuntimeError::ModuleError(
                 ModuleError::TransactionLimitsError(
-                    TransactionLimitsError::MaxWasmInstanceMemoryExceeded(current_instance_memory),
+                    TransactionLimitsError::MaxWasmInstanceMemoryExceeded(
+                        current_call_frame.wasm_memory_usage,
+                    ),
                 ),
             ));
         };
 
         // calculate current maximum consumed memory
         // sum all call stack values
-        let max_value = self.wasm_memory_usage_stack.iter().sum();
+        let max_value = self
+            .call_frames_stack
+            .iter()
+            .map(|item| item.wasm_memory_usage)
+            .sum();
 
         // validate if limit was exceeded
         if max_value > self.max_wasm_memory {
@@ -71,6 +87,10 @@ impl TransactionLimitsModule {
             Ok(())
         }
     }
+
+    fn validate_substates(&self) -> Result<(), RuntimeError> {
+        Ok(())
+    }
 }
 
 impl KernelModule for TransactionLimitsModule {
@@ -82,8 +102,8 @@ impl KernelModule for TransactionLimitsModule {
         // push new empty wasm memory value refencing current call frame to internal stack
         api.kernel_get_module_state()
             .transaction_limits
-            .wasm_memory_usage_stack
-            .push(0);
+            .call_frames_stack
+            .push(CallFrameInfo::default());
         Ok(())
     }
 
@@ -91,9 +111,32 @@ impl KernelModule for TransactionLimitsModule {
         // pop from internal stack
         api.kernel_get_module_state()
             .transaction_limits
-            .wasm_memory_usage_stack
+            .call_frames_stack
             .pop();
         Ok(())
+    }
+
+    fn on_read_substate<Y: KernelModuleApi<RuntimeError>>(
+        api: &mut Y,
+        _lock_handle: LockHandle,
+        _size: usize,
+    ) -> Result<(), RuntimeError> {
+        let depth = api.kernel_get_current_depth();
+        let tlimit = &mut api.kernel_get_module_state().transaction_limits;
+
+        // update current frame substate read count
+        if let Some(val) = tlimit.call_frames_stack.get_mut(depth) {
+            val.substate_store_read += 1;
+        } else {
+            // Kernel can call this event before calling before_push_frame()
+            // in that case we are pushing new itme on internal stack.
+            tlimit.call_frames_stack.push(CallFrameInfo {
+                wasm_memory_usage: 0,
+                substate_store_read: 1,
+            })
+        }
+
+        tlimit.validate_substates()
     }
 
     // This event handler is called from two places:
@@ -107,15 +150,18 @@ impl KernelModule for TransactionLimitsModule {
         let tlimit = &mut api.kernel_get_module_state().transaction_limits;
 
         // update current frame consumed memory
-        if let Some(val) = tlimit.wasm_memory_usage_stack.get_mut(depth) {
-            *val = consumed_memory;
+        if let Some(val) = tlimit.call_frames_stack.get_mut(depth) {
+            val.wasm_memory_usage = consumed_memory;
         } else {
             // When kernel pops the call frame there are some nested calls which
             // are not aligned with before_push_frame() which requires pushing
             // new value on a stack instead of updating it.
-            tlimit.wasm_memory_usage_stack.push(consumed_memory)
+            tlimit.call_frames_stack.push(CallFrameInfo {
+                wasm_memory_usage: consumed_memory,
+                substate_store_read: 0,
+            })
         }
 
-        tlimit.validate()
+        tlimit.validate_wasm_memory()
     }
 }
