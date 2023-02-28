d2aee97411c5fb943651cf249c8898a34c6feffa
Michał Strug <michal.strug@rdx.works>
2023-02-28 13:14:09 +0100

    Updated limits

--- radix-engine-constants/src/lib.rs
+++ radix-engine-constants/src/lib.rs
@@ -56,10 +56,10 @@ pub const DEFAULT_MAX_SUBSTATE_READS_PER_TRANSACTION: usize = 20_000;
 pub const DEFAULT_MAX_SUBSTATE_WRITES_PER_TRANSACTION: usize = 5_000;
 
 /// The default maximum substates read size.
-pub const DEFAULT_MAX_SUBSTATE_READ_SIZE: usize = 1024 * 1024;
+pub const DEFAULT_MAX_SUBSTATE_READ_SIZE: usize = 500 * 1024;
 
 /// The default maximum substates write size.
-pub const DEFAULT_MAX_SUBSTATE_WRITE_SIZE: usize = 1024 * 1024;
+pub const DEFAULT_MAX_SUBSTATE_WRITE_SIZE: usize = 10 * 1024;
 
 /// The default maximum invoke input args size.
 pub const DEFAULT_MAX_INVOKE_INPUT_SIZE: usize = 4 * 1024 * 1024;
