f8c0de1cf10b1c371046ae9c726c14d7f964640d
Lukasz Rubaszewski <lukasz.rubaszewski@rdx.works>
2023-03-01 16:31:42 +0100

    Add support for AFL fuzzer
    some details: https://rust-fuzz.github.io/book/afl.html

--- fuzz-tests/src/transaction.rs
+++ fuzz-tests/src/transaction.rs
@@ -1,9 +1,22 @@
-#![no_main]
+#![cfg_attr(feature = "libfuzzer-sys", no_main)]
 
+#[cfg(feature = "libfuzzer-sys")]
 use libfuzzer_sys::fuzz_target;
+
+#[cfg(feature = "afl")]
+use afl::fuzz;
+
 
 mod fuzz;
 
+#[cfg(feature = "libfuzzer-sys")]
 fuzz_target!(|data: &[u8]| {
     fuzz::fuzz_transaction(data);
 });
+
+#[cfg(feature = "afl")]
+fn main() {
+    fuzz!(|data: &[u8]| {
+        fuzz::fuzz_transaction(data);
+    });
+}
