eb5c7a38cfd7343779a56f4af3623f2f650aba72
Yulong Wu <yulong.wu@rdx.works>
2023-03-02 18:52:13 +0000

    Bump substate size limit

--- radix-engine-constants/src/lib.rs
+++ radix-engine-constants/src/lib.rs
@@ -57,7 +57,7 @@ pub const DEFAULT_MAX_SUBSTATE_WRITES_PER_TRANSACTION: usize = 5_000;
 
 /// The default maximum substate read and write size.
 /// TODO: Apply this limit in create_node too
-pub const DEFAULT_MAX_SUBSTATE_SIZE: usize = 500 * 1024;
+pub const DEFAULT_MAX_SUBSTATE_SIZE: usize = 4 * 1024 * 1024;
 
 /// The default maximum invoke input args size.
 pub const DEFAULT_MAX_INVOKE_INPUT_SIZE: usize = 4 * 1024 * 1024;
