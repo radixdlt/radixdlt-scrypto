5e1925e9b2af2a6030becfbae63db4e2ca23ef67
Yulong Wu <yulong.wu@rdx.works>
2023-03-03 11:25:11 +0000

    Change the format of resource specifier

--- simulator/src/utils/mod.rs
+++ simulator/src/utils/mod.rs
@@ -1,9 +1,11 @@
 mod arg_parser;
+mod resource_specifier;
 mod cargo;
 mod display;
 mod iter;
 
 pub use arg_parser::*;
+pub use resource_specifier::*;
 pub use cargo::*;
 pub use display::list_item_prefix;
 pub use iter::{IdentifyLast, Iter};
