0e1f1fe6394488ba9f5ca2a612eba698b8952003
Lukasz Rubaszewski <lukasz.rubaszewski@rdx.works>
2023-03-06 11:46:05 +0100

    Implement serialize/deserialize for TransactionManifest

--- transaction/src/model/manifest.rs
+++ transaction/src/model/manifest.rs
@@ -1,8 +1,22 @@
+//use crate::data::{manifest_decode, manifest_encode};
 use crate::model::Instruction;
 use radix_engine_interface::*;
+use radix_engine_interface::data::manifest::*;
+use sbor::*;
 
 #[derive(Debug, Clone, Eq, PartialEq, ManifestSbor)]
 pub struct TransactionManifest {
     pub instructions: Vec<Instruction>,
     pub blobs: Vec<Vec<u8>>,
+}
+
+impl TransactionManifest {
+
+    pub fn from_slice(slice: &[u8]) -> Result<Self, DecodeError> {
+        manifest_decode(slice)
+    }
+
+    pub fn to_bytes(&self) -> Result<Vec<u8>, EncodeError> {
+        manifest_encode(self)
+    }
 }
