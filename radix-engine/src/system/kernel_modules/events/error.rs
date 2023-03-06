ade784faeaf662150ba0b32e4407db0019b92675
Omar <OmarAbdulla7@hotmail.com>
2023-03-06 03:25:20 +0300

    Event schema validation at registration time

--- radix-engine/src/system/kernel_modules/events/error.rs
+++ radix-engine/src/system/kernel_modules/events/error.rs
@@ -2,6 +2,7 @@ use radix_engine_interface::*;
 
 #[derive(Debug, Clone, PartialEq, Eq, ScryptoSbor)]
 pub enum EventError {
+    InvalidEventSchema,
     FailedToSborEncodeEventSchema,
     FailedToSborEncodeEvent,
     InvalidActor,
--- radix-engine/src/system/kernel_modules/events/module.rs
+++ radix-engine/src/system/kernel_modules/events/module.rs
@@ -1,6 +1,12 @@
+use crate::errors::{ApplicationError, RuntimeError};
+use crate::kernel::kernel_api::KernelModuleApi;
 use crate::kernel::module::KernelModule;
+use crate::system::node::{RENodeInit, RENodeModuleInit};
+use crate::system::node_modules::event_schema::PackageEventSchemaSubstate;
 use crate::types::*;
 use radix_engine_interface::api::types::*;
+
+use super::EventError;
 
 #[derive(Debug, Default, Clone)]
 pub struct EventsModule(Vec<(EventTypeIdentifier, Vec<u8>)>);
@@ -15,4 +21,38 @@ impl EventsModule {
     }
 }
 
-impl KernelModule for EventsModule {}
+impl KernelModule for EventsModule {
+    fn before_create_node<Y: KernelModuleApi<RuntimeError>>(
+        _api: &mut Y,
+        _node_id: &RENodeId,
+        _node_init: &RENodeInit,
+        node_module_init: &BTreeMap<NodeModuleId, RENodeModuleInit>,
+    ) -> Result<(), RuntimeError> {
+        // Validating the schema before the node is created.
+        if let Some(RENodeModuleInit::PackageEventSchema(PackageEventSchemaSubstate(
+            event_schema,
+        ))) = node_module_init.get(&NodeModuleId::PackageEventSchema)
+        {
+            for (_, event_schemas) in event_schema {
+                for (_, (local_type_index, schema)) in event_schemas {
+                    // Checking that the schema is itself valid
+                    schema.validate().map_err(|_| {
+                        RuntimeError::ApplicationError(ApplicationError::EventError(
+                            EventError::InvalidEventSchema,
+                        ))
+                    })?;
+
+                    // Ensuring that the event is either a struct or an enum
+                    match schema.resolve_type_kind(*local_type_index) {
+                        // Structs and Enums are allowed
+                        Some(TypeKind::Enum { .. } | TypeKind::Tuple { .. }) => Ok(()),
+                        _ => Err(RuntimeError::ApplicationError(
+                            ApplicationError::EventError(EventError::InvalidEventSchema),
+                        )),
+                    }?
+                }
+            }
+        }
+        Ok(())
+    }
+}
