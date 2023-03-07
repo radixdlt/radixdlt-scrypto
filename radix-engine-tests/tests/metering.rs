db08997331ea9c86caf6e7c86e1c166849a078e9
Yulong Wu <yulong.wu@rdx.works>
2023-03-07 16:45:23 +0000

    Fix tests

--- radix-engine-tests/tests/metering.rs
+++ radix-engine-tests/tests/metering.rs
@@ -68,11 +68,11 @@ fn test_basic_transfer() {
     // (cd radix-engine && cargo test --test metering -- test_basic_transfer)
     assert_eq!(
         10000 /* CreateNode */
-        + 65500 /* DropLock */
+        + 66000 /* DropLock */
         + 10000 /* DropNode */
         + 10000 /* Invoke */
-        + 66000 /* LockSubstate */
-        + 46000 /* ReadSubstate */
+        + 66500 /* LockSubstate */
+        + 46500 /* ReadSubstate */
         + 65000 /* RunNative */
         + 0 /* RunWasm */
         + 50000 /* TxBaseCost */
@@ -200,11 +200,11 @@ fn test_radiswap() {
     // (cd radix-engine && cargo test --test metering -- test_radiswap)
     assert_eq!(
         15000 /* CreateNode */
-        + 186000 /* DropLock */
+        + 187500 /* DropLock */
         + 12500 /* DropNode */
         + 24330 /* Invoke */
-        + 187500 /* LockSubstate */
-        + 2554390 /* ReadSubstate */
+        + 189000 /* LockSubstate */
+        + 2555890 /* ReadSubstate */
         + 147500 /* RunNative */
         + 1616610 /* RunWasm */
         + 50000 /* TxBaseCost */
@@ -309,11 +309,11 @@ fn test_flash_loan() {
     // (cd radix-engine && cargo test --test metering -- test_flash_loan)
     assert_eq!(
         22500 /* CreateNode */
-        + 285000 /* DropLock */
+        + 288000 /* DropLock */
         + 22500 /* DropNode */
         + 45210 /* Invoke */
-        + 292500 /* LockSubstate */
-        + 5121140 /* ReadSubstate */
+        + 295500 /* LockSubstate */
+        + 5124140 /* ReadSubstate */
         + 240000 /* RunNative */
         + 1373565 /* RunWasm */
         + 50000 /* TxBaseCost */
