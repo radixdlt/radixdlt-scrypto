a9148346d69c4118077cc0f903c3b60651328488
Joshua Primero <josh.primero@rdx.works>
2023-03-08 18:29:05 -0500

    Remove NonFungibleGlobalId value serializer

--- radix-engine-common/src/data/scrypto/value_serializer.rs
+++ radix-engine-common/src/data/scrypto/value_serializer.rs
@@ -410,26 +410,6 @@ pub fn serialize_custom_value<S: Serializer>(
     }
 }
 
-/*
-impl<'a> ContextualSerialize<ScryptoValueSerializationContext<'a>> for NonFungibleGlobalId {
-    fn contextual_serialize<S: Serializer>(
-        &self,
-        serializer: S,
-        context: &ScryptoValueSerializationContext<'a>,
-    ) -> Result<S::Ok, S::Error> {
-        let mut tuple = serializer.serialize_tuple(2)?;
-        tuple.serialize_element(
-            &self
-                .resource_address()
-                .display(context.display_context.bech32_encoder)
-                .to_string(),
-        )?;
-        tuple.serialize_element(&self.local_id().to_string())?;
-        tuple.end()
-    }
-}
- */
-
 /// We encode custom types in one of two ways:
 /// - As a tagged object { "type": "TypeName", "value": X }
 /// - As a transparent value (ie without a wrapper)
