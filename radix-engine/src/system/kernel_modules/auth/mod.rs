be19f7a5657d2560587c7d5d29a72f6c9f30b750
Yulong Wu <yulong.wu@rdx.works>
2023-03-16 13:29:34 +0000

    Move auth verification to kernel module

--- radix-engine/src/system/kernel_modules/auth/mod.rs
+++ radix-engine/src/system/kernel_modules/auth/mod.rs
@@ -1,7 +1,9 @@
 mod auth_converter;
 mod auth_module;
 mod method_authorization;
+mod verification;
 
 pub use auth_converter::*;
 pub use auth_module::*;
 pub use method_authorization::*;
+pub use verification::*;
