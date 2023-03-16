52c91a7d2be66ba9ecaef10c1d9a765b45f2f6a5
Lukasz Rubaszewski <lukasz.rubaszewski@rdx.works>
2023-03-16 13:04:26 +0100

    fix: Check divisibility to avoid integer overflow and eventually a crash
    This is to avoid following crashes:
    - in debug mode:
      `thread '<name>' panicked at 'attempt to subtract with overflow', radix-engine-interface/src/blueprints/resource/resource_type.rs:42:53`
    - in release mode:
      `thread '<name> panicked at '(bnum) attempt to calculate remainder with a divisor of zero', <home>/.cargo/registry/src/github.com-1ecc6299db9ec823/bnum-0.4.0/src/bint/ops.rs:134:1`

--- radix-engine/src/blueprints/resource/resource_manager.rs
+++ radix-engine/src/blueprints/resource/resource_manager.rs
@@ -22,6 +22,8 @@ use radix_engine_interface::blueprints::resource::*;
 use radix_engine_interface::math::Decimal;
 use radix_engine_interface::*;
 
+const DIVISIBILITY_MAXIMUM: u8 = 18;
+
 #[derive(Debug, Clone, PartialEq, Eq, ScryptoSbor)]
 pub struct ResourceManagerSubstate {
     pub resource_address: ResourceAddress, // TODO: Figure out a way to remove?
@@ -57,6 +59,7 @@ pub enum ResourceManagerError {
     NonFungibleIdTypeDoesNotMatch(NonFungibleIdType, NonFungibleIdType),
     ResourceTypeDoesNotMatch,
     InvalidNonFungibleIdType,
+    InvalidDivisibility(u8),
 }
 
 fn build_non_fungible_resource_manager_substate_with_initial_supply<Y>(
@@ -140,6 +143,10 @@ fn build_fungible_resource_manager_substate_with_initial_supply<Y>(
 where
     Y: KernelNodeApi + KernelSubstateApi + ClientApi<RuntimeError>,
 {
+    if divisibility > DIVISIBILITY_MAXIMUM {
+        return Err(RuntimeError::ApplicationError(ApplicationError::ResourceManagerError(ResourceManagerError::InvalidDivisibility(divisibility,))));
+    }
+
     let mut resource_manager = ResourceManagerSubstate::new(
         ResourceType::Fungible { divisibility },
         None,
@@ -1454,6 +1461,10 @@ where
     Y: KernelNodeApi + KernelSubstateApi + ClientApi<RuntimeError>,
 {
     let resource_address: ResourceAddress = global_node_id.into();
+
+    if divisibility > DIVISIBILITY_MAXIMUM {
+        return Err(RuntimeError::ApplicationError(ApplicationError::ResourceManagerError(ResourceManagerError::InvalidDivisibility(divisibility,))));
+    }
 
     let resource_manager_substate = ResourceManagerSubstate::new(
         ResourceType::Fungible { divisibility },
