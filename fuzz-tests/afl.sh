cf8f8ce686b9e59651ba85604ef987d69b6a44f5
Lukasz Rubaszewski <lukasz.rubaszewski@rdx.works>
2023-03-21 13:48:07 +0100

    Tweak messaging in afl.sh

--- fuzz-tests/afl.sh
+++ fuzz-tests/afl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -x
+#set -x
 set -e
 set -u
 
@@ -20,8 +20,16 @@ function usage() {
     echo "              all      - to run as many instances as CPU cores available"
     echo "              <number> - to run <number> of instances"
     echo "            'timeout' is an AFL timeout in ms"
-    echo "    watch - Monitor AFL instances until they are finished."
+    echo "    watch <interval>"
+    echo "            Monitor AFL instances until they are finished."
     echo "            One can specify interval (default: $DFLT_INTERVAL) to output the status"
+}
+
+function error() {
+    local msg=$1
+    echo "error - $msg"
+    usage
+    exit 1
 }
 
 function get_cpus() {
@@ -41,6 +49,9 @@ cmd=${1:-watch}
 shift
 
 if [ $cmd = "run" ] ; then
+    if [ $# -lt 1 ] ; then
+        error "duration parameter is missing"
+    fi
     duration=${1}
     cpus=${2:-1}
     timeout=${3:-$DFLT_AFL_TIMEOUT}
@@ -48,7 +59,7 @@ if [ $cmd = "run" ] ; then
         cpus=$(get_cpus)
         echo "CPU cores available: $cpus"
     fi
-    echo "Running $cpus AFL instances"
+    echo "Running $cpus AFL instances for $duration seconds"
     mkdir -p afl
 
     for (( i=0; i<$cpus; i++ )) ; do
@@ -63,11 +74,15 @@ if [ $cmd = "run" ] ; then
         fi
         # TODO: use different fuzzing variants per instance
         screen -dmS afl_$name \
-            bash -c "{ ./fuzz.sh afl run -V $duration $fuzzer -T $target -t $timeout >afl/$name.log 2>afl/$name.err ; echo \$? > afl/$name.status; }"
+            bash -c "{ ./fuzz.sh afl run -V $duration $fuzzer -T $name -t $timeout >afl/$name.log 2>afl/$name.err ; echo \$? > afl/$name.status; }"
     done
+    echo "Started below screen sessions with AFL instances"
+    # adding 'true', because screen returns always error, when listing sessions.
+    screen -ls afl_ || true
+
 elif [ $cmd = "watch" ] ; then
     interval=${1:-$DFLT_INTERVAL}
-    while ! screen -ls afl | grep "No Sockets found" ; do
+    while ! screen -ls afl_ | grep "No Sockets found" ; do
         sleep $interval
         # afl folder structure created with some delay after fuzz startup
         if [ -d afl/$target ] ; then
@@ -77,8 +92,7 @@ elif [ $cmd = "watch" ] ; then
     echo "AFL instances status (0 means 'ok'):"
     find afl -name "*.status" | xargs grep -H -v "*"
 else
-    echo "Command '$cmd' not supported"
-    exit 1
+    error "Command '$cmd' not supported"
 fi
 
 
