0823bfcc7592d569c2fe08d3c5a82832204b3c32
Yulong Wu <yulong.wu@rdx.works>
2023-03-23 14:26:21 +0000

    Move OutputId and OutputValue to commit receipt

--- radix-engine/src/state_manager/commit_receipt.rs
+++ radix-engine/src/state_manager/commit_receipt.rs
@@ -1,5 +1,17 @@
-use crate::ledger::*;
 use crate::types::*;
+
+#[derive(Debug, Clone, Hash, PartialEq, Eq, PartialOrd, Ord, ScryptoSbor)]
+pub struct OutputId {
+    pub substate_id: Vec<u8>,
+    pub substate_hash: Hash,
+    pub version: u32,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, ScryptoSbor)]
+pub struct OutputValue {
+    pub substate_value: Vec<u8>,
+    pub version: u32,
+}
 
 pub struct CommitReceipt {
     pub inputs: Vec<OutputId>,
