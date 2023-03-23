80957ec1de5f9e9a9446a586d2b5380f527a393e
Yulong Wu <yulong.wu@rdx.works>
2023-03-23 09:40:14 +0000

    Update substate key model

--- radix-engine/src/ledger/model.rs
+++ radix-engine/src/ledger/model.rs
@@ -36,15 +36,16 @@ pub struct ModuleId(pub u8);
 /// The unique identifier of a substate within a node module.
 #[derive(Debug, Clone, PartialEq, Eq, Hash, PartialOrd, Ord, Sbor)]
 pub enum SubstateKey {
-    Static(u8),
-    Dynamic(DynamicSubstateKey),
+    Config,
+    State(StateIdentifier),
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash, PartialOrd, Ord, Sbor)]
 #[sbor(transparent)]
-pub struct DynamicSubstateKey(Vec<u8>);
+pub struct StateIdentifier(Vec<u8>);
 
-impl DynamicSubstateKey {
+impl StateIdentifier {
+    pub const MIN_LENGTH: usize = 1;
     pub const MAX_LENGTH: usize = 128;
 
     pub fn from_slice(slice: &[u8]) -> Option<Self> {
@@ -53,7 +54,7 @@ impl DynamicSubstateKey {
 
     pub fn from_bytes(bytes: Vec<u8>) -> Option<Self> {
         // TODO: do we want to enforce more constraints on the bytes?
-        if bytes.len() > Self::MAX_LENGTH {
+        if bytes.len() < Self::MIN_LENGTH || bytes.len() > Self::MAX_LENGTH {
             None
         } else {
             Some(Self(bytes))
@@ -61,13 +62,13 @@ impl DynamicSubstateKey {
     }
 }
 
-impl AsRef<[u8]> for DynamicSubstateKey {
+impl AsRef<[u8]> for StateIdentifier {
     fn as_ref(&self) -> &[u8] {
         &self.0
     }
 }
 
-impl Into<Vec<u8>> for DynamicSubstateKey {
+impl Into<Vec<u8>> for StateIdentifier {
     fn into(self) -> Vec<u8> {
         self.0
     }
@@ -82,38 +83,36 @@ pub fn encode_substate_id(
     buffer.extend(&node_id.0);
     buffer.push(module_id.0);
     match substate_key {
-        SubstateKey::Static(offset) => {
+        SubstateKey::Config => {
             buffer.push(0);
-            buffer.push(*offset);
         }
-        SubstateKey::Dynamic(dynamic) => {
+        SubstateKey::State(state_id) => {
             buffer.push(1);
-            buffer.extend(dynamic.as_ref()); // Length is marked by EOF
+            buffer.extend(state_id.as_ref()); // Length is marked by EOF
         }
     }
     buffer
 }
 
 pub fn decode_substate_id(slice: &[u8]) -> (NodeId, ModuleId, SubstateKey) {
-    // Decode node id
-    let mut node_id = [0u8; NodeId::LENGTH];
-    node_id.copy_from_slice(&slice[0..NodeId::LENGTH]);
-    let node_id = NodeId(node_id);
+    if slice.len() >= NodeId::LENGTH + 1 + 1 {
+        // Decode node id
+        let mut node_id = [0u8; NodeId::LENGTH];
+        node_id.copy_from_slice(&slice[0..NodeId::LENGTH]);
+        let node_id = NodeId(node_id);
 
-    // Decode module id
-    let module_id = ModuleId(slice[NodeId::LENGTH]);
+        // Decode module id
+        let module_id = ModuleId(slice[NodeId::LENGTH]);
 
-    // Decode substate key
-    let substate_key = match slice[NodeId::LENGTH + 1] {
-        0 => SubstateKey::Static(slice[NodeId::LENGTH + 2]),
-        1 => SubstateKey::Dynamic(
-            DynamicSubstateKey::from_slice(&slice[NodeId::LENGTH + 2..])
-                .expect("Invalid dynamic substate key"),
-        ),
-        i => panic!("Invalid substate key type: {}", i),
-    };
-
-    (node_id, module_id, substate_key)
+        // Decode substate key
+        let kind = slice[NodeId::LENGTH + 1];
+        if kind == 0 && slice.len() == NodeId::LENGTH + 2 {
+            return (node_id, module_id, SubstateKey::Config);
+        } else if let Some(id) = StateIdentifier::from_slice(&slice[NodeId::LENGTH + 2..]) {
+            return (node_id, module_id, SubstateKey::State(id));
+        }
+    }
+    panic!("Invalid substate id: {}", hex::encode(slice));
 }
 
 pub fn encode_substate_value(value: &IndexedScryptoValue) -> Vec<u8> {
@@ -121,7 +120,10 @@ pub fn encode_substate_value(value: &IndexedScryptoValue) -> Vec<u8> {
 }
 
 pub fn decode_substate_value(slice: &[u8]) -> IndexedScryptoValue {
-    IndexedScryptoValue::from_slice(slice).expect("Failed to decode substate value")
+    match IndexedScryptoValue::from_slice(slice) {
+        Ok(value) => value,
+        Err(_) => panic!("Invalid substate value: {}", hex::encode(slice)),
+    }
 }
 
 #[cfg(test)]
@@ -132,7 +134,7 @@ mod tests {
     fn test_encode_decode_substate_id() {
         let node_id = NodeId([1u8; NodeId::LENGTH]);
         let module_id = ModuleId(2);
-        let substate_key = SubstateKey::Dynamic(DynamicSubstateKey::from_bytes(vec![3]).unwrap());
+        let substate_key = SubstateKey::State(StateIdentifier::from_bytes(vec![3]).unwrap());
         let substate_id = encode_substate_id(&node_id, &module_id, &substate_key);
         assert_eq!(
             substate_id,
