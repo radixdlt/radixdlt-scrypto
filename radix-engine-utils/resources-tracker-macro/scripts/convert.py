6069d164e2e64b58820f2cb6ee56517197d0b818
Michał Strug <michal.strug@rdx.works>
2023-04-04 23:42:17 +0200

    Updated script

--- radix-engine-utils/resources-tracker-macro/scripts/convert.py
+++ radix-engine-utils/resources-tracker-macro/scripts/convert.py
@@ -10,25 +10,12 @@ if len(sys.argv) < 2:
     sys.exit(-1)
 
 input_file = sys.argv[1]
-#input_file = "./test1.xml"
-
-
-#print("started")
-
-api_functions1 = ("kernel_drop_node", "kernel_allocate_node_id", "kernel_create_node", "kernel_get_node_visibility_origin", "kernel_get_current_actor", 
-"kernel_read_bucket", "kernel_read_proof", "kernel_lock_substate", "kernel_get_lock_info", "kernel_drop_lock", "kernel_read_substate", "kernel_get_substate_ref",
-"kernel_get_substate_ref_mut", "kernel_create_wasm_instance", "kernel_invoke" )
-
-api_functions = ("kernel_invoke","kernel_create_node","kernel_lock_substate")
 
 api_functions_ins = {}
 api_functions_info_data = {}
 
-
-
 tree = etree.parse(input_file)
 
-#for f in api_functions:
 for i in range(1):
 #    root = tree.xpath("/root/" + f)
 #    root = tree.xpath("//" + f)
@@ -36,13 +23,12 @@ for i in range(1):
 # Look for all "kernel..." calls
     root = tree.xpath(".//*[starts-with(local-name(), 'kernel')]")
     for child in root:
-        #print("\n",tree.getpath(child), " ", child.attrib["ins"])
 
         key = child.tag
         cpu_instructions = int(child.attrib["ins"])
         execute_cost = child.xpath(".//consume_cost_units")
 
-        if cpu_instructions == 0 and child.attrib["return"] == "true":
+        if cpu_instructions == 0 and "return" in child.attrib and child.attrib["return"] == "true":
             print("Skipping function which returned early: ", child.tag, "\n", file=sys.stderr)
             continue
 
@@ -57,7 +43,6 @@ for i in range(1):
             blueprint_name = resolve[0].attrib["arg0"].replace('"','')
             fcn_name = resolve[0].attrib["arg1"].replace('"','')
             invoke_size = resolve[0].attrib["arg2"]
-            #print("adding resolve:   ", blueprint_name, fcn_name)
             key += "::" + blueprint_name + "::" + fcn_name + "::" + invoke_size
 
         # handle node_id (from kernel_create_node, kernel_drop_node, kernel_get_node_visibility_origin, kernel_lock_substate)
@@ -69,12 +54,13 @@ for i in range(1):
                 key_param = param[0].partition('[')[0]
             else:
                 key_param = param[0]
+            if key_param[-1] == '(': key_param = key_param[:-1]
             key += "::" + key_param
 
         # handle kernel_create_wasm_instance
         param = child.xpath("./self::kernel_create_wasm_instance/@package_address")
         if param:
-            key += "::" + str(param[0])
+            key += "::" + str(param[0])[:20] + "...]"
         param = child.xpath("./self::kernel_create_wasm_instance/create_instance/@arg0")
         if param:
             key += "::" + str(param[0])
@@ -83,12 +69,18 @@ for i in range(1):
         param = child.xpath("./self::kernel_lock_substate[@module_id | @offset]")
         if param:
             key_offset = param[0].attrib["offset"].partition('[')[0]
+            if key_offset[-1] == '(': key_offset = key_offset[:-1]
             key += "::" + param[0].attrib["module_id"] + "::" + key_offset
 
         # handle kernel_allocate_node_id
         param = child.xpath("./self::kernel_allocate_node_id/@node_type")
         if param:
             key += "::" + param[0]
+
+        # correcting parenthesis
+        c1 = key.count('(')
+        c2 = key.count(')')
+        key += ')'*(c1-c2)
 
 #===========================================================================================================#
 #    Subtract nested kernel calls with separate consumed costs CPU instruction from current kernel call     #
@@ -125,26 +117,18 @@ for i in range(1):
         else:
             api_functions_ins[key] = (info_data,[cpu_instructions]) # remove info_data?
 
-    #for child2 in child:
-    #    print("  ", child2.tag, " ", child2.attrib)
-
-
-#print("\n---\n")
-
 output = {}
 output_tab = []
 
 for i in api_functions_ins.keys():
     output_tab.append([i, len(api_functions_ins[i][1]), min(api_functions_ins[i][1]), max(api_functions_ins[i][1]), round(mean(api_functions_ins[i][1])), round(median(api_functions_ins[i][1])) ])
-#    output[i] = [min(api_functions_ins[i][1]), max(api_functions_ins[i][1]), round(mean(api_functions_ins[i][1]))]
-#    print(i,"\t", min(api_functions_ins[i]), max(api_functions_ins[i]), round(mean(api_functions_ins[i]),2))
-#print(len(r))
 
 #sorted(output_tab, key=lambda x: x[0])
 output_tab.sort()
-output_tab.insert(0,["API call with params", "calls count", "max instr.", "max instr.", "mean", "median"])
+for idx, item in enumerate(output_tab):
+    item.insert(0,idx + 1)
 
-#pprint.pprint(api_functions_ins)
+output_tab.insert(0,["No.", "API function with params", "calls count", "min instr.", "max instr.", "mean", "median"])
+
 print(tabulate(output_tab, headers="firstrow"))
-#pprint.pprint(output)
 
