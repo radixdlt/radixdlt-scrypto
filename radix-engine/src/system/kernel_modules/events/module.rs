711a6931ffb19746396c5645b8f3f7a278c01a23
Omar <OmarAbdulla7@hotmail.com>
2023-04-06 23:22:51 +0300

    Revert "remove events from dropped nodes"
    This reverts commit 0fd08283bdf88b0526fb05e7f748ec69d4cff3fc.

--- radix-engine/src/system/kernel_modules/events/module.rs
+++ radix-engine/src/system/kernel_modules/events/module.rs
@@ -3,49 +3,16 @@ use crate::types::*;
 use radix_engine_interface::api::types::*;
 
 #[derive(Debug, Default, Clone)]
-pub struct EventsModule {
-    events: HashMap<RENodeId, Vec<(EventTypeIdentifier, Vec<u8>, u64)>>,
-    counter: u64,
-}
+pub struct EventsModule(Vec<(EventTypeIdentifier, Vec<u8>)>);
 
 impl EventsModule {
     pub fn add_event(&mut self, identifier: EventTypeIdentifier, data: Vec<u8>) {
-        self.events
-            .entry(identifier.node_id())
-            .or_default()
-            .push((identifier, data, self.counter));
-        self.counter += 1;
-    }
-
-    pub fn remove_node_events(&mut self, node_id: &RENodeId) {
-        self.events.remove(node_id);
+        self.0.push((identifier, data))
     }
 
     pub fn events(self) -> Vec<(EventTypeIdentifier, Vec<u8>)> {
-        let mut events = self
-            .events
-            .into_iter()
-            .flat_map(|(_, events)| events)
-            .collect::<Vec<(EventTypeIdentifier, Vec<u8>, u64)>>();
-        events.sort_by(|(_, _, a), (_, _, b)| a.cmp(b));
-
-        events
-            .into_iter()
-            .map(|(identifier, data, _)| (identifier, data))
-            .collect()
+        self.0
     }
 }
 
-impl KernelModule for EventsModule {
-    fn before_drop_node<
-        Y: crate::kernel::kernel_api::KernelModuleApi<crate::errors::RuntimeError>,
-    >(
-        api: &mut Y,
-        node_id: &RENodeId,
-    ) -> Result<(), crate::errors::RuntimeError> {
-        api.kernel_get_module_state()
-            .events
-            .remove_node_events(node_id);
-        Ok(())
-    }
-}
+impl KernelModule for EventsModule {}
