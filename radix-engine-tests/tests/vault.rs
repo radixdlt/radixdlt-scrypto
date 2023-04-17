8700e3c8fa78c4643d498aa66ced5bb0105b7ff6
Joshua Primero <josh.primero@rdx.works>
2023-04-17 00:32:23 -0500

    Add more tests

--- radix-engine-tests/tests/vault.rs
+++ radix-engine-tests/tests/vault.rs
@@ -520,6 +520,38 @@ fn create_mutable_vault_with_get_resource_manager() {
 }
 
 #[test]
+fn take_on_non_fungible_vault() {
+    // Arrange
+    let mut test_runner = TestRunner::builder().build();
+    let package_address = test_runner.compile_and_publish("./tests/blueprints/vault");
+    let manifest = ManifestBuilder::new()
+        .lock_fee(test_runner.faucet_component(), 10.into())
+        .call_function(
+            package_address,
+            "NonFungibleVault",
+            "new_non_fungible_vault",
+            manifest_args!(),
+        )
+        .build();
+    let receipt = test_runner.execute_manifest(manifest, vec![]);
+    let address = receipt.expect_commit_success().new_component_addresses()[0];
+
+    // Act
+    let manifest = ManifestBuilder::new()
+        .lock_fee(test_runner.faucet_component(), 10.into())
+        .call_method(
+            address,
+            "take",
+            manifest_args!()
+        )
+        .build();
+    let receipt = test_runner.execute_manifest(manifest, vec![]);
+
+    // Assert
+    receipt.expect_commit_success();
+}
+
+#[test]
 fn take_twice_on_non_fungible_vault() {
     // Arrange
     let mut test_runner = TestRunner::builder().build();
