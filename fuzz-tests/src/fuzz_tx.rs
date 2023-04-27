801c2e4e18ea3daaf8bf8df4c6aa943a3b0b1e50
Lukasz Rubaszewski <lukasz.rubaszewski@rdx.works>
2023-04-27 20:07:50 +0200

    Use arbitrary to build manifest instructions WIP

--- fuzz-tests/src/fuzz_tx.rs
+++ fuzz-tests/src/fuzz_tx.rs
@@ -1,11 +1,15 @@
-use radix_engine::types::{ComponentAddress, EcdsaSecp256k1PublicKey, ResourceAddress};
+use arbitrary::{Arbitrary, Unstructured};
+use radix_engine::types::*;
 use radix_engine_interface::blueprints::resource::{FromPublicKey, NonFungibleGlobalId};
 use radix_engine_interface::data::manifest::manifest_decode;
 use scrypto_unit::{TestRunner, TestRunnerSnapshot};
+use transaction::builder::ManifestBuilder;
 use transaction::ecdsa_secp256k1::EcdsaSecp256k1PrivateKey;
-#[cfg(feature = "smart_mutate")]
+use transaction::manifest::ast;
 use transaction::model::Instruction;
 use transaction::model::TransactionManifest;
+
+const INSTRUCTION_MAX_CNT: u8 = 3;
 
 #[derive(Debug, Clone)]
 struct Account {
@@ -138,8 +142,86 @@ impl TxFuzzer {
         }
     }
 
+    fn get_random_account(
+        &mut self,
+        unstructured: &mut Unstructured,
+    ) -> (Account, ResourceAddress) {
+        let account_idx = unstructured.choose_index(self.accounts.len()).unwrap();
+        let mut account = self.accounts[account_idx].clone();
+
+        let resource_idx = unstructured.choose_index(account.resources.len()).unwrap();
+        let mut resource_address = account.resources[resource_idx];
+
+        (account, resource_address)
+    }
+
+    fn build_manifest(&mut self, data: &[u8]) -> Result<TransactionManifest, TxStatus> {
+        let mut builder = ManifestBuilder::new();
+        let mut buckets: Vec<ManifestBucket> = vec![];
+        let mut proof_ids: Vec<ManifestProof> = vec![];
+
+        let mut unstructured = Unstructured::new(&data);
+        println!("unstructured init len = {}", unstructured.len());
+
+        let (account, resource_address) = self.get_random_account(&mut unstructured);
+        let component_address = account.address;
+
+        let fee = Decimal::from(100);
+        //builder.lock_fee(self.runner.faucet_component(), fee);
+        builder.lock_fee(component_address, fee);
+
+        let mut i = 0;
+        while i < INSTRUCTION_MAX_CNT && unstructured.len() > 0 {
+            println!("unstructured remaining len = {}", unstructured.len());
+            let next: u8 = unstructured.int_in_range(0..=1).unwrap();
+            println!(
+                "unstructured remaining len = {} next = {}",
+                unstructured.len(),
+                next
+            );
+
+            let instruction = match next {
+                // AssertWorktopContains
+                0 => Some(Instruction::AssertWorktopContains { resource_address }),
+                1 => {
+                    let amount: u128 = unstructured.int_in_range(0..=10_000_000u128).unwrap();
+                    let amount = Decimal::from(amount);
+                    Some(Instruction::AssertWorktopContainsByAmount {
+                        amount,
+                        resource_address,
+                    })
+                }
+                2 => None,
+                _ => unreachable!(
+                    "Not all instructions (current count is {}) covered by this match",
+                    2
+                ),
+            };
+            match instruction {
+                Some(instruction) => {
+                    let (_, bucket_id, proof_id) = builder.add_instruction(instruction);
+                    match bucket_id {
+                        Some(bucket_id) => buckets.push(bucket_id),
+                        None => {}
+                    }
+                    match proof_id {
+                        Some(proof_id) => proof_ids.push(proof_id),
+                        None => {}
+                    }
+                    i += 1;
+                }
+                None => {}
+            }
+        }
+
+        let manifest = builder.build();
+        println!("manifest = {:?}", manifest);
+        Ok(manifest)
+    }
+
     pub fn fuzz_tx_manifest(&mut self, data: &[u8]) -> TxStatus {
-        let result = manifest_decode::<TransactionManifest>(data);
+        //        let result = manifest_decode::<TransactionManifest>(data);
+        let result = self.build_manifest(data);
         match result {
             #[allow(unused_mut)]
             Ok(mut manifest) => {
