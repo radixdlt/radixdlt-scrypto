12893ff3a8c32ce53bc49ee452a3fd1dd161b21e
Yulong Wu <yulong.wu@rdx.works>
2023-04-28 16:43:29 +0100

    Move reference analysis to transaction validator

--- radix-engine/src/transaction/transaction_executor.rs
+++ radix-engine/src/transaction/transaction_executor.rs
@@ -21,8 +21,7 @@ use radix_engine_interface::blueprints::transaction_processor::{
 };
 use radix_engine_stores::interface::*;
 use radix_engine_stores::jmt_support::JmtMapper;
-use sbor::traversal::*;
-use sbor::*;
+use sbor::rust::borrow::Cow;
 use transaction::model::*;
 
 pub struct FeeReserveConfig {
@@ -164,58 +163,10 @@ where
         fee_reserve: SystemLoanFeeReserve,
         fee_table: FeeTable,
     ) -> TransactionReceipt {
-        let transaction_hash = executable.transaction_hash().clone();
-        let runtime_validations = executable.runtime_validations().to_vec();
-        let instructions = manifest_encode(executable.instructions()).unwrap();
-        let mut blobs = BTreeMap::new();
-        for b in executable.blobs() {
-            blobs.insert(hash(b), b.clone());
-        }
-        let references = {
-            let mut references = BTreeSet::new();
-            let mut traverser = ManifestTraverser::new(
-                &instructions,
-                MANIFEST_SBOR_V1_MAX_DEPTH,
-                ExpectedStart::PayloadPrefix(MANIFEST_SBOR_V1_PAYLOAD_PREFIX),
-                true,
-            );
-            loop {
-                let event = traverser.next_event();
-                match event.event {
-                    TraversalEvent::ContainerStart(_) => {}
-                    TraversalEvent::ContainerEnd(_) => {}
-                    TraversalEvent::TerminalValue(r) => {
-                        if let traversal::TerminalValueRef::Custom(c) = r {
-                            match c.0 {
-                                ManifestCustomValue::Address(address) => {
-                                    references.insert(Reference(address.0));
-                                }
-                                ManifestCustomValue::Bucket(_)
-                                | ManifestCustomValue::Proof(_)
-                                | ManifestCustomValue::Expression(_)
-                                | ManifestCustomValue::Blob(_)
-                                | ManifestCustomValue::Decimal(_)
-                                | ManifestCustomValue::PreciseDecimal(_)
-                                | ManifestCustomValue::NonFungibleLocalId(_) => {}
-                            }
-                        }
-                    }
-                    TraversalEvent::TerminalValueBatch(_) => {}
-                    TraversalEvent::End => {
-                        break;
-                    }
-                    TraversalEvent::DecodeError(e) => {
-                        panic!("Unexpected decoding error: {:?}", e);
-                    }
-                }
-            }
-            references
-        };
-
         #[cfg(not(feature = "alloc"))]
         if execution_config.kernel_trace {
             println!("{:-^80}", "Transaction Metadata");
-            println!("Transaction hash: {}", transaction_hash);
+            println!("Transaction hash: {}", executable.transaction_hash());
             println!(
                 "Preallocated Node IDs: {:?}",
                 executable.pre_allocated_ids()
@@ -233,7 +184,7 @@ where
         // Prepare
         let mut track = Track::<_, JmtMapper>::new(self.substate_db);
         let mut id_allocator = IdAllocator::new(
-            transaction_hash.clone(),
+            executable.transaction_hash().clone(),
             executable.pre_allocated_ids().clone(),
         );
         let mut system = SystemConfig {
@@ -241,7 +192,7 @@ where
                 scrypto_vm: self.scrypto_vm,
             },
             modules: SystemModuleMixer::standard(
-                transaction_hash.clone(),
+                executable.transaction_hash().clone(),
                 executable.auth_zone_params().clone(),
                 fee_reserve,
                 fee_table,
@@ -263,11 +214,15 @@ where
                 TRANSACTION_PROCESSOR_BLUEPRINT,
                 TRANSACTION_PROCESSOR_RUN_IDENT,
                 scrypto_encode(&TransactionProcessorRunInput {
-                    transaction_hash,
-                    runtime_validations,
-                    instructions,
-                    blobs,
-                    references,
+                    transaction_hash: executable.transaction_hash().clone(),
+                    runtime_validations: executable.runtime_validations().clone(),
+                    instructions: executable.instructions().clone(),
+                    blobs: executable
+                        .blobs()
+                        .into_iter()
+                        .map(|(hash, blob)| (*hash, Cow::from(*blob)))
+                        .collect(),
+                    references: executable.references().clone(),
                 })
                 .unwrap(),
             )
