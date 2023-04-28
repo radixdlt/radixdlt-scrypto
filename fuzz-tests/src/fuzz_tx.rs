a6062db0fa52838611622246dec0f025b9dc468b
Lukasz Rubaszewski <lukasz.rubaszewski@rdx.works>
2023-04-28 16:32:46 +0200

    Add further instructions to fuzz
    Add Arbitrary trait to ManifestBucket, ManifestProof

--- fuzz-tests/src/fuzz_tx.rs
+++ fuzz-tests/src/fuzz_tx.rs
@@ -1,8 +1,11 @@
 use arbitrary::{Arbitrary, Unstructured};
 use radix_engine::types::*;
+use radix_engine_interface::blueprints::resource::*;
 use radix_engine_interface::blueprints::resource::{FromPublicKey, NonFungibleGlobalId};
 #[cfg(feature = "decode_tx_manifest")]
 use radix_engine_interface::data::manifest::manifest_decode;
+use radix_engine_stores::interface::SubstateDatabase;
+use radix_engine_stores::jmt_support::JmtMapper;
 use scrypto_unit::{TestRunner, TestRunnerSnapshot};
 use strum::EnumCount;
 use transaction::builder::ManifestBuilder;
@@ -28,7 +31,9 @@ pub struct TxFuzzer {
     snapshot: TestRunnerSnapshot,
     accounts: Vec<Account>,
     component_addresses: Vec<ComponentAddress>,
-    resource_addresses: Vec<ResourceAddress>,
+    all_resource_addresses: Vec<ResourceAddress>,
+    fungible_resource_addresses: Vec<ResourceAddress>,
+    non_fungible_resource_addresses: Vec<ResourceAddress>,
     #[allow(unused)]
     package_addresses: Vec<PackageAddress>,
 }
@@ -37,7 +42,7 @@ impl TxFuzzer {
     pub fn new() -> Self {
         let mut runner = TestRunner::builder().without_trace().build();
         let mut component_addresses = vec![runner.faucet_component()];
-        let mut resource_addresses = vec![
+        let mut all_resource_addresses = vec![
             RADIX_TOKEN,
             ECDSA_SECP256K1_TOKEN,
             EDDSA_ED25519_TOKEN,
@@ -49,6 +54,8 @@ impl TxFuzzer {
             IDENTITY_OWNER_TOKEN,
             ACCOUNT_OWNER_TOKEN,
         ];
+        let mut non_fungible_resource_addresses = vec![];
+        let mut fungible_resource_addresses = vec![];
         let package_addresses = vec![
             PACKAGE_PACKAGE,
             RESOURCE_MANAGER_PACKAGE,
@@ -72,7 +79,9 @@ impl TxFuzzer {
                     runner.create_non_fungible_resource(acc.2),
                     runner.create_non_fungible_resource(acc.2),
                 ];
-                resource_addresses.append(&mut resources.clone());
+                all_resource_addresses.append(&mut resources.clone());
+                fungible_resource_addresses.append(&mut resources.clone()[0..2].to_vec());
+                non_fungible_resource_addresses.append(&mut resources.clone()[2..4].to_vec());
                 println!("addr = {:?}", acc.2);
                 component_addresses.push(acc.2);
 
@@ -91,7 +100,9 @@ impl TxFuzzer {
             snapshot,
             accounts,
             component_addresses,
-            resource_addresses,
+            all_resource_addresses,
+            fungible_resource_addresses,
+            non_fungible_resource_addresses,
             package_addresses,
         }
     }
@@ -192,6 +203,43 @@ impl TxFuzzer {
         Ok((account.clone(), resource_address.clone()))
     }
 
+    fn get_non_fungible_local_id(
+        &mut self,
+        component_address: ComponentAddress,
+        resource_address: ResourceAddress,
+    ) -> BTreeSet<NonFungibleLocalId> {
+        let vaults = self
+            .runner
+            .get_component_vaults(component_address, resource_address);
+        let mut btree_ids = btreeset![];
+        for vault in vaults {
+            let vault = self
+                .runner
+                .substate_db()
+                .get_mapped_substate::<JmtMapper, LiquidNonFungibleVault>(
+                    &vault,
+                    SysModuleId::Object.into(),
+                    NonFungibleVaultOffset::LiquidNonFungible.into(),
+                )
+                .map(|vault| vault.ids);
+
+            vault.map(|ids| {
+                let mut substate_iter = self
+                    .runner
+                    .substate_db()
+                    .list_mapped_substates::<JmtMapper>(
+                        ids.as_node_id(),
+                        SysModuleId::Object.into(),
+                    );
+                substate_iter.next().map(|(_key, value)| {
+                    let id: NonFungibleLocalId = scrypto_decode(value.as_slice()).unwrap();
+                    btree_ids.insert(id);
+                });
+            });
+        }
+        btree_ids
+    }
+
     fn build_manifest(&mut self, data: &[u8]) -> Result<TransactionManifest, TxStatus> {
         let mut builder = ManifestBuilder::new();
         let mut buckets: Vec<ManifestBucket> = vec![];
@@ -205,14 +253,25 @@ impl TxFuzzer {
         println!("unstructured init len = {}", unstructured.len());
 
         let resource_address = unstructured
-            .choose(&self.resource_addresses[..])
+            .choose(&self.all_resource_addresses[..])
             .unwrap()
             .clone();
         let component_address = unstructured
             .choose(&self.component_addresses[..])
             .unwrap()
             .clone();
-        //let package_addresses = unstructured.choose(&self.package_addresses[..]).unwrap();
+        let package_address = unstructured
+            .choose(&self.package_addresses[..])
+            .unwrap()
+            .clone();
+        let non_fungible_resource_address = unstructured
+            .choose(&self.non_fungible_resource_addresses[..])
+            .unwrap()
+            .clone();
+        // TODO: if resource_address of not NonFungible resource is given then we got panic in get_mapped_substate
+        // thread 'main' panicked at 'called `Result::unwrap()` on an `Err` value: UnexpectedSize { expected: 2, actual: 1 }', /Users/lukaszrubaszewski/work/radixdlt/radixdlt-scrypto/radix-engine-stores/src/interface.rs:200:41
+        let non_fungible_ids =
+            self.get_non_fungible_local_id(component_address, non_fungible_resource_address);
 
         let fee = Decimal::arbitrary(&mut unstructured).unwrap();
         builder.lock_fee(component_address, fee);
@@ -240,7 +299,48 @@ impl TxFuzzer {
                         resource_address,
                     })
                 }
-                2..=43 => None,
+                // AssertWorktopContainsByIds
+                2 => Some(Instruction::AssertWorktopContainsByIds {
+                    ids: non_fungible_ids.clone(),
+                    resource_address,
+                }),
+                // BurnResource
+                3 => {
+                    let bucket_id = match unstructured.choose(&buckets[..]) {
+                        Ok(bucket_id) => *bucket_id,
+                        Err(_) => ManifestBucket::arbitrary(&mut unstructured).unwrap(),
+                    };
+
+                    Some(Instruction::BurnResource { bucket_id })
+                }
+                // CallFunction
+                4 => {
+                    // TODO
+                    None
+                }
+                // CallMethod
+                5 => {
+                    // TODO
+                    None
+                }
+                // ClaimComponentRoyalty
+                6 => Some(Instruction::ClaimComponentRoyalty { component_address }),
+                // ClaimPackageRoyalty
+                7 => Some(Instruction::ClaimPackageRoyalty { package_address }),
+                // ClearAuthZone
+                8 => Some(Instruction::ClearAuthZone),
+                // ClearSignatureProofs
+                9 => Some(Instruction::ClearSignatureProofs),
+                // CloneProof
+                10 => {
+                    let proof_id = match unstructured.choose(&proof_ids[..]) {
+                        Ok(proof_id) => *proof_id,
+                        Err(_) => ManifestProof::arbitrary(&mut unstructured).unwrap(),
+                    };
+                    Some(Instruction::CloneProof { proof_id })
+                }
+
+                11..=43 => None,
                 _ => unreachable!(
                     "Not all instructions (current count is {}) covered by this match",
                     ast::Instruction::COUNT
