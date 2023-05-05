cec62f38f942d8f7f1a79e40ad8cfe296fa8084b
Lukasz Rubaszewski <lukasz.rubaszewski@rdx.works>
2023-05-05 17:17:43 +0200

    Implement Arbitrary by hand for PackageAddress

--- radix-engine-common/src/types/addresses/package_address.rs
+++ radix-engine-common/src/types/addresses/package_address.rs
@@ -7,6 +7,8 @@ use crate::types::NodeId;
 use crate::types::*;
 use crate::well_known_scrypto_custom_type;
 use crate::*;
+#[cfg(feature = "radix_engine_fuzzing")]
+use arbitrary::{Arbitrary, Result, Unstructured};
 use sbor::rust::prelude::*;
 use sbor::*;
 use utils::{copy_u8_array, ContextualDisplay};
@@ -44,6 +46,24 @@ impl PackageAddress {
 
     pub fn to_hex(&self) -> String {
         self.0.to_hex()
+    }
+}
+
+#[cfg(feature = "radix_engine_fuzzing")]
+// Implementing arbitrary by hand to make sure that EntityType::GlobalPackage marker is present.
+// Otherwise 'InvalidCustomValue' error is returned
+impl<'a> Arbitrary<'a> for PackageAddress {
+    fn arbitrary(u: &mut Unstructured<'a>) -> Result<Self> {
+        use core::cmp::min;
+
+        let mut node_id = [0u8; NodeId::LENGTH];
+        node_id[0] = EntityType::GlobalPackage as u8;
+        // fill NodeId with available random bytes (fill the rest with zeros if data exhausted)
+        let len = min(NodeId::LENGTH - 1, u.len());
+        let (_left, right) = node_id.split_at_mut(NodeId::LENGTH - len);
+        let b = u.bytes(len).unwrap();
+        right.copy_from_slice(&b);
+        Ok(Self::new_unchecked(node_id))
     }
 }
 
