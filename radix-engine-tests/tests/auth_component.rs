59cbec1981ec2afc40c4a49b570279a0d8bcf1e5
Joshua Primero <josh.primero@rdx.works>
2023-05-06 12:37:32 -0500

    Add more tests

--- radix-engine-tests/tests/auth_component.rs
+++ radix-engine-tests/tests/auth_component.rs
@@ -5,13 +5,10 @@ use radix_engine_interface::rule;
 use scrypto_unit::*;
 use transaction::builder::ManifestBuilder;
 
-fn create_resource_secured_component(test_runner: &mut TestRunner, account: ComponentAddress, package_address: PackageAddress) -> (ComponentAddress, NonFungibleGlobalId) {
-    let auth = test_runner.create_non_fungible_resource(account);
-    let auth_local_id = NonFungibleLocalId::integer(1);
-    let auth_global_id = NonFungibleGlobalId::new(auth, auth_local_id);
+fn create_secured_component(test_runner: &mut TestRunner, auth: NonFungibleGlobalId, package_address: PackageAddress) -> ComponentAddress {
     let authorization = AccessRulesConfig::new().method(
         "get_component_state",
-        rule!(require(auth_global_id.clone())),
+        rule!(require(auth)),
         rule!(deny_all),
     );
     let manifest = ManifestBuilder::new()
@@ -25,9 +22,16 @@ fn create_resource_secured_component(test_runner: &mut TestRunner, account: Comp
         .build();
     let receipt = test_runner.execute_manifest(manifest, vec![]);
     let secured_component = receipt.expect_commit(true).new_component_addresses()[0];
-    (secured_component, auth_global_id)
+    secured_component
 }
 
+fn create_resource_secured_component(test_runner: &mut TestRunner, account: ComponentAddress, package_address: PackageAddress) -> (ComponentAddress, NonFungibleGlobalId) {
+    let auth = test_runner.create_non_fungible_resource(account);
+    let auth_local_id = NonFungibleLocalId::integer(1);
+    let auth_global_id = NonFungibleGlobalId::new(auth, auth_local_id);
+    let secured_component = create_secured_component(test_runner, auth_global_id.clone(), package_address);
+    (secured_component, auth_global_id)
+}
 
 fn create_component(test_runner: &mut TestRunner, package_address: PackageAddress) -> ComponentAddress {
     let manifest = ManifestBuilder::new()
@@ -45,7 +49,56 @@ fn create_component(test_runner: &mut TestRunner, package_address: PackageAddres
 }
 
 #[test]
-fn cannot_make_cross_component_process_call_without_authorization() {
+fn cannot_make_cross_component_call_without_correct_global_caller_authorization() {
+    // Arrange
+    let mut test_runner = TestRunner::builder().build();
+    let (_, _, account) = test_runner.new_allocated_account();
+    let package_address = test_runner.compile_and_publish("./tests/blueprints/component");
+    let badge = NonFungibleGlobalId::global_caller_badge(GlobalCaller::GlobalObject(account.into()));
+    let secured_component = create_secured_component(&mut test_runner, badge, package_address);
+    let my_component = create_component(&mut test_runner, package_address);
+
+    // Act
+    let manifest = ManifestBuilder::new()
+        .lock_fee(test_runner.faucet_component(), 10.into())
+        .call_method(
+            my_component,
+            "cross_component_call",
+            manifest_args!(secured_component),
+        )
+        .build();
+    let receipt = test_runner.execute_manifest(manifest, vec![]);
+
+    // Assert
+    receipt.expect_specific_failure(is_auth_error);
+}
+
+#[test]
+fn can_make_cross_component_call_with_correct_global_caller_authorization() {
+    // Arrange
+    let mut test_runner = TestRunner::builder().build();
+    let package_address = test_runner.compile_and_publish("./tests/blueprints/component");
+    let my_component = create_component(&mut test_runner, package_address);
+    let badge = NonFungibleGlobalId::global_caller_badge(GlobalCaller::GlobalObject(my_component.into()));
+    let secured_component = create_secured_component(&mut test_runner, badge, package_address);
+
+    // Act
+    let manifest = ManifestBuilder::new()
+        .lock_fee(test_runner.faucet_component(), 10.into())
+        .call_method(
+            my_component,
+            "cross_component_call",
+            manifest_args!(secured_component),
+        )
+        .build();
+    let receipt = test_runner.execute_manifest(manifest, vec![]);
+
+    // Assert
+    receipt.expect_commit_success();
+}
+
+#[test]
+fn cannot_make_cross_component_call_without_resource_authorization() {
     // Arrange
     let mut test_runner = TestRunner::builder().build();
     let (_, _, account) = test_runner.new_allocated_account();
@@ -69,7 +122,7 @@ fn cannot_make_cross_component_process_call_without_authorization() {
 }
 
 #[test]
-fn can_make_cross_component_process_call_data_authorization() {
+fn can_make_cross_component_call_with_resource_authorization() {
     // Arrange
     let mut test_runner = TestRunner::builder().build();
     let (public_key, _, account) = test_runner.new_allocated_account();
