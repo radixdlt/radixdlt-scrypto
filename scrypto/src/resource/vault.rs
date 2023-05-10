256395d0680e88a0a1a27f2e5f39da4a2ea03dad
Yulong Wu <yulong.wu@rdx.works>
2023-05-10 14:27:08 +0100

    Add ScryptoFungibleVault and ScryptoNonFungibleVault traits

--- scrypto/src/resource/vault.rs
+++ scrypto/src/resource/vault.rs
@@ -15,31 +15,40 @@ use crate::*;
 
 pub trait ScryptoVault {
     fn with_bucket(bucket: Bucket) -> Self;
-    fn amount(&self) -> Decimal;
     fn new(resource_address: ResourceAddress) -> Self;
-    fn take_internal(&mut self, amount: Decimal) -> Bucket;
-    fn lock_fee_internal(&mut self, amount: Decimal) -> ();
-    fn lock_contingent_fee_internal(&mut self, amount: Decimal) -> ();
     fn put(&mut self, bucket: Bucket) -> ();
+    fn amount(&self) -> Decimal;
+    fn resource_address(&self) -> ResourceAddress;
+    fn create_proof(&self) -> Proof;
+    fn create_proof_of_amount(&self, amount: Decimal) -> Proof;
+    fn create_proof_of_non_fungibles(&self, ids: &BTreeSet<NonFungibleLocalId>) -> Proof;
+    fn take<A: Into<Decimal>>(&mut self, amount: A) -> Bucket;
+    fn take_all(&mut self) -> Bucket;
+    fn authorize<F: FnOnce() -> O, O>(&self, f: F) -> O;
+    fn is_empty(&self) -> bool;
+}
+
+pub trait ScryptoFungibleVault {
+    fn lock_fee<A: Into<Decimal>>(&mut self, amount: A);
+
+    fn lock_contingent_fee<A: Into<Decimal>>(&mut self, amount: A);
+}
+
+pub trait ScryptoNonFungibleVault {
+    fn non_fungible_local_ids(&self) -> BTreeSet<NonFungibleLocalId>;
+
+    fn non_fungibles<T: NonFungibleData>(&self) -> Vec<NonFungible<T>>;
+
+    fn non_fungible_local_id(&self) -> NonFungibleLocalId;
+
+    fn non_fungible<T: NonFungibleData>(&self) -> NonFungible<T>;
+
+    fn take_non_fungible(&mut self, non_fungible_local_id: &NonFungibleLocalId) -> Bucket;
+
     fn take_non_fungibles(
         &mut self,
         non_fungible_local_ids: &BTreeSet<NonFungibleLocalId>,
     ) -> Bucket;
-    fn resource_address(&self) -> ResourceAddress;
-    fn non_fungible_local_ids(&self) -> BTreeSet<NonFungibleLocalId>;
-    fn create_proof(&self) -> Proof;
-    fn create_proof_of_amount(&self, amount: Decimal) -> Proof;
-    fn create_proof_of_non_fungibles(&self, ids: &BTreeSet<NonFungibleLocalId>) -> Proof;
-    fn lock_fee<A: Into<Decimal>>(&mut self, amount: A);
-    fn lock_contingent_fee<A: Into<Decimal>>(&mut self, amount: A);
-    fn take<A: Into<Decimal>>(&mut self, amount: A) -> Bucket;
-    fn take_all(&mut self) -> Bucket;
-    fn take_non_fungible(&mut self, non_fungible_local_id: &NonFungibleLocalId) -> Bucket;
-    fn authorize<F: FnOnce() -> O, O>(&self, f: F) -> O;
-    fn is_empty(&self) -> bool;
-    fn non_fungibles<T: NonFungibleData>(&self) -> Vec<NonFungible<T>>;
-    fn non_fungible_local_id(&self) -> NonFungibleLocalId;
-    fn non_fungible<T: NonFungibleData>(&self) -> NonFungible<T>;
 }
 
 impl ScryptoVault for Vault {
@@ -50,18 +59,6 @@ impl ScryptoVault for Vault {
         vault
     }
 
-    fn amount(&self) -> Decimal {
-        let mut env = ScryptoEnv;
-        let rtn = env
-            .call_method(
-                self.0.as_node_id(),
-                VAULT_GET_AMOUNT_IDENT,
-                scrypto_encode(&VaultGetAmountInput {}).unwrap(),
-            )
-            .unwrap();
-        scrypto_decode(&rtn).unwrap()
-    }
-
     fn new(resource_address: ResourceAddress) -> Self {
         let mut env = ScryptoEnv;
         let rtn = env
@@ -69,66 +66,6 @@ impl ScryptoVault for Vault {
                 resource_address.as_node_id(),
                 RESOURCE_MANAGER_CREATE_EMPTY_VAULT_IDENT,
                 scrypto_encode(&ResourceManagerCreateEmptyVaultInput {}).unwrap(),
-            )
-            .unwrap();
-        scrypto_decode(&rtn).unwrap()
-    }
-
-    fn take_internal(&mut self, amount: Decimal) -> Bucket {
-        let mut env = ScryptoEnv;
-        let rtn = env
-            .call_method(
-                self.0.as_node_id(),
-                VAULT_TAKE_IDENT,
-                scrypto_encode(&VaultTakeInput { amount }).unwrap(),
-            )
-            .unwrap();
-        scrypto_decode(&rtn).unwrap()
-    }
-
-    fn lock_fee_internal(&mut self, amount: Decimal) {
-        let mut env = ScryptoEnv;
-        let _rtn = env
-            .call_method(
-                self.0.as_node_id(),
-                FUNGIBLE_VAULT_LOCK_FEE_IDENT,
-                scrypto_encode(&FungibleVaultLockFeeInput {
-                    amount,
-                    contingent: false,
-                })
-                .unwrap(),
-            )
-            .unwrap();
-    }
-
-    fn lock_contingent_fee_internal(&mut self, amount: Decimal) {
-        let mut env = ScryptoEnv;
-        let _rtn = env
-            .call_method(
-                self.0.as_node_id(),
-                FUNGIBLE_VAULT_LOCK_FEE_IDENT,
-                scrypto_encode(&FungibleVaultLockFeeInput {
-                    amount,
-                    contingent: true,
-                })
-                .unwrap(),
-            )
-            .unwrap();
-    }
-
-    fn take_non_fungibles(
-        &mut self,
-        non_fungible_local_ids: &BTreeSet<NonFungibleLocalId>,
-    ) -> Bucket {
-        let mut env = ScryptoEnv;
-        let rtn = env
-            .call_method(
-                self.0.as_node_id(),
-                NON_FUNGIBLE_VAULT_TAKE_NON_FUNGIBLES_IDENT,
-                scrypto_encode(&NonFungibleVaultTakeNonFungiblesInput {
-                    non_fungible_local_ids: non_fungible_local_ids.clone(),
-                })
-                .unwrap(),
             )
             .unwrap();
         scrypto_decode(&rtn).unwrap()
@@ -146,22 +83,22 @@ impl ScryptoVault for Vault {
         scrypto_decode(&rtn).unwrap()
     }
 
-    fn resource_address(&self) -> ResourceAddress {
-        let mut env = ScryptoEnv;
-        let info = env.get_object_info(self.0.as_node_id()).unwrap();
-        ResourceAddress::try_from(info.outer_object.unwrap().as_ref()).unwrap()
-    }
-
-    fn non_fungible_local_ids(&self) -> BTreeSet<NonFungibleLocalId> {
+    fn amount(&self) -> Decimal {
         let mut env = ScryptoEnv;
         let rtn = env
             .call_method(
                 self.0.as_node_id(),
-                NON_FUNGIBLE_VAULT_GET_NON_FUNGIBLE_LOCAL_IDS_IDENT,
-                scrypto_encode(&NonFungibleVaultGetNonFungibleLocalIdsInput {}).unwrap(),
+                VAULT_GET_AMOUNT_IDENT,
+                scrypto_encode(&VaultGetAmountInput {}).unwrap(),
             )
             .unwrap();
         scrypto_decode(&rtn).unwrap()
+    }
+
+    fn resource_address(&self) -> ResourceAddress {
+        let mut env = ScryptoEnv;
+        let info = env.get_object_info(self.0.as_node_id()).unwrap();
+        ResourceAddress::try_from(info.outer_object.unwrap().as_ref()).unwrap()
     }
 
     fn create_proof(&self) -> Proof {
@@ -203,39 +140,25 @@ impl ScryptoVault for Vault {
         scrypto_decode(&rtn).unwrap()
     }
 
-    /// Locks the specified amount as transaction fee.
-    ///
-    /// Unused fee will be refunded to the vaults from the most recently locked to the least.
-    fn lock_fee<A: Into<Decimal>>(&mut self, amount: A) {
-        self.lock_fee_internal(amount.into())
-    }
-
-    /// Locks the given amount of resource as contingent fee.
-    ///
-    /// The locked amount will be used as transaction only if the transaction succeeds;
-    /// Unused amount will be refunded the original vault.
-    fn lock_contingent_fee<A: Into<Decimal>>(&mut self, amount: A) {
-        self.lock_contingent_fee_internal(amount.into())
-    }
-
     /// Takes some amount of resource from this vault into a bucket.
     fn take<A: Into<Decimal>>(&mut self, amount: A) -> Bucket {
-        let bucket = self.take_internal(amount.into());
-        Bucket(bucket.0)
+        let mut env = ScryptoEnv;
+        let rtn = env
+            .call_method(
+                self.0.as_node_id(),
+                VAULT_TAKE_IDENT,
+                scrypto_encode(&VaultTakeInput {
+                    amount: amount.into(),
+                })
+                .unwrap(),
+            )
+            .unwrap();
+        scrypto_decode(&rtn).unwrap()
     }
 
     /// Takes all resource stored in this vault.
     fn take_all(&mut self) -> Bucket {
         self.take(self.amount())
-    }
-
-    /// Takes a specific non-fungible from this vault.
-    ///
-    /// # Panics
-    /// Panics if this is not a non-fungible vault or the specified non-fungible resource is not found.
-    fn take_non_fungible(&mut self, non_fungible_local_id: &NonFungibleLocalId) -> Bucket {
-        let bucket = self.take_non_fungibles(&BTreeSet::from([non_fungible_local_id.clone()]));
-        Bucket(bucket.0)
     }
 
     /// Uses resources in this vault as authorization for an operation.
@@ -249,6 +172,59 @@ impl ScryptoVault for Vault {
     /// Checks if this vault is empty.
     fn is_empty(&self) -> bool {
         self.amount() == 0.into()
+    }
+}
+
+impl ScryptoFungibleVault for Vault {
+    /// Locks the specified amount as transaction fee.
+    ///
+    /// Unused fee will be refunded to the vaults from the most recently locked to the least.
+    fn lock_fee<A: Into<Decimal>>(&mut self, amount: A) {
+        let mut env = ScryptoEnv;
+        let _rtn = env
+            .call_method(
+                self.0.as_node_id(),
+                FUNGIBLE_VAULT_LOCK_FEE_IDENT,
+                scrypto_encode(&FungibleVaultLockFeeInput {
+                    amount: amount.into(),
+                    contingent: false,
+                })
+                .unwrap(),
+            )
+            .unwrap();
+    }
+
+    /// Locks the given amount of resource as contingent fee.
+    ///
+    /// The locked amount will be used as transaction only if the transaction succeeds;
+    /// Unused amount will be refunded the original vault.
+    fn lock_contingent_fee<A: Into<Decimal>>(&mut self, amount: A) {
+        let mut env = ScryptoEnv;
+        let _rtn = env
+            .call_method(
+                self.0.as_node_id(),
+                FUNGIBLE_VAULT_LOCK_FEE_IDENT,
+                scrypto_encode(&FungibleVaultLockFeeInput {
+                    amount: amount.into(),
+                    contingent: true,
+                })
+                .unwrap(),
+            )
+            .unwrap();
+    }
+}
+
+impl ScryptoNonFungibleVault for Vault {
+    fn non_fungible_local_ids(&self) -> BTreeSet<NonFungibleLocalId> {
+        let mut env = ScryptoEnv;
+        let rtn = env
+            .call_method(
+                self.0.as_node_id(),
+                NON_FUNGIBLE_VAULT_GET_NON_FUNGIBLE_LOCAL_IDS_IDENT,
+                scrypto_encode(&NonFungibleVaultGetNonFungibleLocalIdsInput {}).unwrap(),
+            )
+            .unwrap();
+        scrypto_decode(&rtn).unwrap()
     }
 
     /// Returns all the non-fungible units contained.
@@ -285,5 +261,32 @@ impl ScryptoVault for Vault {
             panic!("Expecting singleton NFT vault");
         }
         non_fungibles.into_iter().next().unwrap()
+    }
+
+    /// Takes a specific non-fungible from this vault.
+    ///
+    /// # Panics
+    /// Panics if this is not a non-fungible vault or the specified non-fungible resource is not found.
+    fn take_non_fungible(&mut self, non_fungible_local_id: &NonFungibleLocalId) -> Bucket {
+        let bucket = self.take_non_fungibles(&BTreeSet::from([non_fungible_local_id.clone()]));
+        Bucket(bucket.0)
+    }
+
+    fn take_non_fungibles(
+        &mut self,
+        non_fungible_local_ids: &BTreeSet<NonFungibleLocalId>,
+    ) -> Bucket {
+        let mut env = ScryptoEnv;
+        let rtn = env
+            .call_method(
+                self.0.as_node_id(),
+                NON_FUNGIBLE_VAULT_TAKE_NON_FUNGIBLES_IDENT,
+                scrypto_encode(&NonFungibleVaultTakeNonFungiblesInput {
+                    non_fungible_local_ids: non_fungible_local_ids.clone(),
+                })
+                .unwrap(),
+            )
+            .unwrap();
+        scrypto_decode(&rtn).unwrap()
     }
 }
