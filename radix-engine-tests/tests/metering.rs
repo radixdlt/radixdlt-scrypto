0f37b0a1457af440420a2570421c9d3dc8583394
David Edey <david.edey@rdx.works>
2023-05-13 02:14:39 +0100

    fix: Fix metering tests

--- radix-engine-tests/tests/metering.rs
+++ radix-engine-tests/tests/metering.rs
@@ -70,7 +70,7 @@ fn test_basic_transfer() {
         + 5587 /* DropLock */
         + 1575 /* DropNode */
         + 1050432 /* Invoke */
-        + 526396 /* LockSubstate */
+        + 529146 /* LockSubstate */
         + 7840 /* ReadSubstate */
         + 62500 /* RunNative */
         + 7500 /* RunSystem */
@@ -205,7 +205,7 @@ fn test_radiswap() {
         + 13912 /* DropLock */
         + 3570 /* DropNode */
         + 3305144 /* Invoke */
-        + 5641349 /* LockSubstate */
+        + 5644099 /* LockSubstate */
         + 19488 /* ReadSubstate */
         + 135000 /* RunNative */
         + 15000 /* RunSystem */
@@ -316,7 +316,7 @@ fn test_flash_loan() {
         + 21978 /* DropLock */
         + 5880 /* DropNode */
         + 4407857 /* Invoke */
-        + 6838988 /* LockSubstate */
+        + 6841738 /* LockSubstate */
         + 31192 /* ReadSubstate */
         + 200000 /* RunNative */
         + 40000 /* RunSystem */
