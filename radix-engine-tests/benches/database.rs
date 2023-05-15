dfa80bb2633e817b247662451637c4242afe662c
Michał Strug <michal.strug@rdx.works>
2023-05-15 16:30:20 +0200

    Added new bench

---
+++ radix-engine-tests/benches/database.rs
@@ -0,0 +1,123 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use radix_engine::system::bootstrap::Bootstrapper;
+use radix_engine::transaction::execute_and_commit_transaction;
+use radix_engine::transaction::{ExecutionConfig, FeeReserveConfig};
+use radix_engine::types::*;
+use radix_engine::vm::wasm::WasmInstrumenter;
+use radix_engine::vm::wasm::{DefaultWasmEngine, WasmMeteringConfig};
+use radix_engine::vm::ScryptoVm;
+use radix_engine_constants::DEFAULT_COST_UNIT_LIMIT;
+use radix_engine_interface::dec;
+use radix_engine_interface::rule;
+use transaction::builder::ManifestBuilder;
+use transaction::ecdsa_secp256k1::EcdsaSecp256k1PrivateKey;
+use transaction::model::TestTransaction;
+use std::path::PathBuf;
+
+#[cfg(feature = "rocksdb")]
+use radix_engine_stores::rocks_db::RocksdbSubstateStore;
+
+#[cfg(feature = "rocksdb")]
+fn db_rw_test(c: &mut Criterion) {
+    // Set up environment.
+    let mut scrypto_interpreter = ScryptoVm {
+        wasm_engine: DefaultWasmEngine::default(),
+        wasm_instrumenter: WasmInstrumenter::default(),
+        wasm_metering_config: WasmMeteringConfig::V0,
+    };
+    let path = PathBuf::from(r"/tmp/radix-scrypto-db");
+
+    let mut substate_db = RocksdbSubstateStore::standard(path);
+    Bootstrapper::new(&mut substate_db, &scrypto_interpreter, false)
+        .bootstrap_test_default()
+        .unwrap();
+
+    // Create a key pair
+    let private_key = EcdsaSecp256k1PrivateKey::from_u64(1).unwrap();
+    let public_key = private_key.public_key();
+
+    // Create two accounts
+    let accounts = (0..2)
+        .map(|_| {
+            let config = AccessRulesConfig::new().default(
+                rule!(require(NonFungibleGlobalId::from_public_key(&public_key))),
+                rule!(require(NonFungibleGlobalId::from_public_key(&public_key))),
+            );
+            let manifest = ManifestBuilder::new()
+                .lock_fee(FAUCET, 100.into())
+                .new_account_advanced(config)
+                .build();
+            let account = execute_and_commit_transaction(
+                &mut substate_db,
+                &mut scrypto_interpreter,
+                &FeeReserveConfig::default(),
+                &ExecutionConfig::default(),
+                &TestTransaction::new(manifest.clone(), 1, DEFAULT_COST_UNIT_LIMIT)
+                    .get_executable(btreeset![NonFungibleGlobalId::from_public_key(&public_key)]),
+            )
+            .expect_commit(true)
+            .new_component_addresses()[0];
+
+            account
+        })
+        .collect::<Vec<ComponentAddress>>();
+
+    let account1 = accounts[0];
+    let account2 = accounts[1];
+
+    // Fill first account
+    let manifest = ManifestBuilder::new()
+        .lock_fee(FAUCET, 100.into())
+        .call_method(FAUCET, "free", manifest_args!())
+        .call_method(
+            account1,
+            "deposit_batch",
+            manifest_args!(ManifestExpression::EntireWorktop),
+        )
+        .build();
+    for nonce in 0..1000 {
+        execute_and_commit_transaction(
+            &mut substate_db,
+            &mut scrypto_interpreter,
+            &FeeReserveConfig::default(),
+            &ExecutionConfig::default(),
+            &TestTransaction::new(manifest.clone(), nonce, DEFAULT_COST_UNIT_LIMIT)
+                .get_executable(btreeset![NonFungibleGlobalId::from_public_key(&public_key)]),
+        )
+        .expect_commit(true);
+    }
+
+    // Create a transfer manifest
+    let manifest = ManifestBuilder::new()
+        .lock_fee(FAUCET, 100.into())
+        .withdraw_from_account(account1, RADIX_TOKEN, dec!("0.000001"))
+        .call_method(
+            account2,
+            "deposit_batch",
+            manifest_args!(ManifestExpression::EntireWorktop),
+        )
+        .build();
+
+    // Loop
+    let mut nonce = 3;
+    c.bench_function("Transfer::run", |b| {
+        b.iter(|| {
+            let receipt = execute_and_commit_transaction(
+                &mut substate_db,
+                &mut scrypto_interpreter,
+                &FeeReserveConfig::default(),
+                &ExecutionConfig::default(),
+                &TestTransaction::new(manifest.clone(), nonce, DEFAULT_COST_UNIT_LIMIT)
+                    .get_executable(btreeset![NonFungibleGlobalId::from_public_key(&public_key)]),
+            );
+            receipt.expect_commit_success();
+            nonce += 1;
+        })
+    });
+}
+
+#[cfg(feature = "rocksdb")]
+criterion_group!(database, db_rw_test);
+
+#[cfg(feature = "rocksdb")]
+criterion_main!(database);
