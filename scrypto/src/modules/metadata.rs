b419ca5ef34cde23d22e9659e50b86d414cbaecd
Yulong Wu <yulong.wu@rdx.works>
2023-05-17 12:04:29 +0100

    Fix metadata code size issue

--- scrypto/src/modules/metadata.rs
+++ scrypto/src/modules/metadata.rs
@@ -6,11 +6,20 @@ use radix_engine_interface::api::object_api::ObjectModuleId;
 use radix_engine_interface::api::ClientBlueprintApi;
 use radix_engine_interface::api::ClientObjectApi;
 use radix_engine_interface::constants::METADATA_MODULE_PACKAGE;
+use radix_engine_interface::data::scrypto::ScryptoCustomValueKind;
+use radix_engine_interface::data::scrypto::SCRYPTO_SBOR_V1_MAX_DEPTH;
 use radix_engine_interface::data::scrypto::{scrypto_decode, scrypto_encode};
 use radix_engine_interface::types::NodeId;
 use radix_engine_interface::types::*;
 use sbor::rust::prelude::ToOwned;
 use sbor::rust::string::String;
+use sbor::Decoder;
+use sbor::Encoder;
+use sbor::ValueKind;
+use sbor::VecDecoder;
+use sbor::VecEncoder;
+use sbor::OPTION_VARIANT_NONE;
+use sbor::OPTION_VARIANT_SOME;
 
 #[derive(PartialEq, Eq, Hash, Clone)]
 pub struct Metadata(pub Own);
@@ -51,18 +60,18 @@ pub trait MetadataObject {
     fn set<K: AsRef<str>, V: MetadataVal>(&self, name: K, value: V) {
         let (node_id, module_id) = self.self_id();
 
-        let _rtn = ScryptoEnv
-            .call_method_advanced(
-                node_id,
-                false,
-                module_id,
-                METADATA_SET_IDENT,
-                scrypto_encode(&MetadataSetInput {
-                    key: name.as_ref().to_owned(),
-                    value: value.to_metadata_value(),
-                })
-                .unwrap(),
-            )
+        let mut buffer = Vec::new();
+        let mut encoder =
+            VecEncoder::<ScryptoCustomValueKind>::new(&mut buffer, SCRYPTO_SBOR_V1_MAX_DEPTH);
+        encoder.write_value_kind(ValueKind::Tuple).unwrap();
+        encoder.write_size(2).unwrap();
+        encoder.encode(name.as_ref()).unwrap();
+        encoder.write_value_kind(ValueKind::Enum).unwrap();
+        encoder.write_discriminator(V::TYPE_ID).unwrap();
+        encoder.encode(&value).unwrap();
+
+        ScryptoEnv
+            .call_method_advanced(node_id, false, module_id, METADATA_SET_IDENT, buffer)
             .unwrap();
     }
 
@@ -82,10 +91,29 @@ pub trait MetadataObject {
             )
             .unwrap();
 
-        let value: MetadataGetOutput = scrypto_decode(&rtn).unwrap();
-        match value {
-            None => Err(MetadataError::EmptyEntry),
-            Some(value) => V::from_metadata_value(value),
+        // Option<MetadataValue>
+        let mut decoder =
+            VecDecoder::<ScryptoCustomValueKind>::new(&rtn, SCRYPTO_SBOR_V1_MAX_DEPTH);
+        decoder.read_and_check_value_kind(ValueKind::Enum).unwrap();
+        match decoder.read_discriminator().unwrap() {
+            OPTION_VARIANT_NONE => {
+                return Err(MetadataError::NotFound);
+            }
+            OPTION_VARIANT_SOME => {
+                decoder.read_and_check_value_kind(ValueKind::Enum).unwrap();
+                let id = decoder.read_discriminator().unwrap();
+
+                if id == V::TYPE_ID {
+                    let v: V = decoder.decode().unwrap();
+                    return Ok(v);
+                } else {
+                    return Err(MetadataError::UnexpectedType {
+                        expected_type_id: V::TYPE_ID,
+                        actual_type_id: id,
+                    });
+                }
+            }
+            _ => unreachable!(),
         }
     }
 
