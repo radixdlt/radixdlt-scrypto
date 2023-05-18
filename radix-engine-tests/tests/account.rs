22add8075d1f9b5322201740ac72823228692290
Omar <OmarAbdulla7@hotmail.com>
2023-05-18 05:43:22 +0300

    Additional tests

--- radix-engine-tests/tests/account.rs
+++ radix-engine-tests/tests/account.rs
@@ -5,9 +5,12 @@ use radix_engine::types::*;
 use radix_engine_interface::api::node_modules::metadata::{MetadataEntry, MetadataValue};
 use radix_engine_interface::blueprints::account::{
     AccountAddResourceToAllowedDepositsListInput, AccountChangeAllowedDepositsModeInput,
-    AccountDepositsMode, AccountSecurifyInput, ACCOUNT_ADD_RESOURCE_TO_ALLOWED_DEPOSITS_LIST_IDENT,
+    AccountDepositsMode, AccountRemoveResourceFromAllowedDepositsListInput,
+    AccountRemoveResourceFromDisallowedDepositsListInput, AccountSecurifyInput,
+    ACCOUNT_ADD_RESOURCE_TO_ALLOWED_DEPOSITS_LIST_IDENT,
     ACCOUNT_CHANGE_ALLOWED_DEPOSITS_MODE_IDENT, ACCOUNT_DEPOSIT_BATCH_IDENT,
-    ACCOUNT_SECURIFY_IDENT,
+    ACCOUNT_REMOVE_RESOURCE_FROM_ALLOWED_DEPOSITS_LIST_IDENT,
+    ACCOUNT_REMOVE_RESOURCE_FROM_DISALLOWED_DEPOSITS_LIST_IDENT, ACCOUNT_SECURIFY_IDENT,
 };
 use radix_engine_interface::blueprints::resource::FromPublicKey;
 use radix_engine_queries::typed_substate_layout::FungibleResourceManagerError;
@@ -302,7 +305,7 @@ fn account_deposit_mode_change_test(
     failure: Option<&dyn Fn(&RuntimeError) -> bool>,
 ) {
     // Arrange
-    let mut test_runner = TestRunner::builder().without_trace().build();
+    let mut test_runner = TestRunner::builder().build();
     let (public_key, _, account) = test_runner.new_account(is_virtual);
 
     // Act
@@ -355,7 +358,7 @@ fn allocated_account_deposits_mode_can_be_changed_with_owner_auth() {
 #[test]
 fn cant_add_allowed_resources_without_owner_auth() {
     // Arrange
-    let mut test_runner = TestRunner::builder().without_trace().build();
+    let mut test_runner = TestRunner::builder().build();
     let (_, _, account) = test_runner.new_account(false);
 
     // Act
@@ -377,7 +380,7 @@ fn cant_add_allowed_resources_without_owner_auth() {
 #[test]
 fn cant_add_allowed_resources_when_in_invalid_mode() {
     // Arrange
-    let mut test_runner = TestRunner::builder().without_trace().build();
+    let mut test_runner = TestRunner::builder().build();
     let (public_key, _, account) = test_runner.new_account(false);
 
     // Act
@@ -402,7 +405,7 @@ fn cant_add_allowed_resources_when_in_invalid_mode() {
 #[test]
 fn can_deposit_any_resource_in_allow_all_mode() {
     // Arrange
-    let mut test_runner = TestRunner::builder().without_trace().build();
+    let mut test_runner = TestRunner::builder().build();
     let (public_key, _, account) = test_runner.new_account(false);
     let resource_address = test_runner.create_freely_mintable_fungible_resource(None, 18, account);
 
@@ -427,7 +430,7 @@ fn allow_existing_internal_test(
     failure: Option<&dyn Fn(&RuntimeError) -> bool>,
 ) {
     // Arrange
-    let mut test_runner = TestRunner::builder().without_trace().build();
+    let mut test_runner = TestRunner::builder().build();
     let (public_key, _, account) = test_runner.new_account(false);
 
     let non_existing_resource =
@@ -495,7 +498,7 @@ fn allow_list_internal_test(
     failure: Option<&dyn Fn(&RuntimeError) -> bool>,
 ) {
     // Arrange
-    let mut test_runner = TestRunner::builder().without_trace().build();
+    let mut test_runner = TestRunner::builder().build();
     let (public_key, _, account) = test_runner.new_account(false);
 
     let allowed_resource = test_runner.create_freely_mintable_fungible_resource(None, 18, account);
@@ -565,7 +568,7 @@ fn deny_list_internal_test(
     failure: Option<&dyn Fn(&RuntimeError) -> bool>,
 ) {
     // Arrange
-    let mut test_runner = TestRunner::builder().without_trace().build();
+    let mut test_runner = TestRunner::builder().build();
     let (public_key, _, account) = test_runner.new_account(false);
 
     let allowed_resource = test_runner.create_freely_mintable_fungible_resource(None, 18, account);
@@ -752,6 +755,157 @@ fn test_deny_list_only_deposit_mode() {
     for (should_sign, deposit_denied_resource, safe_deposit, error) in test_vectors.into_iter() {
         deny_list_internal_test(should_sign, deposit_denied_resource, safe_deposit, error)
     }
+}
+
+#[test]
+fn removing_an_allowed_resource_disallows_its_deposit() {
+    // Arrange
+    let mut test_runner = TestRunner::builder().build();
+    let (public_key, _, account) = test_runner.new_account(false);
+
+    let resource_address = test_runner.create_freely_mintable_fungible_resource(None, 18, account);
+
+    {
+        let mut allowed_resources = index_set_new();
+        allowed_resources.insert(resource_address);
+
+        let manifest = ManifestBuilder::new()
+            .call_method(
+                account,
+                ACCOUNT_CHANGE_ALLOWED_DEPOSITS_MODE_IDENT,
+                to_manifest_value(&AccountChangeAllowedDepositsModeInput {
+                    deposit_mode: AccountDepositsMode::AllowList(allowed_resources),
+                }),
+            )
+            .build();
+        let receipt = test_runner.execute_manifest_ignoring_fee(
+            manifest,
+            vec![NonFungibleGlobalId::from_public_key(&public_key)],
+        );
+        receipt.expect_commit_success();
+
+        let manifest = ManifestBuilder::new()
+            .call_method(
+                account,
+                ACCOUNT_REMOVE_RESOURCE_FROM_ALLOWED_DEPOSITS_LIST_IDENT,
+                to_manifest_value(&AccountRemoveResourceFromAllowedDepositsListInput {
+                    resource_address,
+                }),
+            )
+            .build();
+        let receipt = test_runner.execute_manifest_ignoring_fee(
+            manifest,
+            vec![NonFungibleGlobalId::from_public_key(&public_key)],
+        );
+        receipt.expect_commit_success();
+    }
+
+    // Act
+    let manifest = ManifestBuilder::new()
+        .mint_fungible(resource_address, 10.into())
+        .deposit_batch(account)
+        .build();
+    let receipt = test_runner.execute_manifest_ignoring_fee(manifest, vec![]);
+
+    // Assert
+    receipt.expect_specific_failure(is_assert_rule_error)
+}
+
+#[test]
+fn removing_a_disallowed_resource_allows_its_deposit() {
+    // Arrange
+    let mut test_runner = TestRunner::builder().build();
+    let (public_key, _, account) = test_runner.new_account(false);
+
+    let resource_address = test_runner.create_freely_mintable_fungible_resource(None, 18, account);
+
+    {
+        let mut disallowed_resources = index_set_new();
+        disallowed_resources.insert(resource_address);
+
+        let manifest = ManifestBuilder::new()
+            .call_method(
+                account,
+                ACCOUNT_CHANGE_ALLOWED_DEPOSITS_MODE_IDENT,
+                to_manifest_value(&AccountChangeAllowedDepositsModeInput {
+                    deposit_mode: AccountDepositsMode::DisallowList(disallowed_resources),
+                }),
+            )
+            .build();
+        let receipt = test_runner.execute_manifest_ignoring_fee(
+            manifest,
+            vec![NonFungibleGlobalId::from_public_key(&public_key)],
+        );
+        receipt.expect_commit_success();
+
+        let manifest = ManifestBuilder::new()
+            .call_method(
+                account,
+                ACCOUNT_REMOVE_RESOURCE_FROM_DISALLOWED_DEPOSITS_LIST_IDENT,
+                to_manifest_value(&AccountRemoveResourceFromDisallowedDepositsListInput {
+                    resource_address,
+                }),
+            )
+            .build();
+        let receipt = test_runner.execute_manifest_ignoring_fee(
+            manifest,
+            vec![NonFungibleGlobalId::from_public_key(&public_key)],
+        );
+        receipt.expect_commit_success();
+    }
+
+    // Act
+    let manifest = ManifestBuilder::new()
+        .mint_fungible(resource_address, 10.into())
+        .deposit_batch(account)
+        .build();
+    let receipt = test_runner.execute_manifest_ignoring_fee(manifest, vec![]);
+
+    // Assert
+    receipt.expect_commit_success();
+}
+
+#[test]
+fn only_existing_is_dependent_on_balances_not_vault_existence() {
+    // Arrange
+    let mut test_runner = TestRunner::builder().build();
+    let (public_key, _, account) = test_runner.new_account(false);
+
+    let resource_address = test_runner.create_freely_mintable_and_burnable_fungible_resource(
+        Some(1.into()),
+        18,
+        account,
+    );
+
+    {
+        let manifest = ManifestBuilder::new()
+            .call_method(
+                account,
+                ACCOUNT_CHANGE_ALLOWED_DEPOSITS_MODE_IDENT,
+                to_manifest_value(&AccountChangeAllowedDepositsModeInput {
+                    deposit_mode: AccountDepositsMode::AllowExisting,
+                }),
+            )
+            .build();
+        let receipt = test_runner.execute_manifest_ignoring_fee(
+            manifest,
+            vec![NonFungibleGlobalId::from_public_key(&public_key)],
+        );
+        receipt.expect_commit_success();
+    }
+
+    // Act
+    let manifest = ManifestBuilder::new()
+        .withdraw_from_account(account, resource_address, 1.into())
+        .safe_deposit_batch(account)
+        .build();
+    let receipt = test_runner.execute_manifest_ignoring_fee(
+        manifest,
+        vec![NonFungibleGlobalId::from_public_key(&public_key)],
+    );
+
+    // Assert
+    receipt.expect_specific_failure(is_drop_non_empty_bucket_error);
 }
 
 type ErrorCheckingFunction = &'static dyn Fn(&RuntimeError) -> bool;
