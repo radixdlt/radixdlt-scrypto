5434509433f8e60e8e4a3f0f10d121d5dc39d105
David Edey <david.edey@rdx.works>
2023-05-28 17:08:19 +0100

    fix: EpochChangeEvent implements Debug

--- radix-engine/src/blueprints/consensus_manager/events/consensus_manager.rs
+++ radix-engine/src/blueprints/consensus_manager/events/consensus_manager.rs
@@ -1,12 +1,12 @@
 use crate::blueprints::consensus_manager::ActiveValidatorSet;
 use crate::types::*;
 
-#[derive(ScryptoSbor, ScryptoEvent, PartialEq, Eq)]
+#[derive(Debug, Clone, ScryptoSbor, ScryptoEvent, PartialEq, Eq)]
 pub struct RoundChangeEvent {
     pub round: Round,
 }
 
-#[derive(ScryptoSbor, ScryptoEvent, PartialEq, Eq)]
+#[derive(Debug, Clone, ScryptoSbor, ScryptoEvent, PartialEq, Eq)]
 pub struct EpochChangeEvent {
     /// The *new* epoch's number.
     pub epoch: Epoch,
