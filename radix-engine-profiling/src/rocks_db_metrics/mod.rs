30217f34ab025a5df4752bbfd506a7bbc65868c5
Michał Strug <michal.strug@rdx.works>
2023-05-30 00:49:52 +0200

    Updated parameters

--- radix-engine-profiling/src/rocks_db_metrics/mod.rs
+++ radix-engine-profiling/src/rocks_db_metrics/mod.rs
@@ -116,7 +116,7 @@ mod tests {
     /// Range end of the measuremnts
     const MAX_SIZE: usize = 4 * 1024 * 1024;
     /// Range step
-    const SIZE_STEP: usize = 500 * 1024;
+    const SIZE_STEP: usize = 20 * 1024;
     /// Each step write and read
     const COUNT: usize = 20;
     /// Multiplication of each step read (COUNT * READ_REPEATS)
