87bbd366a3f3470d2dae2c0513c3cdd70ab90637
Jakub Krawczyk <jakub.krawczyk@rdx.works>
2023-06-01 12:05:34 +0200

    An ad-hoc resim setup for reproducing panic on resource summary of certain NF operations.

--- simulator/tests/manifest.sh
+++ simulator/tests/manifest.sh
@@ -10,15 +10,11 @@ resim="cargo run --bin resim $@ --"
 $resim reset
 
 export account=`$resim new-account | awk '/Account component address:/ {print $NF}'`
-export owner_badge=`$resim new-simple-badge --name 'OwnerBadge' | awk '/NonFungibleGlobalId:/ {print $NF}'`
-export package=`$resim publish ../examples/hello-world --owner-badge $owner_badge | awk '/Package:/ {print $NF}'`
 
-export xrd=resource_sim1tknxxxxxxxxxradxrdxxxxxxxxx009923554798xxxxxxxxxakj8n3
+$resim run ./tests/setup.rtm
 
-output=`$resim run ./tests/m1.rtm | awk '/Component:|Resource:/ {print $NF}'`
-export component=`echo $output | cut -d " " -f1`
-export resource=`echo $output | cut -d " " -f2`
+$resim show-ledger
 
-$resim run ./tests/m2.rtm
+$resim run ./tests/failing.rtm
 
 $resim show-ledger
