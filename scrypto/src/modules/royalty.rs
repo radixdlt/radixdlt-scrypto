b34813b9393d376bd76e681d887dacf4bb50f52b
Joshua Primero <josh.primero@rdx.works>
2023-06-01 13:14:51 -0500

    Clean code format

--- scrypto/src/modules/royalty.rs
+++ scrypto/src/modules/royalty.rs
@@ -14,8 +14,8 @@ use radix_engine_interface::blueprints::resource::Bucket;
 use radix_engine_interface::constants::ROYALTY_MODULE_PACKAGE;
 use radix_engine_interface::data::scrypto::{scrypto_decode, scrypto_encode};
 use radix_engine_interface::types::RoyaltyConfig;
-use scrypto::modules::Attachable;
 use sbor::rust::string::String;
+use scrypto::modules::Attachable;
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone)]
 pub struct Royalty(pub ModuleHandle);
