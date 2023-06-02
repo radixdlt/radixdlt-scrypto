30dbe71e6fb0f423e3f8f1ad59d5126118004b81
Joshua Primero <josh.primero@rdx.works>
2023-06-01 19:20:38 -0500

    Use of similar trait for blueprint stubs

--- radix-engine-tests/tests/blueprints/static_dependencies/src/package_dependency.rs
+++ radix-engine-tests/tests/blueprints/static_dependencies/src/package_dependency.rs
@@ -1,5 +1,19 @@
 use scrypto::prelude::*;
 
+pub struct Sample2;
+
+pub trait Sample2Functions {
+    fn callee() -> u32 {
+        let package_address = PackageAddress::new_or_panic([
+            13, 144, 99, 24, 198, 49, 140, 100, 247, 152, 202, 204, 99, 24, 198, 49, 140, 247, 189,
+            241, 172, 105, 67, 234, 38, 49, 140, 99, 24, 198,
+        ]);
+        ::scrypto::runtime::Runtime::call_function(package_address, "Sample", "y", scrypto_args!())
+    }
+}
+
+impl Sample2Functions for ::scrypto::component::Blueprint<Sample2> {}
+
 #[blueprint]
 mod cross_package {
     const PACKAGE_ADDRESS_PLACE_HOLDER: PackageAddress = PackageAddress::new_or_panic([
@@ -11,12 +25,7 @@ mod cross_package {
 
     impl Sample {
         pub fn call_external_package() {
-            let _: () = Runtime::call_function(
-                PACKAGE_ADDRESS_PLACE_HOLDER,
-                "Sample",
-                "callee",
-                scrypto_args!(),
-            );
+            Blueprint::<Sample2>::callee();
         }
 
         pub fn callee() {}
