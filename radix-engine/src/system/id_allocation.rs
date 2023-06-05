f21553ed7998522e0bd7172e74c7e65af1d20d54
Yulong Wu <yulong.wu@rdx.works>
2023-06-05 11:09:32 +0100

    Refactor ID allocation

--- radix-engine/src/system/id_allocation.rs
+++ radix-engine/src/system/id_allocation.rs
@@ -1,3 +1,5 @@
+use crate::blueprints::pool::single_resource_pool::SINGLE_RESOURCE_POOL_BLUEPRINT_IDENT;
+use crate::blueprints::pool::two_resource_pool::TWO_RESOURCE_POOL_BLUEPRINT_IDENT;
 use radix_engine_common::types::EntityType;
 use radix_engine_common::{native_addresses::*, ScryptoSbor};
 use radix_engine_interface::blueprints::access_controller::*;
@@ -5,7 +7,6 @@ use radix_engine_interface::blueprints::account::*;
 use radix_engine_interface::blueprints::consensus_manager::*;
 use radix_engine_interface::blueprints::identity::*;
 use radix_engine_interface::blueprints::package::*;
-use radix_engine_interface::blueprints::pool::*;
 use radix_engine_interface::blueprints::resource::*;
 use radix_engine_interface::types::BlueprintId;
 
@@ -38,84 +39,58 @@ impl IDAllocation {
                 blueprint_id,
                 global,
             } => {
-                // FIXME final check before Babylon release!
                 if *global {
-                    match (
-                        blueprint_id.package_address,
-                        blueprint_id.blueprint_name.as_str(),
-                    ) {
-                        (ACCOUNT_PACKAGE, PACKAGE_BLUEPRINT) => EntityType::GlobalPackage,
-                        (RESOURCE_PACKAGE, FUNGIBLE_RESOURCE_MANAGER_BLUEPRINT) => {
-                            EntityType::GlobalFungibleResourceManager
-                        }
-                        (RESOURCE_PACKAGE, NON_FUNGIBLE_RESOURCE_MANAGER_BLUEPRINT) => {
-                            EntityType::GlobalNonFungibleResourceManager
-                        }
-                        (CONSENSUS_MANAGER_PACKAGE, CONSENSUS_MANAGER_BLUEPRINT) => {
-                            EntityType::GlobalConsensusManager
-                        }
-                        (CONSENSUS_MANAGER_PACKAGE, VALIDATOR_BLUEPRINT) => {
-                            EntityType::GlobalValidator
-                        }
-                        (ACCESS_CONTROLLER_PACKAGE, ACCESS_CONTROLLER_BLUEPRINT) => {
-                            EntityType::GlobalAccessController
-                        }
-                        (ACCOUNT_PACKAGE, ACCOUNT_BLUEPRINT) => EntityType::GlobalAccount,
-                        (IDENTITY_PACKAGE, IDENTITY_BLUEPRINT) => EntityType::GlobalIdentity,
-                        (POOL_PACKAGE, SINGLE_RESOURCE_POOL_CONTRIBUTE_IDENT) => {
-                            EntityType::GlobalSingleResourcePool
-                        }
-                        (POOL_PACKAGE, TWO_RESOURCE_POOL_CONTRIBUTE_IDENT) => {
-                            EntityType::GlobalTwoResourcePool
-                        }
-                        _ => EntityType::GlobalGenericComponent,
-                    }
+                    get_global_entity_type(&blueprint_id)
                 } else {
-                    match (
-                        blueprint_id.package_address,
-                        blueprint_id.blueprint_name.as_str(),
-                    ) {
-                        (RESOURCE_PACKAGE, FUNGIBLE_VAULT_BLUEPRINT) => {
-                            EntityType::InternalFungibleVault
-                        }
-                        (RESOURCE_PACKAGE, NON_FUNGIBLE_VAULT_BLUEPRINT) => {
-                            EntityType::InternalNonFungibleVault
-                        }
-                        (ACCOUNT_PACKAGE, ACCOUNT_BLUEPRINT) => EntityType::InternalAccount,
-                        _ => EntityType::InternalGenericComponent,
-                    }
+                    get_internal_entity_type(&blueprint_id)
                 }
             }
             IDAllocation::KeyValueStore => EntityType::InternalKeyValueStore,
             IDAllocation::GlobalAddressOwnership => EntityType::InternalGenericComponent,
-            IDAllocation::GlobalAddressPhantom { blueprint_id } => match (
-                blueprint_id.package_address,
-                blueprint_id.blueprint_name.as_str(),
-            ) {
-                (ACCOUNT_PACKAGE, PACKAGE_BLUEPRINT) => EntityType::GlobalPackage,
-                (RESOURCE_PACKAGE, FUNGIBLE_RESOURCE_MANAGER_BLUEPRINT) => {
-                    EntityType::GlobalFungibleResourceManager
-                }
-                (RESOURCE_PACKAGE, NON_FUNGIBLE_RESOURCE_MANAGER_BLUEPRINT) => {
-                    EntityType::GlobalNonFungibleResourceManager
-                }
-                (CONSENSUS_MANAGER_PACKAGE, CONSENSUS_MANAGER_BLUEPRINT) => {
-                    EntityType::GlobalConsensusManager
-                }
-                (CONSENSUS_MANAGER_PACKAGE, VALIDATOR_BLUEPRINT) => EntityType::GlobalValidator,
-                (ACCESS_CONTROLLER_PACKAGE, ACCESS_CONTROLLER_BLUEPRINT) => {
-                    EntityType::GlobalAccessController
-                }
-                (ACCOUNT_PACKAGE, ACCOUNT_BLUEPRINT) => EntityType::GlobalAccount,
-                (IDENTITY_PACKAGE, IDENTITY_BLUEPRINT) => EntityType::GlobalIdentity,
-                (POOL_PACKAGE, SINGLE_RESOURCE_POOL_CONTRIBUTE_IDENT) => {
-                    EntityType::GlobalSingleResourcePool
-                }
-                (POOL_PACKAGE, TWO_RESOURCE_POOL_CONTRIBUTE_IDENT) => {
-                    EntityType::GlobalTwoResourcePool
-                }
-                _ => EntityType::GlobalGenericComponent,
-            },
+            IDAllocation::GlobalAddressPhantom { blueprint_id } => {
+                get_global_entity_type(&blueprint_id)
+            }
         }
+    }
+}
+
+pub fn get_internal_entity_type(blueprint_id: &BlueprintId) -> EntityType {
+    match (
+        blueprint_id.package_address,
+        blueprint_id.blueprint_name.as_str(),
+    ) {
+        (RESOURCE_PACKAGE, FUNGIBLE_VAULT_BLUEPRINT) => EntityType::InternalFungibleVault,
+        (RESOURCE_PACKAGE, NON_FUNGIBLE_VAULT_BLUEPRINT) => EntityType::InternalNonFungibleVault,
+        (ACCOUNT_PACKAGE, ACCOUNT_BLUEPRINT) => EntityType::InternalAccount,
+        _ => EntityType::InternalGenericComponent,
+    }
+}
+
+pub fn get_global_entity_type(blueprint_id: &BlueprintId) -> EntityType {
+    match (
+        blueprint_id.package_address,
+        blueprint_id.blueprint_name.as_str(),
+    ) {
+        (ACCOUNT_PACKAGE, PACKAGE_BLUEPRINT) => EntityType::GlobalPackage,
+        (RESOURCE_PACKAGE, FUNGIBLE_RESOURCE_MANAGER_BLUEPRINT) => {
+            EntityType::GlobalFungibleResourceManager
+        }
+        (RESOURCE_PACKAGE, NON_FUNGIBLE_RESOURCE_MANAGER_BLUEPRINT) => {
+            EntityType::GlobalNonFungibleResourceManager
+        }
+        (CONSENSUS_MANAGER_PACKAGE, CONSENSUS_MANAGER_BLUEPRINT) => {
+            EntityType::GlobalConsensusManager
+        }
+        (CONSENSUS_MANAGER_PACKAGE, VALIDATOR_BLUEPRINT) => EntityType::GlobalValidator,
+        (ACCESS_CONTROLLER_PACKAGE, ACCESS_CONTROLLER_BLUEPRINT) => {
+            EntityType::GlobalAccessController
+        }
+        (ACCOUNT_PACKAGE, ACCOUNT_BLUEPRINT) => EntityType::GlobalAccount,
+        (IDENTITY_PACKAGE, IDENTITY_BLUEPRINT) => EntityType::GlobalIdentity,
+        (POOL_PACKAGE, SINGLE_RESOURCE_POOL_BLUEPRINT_IDENT) => {
+            EntityType::GlobalSingleResourcePool
+        }
+        (POOL_PACKAGE, TWO_RESOURCE_POOL_BLUEPRINT_IDENT) => EntityType::GlobalTwoResourcePool,
+        _ => EntityType::GlobalGenericComponent,
     }
 }
