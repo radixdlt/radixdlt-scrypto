cac15c427873f7940f518c55f6c9f09553304a9d
Yulong Wu <yulong.wu@rdx.works>
2023-06-16 11:44:11 +0100

    Add intent hash store address constant

--- radix-engine-common/src/native_addresses.rs
+++ radix-engine-common/src/native_addresses.rs
@@ -214,6 +214,12 @@ pub const FAUCET: ComponentAddress = ComponentAddress::new_or_panic([
 // Export an alias for backwards compatibility of dApp developer tests
 pub use FAUCET as FAUCET_COMPONENT;
 
+/// The intent hash store component
+pub const INTENT_HASH_STORE: ComponentAddress = ComponentAddress::new_or_panic([
+    192, 255, 44, 88, 165, 149, 21, 42, 183, 10, 139, 243, 134, 75, 218, 84, 14, 87, 36, 9, 34, 31,
+    221, 247, 41, 156, 23, 169, 61, 30,
+]);
+
 //=========================================================================
 //=========================================================================
 
@@ -361,6 +367,11 @@ mod tests {
             GENESIS_HELPER.as_ref(),
             EntityType::GlobalGenericComponent,
             "component_rdx1cptxxxxxxxxxgenssxxxxxxxxxx000977302539xxxxxxxxxgenssx",
+        );
+        check_address(
+            INTENT_HASH_STORE.as_ref(),
+            EntityType::GlobalGenericComponent,
+            "component_rdx1crljck99j52j4dc230ecvj762s89wfqfyg0amaefnst6j0g7tgfnm6",
         );
     }
 
