743e64d0485e0a1e37c50b111723e929c6f072f6
Yulong Wu <yulong.wu@rdx.works>
2023-06-18 22:40:08 +0100

    Add Function auth

--- radix-engine/src/blueprints/transaction_tracker/package.rs
+++ radix-engine/src/blueprints/transaction_tracker/package.rs
@@ -5,6 +5,7 @@ use native_sdk::modules::access_rules::AccessRules;
 use native_sdk::modules::metadata::Metadata;
 use native_sdk::modules::royalty::ComponentRoyalty;
 use native_sdk::runtime::Runtime;
+use radix_engine_interface::api::node_modules::auth::AuthAddresses;
 use radix_engine_interface::api::{ClientApi, ObjectModuleId};
 use radix_engine_interface::blueprints::package::{
     AuthConfig, BlueprintDefinitionInit, MethodAuthTemplate, PackageDefinition,
@@ -101,6 +102,7 @@ impl TransactionTrackerNativePackage {
                 royalty_config: RoyaltyConfig::default(),
                 auth_config: AuthConfig {
                     function_auth: btreemap!(
+                        TRANSACTION_TRACKER_CREATE_IDENT.to_string() => rule!(require(AuthAddresses::system_role())),
                     ),
                     method_auth: MethodAuthTemplate::Static {
                         auth: btreemap!(),
