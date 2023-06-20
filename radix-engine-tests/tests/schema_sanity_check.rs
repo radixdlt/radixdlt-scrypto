737cff1a2c58b80625dbfbf872573b13081715f9
Yulong Wu <yulong.wu@rdx.works>
2023-06-20 11:59:24 +0100

    Add sanity check over native blueprint scrypto schemas

--- radix-engine-tests/tests/schema_sanity_check.rs
+++ radix-engine-tests/tests/schema_sanity_check.rs
@@ -15,5 +15,48 @@ fn scan_native_blueprint_schemas_and_highlight_unsafe_types() {
             schemas_by_hash.len(),
             package_address.to_string(&bech32)
         );
+
+        for schema in schemas_by_hash.values() {
+            for (i, type_kind) in schema.type_kinds.iter().enumerate() {
+                match type_kind {
+                    ScryptoTypeKind::Custom(ScryptoCustomTypeKind::Own) => {
+                        match &schema.type_validations[i] {
+                            TypeValidation::Custom(ScryptoCustomTypeValidation::Own(x)) => {
+                                match x {
+                                    OwnValidation::IsTypedObject(_, _) => {}
+                                    OwnValidation::IsKeyValueStore => {
+                                        println!("Warning: non typed KeyValueStore is used");
+                                    }
+                                    OwnValidation::IsGlobalAddressReservation => {
+                                        println!(
+                                            "Warning: non typed GlobalAddressReservation is used"
+                                        );
+                                    }
+                                    _ => {
+                                        println!("Warning: non typed validation {:?} used", x);
+                                    }
+                                }
+                            }
+                            _ => panic!("Wrong type validation attached to `Own` type kind"),
+                        }
+                    }
+                    ScryptoTypeKind::Custom(ScryptoCustomTypeKind::Reference) => {
+                        match &schema.type_validations[i] {
+                            TypeValidation::Custom(ScryptoCustomTypeValidation::Reference(x)) => {
+                                match x {
+                                    ReferenceValidation::IsGlobalTyped(_, _)
+                                    | ReferenceValidation::IsInternalTyped(_, _) => {}
+                                    _ => {
+                                        println!("Warning: non typed validation {:?} used", x);
+                                    }
+                                }
+                            }
+                            _ => panic!("Wrong type validation attached to `Reference` type kind"),
+                        }
+                    }
+                    _ => {}
+                }
+            }
+        }
     }
 }
