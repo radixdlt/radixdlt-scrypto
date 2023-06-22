4d3962c321809fb99b50f0be81b08280316768de
Joshua Primero <josh.primero@rdx.works>
2023-06-22 09:39:31 +0100

    Add kv_store_init! macro

--- radix-engine-interface/src/types/kv_store_init.rs
+++ radix-engine-interface/src/types/kv_store_init.rs
@@ -1,28 +1,34 @@
 use crate::*;
-use core::fmt::Formatter;
 use radix_engine_common::address::{AddressDisplayContext, NO_NETWORK};
 use sbor::rust::prelude::*;
 use sbor::rust::string::String;
-use utils::ContextualDisplay;
 
 #[cfg_attr(feature = "radix_engine_fuzzing", derive(Arbitrary))]
 #[derive(Debug, Clone, Eq, PartialEq, ScryptoSbor, ManifestSbor)]
 #[sbor(transparent)]
-pub struct LockableKeyValueStoreInit<K: Ord, V> {
-    pub data: BTreeMap<K, LockableEntry<V>>,
+pub struct KeyValueStoreInit<K: Ord, V> {
+    pub data: BTreeMap<K, KeyValueStoreInitEntry<V>>,
 }
 
-impl<K: Ord, V> LockableKeyValueStoreInit<K, V> {
+impl<K: Ord, V> KeyValueStoreInit<K, V> {
     pub fn new() -> Self {
-        LockableKeyValueStoreInit {
+        KeyValueStoreInit {
             data: BTreeMap::new()
         }
     }
 
     pub fn set(&mut self, key: K, value: V) {
-        let entry = LockableEntry {
+        let entry = KeyValueStoreInitEntry {
             value,
             lock: false,
+        };
+        self.data.insert(key, entry);
+    }
+
+    pub fn set_and_lock(&mut self, key: K, value: V) {
+        let entry = KeyValueStoreInitEntry {
+            value,
+            lock: true,
         };
         self.data.insert(key, entry);
     }
@@ -30,7 +36,31 @@ impl<K: Ord, V> LockableKeyValueStoreInit<K, V> {
 
 #[cfg_attr(feature = "radix_engine_fuzzing", derive(Arbitrary))]
 #[derive(Debug, Clone, Eq, PartialEq, ScryptoSbor, ManifestSbor)]
-pub struct LockableEntry<V> {
+pub struct KeyValueStoreInitEntry<V> {
     pub value: V,
     pub lock: bool,
+}
+
+#[macro_export]
+macro_rules! kv_store_init_set_entry {
+    ($store:expr, $key:expr, $value:expr, updatable) => ({
+        $store.set($key, $value);
+    });
+    ($store:expr, $key:expr, $value:expr, locked) => ({
+        $store.set_and_lock($key, $value);
+    });
+}
+
+#[macro_export]
+macro_rules! kv_store_init {
+    ( ) => ({
+        KeyValueStoreInit::new()
+    });
+    ( $($key:expr => $value:expr, $lock:ident;)* ) => ({
+        let mut kv_store_init = KeyValueStoreInit::new();
+        $(
+            kv_store_init_set_entry!(kv_store_init, $key, $value, $lock);
+        )*
+        kv_store_init
+    });
 }
--- radix-engine/src/blueprints/pool/two_resource_pool/blueprint.rs
+++ radix-engine/src/blueprints/pool/two_resource_pool/blueprint.rs
@@ -78,14 +78,14 @@ impl TwoResourcePoolBlueprint {
         // FIXME: The following fields must ALL be LOCKED. No entity with any authority should be
         // able to update them later on. Implement this once metadata locking is done.
         let metadata = Metadata::create_with_data(
-            btreemap!(
-                "pool_vault_number".into() => MetadataValue::U8(2),
-                "pool_resources".into() => MetadataValue::GlobalAddressArray(vec![
+            metadata_init! {
+                "pool_vault_number" => MetadataValue::U8(2u8), locked;
+                "pool_resources" => MetadataValue::GlobalAddressArray(vec![
                     resource_address1.into(),
                     resource_address2.into()
-                ]),
-                "pool_unit".into() => MetadataValue::GlobalAddress(pool_unit_resource_manager.0.into()),
-            ).into(),
+                ]), locked;
+                "pool_unit" => MetadataValue::GlobalAddress(pool_unit_resource_manager.0.into()), locked;
+            },
             api,
         )?;
         let royalty = ComponentRoyalty::create(ComponentRoyaltyConfig::default(), api)?;
