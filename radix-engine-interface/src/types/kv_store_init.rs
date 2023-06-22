ef345055acc089aa181ac700ec24030eaceb71ad
Joshua Primero <josh.primero@rdx.works>
2023-06-22 08:35:59 +0100

    Add LockableEntry

--- radix-engine-interface/src/types/kv_store_init.rs
+++ radix-engine-interface/src/types/kv_store_init.rs
@@ -1,9 +1,6 @@
-use crate::api::ObjectModuleId;
-use crate::types::*;
 use crate::*;
 use core::fmt::Formatter;
 use radix_engine_common::address::{AddressDisplayContext, NO_NETWORK};
-use radix_engine_common::types::*;
 use sbor::rust::prelude::*;
 use sbor::rust::string::String;
 use utils::ContextualDisplay;
@@ -12,11 +9,28 @@ use utils::ContextualDisplay;
 #[derive(Debug, Clone, Eq, PartialEq, ScryptoSbor, ManifestSbor)]
 #[sbor(transparent)]
 pub struct LockableKeyValueStoreInit<K: Ord, V> {
-    pub data: BTreeMap<K, V>,
+    pub data: BTreeMap<K, LockableEntry<V>>,
 }
 
 impl<K: Ord, V> LockableKeyValueStoreInit<K, V> {
-    pub fn insert(&mut self, key: K, value: V) {
-        self.data.insert(key, value);
+    pub fn new() -> Self {
+        LockableKeyValueStoreInit {
+            data: BTreeMap::new()
+        }
     }
+
+    pub fn set(&mut self, key: K, value: V) {
+        let entry = LockableEntry {
+            value,
+            lock: false,
+        };
+        self.data.insert(key, entry);
+    }
+}
+
+#[cfg_attr(feature = "radix_engine_fuzzing", derive(Arbitrary))]
+#[derive(Debug, Clone, Eq, PartialEq, ScryptoSbor, ManifestSbor)]
+pub struct LockableEntry<V> {
+    pub value: V,
+    pub lock: bool,
 }
