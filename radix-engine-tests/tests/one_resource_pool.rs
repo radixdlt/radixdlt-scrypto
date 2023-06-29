cfa28bf266b33cea8625f482923236c48a8fbea0
Michał Strug <michal.strug@rdx.works>
2023-06-29 14:47:04 +0200

    Added test for function get_vault_amount_one_resource_pool

--- radix-engine-tests/tests/one_resource_pool.rs
+++ radix-engine-tests/tests/one_resource_pool.rs
@@ -68,6 +68,19 @@ fn initial_contribution_to_pool_mints_expected_amount() {
 }
 
 #[test]
+fn initial_contribution_to_pool_check_amount() {
+    // Arrange
+    let mut test_runner = TestEnvironment::new(18);
+
+    // Act
+    test_runner.contribute(10, true).expect_commit_success();
+    let amount = test_runner.get_vault_amount(true);
+
+    // Assert
+    assert_eq!(amount, 10.into());
+}
+
+#[test]
 fn contribution_to_pool_mints_expected_amount_1() {
     // Arrange
     let mut test_runner = TestEnvironment::new(18);
@@ -632,7 +645,6 @@ impl TestEnvironment {
         receipt.expect_commit_success().output(1)
     }
 
-    #[allow(unused)]
     fn get_vault_amount(&mut self, sign: bool) -> Decimal {
         let manifest = ManifestBuilder::new()
             .call_method(
