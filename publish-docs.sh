a137dcc423fe04a54149c502c45d00fb7306f886
Yulong Wu <yulong.wu@rdx.works>
2023-07-06 15:40:51 +0100

    Fix publish-docs script

--- publish-docs.sh
+++ publish-docs.sh
@@ -5,8 +5,8 @@ set -e
 
 cd "$(dirname "$0")"
 
-(cd radix-engine; cargo doc --release --no-deps --document-private-items --package scrypto --package sbor --package radix-engine)
+(cargo doc --release --no-deps --document-private-items --package scrypto --package sbor --package radix-engine)
 
 rm -rf ./docs
-cp -r ./radix-engine/target/doc ./docs
+cp -r ./target/doc ./docs
 echo "<meta http-equiv=\"refresh\" content=\"0; url=scrypto\">" > ./docs/index.html
