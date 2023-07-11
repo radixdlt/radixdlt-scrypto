355496db492ef1a5bf2dfd9b802669590f2aae49
Joshua Primero <josh.primero@rdx.works>
2023-07-11 16:13:35 -0500

    Add failing test

---
+++ radix-engine-tests/tests/system_actor_field.rs
@@ -0,0 +1,90 @@
+use native_sdk::modules::access_rules::AccessRules;
+use native_sdk::modules::metadata::Metadata;
+use native_sdk::modules::royalty::ComponentRoyalty;
+use radix_engine::errors::{RuntimeError, SystemError};
+use radix_engine::kernel::kernel_api::{KernelNodeApi, KernelSubstateApi};
+use radix_engine::system::system_callback::SystemLockData;
+use radix_engine::types::*;
+use radix_engine::vm::{OverridePackageCode, VmInvoke};
+use radix_engine_interface::api::node_modules::royalty::{
+    ComponentRoyaltySetInput, COMPONENT_ROYALTY_SET_ROYALTY_IDENT,
+};
+use radix_engine_interface::api::{ClientApi, FieldValue, LockFlags, ObjectModuleId, OBJECT_HANDLE_SELF, OBJECT_HANDLE_OUTER_OBJECT};
+use radix_engine_interface::blueprints::package::PackageDefinition;
+use scrypto_unit::*;
+use transaction::builder::ManifestBuilder;
+
+#[test]
+fn opening_non_existent_outer_object_fields_should_not_panic() {
+    // Arrange
+    const BLUEPRINT_NAME: &str = "MyBlueprint";
+    const CUSTOM_PACKAGE_CODE_ID: u64 = 1024;
+    #[derive(Clone)]
+    struct TestInvoke;
+    impl VmInvoke for TestInvoke {
+        fn invoke<Y>(
+            &mut self,
+            export_name: &str,
+            _input: &IndexedScryptoValue,
+            api: &mut Y,
+        ) -> Result<IndexedScryptoValue, RuntimeError>
+            where
+                Y: ClientApi<RuntimeError> + KernelNodeApi + KernelSubstateApi<SystemLockData>,
+        {
+            match export_name {
+                "test" => {
+                    api.actor_open_field(OBJECT_HANDLE_OUTER_OBJECT, 0u8, LockFlags::read_only())?;
+                }
+                "new" => {
+                    let metadata = Metadata::create(api)?;
+                    let access_rules = AccessRules::create(OwnerRole::None, btreemap!(), api)?;
+                    let node_id = api.new_simple_object(BLUEPRINT_NAME, vec![])?;
+                    api.globalize(
+                        btreemap!(
+                            ObjectModuleId::Main => node_id,
+                            ObjectModuleId::Metadata => metadata.0,
+                            ObjectModuleId::AccessRules => access_rules.0.0,
+                        ),
+                        None,
+                    )?;
+                }
+                _ => {}
+            }
+
+            Ok(IndexedScryptoValue::from_typed(&()))
+        }
+    }
+    let mut test_runner = TestRunnerBuilder::new().build_with_native_vm_extension(
+        OverridePackageCode::new(CUSTOM_PACKAGE_CODE_ID, TestInvoke),
+    );
+    let package_address = test_runner.publish_native_package(
+        CUSTOM_PACKAGE_CODE_ID,
+        PackageDefinition::new_functions_only_test_definition(
+            BLUEPRINT_NAME,
+            vec![
+                ("test", "test", true),
+                ("new", "new", false),
+            ],
+        ),
+    );
+    let receipt = test_runner.execute_manifest(
+        ManifestBuilder::new()
+            .lock_fee(test_runner.faucet_component(), 500u32)
+            .call_function(package_address, BLUEPRINT_NAME, "new", manifest_args!())
+            .build(),
+        vec![],
+    );
+    let component_address = receipt.expect_commit_success().new_component_addresses()[0];
+
+    // Act
+    let receipt = test_runner.execute_manifest(
+        ManifestBuilder::new()
+            .lock_fee(test_runner.faucet_component(), 500u32)
+            .call_method(component_address, "test", manifest_args!())
+            .build(),
+        vec![],
+    );
+
+    // Assert
+    receipt.expect_failure();
+}
