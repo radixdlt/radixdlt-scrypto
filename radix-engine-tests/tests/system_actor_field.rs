7d8e5b8a6188cf2b17d3601e14b38e08f41a54b7
Joshua Primero <josh.primero@rdx.works>
2023-07-30 16:51:44 -0500

    Add test which should fail

--- radix-engine-tests/tests/system_actor_field.rs
+++ radix-engine-tests/tests/system_actor_field.rs
@@ -5,9 +5,7 @@ use radix_engine::kernel::kernel_api::{KernelNodeApi, KernelSubstateApi};
 use radix_engine::system::system_callback::SystemLockData;
 use radix_engine::types::*;
 use radix_engine::vm::{OverridePackageCode, VmInvoke};
-use radix_engine_interface::api::{
-    ClientApi, LockFlags, ObjectModuleId, OBJECT_HANDLE_OUTER_OBJECT,
-};
+use radix_engine_interface::api::{ClientApi, LockFlags, ObjectModuleId, OBJECT_HANDLE_OUTER_OBJECT, OBJECT_HANDLE_SELF, FieldValue};
 use radix_engine_interface::blueprints::package::PackageDefinition;
 use scrypto_unit::*;
 use transaction::builder::ManifestBuilder;
@@ -82,4 +80,57 @@ fn opening_non_existent_outer_object_fields_should_not_panic() {
 
     // Assert
     receipt.expect_failure();
+}
+
+#[test]
+fn cannot_store_reference_in_non_transient_blueprint() {
+    // Arrange
+    const BLUEPRINT_NAME: &str = "MyBlueprint";
+    const CUSTOM_PACKAGE_CODE_ID: u64 = 1024;
+    #[derive(Clone)]
+    struct TestInvoke;
+    impl VmInvoke for TestInvoke {
+        fn invoke<Y>(
+            &mut self,
+            export_name: &str,
+            _input: &IndexedScryptoValue,
+            api: &mut Y,
+        ) -> Result<IndexedScryptoValue, RuntimeError>
+            where
+                Y: ClientApi<RuntimeError> + KernelNodeApi + KernelSubstateApi<SystemLockData>,
+        {
+            match export_name {
+                "new" => {
+                    let auth_zone = api.get_auth_zone()?;
+                    let node_id = api.new_simple_object(BLUEPRINT_NAME, vec![FieldValue::new(Reference(auth_zone))])?;
+                    api.drop_object(&node_id)?;
+                }
+                _ => {}
+            }
+
+            Ok(IndexedScryptoValue::from_typed(&()))
+        }
+    }
+    let mut test_runner = TestRunnerBuilder::new()
+        .with_custom_extension(OverridePackageCode::new(CUSTOM_PACKAGE_CODE_ID, TestInvoke))
+        .build();
+    let package_address = test_runner.publish_native_package(
+        CUSTOM_PACKAGE_CODE_ID,
+        PackageDefinition::new_with_field_test_definition(
+            BLUEPRINT_NAME,
+            vec![("new", "new", false)],
+        ),
+    );
+
+    // Act
+    let receipt = test_runner.execute_manifest(
+        ManifestBuilder::new()
+            .lock_fee(test_runner.faucet_component(), 500u32)
+            .call_function(package_address, BLUEPRINT_NAME, "new", manifest_args!())
+            .build(),
+        vec![],
+    );
+
+    // Assert
+    receipt.expect_commit_success();
 }
