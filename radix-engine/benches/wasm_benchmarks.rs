7805260c3b21414617f05a68810d122925c52692
Lukasz Rubaszewski <lukasz.rubaszewski@rdx.works>
2023-08-09 23:43:42 +0200

    Add wasmi-call-native and wasmer-call-native benchmarks
    Both benchmarks pass the Decimal params via WASM memory.

--- radix-engine/benches/wasm_benchmarks.rs
+++ radix-engine/benches/wasm_benchmarks.rs
@@ -1,9 +1,10 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use radix_engine_common::math::Decimal;
 use std::process::Command;
 use wasm_benchmarks_lib::*;
-use wasmer;
+use wasmer::{self, WasmerEnv};
 use wasmer_compiler_singlepass;
-use wasmi::{self, AsContextMut};
+use wasmi::{self, AsContext, AsContextMut};
 
 type HostState = u32;
 
@@ -29,14 +30,71 @@ fn get_wasm_file() -> Vec<u8> {
         .expect(&format!("Cannot read file {:?}", WASM_BENCHMARKS_WASM_FILE))
 }
 
+fn wasmi_read_memory(
+    store: impl AsContext,
+    memory: wasmi::Memory,
+    ptr: u32,
+    len: usize,
+) -> Vec<u8> {
+    let store_ctx = store.as_context();
+    let data = memory.data(&store_ctx);
+    let ptr = ptr as usize;
+
+    if ptr > data.len() || ptr + len > data.len() {
+        panic!("Memory access error");
+    }
+    data[ptr..ptr + len].to_vec()
+}
+
+fn wasmi_write_memory(mut store: impl AsContextMut, memory: wasmi::Memory, ptr: u32, data: &[u8]) {
+    let mem_data = memory.data(store.as_context());
+
+    if ptr as usize > mem_data.len() || ptr as usize + data.len() > mem_data.len() {
+        panic!("Memory access error");
+    }
+
+    memory
+        .write(&mut store.as_context_mut(), ptr as usize, data)
+        .expect("Memory access error");
+}
+
+fn wasmi_decimal_mul_native(
+    mut caller: wasmi::Caller<'_, HostState>,
+    a_ptr: u32,
+    b_ptr: u32,
+    c_ptr: u32,
+) -> Result<i64, wasmi::core::Trap> {
+    let memory = match caller.get_export("memory") {
+        Some(wasmi::Extern::Memory(memory)) => memory,
+        _ => panic!("Failed to find memory export"),
+    };
+
+    let a_vec = wasmi_read_memory(caller.as_context(), memory, a_ptr, Decimal::BITS / 8);
+    let b_vec = wasmi_read_memory(caller.as_context(), memory, b_ptr, Decimal::BITS / 8);
+    let a = Decimal::try_from(&a_vec[..]).unwrap();
+    let b = Decimal::try_from(&b_vec[..]).unwrap();
+
+    let c = a * b;
+    let c_vec = c.to_vec();
+    wasmi_write_memory(caller.as_context_mut(), memory, c_ptr, &c_vec[..]);
+
+    Ok(1)
+}
+
 // Instantiate WASMI instance with given WASM code
-fn get_wasmi_instance(
+fn wasmi_get_instance(
     engine: &wasmi::Engine,
     mut store: wasmi::StoreContextMut<HostState>,
     code: &[u8],
 ) -> wasmi::Instance {
     let module = wasmi::Module::new(&engine, code).unwrap();
-    let linker = <wasmi::Linker<HostState>>::new();
+    let mut linker = <wasmi::Linker<HostState>>::new();
+
+    let decimal_mul_native = wasmi::Func::wrap(&mut store, wasmi_decimal_mul_native);
+    linker
+        .define("env", "decimal_mul_native", decimal_mul_native)
+        .unwrap();
+
     linker
         .instantiate(store.as_context_mut(), &module)
         .unwrap()
@@ -44,14 +102,92 @@ fn get_wasmi_instance(
         .unwrap()
 }
 
-fn get_wasmer_instance(code: &[u8]) -> wasmer::Instance {
+// wasmer
+#[derive(Clone)]
+struct WasmerInstanceEnv {
+    instance: wasmer::LazyInit<wasmer::Instance>,
+}
+
+impl WasmerEnv for WasmerInstanceEnv {
+    fn init_with_instance(
+        &mut self,
+        instance: &wasmer::Instance,
+    ) -> Result<(), wasmer::HostEnvInitError> {
+        self.instance.initialize(instance.clone());
+        Ok(())
+    }
+}
+
+fn wasmer_read_memory(memory: &wasmer::Memory, ptr: u32, len: usize) -> Vec<u8> {
+    let ptr = ptr as usize;
+
+    let memory_slice = unsafe { memory.data_unchecked() };
+    let memory_size = memory_slice.len();
+    if ptr > memory_size || ptr + len > memory_size {
+        panic!("Memory access error");
+    }
+
+    memory_slice[ptr..ptr + len].to_vec()
+}
+
+fn wasmer_write_memory(memory: &wasmer::Memory, ptr: u32, data: &[u8]) {
+    let ptr = ptr as usize;
+    let len = data.len();
+
+    let memory_slice = unsafe { memory.data_unchecked_mut() };
+    let memory_size = memory_slice.len();
+    if ptr > memory_size || ptr + len > memory_size {
+        panic!("Memory access error");
+    }
+
+    memory_slice[ptr..ptr + data.len()].copy_from_slice(data);
+}
+
+fn wasmer_decimal_mul_native(
+    env: &WasmerInstanceEnv,
+    a_ptr: u32,
+    b_ptr: u32,
+    c_ptr: u32,
+) -> Result<i64, wasmi::core::Trap> {
+    let instance = unsafe { env.instance.get_unchecked() };
+    let memory = instance
+        .exports
+        .get_memory("memory")
+        .expect("Memory access error");
+
+    let a_vec = wasmer_read_memory(&memory, a_ptr, Decimal::BITS / 8);
+    let b_vec = wasmer_read_memory(&memory, b_ptr, Decimal::BITS / 8);
+    let a = Decimal::try_from(&a_vec[..]).unwrap();
+    let b = Decimal::try_from(&b_vec[..]).unwrap();
+
+    let c = a * b;
+    let c_vec = c.to_vec();
+    wasmer_write_memory(&memory, c_ptr, &c_vec[..]);
+
+    Ok(1)
+}
+
+fn wasmer_get_instance(code: &[u8]) -> wasmer::Instance {
     let compiler = wasmer_compiler_singlepass::Singlepass::new();
     let store = wasmer::Store::new(&wasmer::Universal::new(compiler).engine());
     let module = wasmer::Module::new(&store, code).unwrap();
-    let import_object = wasmer::imports! {};
+    let mut env = WasmerInstanceEnv {
+        instance: wasmer::LazyInit::new(),
+    };
+
+    let import_object = wasmer::imports! {
+        "env" => {
+            "decimal_mul_native" => wasmer::Function::new_native_with_env(module.store(), env.clone(), wasmer_decimal_mul_native)
+        }
+    };
 
     // instantiate
-    wasmer::Instance::new(&module, &import_object).expect("Failed to instantiate module")
+    let instance =
+        wasmer::Instance::new(&module, &import_object).expect("Failed to instantiate module");
+
+    env.init_with_instance(&instance.clone()).unwrap();
+
+    instance
 }
 
 macro_rules! bench_ops {
@@ -59,20 +195,26 @@ macro_rules! bench_ops {
         paste::item! {
             pub fn [< $t _ $ops _benchmark >] (c: &mut Criterion) {
                 let func_name = concat!($t, "_", $ops);
+                let func_call_native_name = &format!("{}_call_native", func_name);
                 let mut group = c.benchmark_group(func_name);
                 let wasm_code = get_wasm_file();
 
                 // wasmi stuff
                 let engine = wasmi::Engine::default();
                 let mut store = wasmi::Store::new(&engine, 42);
-                let wasmi_instance = get_wasmi_instance(&engine, store.as_context_mut(), &wasm_code[..]);
+                let wasmi_instance = wasmi_get_instance(&engine, store.as_context_mut(), &wasm_code[..]);
                 let wasmi_func = wasmi_instance
                     .get_typed_func::<(), i64>(store.as_context_mut(), func_name)
                     .unwrap();
 
+                let wasmi_func_call_native = wasmi_instance
+                    .get_typed_func::<(), i64>(store.as_context_mut(), func_call_native_name)
+                    .unwrap();
+
                 // wasmer stuff
-                let wasmer_instance = get_wasmer_instance(&wasm_code[..]);
+                let wasmer_instance = wasmer_get_instance(&wasm_code[..]);
                 let wasmer_func = wasmer_instance.exports.get_function(func_name).unwrap();
+                let wasmer_func_call_native = wasmer_instance.exports.get_function(func_call_native_name).unwrap();
 
                 // native
                 group.bench_function("rust-native", |b| {
@@ -96,21 +238,35 @@ macro_rules! bench_ops {
                     })
                 });
 
+                // wasmi call native
+                group.bench_function("wasmi-call-native", |b| {
+                    b.iter(|| {
+                        black_box(wasmi_func_call_native.call(store.as_context_mut(), ()).unwrap());
+                    })
+                });
+
+                // wasmer call native
+                group.bench_function("wasmer-call-native", |b| {
+                    b.iter(|| {
+                        black_box(wasmer_func_call_native.call(&[]).unwrap());
+                    })
+                });
+
                 group.finish();
             }
         }
     };
 }
 
-bench_ops!("primitive", "add");
-bench_ops!("primitive", "mul");
+//bench_ops!("primitive", "add");
+//bench_ops!("primitive", "mul");
 
-bench_ops!("decimal", "add");
+//bench_ops!("decimal", "add");
 bench_ops!("decimal", "mul");
 
-bench_ops!("precise_decimal", "add");
-bench_ops!("precise_decimal", "mul");
-
+//bench_ops!("precise_decimal", "add");
+//bench_ops!("precise_decimal", "mul");
+/*
 criterion_group! {
     name = primitive_benches;
     config = Criterion::default()
@@ -120,15 +276,17 @@ criterion_group! {
     targets = primitive_add_benchmark,
         primitive_mul_benchmark
 }
+*/
 criterion_group! {
     name = decimal_benches;
     config = Criterion::default()
                 .sample_size(10)
                 .measurement_time(core::time::Duration::from_secs(2))
                 .warm_up_time(core::time::Duration::from_millis(500));
-    targets = decimal_add_benchmark,
+    targets = //decimal_add_benchmark,
         decimal_mul_benchmark
 }
+/*
 criterion_group! {
     name = precise_decimal_benches;
     config = Criterion::default()
@@ -138,5 +296,6 @@ criterion_group! {
     targets = precise_decimal_add_benchmark,
         precise_decimal_mul_benchmark
 }
-
 criterion_main!(primitive_benches, decimal_benches, precise_decimal_benches);
+*/
+criterion_main!(decimal_benches);
--- radix-engine/wasm-benchmarks-lib/src/decimal.rs
+++ radix-engine/wasm-benchmarks-lib/src/decimal.rs
@@ -1,5 +1,15 @@
 use radix_engine_common::math::Decimal;
 
+#[cfg(target_arch = "wasm32")]
+extern "C" {
+    pub fn decimal_mul_native(a_ptr: *mut u8, b_ptr: *mut u8, c_ptr: *mut u8) -> u64;
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+pub unsafe fn decimal_mul_native(_a_ptr: *mut u8, _b_ptr: *mut u8, _c_ptr: *mut u8) -> u64 {
+    unreachable!()
+}
+
 #[no_mangle]
 pub fn decimal_add() -> i64 {
     let x = Decimal::ONE;
@@ -13,5 +23,26 @@ pub fn decimal_mul() -> i64 {
     let x = Decimal::ONE;
     let y = Decimal::ONE;
     let z = x * y;
+    z.is_positive().into()
+}
+#[no_mangle]
+pub fn decimal_mul_call_native() -> i64 {
+    let x = Decimal::ONE;
+    let mut x_vec = x.to_vec();
+    let x_ptr = x_vec.as_mut_ptr();
+
+    let y = Decimal::ONE;
+    let mut y_vec = y.to_vec();
+    let y_ptr = y_vec.as_mut_ptr();
+
+    let mut z_vec = Vec::<u8>::with_capacity(Decimal::BITS / 8);
+    let z_ptr = z_vec.as_mut_ptr();
+
+    unsafe {
+        decimal_mul_native(x_ptr, y_ptr, z_ptr);
+        z_vec.set_len(Decimal::BITS / 8);
+    };
+
+    let z = Decimal::try_from(&z_vec[..]).unwrap();
     z.is_positive().into()
 }
