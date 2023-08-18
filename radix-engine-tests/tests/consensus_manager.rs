cf79a936e36201a9c1809705396ce5a68c553348
Yulong Wu <yulong.wu@rdx.works>
2023-08-18 11:17:16 +0100

    Fix tests

--- radix-engine-tests/tests/consensus_manager.rs
+++ radix-engine-tests/tests/consensus_manager.rs
@@ -478,7 +478,7 @@ fn create_validator_with_wrong_resource_should_fail() {
     let mut test_runner = TestRunnerBuilder::new().build();
     let (public_key, _, account) = test_runner.new_allocated_account();
     let resource_address =
-        test_runner.create_fungible_resource(*DEFAULT_VALIDATOR_XRD_COST, 0u8, account);
+        test_runner.create_fungible_resource(*DEFAULT_VALIDATOR_XRD_COST, 18u8, account);
 
     // Act
     let receipt = test_runner.execute_manifest(
@@ -3067,6 +3067,7 @@ fn significant_protocol_updates_are_emitted_in_epoch_change_event() {
     costing_params.execution_cost_unit_price = Decimal::zero();
     costing_params.finalization_cost_unit_price = Decimal::zero();
     costing_params.state_storage_price = Decimal::zero();
+    costing_params.archive_storage_price = Decimal::zero();
 
     let receipt = test_runner.execute_manifest_with_costing_params(
         manifest,
--- radix-engine-tests/tests/storage.rs
+++ radix-engine-tests/tests/storage.rs
@@ -5,7 +5,7 @@ use transaction::prelude::*;
 #[test]
 fn test_kv_store_with_many_large_keys() {
     // Arrange
-    let mut test_runner = TestRunnerBuilder::new().build();
+    let mut test_runner = TestRunnerBuilder::new().without_trace().build();
     let package_address = test_runner.compile_and_publish("tests/blueprints/storage");
 
     // Act
@@ -21,11 +21,7 @@ fn test_kv_store_with_many_large_keys() {
 
     let receipt = test_runner.execute_manifest(manifest, vec![]);
     assert_eq!(
-        receipt
-            .fee_summary
-            .total_storage_cost_in_xrd
-            .safe_div(receipt.costing_parameters.state_storage_price)
-            .unwrap(),
-        dec!("419292")
+        receipt.fee_summary.total_storage_cost_in_xrd,
+        dec!("66.670735677079698048")
     );
 }
