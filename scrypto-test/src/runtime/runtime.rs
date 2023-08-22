3b5b24a4c010b59c995f5e56034fd8ebcaf90f10
Omar <OmarAbdulla7@hotmail.com>
2023-08-22 08:16:31 +0300

    Ability to enable and disable kernel modules

--- scrypto-test/src/runtime/runtime.rs
+++ scrypto-test/src/runtime/runtime.rs
@@ -13,8 +13,246 @@ use crate::prelude::*;
 pub struct TestRuntime(TestRuntimeInternal);
 
 impl TestRuntime {
+    /// Creates a new [`TestRuntime`] with the default configuration.
+    ///
+    /// By default, the [`TestRuntime`] has the auth and limits kernel modules enabled while all
+    /// other kernel modules are disabled, including the costing module. The kernel has is created
+    /// with two call-frames: a root call-frame and a function-actor call-frame.
     pub fn new() -> Self {
         Self::default()
+    }
+
+    /* Manipulation of Kernel Modules */
+
+    /// Enables the kernel trace kernel module of the Radix Engine.
+    pub fn enable_kernel_trace_module(&mut self) {
+        self.enable_module(EnabledModules::KERNEL_TRACE)
+    }
+
+    /// Enables the limits kernel module of the Radix Engine.
+    pub fn enable_limits_module(&mut self) {
+        self.enable_module(EnabledModules::LIMITS)
+    }
+
+    /// Enables the costing kernel module of the Radix Engine.
+    pub fn enable_costing_module(&mut self) {
+        self.enable_module(EnabledModules::COSTING)
+    }
+
+    /// Enables the auth kernel module of the Radix Engine.
+    pub fn enable_auth_module(&mut self) {
+        self.enable_module(EnabledModules::AUTH)
+    }
+
+    /// Enables the transaction runtime kernel module of the Radix Engine.
+    pub fn enable_transaction_runtime_module(&mut self) {
+        self.enable_module(EnabledModules::TRANSACTION_RUNTIME)
+    }
+
+    /// Enables the execution trace kernel module of the Radix Engine.
+    pub fn enable_execution_trace_module(&mut self) {
+        self.enable_module(EnabledModules::EXECUTION_TRACE)
+    }
+
+    /// Disables the kernel trace kernel module of the Radix Engine.
+    pub fn disable_kernel_trace_module(&mut self) {
+        self.disable_module(EnabledModules::KERNEL_TRACE)
+    }
+
+    /// Disables the limits kernel module of the Radix Engine.
+    pub fn disable_limits_module(&mut self) {
+        self.disable_module(EnabledModules::LIMITS)
+    }
+
+    /// Disables the costing kernel module of the Radix Engine.
+    pub fn disable_costing_module(&mut self) {
+        self.disable_module(EnabledModules::COSTING)
+    }
+
+    /// Disables the auth kernel module of the Radix Engine.
+    pub fn disable_auth_module(&mut self) {
+        self.disable_module(EnabledModules::AUTH)
+    }
+
+    /// Disables the transaction runtime kernel module of the Radix Engine.
+    pub fn disable_transaction_runtime_module(&mut self) {
+        self.disable_module(EnabledModules::TRANSACTION_RUNTIME)
+    }
+
+    /// Disables the execution trace kernel module of the Radix Engine.
+    pub fn disable_execution_trace_module(&mut self) {
+        self.disable_module(EnabledModules::EXECUTION_TRACE)
+    }
+
+    /// Calls the passed `callback` with the kernel trace kernel module enabled.
+    pub fn with_kernel_trace_module_enabled<F, O>(&mut self, callback: F) -> O
+    where
+        F: FnOnce(&mut Self) -> O,
+    {
+        let enabled_modules = self.enabled_modules();
+        self.enable_kernel_trace_module();
+        let rtn = callback(self);
+        self.set_enabled_modules(enabled_modules);
+        rtn
+    }
+
+    /// Calls the passed `callback` with the limits kernel module enabled.
+    pub fn with_limits_module_enabled<F, O>(&mut self, callback: F) -> O
+    where
+        F: FnOnce(&mut Self) -> O,
+    {
+        let enabled_modules = self.enabled_modules();
+        self.enable_limits_module();
+        let rtn = callback(self);
+        self.set_enabled_modules(enabled_modules);
+        rtn
+    }
+
+    /// Calls the passed `callback` with the costing kernel module enabled.
+    pub fn with_costing_module_enabled<F, O>(&mut self, callback: F) -> O
+    where
+        F: FnOnce(&mut Self) -> O,
+    {
+        let enabled_modules = self.enabled_modules();
+        self.enable_costing_module();
+        let rtn = callback(self);
+        self.set_enabled_modules(enabled_modules);
+        rtn
+    }
+
+    /// Calls the passed `callback` with the auth kernel module enabled.
+    pub fn with_auth_module_enabled<F, O>(&mut self, callback: F) -> O
+    where
+        F: FnOnce(&mut Self) -> O,
+    {
+        let enabled_modules = self.enabled_modules();
+        self.enable_auth_module();
+        let rtn = callback(self);
+        self.set_enabled_modules(enabled_modules);
+        rtn
+    }
+
+    /// Calls the passed `callback` with the transaction runtime kernel module enabled.
+    pub fn with_transaction_runtime_module_enabled<F, O>(&mut self, callback: F) -> O
+    where
+        F: FnOnce(&mut Self) -> O,
+    {
+        let enabled_modules = self.enabled_modules();
+        self.enable_transaction_runtime_module();
+        let rtn = callback(self);
+        self.set_enabled_modules(enabled_modules);
+        rtn
+    }
+
+    /// Calls the passed `callback` with the execution trace kernel module enabled.
+    pub fn with_execution_trace_module_enabled<F, O>(&mut self, callback: F) -> O
+    where
+        F: FnOnce(&mut Self) -> O,
+    {
+        let enabled_modules = self.enabled_modules();
+        self.enable_execution_trace_module();
+        let rtn = callback(self);
+        self.set_enabled_modules(enabled_modules);
+        rtn
+    }
+
+    /// Calls the passed `callback` with the kernel trace kernel module disabled.
+    pub fn with_kernel_trace_module_disabled<F, O>(&mut self, callback: F) -> O
+    where
+        F: FnOnce(&mut Self) -> O,
+    {
+        let enabled_modules = self.enabled_modules();
+        self.disable_kernel_trace_module();
+        let rtn = callback(self);
+        self.set_enabled_modules(enabled_modules);
+        rtn
+    }
+
+    /// Calls the passed `callback` with the limits kernel module disabled.
+    pub fn with_limits_module_disabled<F, O>(&mut self, callback: F) -> O
+    where
+        F: FnOnce(&mut Self) -> O,
+    {
+        let enabled_modules = self.enabled_modules();
+        self.disable_limits_module();
+        let rtn = callback(self);
+        self.set_enabled_modules(enabled_modules);
+        rtn
+    }
+
+    /// Calls the passed `callback` with the costing kernel module disabled.
+    pub fn with_costing_module_disabled<F, O>(&mut self, callback: F) -> O
+    where
+        F: FnOnce(&mut Self) -> O,
+    {
+        let enabled_modules = self.enabled_modules();
+        self.disable_costing_module();
+        let rtn = callback(self);
+        self.set_enabled_modules(enabled_modules);
+        rtn
+    }
+
+    /// Calls the passed `callback` with the auth kernel module disabled.
+    pub fn with_auth_module_disabled<F, O>(&mut self, callback: F) -> O
+    where
+        F: FnOnce(&mut Self) -> O,
+    {
+        let enabled_modules = self.enabled_modules();
+        self.disable_auth_module();
+        let rtn = callback(self);
+        self.set_enabled_modules(enabled_modules);
+        rtn
+    }
+
+    /// Calls the passed `callback` with the transaction runtime kernel module disabled.
+    pub fn with_transaction_runtime_module_disabled<F, O>(&mut self, callback: F) -> O
+    where
+        F: FnOnce(&mut Self) -> O,
+    {
+        let enabled_modules = self.enabled_modules();
+        self.disable_transaction_runtime_module();
+        let rtn = callback(self);
+        self.set_enabled_modules(enabled_modules);
+        rtn
+    }
+
+    /// Calls the passed `callback` with the execution trace kernel module disabled.
+    pub fn with_execution_trace_module_disabled<F, O>(&mut self, callback: F) -> O
+    where
+        F: FnOnce(&mut Self) -> O,
+    {
+        let enabled_modules = self.enabled_modules();
+        self.disable_execution_trace_module();
+        let rtn = callback(self);
+        self.set_enabled_modules(enabled_modules);
+        rtn
+    }
+
+    /// Returns the bit flags representing the currently enabled kernel modules.
+    pub fn enabled_modules(&self) -> EnabledModules {
+        self.0
+            .with_kernel(|kernel| kernel.kernel_callback().modules.enabled_modules)
+    }
+
+    /// Sets the bit flags representing the enabled kernel modules.
+    pub fn set_enabled_modules(&mut self, enabled_modules: EnabledModules) {
+        self.0.with_kernel_mut(|kernel| {
+            kernel.kernel_callback_mut().modules.enabled_modules = enabled_modules
+        })
+    }
+
+    /// Enables specific kernel module(s).
+    pub fn enable_module(&mut self, module: EnabledModules) {
+        self.0.with_kernel_mut(|kernel| {
+            kernel.kernel_callback_mut().modules.enabled_modules |= module
+        })
+    }
+
+    /// Disables specific kernel module(s).
+    pub fn disable_module(&mut self, module: EnabledModules) {
+        self.0.with_kernel_mut(|kernel| {
+            kernel.kernel_callback_mut().modules.enabled_modules &= !module
+        })
     }
 }
 
--- scrypto-test/tests/test_runtime.rs
+++ scrypto-test/tests/test_runtime.rs
@@ -1,6 +1,43 @@
-use scrypto_test::runtime::TestRuntime;
+use native_sdk::resource::*;
+use scrypto_test::prelude::*;
 
 #[test]
 fn test_runtime_can_be_created() {
     TestRuntime::default();
+}
+
+#[test]
+fn privileged_action_can_be_performed_when_auth_module_is_disabled() {
+    // Arrange
+    let mut test_runtime = TestRuntime::new();
+    let mut resource_manager = ResourceManager(XRD);
+
+    // Act
+    let bucket = test_runtime
+        .with_auth_module_disabled(|test_runtime| {
+            resource_manager.mint_fungible(100.into(), test_runtime)
+        })
+        .unwrap();
+
+    // Assert
+    assert_eq!(bucket.amount(&mut test_runtime).unwrap(), dec!("100"));
+}
+
+#[test]
+fn with_module_disabled_resets_modules_when_callback_is_finished() {
+    // Arrange
+    let mut test_runtime = TestRuntime::new();
+    let mut resource_manager = ResourceManager(XRD);
+
+    // Act
+    test_runtime.with_auth_module_disabled(|_| {});
+    let rtn = resource_manager.mint_fungible(100.into(), &mut test_runtime);
+
+    // Assert
+    assert!(matches!(
+        rtn,
+        Err(RuntimeError::SystemModuleError(
+            SystemModuleError::AuthError(..)
+        ))
+    ))
 }
