a93bc581c74c280761fd65cc28983a4bad318ed3
Lukasz Rubaszewski <lukasz.rubaszewski@rdx.works>
2023-08-28 17:27:22 +0200

    Test WASM buffers more tests

--- radix-engine-tests/tests/blueprints/wasm_buffers/src/wasm_buffers.rs
+++ radix-engine-tests/tests/blueprints/wasm_buffers/src/wasm_buffers.rs
@@ -1,63 +1,98 @@
+use sbor::*;
 use scrypto::prelude::wasm_api::*;
 use scrypto::prelude::*;
-
-const BUFFER_LENGTH: usize = 8 * 1024;
 
 #[blueprint]
 mod wasm_buffers {
 
     struct WasmBuffersTest {
-        node_id: NodeId,
-        handle: u32,
+        kv_store: KeyValueStore<u32, Vec<u8>>,
+        key: Vec<u8>,
     }
 
     impl WasmBuffersTest {
         pub fn new() -> Global<WasmBuffersTest> {
-            let schema = scrypto_encode(&KeyValueStoreGenericArgs::new::<u32, Own>(true)).unwrap();
-            let bytes =
-                copy_buffer(unsafe { kv_store::kv_store_new(schema.as_ptr(), schema.len()) });
-            let node_id: NodeId = scrypto_decode(&bytes).unwrap();
+            let kv_store = KeyValueStore::<u32, Vec<u8>>::new();
+            let key = scrypto_encode(&1u32).unwrap();
 
-            let key = scrypto_encode(&0u32).unwrap();
-
-            let handle = unsafe {
-                kv_store::kv_store_open_entry(
-                    node_id.as_ref().as_ptr(),
-                    node_id.as_ref().len(),
-                    key.as_ptr(),
-                    key.len(),
-                    LockFlags::MUTABLE.bits(),
-                )
-            };
-
-            Self { node_id, handle }
+            Self { kv_store, key }
                 .instantiate()
                 .prepare_to_globalize(OwnerRole::None)
                 .globalize()
         }
 
+        fn get_kv_store_handle(&self) -> u32 {
+            let node_id = self.kv_store.id.as_node_id();
+            let handle = unsafe {
+                kv_store::kv_store_open_entry(
+                    node_id.as_ref().as_ptr(),
+                    node_id.as_ref().len(),
+                    self.key.as_ptr(),
+                    self.key.len(),
+                    LockFlags::MUTABLE.bits(),
+                )
+            };
+            handle
+        }
+
         /// Let native function "kv_entry_write" read the data from WASM memory and write it to
         /// the KV store
-        pub fn read_memory(&self, offs: usize, len: usize) {
-            // allocate some data but let
-            let buffer = vec![0u8; BUFFER_LENGTH];
+        /// Arguments:
+        /// - buffer_size - WASM buffer to allocate
+        /// - read_memory_offs - buffer offset to start reading data from
+        /// - read_memory_len - number of bytes to read from memory
+        /// WASM memory grows in 64KB chunks.
+        /// If attempting to access outside WASM memory, make sure that
+        /// read_memory_offset + read_memory_len > buffer_size + 64KB
+        pub fn read_memory(
+            &self,
+            buffer_size: usize,
+            read_memory_offs: usize,
+            read_memory_len: usize,
+        ) {
+            // SBOR encoding of Vec<u8>
+            let mut buffer = Vec::new();
+            let mut encoder = VecEncoder::<ScryptoCustomValueKind>::new(&mut buffer, 100);
+            encoder
+                .write_payload_prefix(SCRYPTO_SBOR_V1_PAYLOAD_PREFIX)
+                .unwrap();
+            encoder.write_value_kind(ValueKind::Array).unwrap();
+            encoder.write_value_kind(ValueKind::U8).unwrap();
+            encoder.write_size(buffer_size).unwrap();
+            buffer.reserve(buffer_size);
+            let new_size = buffer.len() + buffer_size;
+            unsafe { buffer.set_len(new_size) };
 
-            unsafe { kv_entry::kv_entry_write(self.handle, buffer.as_ptr().add(offs), len) };
-            unsafe { kv_entry::kv_entry_close(self.handle) };
+            let handle = self.get_kv_store_handle();
+
+            unsafe {
+                kv_entry::kv_entry_write(
+                    handle,
+                    buffer.as_ptr().add(read_memory_offs),
+                    read_memory_len,
+                )
+            };
+            unsafe { kv_entry::kv_entry_close(handle) };
         }
 
         /// Let native function "kv_entry_read" get the data from the KV store and then native
-        /// functiion "buffer_consume" writes the data to the WASM memory
-        pub fn write_memory(&self, offs: usize, len: usize) {
-            let buffer = unsafe { kv_entry::kv_entry_read(self.handle) };
+        /// function "buffer_consume" writes the data to the WASM memory
+        /// Arguments:
+        /// - buffer_size - WASM buffer to allocate
+        /// - write_memory_offs - buffer offset to start reading data from
+        /// WASM memory grows in 64KB chunks.
+        /// If attempting to access outside WASM memory, make sure that
+        /// write_memory_offs > buffer_size + 64KB
+        pub fn write_memory(&self, buffer_size: usize, write_memory_offs: usize) {
+            let handle = self.get_kv_store_handle();
+            let buffer = unsafe { kv_entry::kv_entry_read(handle) };
 
             // copy buffer
             let _vec = {
-                //let len = buffer.len() as usize;
-                let mut vec = Vec::<u8>::with_capacity(len);
+                let mut vec = Vec::<u8>::with_capacity(buffer_size);
                 unsafe {
-                    buffer::buffer_consume(buffer.id(), vec.as_mut_ptr().add(offs));
-                    vec.set_len(len);
+                    buffer::buffer_consume(buffer.id(), vec.as_mut_ptr().add(write_memory_offs));
+                    vec.set_len(buffer_size);
                 };
                 vec
             };
--- radix-engine-tests/tests/wasm_buffers.rs
+++ radix-engine-tests/tests/wasm_buffers.rs
@@ -1,17 +1,59 @@
 use radix_engine::{
-    errors::{RuntimeError, SystemModuleError},
-    system::system_modules::limits::TransactionLimitsError,
-    transaction::{CostingParameters, ExecutionConfig},
+    errors::*,
+    transaction::TransactionReceipt,
     types::*,
+    vm::{wasm::WasmRuntimeError, NativeVmExtension, NoExtension},
 };
+use radix_engine_stores::memory_db::InMemorySubstateDatabase;
 use scrypto_unit::*;
 use transaction::prelude::*;
 
-#[test]
-fn test_wasm_buffers_read() {
+const KB: usize = 1024;
+const MB: usize = 1024 * KB;
+
+struct ReadMemory {
+    buffer_size: usize,
+    memory_offs: usize,
+    memory_len: usize,
+}
+
+struct WriteMemory {
+    buffer_size: usize,
+    memory_offs: usize,
+}
+
+fn build_and_execute_manifest<E: NativeVmExtension, D: TestDatabase>(
+    test_runner: &mut TestRunner<E, D>,
+    component_address: ComponentAddress,
+    read_memory: ReadMemory,
+    write_memory: Option<WriteMemory>,
+) -> TransactionReceipt {
+    let mut manifest_builder = ManifestBuilder::new().lock_fee_from_faucet().call_method(
+        component_address,
+        "read_memory",
+        manifest_args!(
+            read_memory.buffer_size,
+            read_memory.memory_offs,
+            read_memory.memory_len
+        ),
+    );
+    if let Some(write_memory) = write_memory {
+        manifest_builder = manifest_builder.call_method(
+            component_address,
+            "write_memory",
+            manifest_args!(write_memory.buffer_size, write_memory.memory_offs),
+        );
+    }
+    let manifest = manifest_builder.build();
+    let receipt = test_runner.execute_manifest(manifest, vec![]);
+    receipt
+}
+
+fn get_test_runner() -> (
+    TestRunner<NoExtension, InMemorySubstateDatabase>,
+    ComponentAddress,
+) {
     let (code, definition) = Compile::compile("tests/blueprints/wasm_buffers");
-    let code_len = code.len();
-    let definition_len = scrypto_encode(&definition).unwrap().len();
 
     // Arrange
     let mut test_runner = TestRunnerBuilder::new().build();
@@ -28,17 +70,189 @@ fn test_wasm_buffers_read() {
         .expect_commit_success()
         .new_component_addresses()[0];
 
+    (test_runner, component_address)
+}
+
+#[test]
+fn test_wasm_buffers_small_success() {
+    // Arrange
+    let (mut test_runner, component_address) = get_test_runner();
+
     // Act
-    let manifest = ManifestBuilder::new()
-        .lock_fee_from_faucet()
-        .call_method(
-            component_address,
-            "read_memory",
-            manifest_args!(0usize, 1024usize),
-        )
-        .build();
-    let receipt = test_runner.execute_manifest(manifest, vec![]);
+    let receipt = build_and_execute_manifest(
+        &mut test_runner,
+        component_address,
+        ReadMemory {
+            buffer_size: 10 * KB,
+            memory_offs: 0usize,
+            memory_len: 10 * KB + 5, // +5 for SBOR headers
+        },
+        Some(WriteMemory {
+            buffer_size: 10 * KB,
+            memory_offs: 0usize,
+        }),
+    );
 
     // Assert
     receipt.expect_commit_success();
+}
+
+#[test]
+fn test_wasm_buffers_large_success() {
+    // Arrange
+    let (mut test_runner, component_address) = get_test_runner();
+
+    // Act
+    let receipt = build_and_execute_manifest(
+        &mut test_runner,
+        component_address,
+        ReadMemory {
+            buffer_size: 1 * MB,
+            memory_offs: 0usize,
+            memory_len: 1 * MB + 6, // +6 for SBOR headers
+        },
+        Some(WriteMemory {
+            buffer_size: 1 * MB,
+            memory_offs: 0usize,
+        }),
+    );
+
+    // Assert
+    receipt.expect_commit_success();
+}
+
+#[test]
+fn test_wasm_buffers_small_read_memory_access_error_1() {
+    // Arrange
+    let (mut test_runner, component_address) = get_test_runner();
+
+    // Act
+    let receipt = build_and_execute_manifest(
+        &mut test_runner,
+        component_address,
+        ReadMemory {
+            buffer_size: 10 * KB,
+            memory_offs: 0usize,
+            memory_len: 10 * KB + 128 * KB, // Add 128KB to make sure we are accessing beyond
+                                            // WASM memory
+        },
+        None,
+    );
+
+    // Assert
+    receipt.expect_specific_failure(|e| {
+        matches!(
+            e,
+            RuntimeError::VmError(VmError::Wasm(WasmRuntimeError::MemoryAccessError)),
+        )
+    });
+}
+
+#[test]
+fn test_wasm_buffers_large_read_memory_access_error_1() {
+    // Arrange
+    let (mut test_runner, component_address) = get_test_runner();
+
+    // Act
+    let receipt = build_and_execute_manifest(
+        &mut test_runner,
+        component_address,
+        ReadMemory {
+            buffer_size: 1 * MB,
+            memory_offs: 0usize,
+            memory_len: 1 * MB + 128 * KB, // Add 128KB to make sure we are accessing not only
+                                           // beyond the buffer but also whole WASM memory
+        },
+        None,
+    );
+
+    // Assert
+    receipt.expect_specific_failure(|e| {
+        matches!(
+            e,
+            RuntimeError::VmError(VmError::Wasm(WasmRuntimeError::MemoryAccessError)),
+        )
+    });
+}
+
+#[test]
+fn test_wasm_buffers_read_memory_access_error_2() {
+    // Arrange
+    let (mut test_runner, component_address) = get_test_runner();
+
+    // Act
+    let receipt = build_and_execute_manifest(
+        &mut test_runner,
+        component_address,
+        ReadMemory {
+            buffer_size: 10 * KB,
+            memory_offs: 64 * KB, // memory offset + length is 128KB beyond the buffer and also
+            memory_len: 74 * KB,  // beyond WASM memory
+        },
+        None,
+    );
+
+    // Assert
+    receipt.expect_specific_failure(|e| {
+        matches!(
+            e,
+            RuntimeError::VmError(VmError::Wasm(WasmRuntimeError::MemoryAccessError)),
+        )
+    });
+}
+
+#[test]
+fn test_wasm_buffers_read_memory_access_error_3() {
+    // Arrange
+    let (mut test_runner, component_address) = get_test_runner();
+
+    // Act
+    let receipt = build_and_execute_manifest(
+        &mut test_runner,
+        component_address,
+        ReadMemory {
+            buffer_size: 10 * KB,
+            memory_offs: 10 * KB + 127 * KB, // memory offset + length is 128KB beyond the buffer and also
+            memory_len: 1 * KB,
+        },
+        None,
+    );
+
+    // Assert
+    receipt.expect_specific_failure(|e| {
+        matches!(
+            e,
+            RuntimeError::VmError(VmError::Wasm(WasmRuntimeError::MemoryAccessError)),
+        )
+    });
+}
+
+#[test]
+fn test_wasm_buffers_write_memory_access_error_1() {
+    // Arrange
+    let (mut test_runner, component_address) = get_test_runner();
+
+    // Act
+    let receipt = build_and_execute_manifest(
+        &mut test_runner,
+        component_address,
+        ReadMemory {
+            buffer_size: 10 * KB,
+            memory_offs: 0usize,
+            memory_len: 10 * KB + 5, // +5 for SBOR headers
+        },
+        Some(WriteMemory {
+            buffer_size: 10 * KB,
+            memory_offs: 10 * KB + 128 * KB, // Add 128KB to make sure we are accessing not only
+                                             // beyond the buffer but also whole WASM memory
+        }),
+    );
+
+    // Assert
+    receipt.expect_specific_failure(|e| {
+        matches!(
+            e,
+            RuntimeError::VmError(VmError::Wasm(WasmRuntimeError::MemoryAccessError)),
+        )
+    });
 }
