f8ff1e58a33df4bc3c108dc3b86d5aae73572dae
Yulong Wu <yulong.wu@rdx.works>
2023-08-31 09:44:21 +0100

    Fix test blueprints

--- radix-engine-tests/tests/component.rs
+++ radix-engine-tests/tests/component.rs
@@ -1,13 +1,11 @@
 use radix_engine::errors::RuntimeError;
 use radix_engine::types::*;
-use radix_engine_interface::blueprints::resource::FromPublicKey;
 use scrypto_unit::*;
 use transaction::prelude::*;
 
 #[test]
 fn test_component() {
     let mut test_runner = TestRunnerBuilder::new().build();
-    let (public_key, _, account) = test_runner.new_allocated_account();
     let package = test_runner.compile_and_publish("./tests/blueprints/component");
 
     // Create component
@@ -22,28 +20,6 @@ fn test_component() {
         .build();
     let receipt1 = test_runner.execute_manifest(manifest1, vec![]);
     receipt1.expect_commit_success();
-
-    // Find the component address from receipt
-    let component = receipt1.expect_commit(true).new_component_addresses()[0];
-
-    // Call functions & methods
-    let manifest2 = ManifestBuilder::new()
-        .lock_fee_from_faucet()
-        .call_function(
-            package,
-            "ComponentTest",
-            "get_component_info",
-            manifest_args!(component),
-        )
-        .call_method(component, "get_component_state", manifest_args!())
-        .call_method(component, "put_component_state", manifest_args!())
-        .try_deposit_entire_worktop_or_abort(account, None)
-        .build();
-    let receipt2 = test_runner.execute_manifest(
-        manifest2,
-        vec![NonFungibleGlobalId::from_public_key(&public_key)],
-    );
-    receipt2.expect_commit_success();
 }
 
 #[test]
