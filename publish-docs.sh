972d53bbc98654e8d3fef936e08806e394962147
Yulong Wu <yulong.wu@rdx.works>
2023-08-31 15:32:01 +0100

    Add more packages

--- publish-docs.sh
+++ publish-docs.sh
@@ -5,7 +5,19 @@ set -e
 
 cd "$(dirname "$0")"
 
-(cargo doc --release --no-deps --document-private-items --package scrypto --package sbor --package radix-engine)
+rm -rf ./target/doc
+(cargo doc --release --no-deps --document-private-items \
+    --package sbor  \
+    --package scrypto \
+    --package scrypto-test \
+    --package scrypto-unit \
+    --package radix-engine-common \
+    --package radix-engine-interface \
+    --package radix-engine-store-interface \
+    --package radix-engine-stores \
+    --package radix-engine-queries \
+    --package radix-engine \
+)
 
 rm -rf ./docs
 cp -r ./target/doc ./docs
