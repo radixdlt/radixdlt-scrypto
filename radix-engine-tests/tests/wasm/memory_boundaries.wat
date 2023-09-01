5be698f0ee70f504b969240e750f7e943f15e2d1
Lukasz Rubaszewski <lukasz.rubaszewski@rdx.works>
2023-09-01 12:34:05 +0200

    Add WASM memory boundaries lowlevel tests

---
+++ radix-engine-tests/tests/wasm/memory_boundaries.wat
@@ -0,0 +1,86 @@
+(module
+    (import "env" "test_host_read_memory" (func $host_read_memory (param i32 i32)))
+    (import "env" "test_host_write_memory" (func $host_write_memory (param i32 i32)))
+
+    (func $Test_grow_memory (param $pages i64)  (result i64)
+        ;; grow memory by given pages cnt
+        (memory.grow
+            (i32.wrap_i64
+                (local.get $pages)
+            )
+        )
+        ;; drop operand from stack put by memory grow
+        drop
+
+        ;; Encode () in SBOR at address 0x0
+        (i32.const 0)
+        (i32.const 92)  ;; prefix
+        (i32.store8)
+        (i32.const 1)
+        (i32.const 33)  ;; tuple value kind
+        (i32.store8)
+        (i32.const 2)
+        (i32.const 0)  ;; tuple length
+        (i32.store8)
+
+        ;; Return slice (ptr = 0, len = 3)
+        (i64.const 3)
+    )
+
+    (func $Test_read_memory (param $offs i64) (param $len i64) (result i64)
+        ;; read_memory
+        (call $host_read_memory
+            (i32.wrap_i64
+                (local.get $offs)
+            )
+            (i32.wrap_i64
+                (local.get $len)
+            )
+        )
+
+        ;; Encode () in SBOR at address 0x0
+        (i32.const 0)
+        (i32.const 92)  ;; prefix
+        (i32.store8)
+        (i32.const 1)
+        (i32.const 33)  ;; tuple value kind
+        (i32.store8)
+        (i32.const 2)
+        (i32.const 0)  ;; tuple length
+        (i32.store8)
+
+        ;; Return slice (ptr = 0, len = 3)
+        (i64.const 3)
+    )
+
+    (func $Test_write_memory (param $offs i64) (param $len i64) (result i64)
+        ;; write_memory
+        (call $host_write_memory
+            (i32.wrap_i64
+                (local.get $offs)
+            )
+            (i32.wrap_i64
+                (local.get $len)
+            )
+        )
+
+        ;; Encode () in SBOR at address 0x0
+        (i32.const 0)
+        (i32.const 92)  ;; prefix
+        (i32.store8)
+        (i32.const 1)
+        (i32.const 33)  ;; tuple value kind
+        (i32.store8)
+        (i32.const 2)
+        (i32.const 0)  ;; tuple length
+        (i32.store8)
+
+        ;; Return slice (ptr = 0, len = 3)
+        (i64.const 3)
+    )
+    (memory $0 1)
+    (export "memory" (memory $0))
+    (export "Test_grow_memory" (func $Test_grow_memory))
+    (export "Test_read_memory" (func $Test_read_memory))
+    (export "Test_write_memory" (func $Test_write_memory))
+)
--- radix-engine-tests/tests/wasm_buffers.rs
+++ radix-engine-tests/tests/wasm_buffers.rs
@@ -1,25 +1,30 @@
-use radix_engine::{
-    errors::*,
-    transaction::TransactionReceipt,
-    types::*,
-    vm::{wasm::WasmRuntimeError, NativeVmExtension, NoExtension},
-};
+use radix_engine::errors::*;
+use radix_engine::system::system_modules::costing::SystemLoanFeeReserve;
+use radix_engine::transaction::{CostingParameters, TransactionReceipt};
+use radix_engine::types::*;
+use radix_engine::vm::wasm::*;
+use radix_engine::vm::wasm_runtime::NoOpWasmRuntime;
+use radix_engine::vm::*;
+use radix_engine_common::crypto::Hash;
+use radix_engine_interface::blueprints::package::CodeHash;
 use radix_engine_stores::memory_db::InMemorySubstateDatabase;
 use scrypto_unit::*;
+use transaction::model::TransactionCostingParameters;
 use transaction::prelude::*;
+use wabt::wat2wasm;
 
-const KB: usize = 1024;
-const MB: usize = 1024 * KB;
+const KB: u64 = 1024;
+const MB: u64 = 1024 * KB;
 
 struct ReadMemory {
-    buffer_size: usize,
-    memory_offs: usize,
-    memory_len: usize,
+    buffer_size: u64,
+    memory_offs: u64,
+    memory_len: u64,
 }
 
 struct WriteMemory {
-    buffer_size: usize,
-    memory_offs: usize,
+    buffer_size: u64,
+    memory_offs: u64,
 }
 
 fn build_and_execute_manifest<E: NativeVmExtension, D: TestDatabase>(
@@ -32,16 +37,19 @@ fn build_and_execute_manifest<E: NativeVmExtension, D: TestDatabase>(
         component_address,
         "read_memory",
         manifest_args!(
-            read_memory.buffer_size,
-            read_memory.memory_offs,
-            read_memory.memory_len
+            read_memory.buffer_size as usize,
+            read_memory.memory_offs as usize,
+            read_memory.memory_len as usize
         ),
     );
     if let Some(write_memory) = write_memory {
         manifest_builder = manifest_builder.call_method(
             component_address,
             "write_memory",
-            manifest_args!(write_memory.buffer_size, write_memory.memory_offs),
+            manifest_args!(
+                write_memory.buffer_size as usize,
+                write_memory.memory_offs as usize
+            ),
         );
     }
     let manifest = manifest_builder.build();
@@ -84,12 +92,12 @@ fn test_wasm_buffers_small_success() {
         component_address,
         ReadMemory {
             buffer_size: 10 * KB,
-            memory_offs: 0usize,
+            memory_offs: 0,
             memory_len: 10 * KB + 5, // +5 for SBOR headers
         },
         Some(WriteMemory {
             buffer_size: 10 * KB,
-            memory_offs: 0usize,
+            memory_offs: 0,
         }),
     );
 
@@ -108,12 +116,12 @@ fn test_wasm_buffers_large_success() {
         component_address,
         ReadMemory {
             buffer_size: 1 * MB,
-            memory_offs: 0usize,
+            memory_offs: 0,
             memory_len: 1 * MB + 6, // +6 for SBOR headers
         },
         Some(WriteMemory {
             buffer_size: 1 * MB,
-            memory_offs: 0usize,
+            memory_offs: 0,
         }),
     );
 
@@ -132,7 +140,7 @@ fn test_wasm_buffers_small_read_memory_access_error_1() {
         component_address,
         ReadMemory {
             buffer_size: 10 * KB,
-            memory_offs: 0usize,
+            memory_offs: 0,
             memory_len: 10 * KB + 128 * KB, // Add 128KB to make sure we are accessing beyond
                                             // WASM memory
         },
@@ -159,7 +167,7 @@ fn test_wasm_buffers_large_read_memory_access_error_1() {
         component_address,
         ReadMemory {
             buffer_size: 1 * MB,
-            memory_offs: 0usize,
+            memory_offs: 0,
             memory_len: 1 * MB + 128 * KB, // Add 128KB to make sure we are accessing not only
                                            // beyond the buffer but also whole WASM memory
         },
@@ -238,7 +246,7 @@ fn test_wasm_buffers_write_memory_access_error_1() {
         component_address,
         ReadMemory {
             buffer_size: 10 * KB,
-            memory_offs: 0usize,
+            memory_offs: 0,
             memory_len: 10 * KB + 5, // +5 for SBOR headers
         },
         Some(WriteMemory {
@@ -255,4 +263,118 @@ fn test_wasm_buffers_write_memory_access_error_1() {
             RuntimeError::VmError(VmError::Wasm(WasmRuntimeError::MemoryAccessError)),
         )
     });
+}
+
+#[test]
+fn test_wasm_memory_boundaries() {
+    // Arrange
+    let code = wat2wasm(&include_str!("wasm/memory_boundaries.wat")).unwrap();
+    let wasm_engine = DefaultWasmEngine::default();
+    let mut instance = wasm_engine.instantiate(CodeHash(Hash([0u8; 32])), &code);
+
+    let fee_reserve = SystemLoanFeeReserve::new(
+        &CostingParameters::default(),
+        &TransactionCostingParameters::default(),
+        false,
+    );
+    let mut wasm_execution_units_consumed = 0;
+    let mut runtime: Box<dyn WasmRuntime> = Box::new(NoOpWasmRuntime::new(
+        fee_reserve,
+        &mut wasm_execution_units_consumed,
+    ));
+    macro_rules! grow_memory {
+        ($len:expr) => {
+            let pages = $len / (64 * KB);
+            let result =
+                instance.invoke_export("Test_grow_memory", vec![Buffer(pages)], &mut runtime);
+            assert!(result.is_ok());
+        };
+    }
+    macro_rules! read_memory_ok {
+        ($offs:expr, $len:expr) => {
+            let result = instance.invoke_export(
+                "Test_read_memory",
+                vec![Buffer($offs), Buffer($len)],
+                &mut runtime,
+            );
+            assert!(result.is_ok());
+        };
+    }
+    macro_rules! read_memory_err {
+        ($offs:expr, $len:expr, $err:path) => {
+            let result = instance.invoke_export(
+                "Test_read_memory",
+                vec![Buffer($offs), Buffer($len)],
+                &mut runtime,
+            );
+            assert!(matches!(result.unwrap_err(), InvokeError::SelfError($err)));
+        };
+    }
+    macro_rules! write_memory_ok {
+        ($offs:expr, $len:expr) => {
+            let result = instance.invoke_export(
+                "Test_write_memory",
+                vec![Buffer($offs), Buffer($len)],
+                &mut runtime,
+            );
+            assert!(result.is_ok());
+        };
+    }
+    macro_rules! write_memory_err {
+        ($offs:expr, $len:expr, $err:path) => {
+            let result = instance.invoke_export(
+                "Test_write_memory",
+                vec![Buffer($offs), Buffer($len)],
+                &mut runtime,
+            );
+            assert!(matches!(result.unwrap_err(), InvokeError::SelfError($err)));
+        };
+    }
+
+    // Initially there is 64KB memory (1 page) available
+    let initial_size = 64 * KB;
+    let mut current_size = initial_size;
+
+    // Act & Assert
+    for size in [
+        current_size,
+        128 * KB,
+        2 * MB,
+        4 * MB, // this is RE memory limit (MAX_MEMORY_SIZE_IN_PAGES) limit
+        5 * MB, // but it is not honored at this level
+    ] {
+        if size != initial_size {
+            grow_memory!(size - current_size);
+            current_size = size;
+        }
+        write_memory_ok!(0, 64 * KB);
+        read_memory_ok!(0, 64 * KB);
+
+        write_memory_ok!(0, current_size);
+        read_memory_ok!(0, current_size);
+
+        write_memory_ok!(current_size - 4 * KB, 2 * KB);
+        read_memory_ok!(current_size - 4 * KB, 2 * KB);
+
+        write_memory_ok!(current_size - 1, 1);
+        read_memory_ok!(current_size - 1, 1);
+
+        // Access outside memory
+        write_memory_err!(0, current_size + 1, WasmRuntimeError::MemoryAccessError);
+        read_memory_err!(0, current_size + 1, WasmRuntimeError::MemoryAccessError);
+
+        read_memory_err!(
+            current_size - 4 * KB,
+            4 * KB + 1,
+            WasmRuntimeError::MemoryAccessError
+        );
+        write_memory_err!(
+            current_size - 4 * KB,
+            4 * KB + 1,
+            WasmRuntimeError::MemoryAccessError
+        );
+
+        read_memory_err!(current_size - 1, 2, WasmRuntimeError::MemoryAccessError);
+        write_memory_err!(current_size - 1, 2, WasmRuntimeError::MemoryAccessError);
+    }
 }
