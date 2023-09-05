811aa541ed0f648717991bedee908abdda0d0243
Yulong Wu <yulong.wu@rdx.works>
2023-09-05 22:17:21 +0100

    Remove unused imports

--- radix-engine-tests/tests/system_actor_collection.rs
+++ radix-engine-tests/tests/system_actor_collection.rs
@@ -3,7 +3,6 @@ use native_sdk::modules::role_assignment::RoleAssignment;
 use radix_engine::errors::RuntimeError;
 use radix_engine::kernel::kernel_api::{KernelNodeApi, KernelSubstateApi};
 use radix_engine::system::system_callback::SystemLockData;
-use radix_engine::track::LegacyStateUpdates;
 use radix_engine::types::*;
 use radix_engine::vm::{OverridePackageCode, VmInvoke};
 use radix_engine_interface::api::{AttachedModuleId, ClientApi, LockFlags, ACTOR_STATE_SELF};
