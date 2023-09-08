6d0bd2ab094bbc4a6b44b16f7429a470d460e97d
Yulong Wu <yulong.wu@rdx.works>
2023-09-08 18:28:32 +0100

    Fix test_kv_store_with_many_large_keys

--- radix-engine-tests/tests/storage.rs
+++ radix-engine-tests/tests/storage.rs
@@ -25,6 +25,6 @@ fn test_kv_store_with_many_large_keys() {
     let receipt = test_runner.execute_manifest(manifest, vec![]);
     assert_eq!(
         receipt.fee_summary.total_storage_cost_in_xrd,
-        dec!("40.11812141067")
+        dec!("40.12508323306")
     );
 }
