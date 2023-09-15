43f1753a26240b108ad8f3c5aa7f77e593900217
Yulong Wu <yulong.wu@rdx.works>
2023-09-15 13:57:57 +0100

    Test cannot_claim_unstake_after_epochs_with_wrong_resource

--- radix-engine-tests/tests/consensus_manager.rs
+++ radix-engine-tests/tests/consensus_manager.rs
@@ -1,6 +1,7 @@
 mod package_loader;
 
 use package_loader::PackageLoader;
+use radix_engine::blueprints::consensus_manager::UnstakeData;
 use radix_engine::blueprints::consensus_manager::{
     Validator, ValidatorEmissionAppliedEvent, ValidatorError,
 };
@@ -20,6 +21,7 @@ use rand::Rng;
 use rand_chacha;
 use rand_chacha::rand_core::SeedableRng;
 use rand_chacha::ChaCha8Rng;
+use scrypto::api::node_modules::*;
 use scrypto_unit::*;
 use transaction::prelude::*;
 
@@ -3288,4 +3290,85 @@ fn cannot_unstake_with_wrong_resource() {
             RuntimeError::SystemError(SystemError::InvalidDropAccess(_))
         )
     });
+}
+
+#[test]
+fn cannot_claim_unstake_after_epochs_with_wrong_resource() {
+    // Arrange
+    let genesis_epoch = Epoch::of(5);
+    let initial_epoch = genesis_epoch.next().unwrap();
+    let num_unstake_epochs = 7;
+    let validator_pub_key = Secp256k1PrivateKey::from_u64(2u64).unwrap().public_key();
+    let account_pub_key = Secp256k1PrivateKey::from_u64(1u64).unwrap().public_key();
+    let account_with_su = ComponentAddress::virtual_account_from_public_key(&account_pub_key);
+    let genesis = CustomGenesis::single_validator_and_staker(
+        validator_pub_key,
+        Decimal::from(10),
+        Decimal::ZERO,
+        account_with_su,
+        genesis_epoch,
+        CustomGenesis::default_consensus_manager_config()
+            .with_num_unstake_epochs(num_unstake_epochs),
+    );
+    let mut test_runner = TestRunnerBuilder::new()
+        .with_custom_genesis(genesis)
+        .build();
+    let validator_address = test_runner.get_active_validator_with_key(&validator_pub_key);
+    let validator_substate = test_runner.get_validator_info(validator_address);
+    let manifest = ManifestBuilder::new()
+        .lock_fee_from_faucet()
+        .withdraw_from_account(account_with_su, validator_substate.stake_unit_resource, 1)
+        .take_all_from_worktop(validator_substate.stake_unit_resource, "stake_units")
+        .unstake_validator(validator_address, "stake_units")
+        .try_deposit_entire_worktop_or_abort(account_with_su, None)
+        .build();
+    let receipt = test_runner.execute_manifest(
+        manifest,
+        vec![NonFungibleGlobalId::from_public_key(&account_pub_key)],
+    );
+    receipt.expect_commit_success();
+    test_runner.set_current_epoch(initial_epoch.after(1 + num_unstake_epochs).unwrap());
+
+    // Fake unstake receipt
+    let manifest = ManifestBuilder::new()
+        .lock_fee_from_faucet()
+        .create_ruid_non_fungible_resource(
+            OwnerRole::None,
+            false,
+            metadata!(),
+            NonFungibleResourceRoles::single_locked_rule(rule!(allow_all)),
+            Some(vec![UnstakeData {
+                name: "Fake".to_owned(),
+                claim_epoch: Epoch::of(1),
+                claim_amount: dec!(1),
+            }]),
+        )
+        .try_deposit_entire_worktop_or_abort(account_with_su, None)
+        .build();
+    let receipt = test_runner.execute_manifest(manifest, vec![]);
+    let fake = receipt.expect_commit(true).new_resource_addresses()[0];
+
+    // Act
+    let manifest = ManifestBuilder::new()
+        .lock_fee_from_faucet()
+        .withdraw_from_account(account_with_su, fake, 1)
+        .take_all_from_worktop(fake, "unstake_receipt")
+        .claim_xrd(validator_address, "unstake_receipt")
+        .try_deposit_entire_worktop_or_abort(account_with_su, None)
+        .build();
+    let receipt = test_runner.execute_manifest(
+        manifest,
+        vec![NonFungibleGlobalId::from_public_key(&account_pub_key)],
+    );
+
+    // Assert
+    receipt.expect_specific_failure(|e| {
+        matches!(
+            e,
+            RuntimeError::ApplicationError(ApplicationError::ValidatorError(
+                ValidatorError::InvalidClaimResource
+            ))
+        )
+    });
+    receipt.expect_commit_success();
 }
