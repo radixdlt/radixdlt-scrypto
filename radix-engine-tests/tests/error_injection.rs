0f34a2509c6efa9b26c057057a92fd68ac6bfe2b
Joshua Primero <josh.primero@rdx.works>
2023-09-15 16:57:11 -0500

    Add lock_fee_from_faucet_twice test

---
+++ radix-engine-tests/tests/error_injection.rs
@@ -0,0 +1,49 @@
+use radix_engine::vm::NoExtension;
+use scrypto_unit::{InjectSystemCostingError, TestRunnerBuilder};
+use transaction::builder::ManifestBuilder;
+
+#[test]
+fn lock_fee_from_faucet_error_injection() {
+    let mut test_runner = TestRunnerBuilder::new().build();
+
+    let mut inject_err_after_count = 1u64;
+
+    loop {
+        let manifest = ManifestBuilder::new()
+            .lock_fee_from_faucet()
+            .build();
+        let receipt = test_runner.execute_manifest_with_system::<_, InjectSystemCostingError<'_, NoExtension>>(
+            manifest, vec![], inject_err_after_count);
+        if receipt.is_commit_success() {
+            break;
+        }
+
+        inject_err_after_count += 1u64;
+    }
+
+    println!("Count: {:?}", inject_err_after_count);
+}
+
+#[test]
+fn lock_fee_from_faucet_twice_error_injection() {
+    let mut test_runner = TestRunnerBuilder::new().build();
+
+    let mut inject_err_after_count = 1u64;
+
+    // TODO: Use state from InjectSystemCostingError to tell when we haven't progressed
+    for _ in 0..600 {
+        let manifest = ManifestBuilder::new()
+            .lock_fee_from_faucet()
+            .lock_fee_from_faucet()
+            .build();
+        let receipt = test_runner.execute_manifest_with_system::<_, InjectSystemCostingError<'_, NoExtension>>(
+            manifest, vec![], inject_err_after_count);
+        if receipt.is_commit_success() {
+            break;
+        }
+
+        inject_err_after_count += 1u64;
+    }
+
+    println!("Count: {:?}", inject_err_after_count);
+}
