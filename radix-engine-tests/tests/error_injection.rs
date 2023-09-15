650e9c39fbfeaa630bdee8b938366b3f2018409d
Joshua Primero <josh.primero@rdx.works>
2023-09-15 17:08:02 -0500

    Add failing lock from faucet and account test

--- radix-engine-tests/tests/error_injection.rs
+++ radix-engine-tests/tests/error_injection.rs
@@ -1,4 +1,6 @@
 use radix_engine::vm::NoExtension;
+use radix_engine_common::dec;
+use radix_engine_interface::prelude::{FromPublicKey, NonFungibleGlobalId};
 use scrypto_unit::{InjectSystemCostingError, TestRunnerBuilder};
 use transaction::builder::ManifestBuilder;
 
@@ -38,6 +40,30 @@ fn lock_fee_from_faucet_twice_error_injection() {
             .build();
         let receipt = test_runner.execute_manifest_with_system::<_, InjectSystemCostingError<'_, NoExtension>>(
             manifest, vec![], inject_err_after_count);
+        if receipt.is_commit_success() {
+            break;
+        }
+
+        inject_err_after_count += 1u64;
+    }
+
+    println!("Count: {:?}", inject_err_after_count);
+}
+
+#[test]
+fn lock_fee_from_faucet_and_account_error_injection() {
+    let mut test_runner = TestRunnerBuilder::new().build();
+    let (pub_key, _, account) = test_runner.new_account(false);
+
+    let mut inject_err_after_count = 1u64;
+
+    loop {
+        let manifest = ManifestBuilder::new()
+            .lock_fee_from_faucet()
+            .lock_fee(account, dec!("500"))
+            .build();
+        let receipt = test_runner.execute_manifest_with_system::<_, InjectSystemCostingError<'_, NoExtension>>(
+            manifest, vec![NonFungibleGlobalId::from_public_key(&pub_key)], inject_err_after_count);
         if receipt.is_commit_success() {
             break;
         }
