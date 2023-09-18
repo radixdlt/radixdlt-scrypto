8fa5f0eb20b3b5c366ff3d26989f5879b21c04a9
Omar <OmarAbdulla7@hotmail.com>
2023-09-18 15:16:32 +0300

    Move `OverridePackageCode` to `mokey-tests` crate

--- monkey-tests/tests/fuzz_system.rs
+++ monkey-tests/tests/fuzz_system.rs
@@ -1,3 +1,4 @@
+use monkey_tests::OverridePackageCode;
 use native_sdk::modules::metadata::Metadata;
 use native_sdk::modules::role_assignment::RoleAssignment;
 use radix_engine::errors::RuntimeError;
@@ -5,7 +6,7 @@ use radix_engine::kernel::kernel_api::{KernelNodeApi, KernelSubstateApi};
 use radix_engine::system::system_callback::SystemLockData;
 use radix_engine::transaction::TransactionReceipt;
 use radix_engine::types::*;
-use radix_engine::vm::{OverridePackageCode, VmInvoke};
+use radix_engine::vm::VmInvoke;
 use radix_engine_interface::api::{AttachedModuleId, ClientApi, LockFlags, ACTOR_STATE_SELF};
 use radix_engine_interface::blueprints::package::PackageDefinition;
 use rand::Rng;
