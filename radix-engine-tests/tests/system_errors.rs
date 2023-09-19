d77ff100fa078c831f71e4a5bef40949d9bce642
Joshua Primero <josh.primero@rdx.works>
2023-09-19 13:47:40 -0500

    Add invalid address reservation test

--- radix-engine-tests/tests/system_errors.rs
+++ radix-engine-tests/tests/system_errors.rs
@@ -4,6 +4,7 @@ use native_sdk::modules::role_assignment::RoleAssignment;
 use radix_engine::errors::{RuntimeError, SystemError};
 use radix_engine::kernel::kernel_api::{KernelNodeApi, KernelSubstateApi};
 use radix_engine::system::system_callback::SystemLockData;
+use radix_engine::transaction::TransactionReceipt;
 use radix_engine::types::*;
 use radix_engine::vm::{OverridePackageCode, VmInvoke};
 use radix_engine_interface::api::{
@@ -28,8 +29,15 @@ impl VmInvoke for TestInvoke {
             Y: ClientApi<RuntimeError> + KernelNodeApi + KernelSubstateApi<SystemLockData>,
     {
         match export_name {
-            "test" => {
+            "invalid_state_handle" => {
                 api.actor_open_field(2u32, 0u8, LockFlags::read_only())?;
+            }
+            "invalid_ref_handle" => {
+                api.actor_get_node_id(9u32)?;
+            }
+            "invalid_address_reservation" => {
+                let self_node_id = api.actor_get_node_id(ACTOR_REF_SELF)?;
+                api.get_reservation_address(&self_node_id)?;
             }
             "new" => {
                 let metadata = Metadata::create(api)?;
@@ -51,8 +59,7 @@ impl VmInvoke for TestInvoke {
     }
 }
 
-#[test]
-fn invalid_actor_state_handle_should_error() {
+fn run<F: FnOnce(TransactionReceipt)>(method: &str, on_receipt: F) {
     // Arrange
     let mut test_runner = TestRunnerBuilder::new()
         .with_custom_extension(OverridePackageCode::new(CUSTOM_PACKAGE_CODE_ID, TestInvoke))
@@ -61,7 +68,12 @@ fn invalid_actor_state_handle_should_error() {
         CUSTOM_PACKAGE_CODE_ID,
         PackageDefinition::new_functions_only_test_definition(
             BLUEPRINT_NAME,
-            vec![("test", "test", true), ("new", "new", false)],
+            vec![
+                ("new", "new", false),
+                ("invalid_state_handle", "invalid_state_handle", true),
+                ("invalid_ref_handle", "invalid_ref_handle", true),
+                ("invalid_address_reservation", "invalid_address_reservation", true),
+            ],
         ),
     );
     let receipt = test_runner.execute_manifest(
@@ -77,11 +89,32 @@ fn invalid_actor_state_handle_should_error() {
     let receipt = test_runner.execute_manifest(
         ManifestBuilder::new()
             .lock_fee(test_runner.faucet_component(), 500u32)
-            .call_method(component_address, "test", manifest_args!())
+            .call_method(component_address, method, manifest_args!())
             .build(),
         vec![],
     );
 
     // Assert
-    receipt.expect_specific_failure(|e| matches!(e, RuntimeError::SystemError(SystemError::InvalidActorStateHandle)));
+    on_receipt(receipt);
+}
+
+#[test]
+fn invalid_actor_state_handle_should_error() {
+    run("invalid_state_handle", |receipt| {
+        receipt.expect_specific_failure(|e| matches!(e, RuntimeError::SystemError(SystemError::InvalidActorStateHandle)));
+    });
+}
+
+#[test]
+fn invalid_actor_ref_handle_should_error() {
+    run("invalid_ref_handle", |receipt| {
+        receipt.expect_specific_failure(|e| matches!(e, RuntimeError::SystemError(SystemError::InvalidActorRefHandle)));
+    });
+}
+
+#[test]
+fn invalid_address_reservation_should_error() {
+    run("invalid_address_reservation", |receipt| {
+        receipt.expect_specific_failure(|e| matches!(e, RuntimeError::SystemError(SystemError::NotAnAddressReservation)));
+    });
 }
