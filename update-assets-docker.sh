d245ff657ff2b473f3808e5cb7b8147c2037e8cb
Santiago Baldassin <santiago.baldassin@rdx.works>
2023-09-19 17:25:29 -0300

    adding image tag to run the script

--- update-assets-docker.sh
+++ update-assets-docker.sh
@@ -3,6 +3,7 @@ set -ex
 
 # Default values
 IMAGE_NAME="radixdlt/simulator"
+IMAGE_TAG="latest"
 REUSE_IMAGE=false
 
 # Parse command line arguments
@@ -11,6 +12,10 @@ while [[ $# -gt 0 ]]; do
     --reuse-image)
       REUSE_IMAGE=true
       ;;
+    --image-tag)
+      shift
+      IMAGE_TAG="$1"
+      ;;
     *)
       echo "Invalid argument: $1"
       exit 1
@@ -18,6 +23,8 @@ while [[ $# -gt 0 ]]; do
   esac
   shift
 done
+
+
 
 docker images -q "$IMAGE_NAME" 
 IMAGE_EXISTS=$(docker images -q "$IMAGE_NAME" 2>/dev/null)
@@ -33,6 +40,6 @@ fi
 for crate_name in "faucet" "radiswap" "flash_loan" "genesis_helper" "metadata" "test_environment" "global_n_owned" "kv_store"
 do
   echo "Building $crate_name..."
-  docker run --entrypoint=scrypto -v $PWD:/src $IMAGE_NAME build --path assets/blueprints/$crate_name
+  docker run --entrypoint=scrypto -v $PWD:/src $IMAGE_NAME:$IMAGE_TAG build --path assets/blueprints/$crate_name
   echo "Done $crate_name!"
 done
