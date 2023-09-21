10eb3d249976e65c450d2cbfe8ad990281709187
Joshua Primero <josh.primero@rdx.works>
2023-09-21 17:20:06 -0500

    Add more checks to Decimal fuzzer

--- fuzz-tests/src/bin/decimal.rs
+++ fuzz-tests/src/bin/decimal.rs
@@ -22,6 +22,7 @@ fuzz_target!(|decimal: OneDecimal| {
     let _ = decimal.0.checked_floor();
     let _ = decimal.0.checked_powi(decimal.2);
     let _ = decimal.0.checked_nth_root(decimal.3);
+    let _ = decimal.0.checked_neg();
     if decimal.4 >= 0 && decimal.4 <= Decimal::SCALE as i32 {
         let _ = decimal.0.checked_round(decimal.4, decimal.5);
     }
@@ -29,5 +30,7 @@ fuzz_target!(|decimal: OneDecimal| {
     let _ = decimal.0.checked_sub(decimal.1);
     let _ = decimal.0.checked_mul(decimal.1);
     let _ = decimal.0.checked_div(decimal.1);
-    let _ = decimal.0.to_string();
+
+    let string = decimal.0.to_string();
+    assert_eq!(Decimal::try_from(string).unwrap(), decimal.0);
 });
