65b118fa81169616aa9cec994e426d2cf8050f78
Lukasz Rubaszewski <lukasz.rubaszewski@rdx.works>
2023-09-27 14:13:26 +0200

    Get coverage for all available fuzz targets if no target specified

--- fuzz-tests/get-coverage-data.sh
+++ fuzz-tests/get-coverage-data.sh
@@ -1,42 +1,64 @@
 #!/bin/bash
 
-set -x
+#set -x
 set -e
-set -u
+set -o pipefail
 
-target=$1
+targets="$1"
+if [ $# -ge 1 ] ; then
+    targets=$1
+    shift
+else
+    targets=$(./list-fuzz-targets.sh)
+fi
 
 get_libfuzzer_corpus_files() {
-    local corpus_dir=corpus/${target}
-    find $corpus_dir -type f
+    local target=$1
+    local corpus_dir="corpus/${target}"
+
+    if [ -d $corpus_dir ] ; then
+        find $corpus_dir -type f
+    fi
 }
 
 get_afl_corpus_files() {
-    local corpus_dir=afl/${target}
-    find $corpus_dir -maxdepth 3 -mindepth 3 -path "*/queue/*" -type f
+    local target=$1
+    local corpus_dir="afl/${target}"
+
+    if [ -d $corpus_dir ] ; then
+        find $corpus_dir -maxdepth 3 -mindepth 3 -path "*/queue/*" -type f
+    fi
 }
 
-run_fuzzer_in_parallel() {
+process_corpus_files() {
+    local target=$1
     if which parallel && parallel --version | grep -q 'GNU parallel' ; then
         # parallel is nicer because is serializes output from commands in parallel.
         # "halt now,fail=1" - exit when any job has failed. Kill other running jobs
         parallel --halt now,fail=1 -- \
-            ./fuzz.sh simple run $target "{}"
+            './fuzz.sh simple run $target "{}" || true' # true to not exit upon error
     else
         xargs -P 8 -I {} \
-            ./fuzz.sh simple run $target "{}"
+            sh -c './fuzz.sh simple run $target "{} || true' # true to not exit upon error
     fi
 }
 
 export CARGO_INCREMENTAL=0
 export RUSTFLAGS='-Cinstrument-coverage'
-export LLVM_PROFILE_FILE="fuzz-${target}-%p-%m.profraw"
 
-./fuzz.sh simple build $target
+for t in $targets ; do
+    export LLVM_PROFILE_FILE="fuzz-${t}-%p-%m.profraw"
+    list=corpus_files_$t.lst
+    rm -f $list
+    get_afl_corpus_files $t > $list
+    get_libfuzzer_corpus_files $t >> $list
 
-set +e
-# Run simple_fuzzer for each corpus file
-get_afl_corpus_files | run_fuzzer_in_parallel
-get_libfuzzer_corpus_files | run_fuzzer_in_parallel
-
+    if [ -s $list ] ; then
+        echo "Getting code coverage data for target $t"
+        ./fuzz.sh simple build $t
+        cat $list | process_corpus_files $t
+    else
+        echo "Skipping target $t - no corpus files"
+    fi
+done
 
