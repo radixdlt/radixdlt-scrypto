76c28f2723a5738c85074103dcb415a4d6d5ac4e
Lukasz Rubaszewski <lukasz.rubaszewski@rdx.works>
2023-11-29 15:13:11 +0100

    Add BLS signature sign/verify support

---
+++ transaction/src/signing/bls/signature.rs
@@ -0,0 +1,82 @@
+use sbor::rust::borrow::ToOwned;
+use sbor::rust::fmt;
+use sbor::rust::str::FromStr;
+use sbor::rust::string::String;
+use sbor::rust::vec::Vec;
+use sbor::*;
+use utils::copy_u8_array;
+
+pub const BLS_SCHEME: &[u8] = b"BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_NUL_";
+
+/// Represents a BLS signature (variant with 96-byte signature and 48-byte public key)
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Sbor)]
+pub struct BlsSignature(
+    #[cfg_attr(feature = "serde", serde(with = "hex::serde"))] pub [u8; Self::LENGTH],
+);
+
+impl BlsSignature {
+    pub const LENGTH: usize = 96;
+
+    pub fn to_vec(&self) -> Vec<u8> {
+        self.0.to_vec()
+    }
+}
+
+impl TryFrom<&[u8]> for BlsSignature {
+    type Error = ParseBlsSignatureError;
+
+    fn try_from(slice: &[u8]) -> Result<Self, Self::Error> {
+        if slice.len() != BlsSignature::LENGTH {
+            return Err(ParseBlsSignatureError::InvalidLength(slice.len()));
+        }
+
+        Ok(BlsSignature(copy_u8_array(slice)))
+    }
+}
+
+//======
+// error
+//======
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum ParseBlsSignatureError {
+    InvalidHex(String),
+    InvalidLength(usize),
+}
+
+/// Represents an error when parsing ED25519 signature from hex.
+#[cfg(not(feature = "alloc"))]
+impl std::error::Error for ParseBlsSignatureError {}
+
+#[cfg(not(feature = "alloc"))]
+impl fmt::Display for ParseBlsSignatureError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+//======
+// text
+//======
+
+impl FromStr for BlsSignature {
+    type Err = ParseBlsSignatureError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let bytes = hex::decode(s).map_err(|_| ParseBlsSignatureError::InvalidHex(s.to_owned()))?;
+        Self::try_from(bytes.as_slice())
+    }
+}
+
+impl fmt::Display for BlsSignature {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        write!(f, "{}", hex::encode(self.to_vec()))
+    }
+}
+
+impl fmt::Debug for BlsSignature {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        write!(f, "{}", self)
+    }
+}
