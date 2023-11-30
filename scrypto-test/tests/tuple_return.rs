f080fae775b2a9102d352c1aa75030406764e3df
Michał Strug <michal.strug@rdx.works>
2023-11-30 17:46:52 +0100

    Fixed formatting

--- scrypto-test/tests/tuple_return.rs
+++ scrypto-test/tests/tuple_return.rs
@@ -1,13 +1,16 @@
-use tuple_return::test_bindings::*;
 use scrypto::*;
 use scrypto_test::prelude::*;
+use tuple_return::test_bindings::*;
 
 #[test]
 fn tuple_returns_work_with_scrypto_test() {
     // Arrange
     let mut env = TestEnvironment::new();
-    let package_address =
-        Package::compile_and_publish(concat!(env!("CARGO_MANIFEST_DIR"), "/tests/blueprints/tuple-return"), &mut env).unwrap();
+    let package_address = Package::compile_and_publish(
+        concat!(env!("CARGO_MANIFEST_DIR"), "/tests/blueprints/tuple-return"),
+        &mut env,
+    )
+    .unwrap();
 
     // Act
     let rtn = TupleReturn::instantiate(package_address, &mut env);
@@ -15,4 +18,3 @@ fn tuple_returns_work_with_scrypto_test() {
     // Assert
     assert!(rtn.is_ok())
 }
-
