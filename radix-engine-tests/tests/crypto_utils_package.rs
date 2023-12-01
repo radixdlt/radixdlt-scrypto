a8dfbdad78c5a3868ee6771bdc154d0d1534cb2f
Lukasz Rubaszewski <lukasz.rubaszewski@rdx.works>
2023-12-01 14:42:09 +0100

    Add more CryptoUtils package tests

--- radix-engine-tests/tests/crypto_utils_package.rs
+++ radix-engine-tests/tests/crypto_utils_package.rs
@@ -12,11 +12,11 @@ use transaction::builder::ManifestBuilder;
 #[cfg(test)]
 fn crypto_utils_bls_verify(
     runner: &mut TestRunner<NoExtension, InMemorySubstateDatabase>,
-    msg: &str,
+    msg_hash: &str,
     pk: &str,
     sig: &str,
 ) -> bool {
-    let msg_hash = hash(msg);
+    let msg_hash = Hash::from_str(msg_hash).unwrap();
     let pub_key = BlsPublicKey::from_str(pk).unwrap();
     let signature = BlsSignature::from_str(sig).unwrap();
 
@@ -74,14 +74,14 @@ fn test_crypto_package_bls_verify() {
         CRYPTO_UTILS_PACKAGE,
     );
 
-    let msg1 = "Test";
-    let msg2 = "ExpectFailureTest";
+    let msg1 = hash("Test").to_string();
+    let msg2 = hash("ExpectFailureTest").to_string();
     let pk = "93b1aa7542a5423e21d8e84b4472c31664412cc604a666e9fdf03baf3c758e728c7a11576ebb01110ac39a0df95636e2";
     let msg1_signature = "a2ba96a1fc1e698b7688e077f171fbd7fe99c6bbf240b1421a08e3faa5d6b55523a18b8c77fba5830181dfec716edc3d18a8657bcadd0a83e3cafdad33998d10417f767c536b26b98df41d67ab416c761ad55438f23132a136fc82eb7b290571";
 
     // Act
-    let msg1_verify = crypto_utils_bls_verify(&mut test_runner, msg1, pk, msg1_signature);
-    let msg2_verify = crypto_utils_bls_verify(&mut test_runner, msg2, pk, msg1_signature);
+    let msg1_verify = crypto_utils_bls_verify(&mut test_runner, &msg1, pk, msg1_signature);
+    let msg2_verify = crypto_utils_bls_verify(&mut test_runner, &msg2, pk, msg1_signature);
 
     // Assert
     assert!(msg1_verify);
@@ -119,6 +119,34 @@ fn test_crypto_package_keccak_hash() {
 }
 
 #[cfg(test)]
+fn crypto_scrypto_bls_verify(
+    runner: &mut TestRunner<NoExtension, InMemorySubstateDatabase>,
+    package_address: PackageAddress,
+    msg_hash: &str,
+    pk: &str,
+    sig: &str,
+) -> bool {
+    let msg_hash = Hash::from_str(msg_hash).unwrap();
+    let pub_key = BlsPublicKey::from_str(pk).unwrap();
+    let signature = BlsSignature::from_str(sig).unwrap();
+
+    let receipt = runner.execute_manifest(
+        ManifestBuilder::new()
+            .lock_fee(runner.faucet_component(), 500u32)
+            .call_function(
+                package_address,
+                "CryptoScrypto",
+                "bls_verify",
+                manifest_args!(msg_hash, pub_key, signature),
+            )
+            .build(),
+        vec![],
+    );
+    let result = receipt.expect_commit_success();
+    result.output(1)
+}
+
+#[cfg(test)]
 fn crypto_scrypto_keccak_hash(
     runner: &mut TestRunner<NoExtension, InMemorySubstateDatabase>,
     package_address: PackageAddress,
@@ -140,6 +168,36 @@ fn crypto_scrypto_keccak_hash(
     );
     let result = receipt.expect_commit_success();
     result.output(1)
+}
+
+#[test]
+fn test_crypto_scrypto_bls_verify() {
+    // Arrange
+    let mut test_runner = TestRunnerBuilder::new().build();
+
+    // Publish CryptoUtils package, it will be called by below CryptoScrypto package
+    test_runner.publish_native_package_at_address(
+        CRYPTO_UTILS_CODE_ID,
+        CryptoUtilsNativePackage::definition(),
+        CRYPTO_UTILS_PACKAGE,
+    );
+
+    let package_address = test_runner.publish_package_simple(PackageLoader::get("crypto_scrypto"));
+
+    let msg1 = hash("Test").to_string();
+    let msg2 = hash("ExpectFailureTest").to_string();
+    let pk = "93b1aa7542a5423e21d8e84b4472c31664412cc604a666e9fdf03baf3c758e728c7a11576ebb01110ac39a0df95636e2";
+    let msg1_signature = "a2ba96a1fc1e698b7688e077f171fbd7fe99c6bbf240b1421a08e3faa5d6b55523a18b8c77fba5830181dfec716edc3d18a8657bcadd0a83e3cafdad33998d10417f767c536b26b98df41d67ab416c761ad55438f23132a136fc82eb7b290571";
+
+    // Act
+    let msg1_verify =
+        crypto_scrypto_bls_verify(&mut test_runner, package_address, &msg1, pk, msg1_signature);
+    let msg2_verify =
+        crypto_scrypto_bls_verify(&mut test_runner, package_address, &msg2, pk, msg1_signature);
+
+    // Assert
+    assert!(msg1_verify);
+    assert!(!msg2_verify);
 }
 
 #[test]
@@ -172,4 +230,43 @@ fn test_crypto_scrypto_keccak_hash() {
         data2_hash,
         Hash::from_str("48f1bd08444b5e713db9e14caac2faae71836786ac94d645b00679728202a935").unwrap()
     );
+}
+
+#[test]
+fn test_crypto_scrypto_flow() {
+    // Arrange
+    let mut test_runner = TestRunnerBuilder::new().build();
+
+    // Publish CryptoUtils package, it will be called by below CryptoScrypto package
+    test_runner.publish_native_package_at_address(
+        CRYPTO_UTILS_CODE_ID,
+        CryptoUtilsNativePackage::definition(),
+        CRYPTO_UTILS_PACKAGE,
+    );
+
+    let package_address = test_runner.publish_package_simple(PackageLoader::get("crypto_scrypto"));
+
+    let msg = "Important message";
+
+    // Act
+    // Get the hash of the message using CryptoScrypto package
+    let msg_hash = crypto_scrypto_keccak_hash(&mut test_runner, package_address, msg);
+
+    let secret_key = BlsPrivateKey::from_u64(1).unwrap();
+    let public_key = secret_key.public_key();
+
+    // Sign the message hash using BLS
+    let msg_signature = secret_key.sign(&msg_hash);
+
+    // Verify the BLS signature using CryptoScrypto package
+    let result = crypto_scrypto_bls_verify(
+        &mut test_runner,
+        package_address,
+        &msg_hash.to_string(),
+        &public_key.to_string(),
+        &msg_signature.to_string(),
+    );
+
+    // Assert
+    assert!(result);
 }
