6f00e8a35c2e01d146934e33d2f4fe8642b127c4
Lukasz Rubaszewski <lukasz.rubaszewski@rdx.works>
2023-12-04 17:55:42 +0100

    Fix compilation issues in radix-engine-tests

--- radix-engine-tests/benches/transaction_validation.rs
+++ radix-engine-tests/benches/transaction_validation.rs
@@ -2,6 +2,7 @@ use std::hint::black_box;
 
 use criterion::{criterion_group, criterion_main, Criterion};
 use radix_engine::types::*;
+use radix_engine_common::crypto::{recover_secp256k1, verify_secp256k1};
 use transaction::prelude::*;
 use transaction::validation::*;
 
