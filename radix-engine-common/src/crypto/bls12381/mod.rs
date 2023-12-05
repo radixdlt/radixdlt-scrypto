0262df0329831b72d00ce952f9e1d9721e8bd194
Lukasz Rubaszewski <lukasz.rubaszewski@rdx.works>
2023-12-05 12:55:39 +0100

    Get rid of crypto-related dependencies for WASM target
    ATM it is not required in WASM in Scrypto.
    Partial support (BLS verification, Keccak hash) is implemented natively via
    Client API.

--- radix-engine-common/src/crypto/bls12381/mod.rs
+++ radix-engine-common/src/crypto/bls12381/mod.rs
@@ -1,7 +1,9 @@
+#[cfg(not(target_arch = "wasm32"))]
 mod private_key;
 mod public_key;
 mod signature;
 
+#[cfg(not(target_arch = "wasm32"))]
 pub use private_key::*;
 pub use public_key::*;
 pub use signature::*;
