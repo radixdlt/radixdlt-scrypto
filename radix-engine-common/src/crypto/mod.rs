26852a44c01e3fbd3af2ff75159fce10f30d7ec2
Lukasz Rubaszewski <lukasz.rubaszewski@rdx.works>
2023-12-06 12:44:59 +0100

    Moved some general abstraction objects back to 'transaction'
    Included:
    - PrivateKey
    - SignatureV1
    - SignatureWithPublicKeyV1

--- radix-engine-common/src/crypto/mod.rs
+++ radix-engine-common/src/crypto/mod.rs
@@ -5,17 +5,11 @@ mod hash;
 mod hash_accumulator;
 #[cfg(not(target_arch = "wasm32"))]
 mod keccak256;
-#[cfg(not(target_arch = "wasm32"))]
-mod private_key;
 mod public_key;
 mod public_key_hash;
 mod secp256k1;
-mod signature;
 #[cfg(not(target_arch = "wasm32"))]
 mod signature_validator;
-#[cfg(not(target_arch = "wasm32"))]
-mod signer;
-
 pub use self::blake2b::*;
 pub use self::bls12381::*;
 pub use self::ed25519::*;
@@ -23,13 +17,8 @@ pub use self::hash::*;
 pub use self::hash_accumulator::*;
 #[cfg(not(target_arch = "wasm32"))]
 pub use self::keccak256::*;
-#[cfg(not(target_arch = "wasm32"))]
-pub use self::private_key::*;
 pub use self::public_key::*;
 pub use self::public_key_hash::*;
 pub use self::secp256k1::*;
-pub use self::signature::*;
 #[cfg(not(target_arch = "wasm32"))]
 pub use self::signature_validator::*;
-#[cfg(not(target_arch = "wasm32"))]
-pub use self::signer::*;
--- radix-engine-common/src/crypto/signature_validator.rs
+++ radix-engine-common/src/crypto/signature_validator.rs
@@ -1,14 +1,5 @@
 use crate::internal_prelude::*;
 
-pub fn recover(signed_hash: &Hash, signature: &SignatureWithPublicKeyV1) -> Option<PublicKey> {
-    match signature {
-        SignatureWithPublicKeyV1::Secp256k1 { signature } => {
-            recover_secp256k1(signed_hash, signature).map(Into::into)
-        }
-        SignatureWithPublicKeyV1::Ed25519 { public_key, .. } => Some(public_key.clone().into()),
-    }
-}
-
 pub fn recover_secp256k1(
     signed_hash: &Hash,
     signature: &Secp256k1Signature,
@@ -27,18 +18,6 @@ pub fn recover_secp256k1(
         }
     }
     None
-}
-
-pub fn verify(signed_hash: &Hash, public_key: &PublicKey, signature: &SignatureV1) -> bool {
-    match (public_key, signature) {
-        (PublicKey::Secp256k1(pk), SignatureV1::Secp256k1(sig)) => {
-            verify_secp256k1(&signed_hash, pk, sig)
-        }
-        (PublicKey::Ed25519(pk), SignatureV1::Ed25519(sig)) => {
-            verify_ed25519(&signed_hash, pk, sig)
-        }
-        _ => false,
-    }
 }
 
 pub fn verify_secp256k1(
