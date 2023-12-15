19e343a890775e4c8f922430661abd1376a0c45c
Lukasz Rubaszewski <lukasz.rubaszewski@rdx.works>
2023-12-15 13:27:36 +0100

    Add more Keccak256 and BLS12381 tests for costing

--- radix-engine-tests/tests/system/crypto_utils.rs
+++ radix-engine-tests/tests/system/crypto_utils.rs
@@ -146,3 +146,119 @@ fn test_crypto_scrypto_flow() {
     // Assert
     assert!(result);
 }
+
+#[test]
+fn test_crypto_keccak256_100b() {
+    let data = "m".repeat(100);
+    let _ = keccak256_hash(data);
+}
+#[test]
+fn test_crypto_keccak256_1kb() {
+    let data = "m".repeat(1024);
+    let _ = keccak256_hash(data);
+}
+#[test]
+fn test_keccak256_100kb() {
+    let data = "m".repeat(100 * 1024);
+    let _ = keccak256_hash(data);
+}
+#[test]
+fn test_crypto_keccak256_1mb() {
+    let data = "m".repeat(1024 * 1024);
+    let _ = keccak256_hash(data);
+}
+
+#[test]
+fn test_crypto_sign_and_verify_100b() {
+    let test_sk = "408157791befddd702672dcfcfc99da3512f9c0ea818890fcb6ab749580ef2cf";
+    let test_message = vec![0; 100];
+    let sk = Bls12381G1PrivateKey::from_bytes(&hex::decode(test_sk).unwrap()).unwrap();
+    let pk = sk.public_key();
+
+    let sig = sk.sign_v1(&test_message);
+    let _ = verify_bls12381_v1(&test_message, &pk, &sig);
+}
+#[test]
+fn test_crypto_sign_and_verify_1kb() {
+    let test_sk = "408157791befddd702672dcfcfc99da3512f9c0ea818890fcb6ab749580ef2cf";
+    let test_message = vec![0; 1024];
+    let sk = Bls12381G1PrivateKey::from_bytes(&hex::decode(test_sk).unwrap()).unwrap();
+    let pk = sk.public_key();
+
+    let sig = sk.sign_v1(&test_message);
+    let _ = verify_bls12381_v1(&test_message, &pk, &sig);
+}
+#[test]
+fn test_crypto_sign_and_verify_100kb() {
+    let test_sk = "408157791befddd702672dcfcfc99da3512f9c0ea818890fcb6ab749580ef2cf";
+    let test_message = vec![0; 100 * 1024];
+    let sk = Bls12381G1PrivateKey::from_bytes(&hex::decode(test_sk).unwrap()).unwrap();
+    let pk = sk.public_key();
+
+    let sig = sk.sign_v1(&test_message);
+    let _ = verify_bls12381_v1(&test_message, &pk, &sig);
+}
+#[test]
+fn test_crypto_sign_and_verify_1mb() {
+    let test_sk = "408157791befddd702672dcfcfc99da3512f9c0ea818890fcb6ab749580ef2cf";
+    let test_message = vec![0; 1024 * 1024];
+    let sk = Bls12381G1PrivateKey::from_bytes(&hex::decode(test_sk).unwrap()).unwrap();
+    let pk = sk.public_key();
+
+    let sig = sk.sign_v1(&test_message);
+    let _ = verify_bls12381_v1(&test_message, &pk, &sig);
+}
+
+#[test]
+fn test_crypto_scrypto_keccak256_costing() {
+    // Arrange
+    let mut test_runner = TestRunnerBuilder::new().build();
+
+    let package_address = test_runner.publish_package_simple(PackageLoader::get("crypto_scrypto"));
+
+    for size in [100usize, 1024, 100 * 1024, 900 * 1024] {
+        let data = vec![0u8; size];
+        let receipt = test_runner.execute_manifest(
+            ManifestBuilder::new()
+                .lock_fee(test_runner.faucet_component(), 500u32)
+                .call_function(
+                    package_address,
+                    "CryptoScrypto",
+                    "keccak256_hash",
+                    manifest_args!(data),
+                )
+                .build(),
+            vec![],
+        );
+        let _ = receipt.expect_commit_success();
+    }
+}
+
+#[test]
+fn test_crypto_scrypto_verify_bls12381_v1_costing() {
+    // Arrange
+    let mut test_runner = TestRunnerBuilder::new().build();
+
+    let package_address = test_runner.publish_package_simple(PackageLoader::get("crypto_scrypto"));
+
+    let secret_key = Bls12381G1PrivateKey::from_u64(1).unwrap();
+    let public_key = secret_key.public_key();
+
+    for size in [100usize, 1024, 100 * 1024, 900 * 1024] {
+        let data = vec![0u8; size];
+        let signature = secret_key.sign_v1(data.as_slice());
+        let receipt = test_runner.execute_manifest(
+            ManifestBuilder::new()
+                .lock_fee(test_runner.faucet_component(), 500u32)
+                .call_function(
+                    package_address,
+                    "CryptoScrypto",
+                    "bls12381_v1_verify",
+                    manifest_args!(data, public_key, signature),
+                )
+                .build(),
+            vec![],
+        );
+        let _ = receipt.expect_commit_success();
+    }
+}
