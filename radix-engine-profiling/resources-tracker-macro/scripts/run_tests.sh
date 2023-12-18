30edef1c31f35238171a3153ef26c4ea49caf3f0
Michał Strug <114988195+mstrug-rdx@users.noreply.github.com>
2023-12-18 16:45:10 +0100

    Disabled cpu parameter in qemu call due to some incompatibility issues.

--- radix-engine-profiling/resources-tracker-macro/scripts/run_tests.sh
+++ radix-engine-profiling/resources-tracker-macro/scripts/run_tests.sh
@@ -4,7 +4,7 @@
 
 qemu_app=/home/ubuntu/qemu/qemu-x86_64
 qemu_plugin=/home/ubuntu/qemu/libscrypto-qemu-plugin.so
-qemu_cpu="Westmere-v1"
+#qemu_cpu="Westmere-v1" # disabled due to some issue with missing CPU instructions in BLS implementation
 
 if [ -z "$*" ]; then echo "Provide path to folder with binaries to execute."; exit; fi
 
@@ -18,7 +18,7 @@ for i in $list; do [ -x $i ] && list_count=$((list_count+1)); done
 for i in $list; do
     if [ -x $i ]; then
         echo "Running $idx/$list_count: $i"
-        $qemu_app -cpu $qemu_cpu -plugin $qemu_plugin -d plugin -D log.txt $i --show-output --test --test-threads 1 > out.txt
+        $qemu_app -plugin $qemu_plugin -d plugin -D log.txt $i --show-output --test --test-threads 1 > out.txt
         idx=$((idx+1))
     fi
 done
