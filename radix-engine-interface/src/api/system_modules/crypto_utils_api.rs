453131e439cc1de460fa23d968e963d1402a7ea4
Lukasz Rubaszewski <lukasz.rubaszewski@rdx.works>
2023-12-20 15:26:08 +0100

    Add bls12381_g2_signature_aggregate() to CryptoUtils API

--- radix-engine-interface/src/api/system_modules/crypto_utils_api.rs
+++ radix-engine-interface/src/api/system_modules/crypto_utils_api.rs
@@ -9,5 +9,10 @@ pub trait ClientCryptoUtilsApi<E> {
         signature: Bls12381G2Signature,
     ) -> Result<u32, E>;
 
+    fn bls12381_g2_signature_aggregate(
+        &mut self,
+        signatures: Vec<Bls12381G2Signature>,
+    ) -> Result<Bls12381G2Signature, E>;
+
     fn keccak256_hash(&mut self, data: Vec<u8>) -> Result<Hash, E>;
 }
