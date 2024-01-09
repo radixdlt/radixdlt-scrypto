066448d99f80eecbcfbb1636af63d84a275b3eb7
Lukasz Rubaszewski <lukasz.rubaszewski@rdx.works>
2024-01-09 13:37:47 +0100

    Enable crypto features for wasm32 target as well

--- radix-engine-common/src/crypto/ed25519/mod.rs
+++ radix-engine-common/src/crypto/ed25519/mod.rs
@@ -1,9 +1,7 @@
-#[cfg(not(target_arch = "wasm32"))]
 mod private_key;
 mod public_key;
 mod signature;
 
-#[cfg(not(target_arch = "wasm32"))]
 pub use private_key::*;
 pub use public_key::*;
 pub use signature::*;
--- radix-engine-common/src/crypto/mod.rs
+++ radix-engine-common/src/crypto/mod.rs
@@ -3,22 +3,18 @@ mod bls12381;
 mod ed25519;
 mod hash;
 mod hash_accumulator;
-#[cfg(not(target_arch = "wasm32"))]
 mod keccak256;
 mod public_key;
 mod public_key_hash;
 mod secp256k1;
-#[cfg(not(target_arch = "wasm32"))]
 mod signature_validator;
 pub use self::blake2b::*;
 pub use self::bls12381::*;
 pub use self::ed25519::*;
 pub use self::hash::*;
 pub use self::hash_accumulator::*;
-#[cfg(not(target_arch = "wasm32"))]
 pub use self::keccak256::*;
 pub use self::public_key::*;
 pub use self::public_key_hash::*;
 pub use self::secp256k1::*;
-#[cfg(not(target_arch = "wasm32"))]
 pub use self::signature_validator::*;
