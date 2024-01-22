c40056e9cdd0751c00e0e2e389d9f9f6ef042022
Omar <OmarAbdulla7@hotmail.com>
2024-01-22 14:52:35 +0300

    Remove logs clone in logs printing in scrypto-test.

--- scrypto-test/src/environment/client_api.rs
+++ scrypto-test/src/environment/client_api.rs
@@ -83,7 +83,7 @@ macro_rules! implement_client_api {
                                     .system
                                     .modules
                                     .transaction_runtime()
-                                    .map(|module| module.logs.clone())
+                                    .map(|module| module.logs.iter())
                                     .unwrap_or_default()
                                     .into_iter()
                                     .skip(logs_before)
@@ -316,10 +316,30 @@ implement_client_api! {
         fee_balance: (&mut self) -> Result<Decimal, RuntimeError>,
     },
     ClientCryptoUtilsApi: {
-        bls12381_v1_verify: (&mut self, message: &[u8], public_key: &Bls12381G1PublicKey, signature: &Bls12381G2Signature) -> Result<u32, RuntimeError>,
-        bls12381_v1_aggregate_verify: (&mut self, pub_keys_and_msgs: &[(Bls12381G1PublicKey, Vec<u8>)], signature: &Bls12381G2Signature) -> Result<u32, RuntimeError>,
-        bls12381_v1_fast_aggregate_verify: (&mut self, message: &[u8], public_keys: &[Bls12381G1PublicKey], signature: &Bls12381G2Signature) -> Result<u32, RuntimeError>,
-        bls12381_g2_signature_aggregate: (&mut self, signatures: &[Bls12381G2Signature]) -> Result<Bls12381G2Signature, RuntimeError>,
-        keccak256_hash: (&mut self, data: &[u8]) -> Result<Hash, RuntimeError>,
+        bls12381_v1_verify: (
+            &mut self,
+            message: &[u8],
+            public_key: &Bls12381G1PublicKey,
+            signature: &Bls12381G2Signature
+        ) -> Result<u32, RuntimeError>,
+        bls12381_v1_aggregate_verify: (
+            &mut self,
+            pub_keys_and_msgs: &[(Bls12381G1PublicKey, Vec<u8>)],
+            signature: &Bls12381G2Signature
+        ) -> Result<u32, RuntimeError>,
+        bls12381_v1_fast_aggregate_verify: (
+            &mut self,
+            message: &[u8],
+            public_keys: &[Bls12381G1PublicKey],
+            signature: &Bls12381G2Signature
+        ) -> Result<u32, RuntimeError>,
+        bls12381_g2_signature_aggregate: (
+            &mut self,
+            signatures: &[Bls12381G2Signature]
+        ) -> Result<Bls12381G2Signature, RuntimeError>,
+        keccak256_hash: (
+            &mut self,
+            data: &[u8]
+        ) -> Result<Hash, RuntimeError>,
     },
 }
