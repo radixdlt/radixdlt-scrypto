d0e401b5881e8914585f927e07fd80cf7f243ac1
Kim Fehrs <kim.fehrs@rdx.works>
2024-02-06 14:23:44 +0100

    add debugging for my tests

--- simulator/tests/scrypto.sh
+++ simulator/tests/scrypto.sh
@@ -20,17 +20,36 @@ $scrypto test --path $test_pkg
 $scrypto test --path $test_pkg -- test_hello --nocapture
 $scrypto test --path $test_pkg -- --nocapture
 
+env
+echo "$(whoami)"
+echo "$(cut --version)"
 # Logging
 $scrypto build --path ../examples/everything --log-level ERROR
 size1=$(ls -la ../examples/everything/target/wasm32-unknown-unknown/release/everything.wasm | cut -d ' ' -f 5)
+echo "size1"
+ls -la ../examples/everything/target/wasm32-unknown-unknown/release/everything.wasm
+ls -la ../examples/everything/target/wasm32-unknown-unknown/release/everything.wasm | cut -d ' ' -f 5
+
 $scrypto build --path ../examples/everything --log-level WARN
 size2=$(ls -la ../examples/everything/target/wasm32-unknown-unknown/release/everything.wasm | cut -d ' ' -f 5)
+echo "size2"
+ls -la ../examples/everything/target/wasm32-unknown-unknown/release/everything.wasm
+ls -la ../examples/everything/target/wasm32-unknown-unknown/release/everything.wasm | cut -d ' ' -f 5
 $scrypto build --path ../examples/everything --log-level INFO
 size3=$(ls -la ../examples/everything/target/wasm32-unknown-unknown/release/everything.wasm | cut -d ' ' -f 5)
+echo "size3"
+ls -la ../examples/everything/target/wasm32-unknown-unknown/release/everything.wasm
+ls -la ../examples/everything/target/wasm32-unknown-unknown/release/everything.wasm | cut -d ' ' -f 5
 $scrypto build --path ../examples/everything --log-level DEBUG
 size4=$(ls -la ../examples/everything/target/wasm32-unknown-unknown/release/everything.wasm | cut -d ' ' -f 5)
+echo "size4"
+ls -la ../examples/everything/target/wasm32-unknown-unknown/release/everything.wasm
+ls -la ../examples/everything/target/wasm32-unknown-unknown/release/everything.wasm | cut -d ' ' -f 5
 $scrypto build --path ../examples/everything --log-level TRACE
 size5=$(ls -la ../examples/everything/target/wasm32-unknown-unknown/release/everything.wasm | cut -d ' ' -f 5)
+echo "size5"
+ls -la ../examples/everything/target/wasm32-unknown-unknown/release/everything.wasm
+ls -la ../examples/everything/target/wasm32-unknown-unknown/release/everything.wasm | cut -d ' ' -f 5
 
 if [ $size1 -lt $size2 ] && [ $size2 -lt $size3 ] && [ $size3 -lt $size4 ] && [ $size4 -lt $size5 ] ; then
   echo "Size check is okay"
