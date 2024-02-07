c96a2c5428300b52c967d95d67d13514bd3229d1
Lukasz Rubaszewski <lukasz.rubaszewski@rdx.works>
2024-02-07 14:00:23 +0100

    CryptoUtils flash tests using TestEnvironment

--- radix-engine-tests/tests/flash/crypto_utils.rs
+++ radix-engine-tests/tests/flash/crypto_utils.rs
@@ -1,12 +1,14 @@
+use radix_engine::blueprints::package::PackageError;
+use radix_engine::errors::ApplicationError;
 use radix_engine::errors::RuntimeError;
+use radix_engine::types::*;
+use radix_engine::utils::generate_vm_boot_scrypto_minor_version_state_updates;
 use radix_engine_store_interface::db_key_mapper::SpreadPrefixKeyMapper;
 use radix_engine_store_interface::interface::CommittableSubstateDatabase;
 use radix_engine_tests::common::PackageLoader;
+use radix_engine_tests::common::*;
+use scrypto_test::prelude::*;
 use scrypto_test::prelude::{CustomGenesis, TestRunnerBuilder};
-use radix_engine::utils::generate_vm_boot_scrypto_minor_version_state_updates;
-use radix_engine::types::*;
-use radix_engine::errors::ApplicationError;
-use radix_engine::blueprints::package::PackageError;
 
 #[test]
 fn publishing_crypto_utils_without_state_flash_should_fail() {
@@ -43,8 +45,49 @@ fn run_flash_test(flash_substates: bool, expect_success: bool) {
         receipt.expect_specific_failure(|e| {
             matches!(
                 e,
-                RuntimeError::ApplicationError(ApplicationError::PackageError(PackageError::InvalidWasm(..)))
+                RuntimeError::ApplicationError(ApplicationError::PackageError(
+                    PackageError::InvalidWasm(..)
+                ))
             )
         });
+    }
+}
+
+#[test]
+fn publishing_crypto_utils_using_test_environment_without_state_flash_should_fail() {
+    run_flash_test_test_environment(false, false);
+}
+
+#[test]
+fn publishing_crypto_utils_using_test_environment_with_state_flash_should_succeed() {
+    run_flash_test_test_environment(true, true);
+}
+
+fn run_flash_test_test_environment(flash_substates: bool, expect_success: bool) {
+    // Arrange
+    let test_env_builder = TestEnvironmentBuilder::new();
+
+    let mut test_env = if flash_substates {
+        test_env_builder.with_crypto_utils_protocol_update()
+    } else {
+        test_env_builder.without_crypto_utils_protocol_update()
+    }
+    .build();
+
+    // Act
+    let result =
+        Package::compile_and_publish(path_local_blueprint!("crypto_scrypto"), &mut test_env);
+
+    // Assert
+    if expect_success {
+        let _package_address = result.unwrap();
+    } else {
+        let err = result.unwrap_err();
+        assert!(matches!(
+            err,
+            RuntimeError::ApplicationError(ApplicationError::PackageError(
+                PackageError::InvalidWasm(..)
+            ))
+        ));
     }
 }
