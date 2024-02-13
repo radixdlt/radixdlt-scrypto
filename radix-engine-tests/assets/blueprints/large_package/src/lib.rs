756701510ef8606c37316f6a46e9eede6f3e2a19
Jakub Krawczyk <jakub.krawczyk@rdx.works>
2024-02-13 14:17:22 +0100

    Updating assets (using docker-based script) + Updating the `large_package`'s size.

--- radix-engine-tests/assets/blueprints/large_package/src/lib.rs
+++ radix-engine-tests/assets/blueprints/large_package/src/lib.rs
@@ -1,11 +1,7 @@
 #![allow(unused_imports)]
 
 use address;
-use allocated_address;
 use auth_scenarios;
 use bucket;
-use fake_bucket;
-use fee_reserve_states;
 use non_fungible;
-use package;
 use proof;
