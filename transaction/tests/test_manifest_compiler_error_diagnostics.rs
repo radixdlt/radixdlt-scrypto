2f58730cff858a0906330fe870eca800602fd47e
Lukasz Rubaszewski <lukasz.rubaszewski@rdx.works>
2024-03-11 17:47:49 +0100

    Use compile_error_diagnostics in tests

--- transaction/tests/test_manifest_compiler_error_diagnostics.rs
+++ transaction/tests/test_manifest_compiler_error_diagnostics.rs
@@ -1,17 +1,21 @@
-use transaction::manifest::lexer::*;
-use transaction::manifest::parser::*;
+use radix_engine_common::network::NetworkDefinition;
+use transaction::manifest::blob_provider::*;
+use transaction::manifest::compiler::*;
 
 macro_rules! check_manifest {
     ( $manifest:expr) => {{
         let manifest = include_str!(concat!($manifest, ".rtm"));
         let diagnostic = include_str!(concat!($manifest, ".diag"));
 
-        let tokens = tokenize(manifest).unwrap();
-        let err = Parser::new(tokens, PARSER_MAX_DEPTH)
-            .parse_manifest()
-            .unwrap_err();
+        let err = compile(
+            manifest,
+            &NetworkDefinition::simulator(),
+            BlobProvider::default(),
+        )
+        .unwrap_err();
 
-        let x = parser_error_diagnostics(manifest, err);
+        let x = compile_error_diagnostics(manifest, err);
+
         if x != diagnostic {
             std::fs::write(format!("tests/{}.diag.res", $manifest), &x)
                 .expect("Unable to write file");
