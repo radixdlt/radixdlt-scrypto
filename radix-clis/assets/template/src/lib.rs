704eb010d4d99ea9a26a208bdd201d0caaa2daee
Yulong Wu <yulong.wu@rdx.works>
2024-04-04 11:03:17 +0100

    Add radix-cli assets

---
+++ radix-clis/assets/template/src/lib.rs
@@ -0,0 +1,47 @@
+use scrypto::prelude::*;
+
+#[blueprint]
+mod hello {
+    struct Hello {
+        // Define what resources and data will be managed by Hello components
+        sample_vault: Vault,
+    }
+
+    impl Hello {
+        // Implement the functions and methods which will manage those resources and data
+
+        // This is a function, and can be called directly on the blueprint once deployed
+        pub fn instantiate_hello() -> Global<Hello> {
+            // Create a new token called "HelloToken," with a fixed supply of 1000, and put that supply into a bucket
+            let my_bucket: Bucket = ResourceBuilder::new_fungible(OwnerRole::None)
+                .divisibility(DIVISIBILITY_MAXIMUM)
+                .metadata(metadata! {
+                    init {
+                        "name" => "HelloToken", locked;
+                        "symbol" => "HT", locked;
+                    }
+                })
+                .mint_initial_supply(1000)
+                .into();
+
+            // Instantiate a Hello component, populating its vault with our supply of 1000 HelloToken
+            Self {
+                sample_vault: Vault::with_bucket(my_bucket),
+            }
+            .instantiate()
+            .prepare_to_globalize(OwnerRole::None)
+            .globalize()
+        }
+
+        // This is a method, because it needs a reference to self.  Methods can only be called on components
+        pub fn free_token(&mut self) -> Bucket {
+            info!(
+                "My balance is: {} HelloToken. Now giving away a token!",
+                self.sample_vault.amount()
+            );
+            // If the semi-colon is omitted on the last line, the last value seen is automatically returned
+            // In this case, a bucket containing 1 HelloToken is returned
+            self.sample_vault.take(1)
+        }
+    }
+}
---
+++ radix-clis/assets/template/tests/lib.rs
@@ -0,0 +1,67 @@
+use scrypto_test::prelude::*;
+
+use ${wasm_name}::hello_test::*;
+
+#[test]
+fn test_hello() {
+    // Setup the environment
+    let mut ledger = LedgerSimulatorBuilder::new().build();
+
+    // Create an account
+    let (public_key, _private_key, account) = ledger.new_allocated_account();
+
+    // Publish package
+    let package_address = ledger.compile_and_publish(this_package!());
+
+    // Test the `instantiate_hello` function.
+    let manifest = ManifestBuilder::new()
+        .lock_fee_from_faucet()
+        .call_function(
+            package_address,
+            "Hello",
+            "instantiate_hello",
+            manifest_args!(),
+        )
+        .build();
+    let receipt = ledger.execute_manifest(
+        manifest,
+        vec![NonFungibleGlobalId::from_public_key(&public_key)],
+    );
+    println!("{:?}\n", receipt);
+    let component = receipt.expect_commit(true).new_component_addresses()[0];
+
+    // Test the `free_token` method.
+    let manifest = ManifestBuilder::new()
+        .lock_fee_from_faucet()
+        .call_method(component, "free_token", manifest_args!())
+        .call_method(
+            account,
+            "deposit_batch",
+            manifest_args!(ManifestExpression::EntireWorktop),
+        )
+        .build();
+    let receipt = ledger.execute_manifest(
+        manifest,
+        vec![NonFungibleGlobalId::from_public_key(&public_key)],
+    );
+    println!("{:?}\n", receipt);
+    receipt.expect_commit_success();
+}
+
+#[test]
+fn test_hello_with_test_environment() -> Result<(), RuntimeError> {
+    // Arrange
+    let mut env = TestEnvironment::new();
+    let package_address = PackageFactory::compile_and_publish(this_package!(), &mut env)?;
+
+    let mut hello = Hello::instantiate_hello(package_address, &mut env)?;
+
+    // Act
+    let bucket = hello.free_token(&mut env)?;
+
+    // Assert
+    let amount = bucket.amount(&mut env)?;
+    assert_eq!(amount, dec!("1"));
+
+    Ok(())
+}
