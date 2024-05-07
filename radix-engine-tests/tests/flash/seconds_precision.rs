06a448d3b0bb9c1db767855c3be6ec39dbe07520
Jakub Krawczyk <jakub.krawczyk@rdx.works>
2024-05-07 12:11:19 +0200

    Add `ProtocolUpdateBatchGenerator.batch_count()` for better integration of post-PU-scenarios in Node.

--- radix-engine-tests/tests/flash/seconds_precision.rs
+++ radix-engine-tests/tests/flash/seconds_precision.rs
@@ -41,15 +41,12 @@ fn run_flash_test(flash_substates: bool, expect_success: bool) {
         let anemone_protocol_update_batch_generator = AnemoneSettings::all_disabled()
             .enable(|item| &mut item.seconds_precision)
             .create_batch_generator();
-        let mut i = 0;
-        while let Some(batch) =
-            anemone_protocol_update_batch_generator.generate_batch(ledger.substate_db(), i)
-        {
-            i += 1;
+        for batch_index in 0..anemone_protocol_update_batch_generator.batch_count() {
+            let batch = anemone_protocol_update_batch_generator
+                .generate_batch(ledger.substate_db(), batch_index);
             for ProtocolUpdateTransactionDetails::FlashV1Transaction(
                 FlashProtocolUpdateTransactionDetails { state_updates, .. },
-            ) in batch.transactions
-            {
+            ) in batch.transactions {
                 ledger
                     .substate_db_mut()
                     .commit(&state_updates.create_database_updates::<SpreadPrefixKeyMapper>())
