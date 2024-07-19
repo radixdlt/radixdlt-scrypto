d97d182380b90c6574b9e21a23fc8ab8caa42e19
Lukasz Rubaszewski <117115317+lrubasze@users.noreply.github.com>
2024-07-19 12:29:38 +0200

    Remove wasmer support from Radix Engine

--- radix-engine/src/vm/wasm/mod.rs
+++ radix-engine/src/vm/wasm/mod.rs
@@ -4,13 +4,9 @@ mod prepare;
 mod traits;
 mod wasm_validator;
 mod wasm_validator_config;
-#[cfg(feature = "wasmer")]
-mod wasmer;
 mod wasmi;
 mod weights;
 
-#[cfg(feature = "wasmer")]
-pub use self::wasmer::*;
 pub use self::wasmi::*;
 pub use constants::*;
 pub use errors::*;
@@ -20,12 +16,5 @@ pub use wasm_validator::*;
 pub use wasm_validator_config::*;
 pub use weights::*;
 
-#[cfg(feature = "wasmer")]
-pub type DefaultWasmEngine = WasmerEngine;
-#[cfg(feature = "wasmer")]
-pub type DefaultWasmInstance = WasmerInstance;
-
-#[cfg(not(feature = "wasmer"))]
 pub type DefaultWasmEngine = WasmiEngine;
-#[cfg(not(feature = "wasmer"))]
 pub type DefaultWasmInstance = WasmiInstance;
