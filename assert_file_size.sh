c778a2e0a95a8dcf955b253e798d5b6869e50ce0
Lukasz Rubaszewski <117115317+lrubasze@users.noreply.github.com>
2024-07-19 14:23:43 +0200

    Print asserted file size and return if upon error

--- assert_file_size.sh
+++ assert_file_size.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+set -e
+set -o pipefail
+
 actual=$(du -sb $1 | awk '{ print $1 }')
 limit=$(($2))
 
 if [ $actual -gt $limit ]; then
     echo "File $1 is too large: actual = $actual, limit = $limit"
     exit 1
+else
+    echo "File $1 size ok: actual = $actual, limit = $limit"
 fi
