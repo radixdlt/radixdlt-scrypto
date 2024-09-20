33902cb194ea75dc4901ab88578b1a50899647a8
gguutttss <fonteinbart@gmail.com>
2024-09-20 03:50:23 +0200

    fix: made locker package visible in scrypto-test
    Using TestEnvironment in combination with packages that made use of the AccountLocker blueprint wasn't possible. Now it is.

--- scrypto-test/src/environment/constants.rs
+++ scrypto-test/src/environment/constants.rs
@@ -2,7 +2,7 @@ use crate::prelude::*;
 
 /// Defines the set of Nodes that all test [`CallFrame`]s have visibility to when they're first
 /// created. This contains all of the well-known addresses of nodes.
-pub(super) const GLOBAL_VISIBLE_NODES: [NodeId; 28] = [
+pub(super) const GLOBAL_VISIBLE_NODES: [NodeId; 29] = [
     XRD.into_node_id(),
     SECP256K1_SIGNATURE_RESOURCE.into_node_id(),
     ED25519_SIGNATURE_RESOURCE.into_node_id(),
@@ -20,6 +20,7 @@ pub(super) const GLOBAL_VISIBLE_NODES: [NodeId; 28] = [
     CONSENSUS_MANAGER_PACKAGE.into_node_id(),
     ACCESS_CONTROLLER_PACKAGE.into_node_id(),
     POOL_PACKAGE.into_node_id(),
+    LOCKER_PACKAGE.into_node_id(),
     TRANSACTION_PROCESSOR_PACKAGE.into_node_id(),
     METADATA_MODULE_PACKAGE.into_node_id(),
     ROYALTY_MODULE_PACKAGE.into_node_id(),
