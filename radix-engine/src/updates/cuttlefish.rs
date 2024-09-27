d3b88f1fdc6e5f7622a4f323b77b1ef99c309eb9
Omar <OmarAbdulla7@hotmail.com>
2024-09-27 16:50:31 +0300

    Account getter methods protocol update

--- radix-engine/src/updates/cuttlefish.rs
+++ radix-engine/src/updates/cuttlefish.rs
@@ -1,7 +1,11 @@
+use radix_engine_interface::blueprints::account::*;
 use radix_transactions::validation::*;
 
 use super::*;
-use crate::{kernel::kernel::KernelBoot, system::system_callback::*};
+use crate::blueprints::account::*;
+use crate::kernel::kernel::KernelBoot;
+use crate::system::system_callback::*;
+use crate::system::system_db_reader::*;
 
 #[derive(Clone)]
 pub struct CuttlefishSettings {
@@ -11,6 +15,8 @@ pub struct CuttlefishSettings {
     pub kernel_version_update: UpdateSetting<NoSettings>,
     /// Add transaction validation changes
     pub transaction_validation_update: UpdateSetting<NoSettings>,
+    /// Adds getter methods for the account blueprint.
+    pub account_getter_methods: UpdateSetting<NoSettings>,
 }
 
 impl UpdateSettings for CuttlefishSettings {
@@ -25,6 +31,7 @@ impl UpdateSettings for CuttlefishSettings {
             system_logic_update: UpdateSetting::enabled_as_default_for_network(network),
             kernel_version_update: UpdateSetting::enabled_as_default_for_network(network),
             transaction_validation_update: UpdateSetting::enabled_as_default_for_network(network),
+            account_getter_methods: UpdateSetting::enabled_as_default_for_network(network),
         }
     }
 
@@ -33,6 +40,7 @@ impl UpdateSettings for CuttlefishSettings {
             system_logic_update: UpdateSetting::Disabled,
             kernel_version_update: UpdateSetting::Disabled,
             transaction_validation_update: UpdateSetting::Disabled,
+            account_getter_methods: UpdateSetting::Disabled,
         }
     }
 
@@ -85,6 +93,7 @@ fn generate_principal_batch(
         system_logic_update,
         kernel_version_update: always_visible_global_nodes_update,
         transaction_validation_update,
+        account_getter_methods,
     }: &CuttlefishSettings,
 ) -> ProtocolUpdateBatch {
     let mut transactions = vec![];
@@ -106,7 +115,22 @@ fn generate_principal_batch(
             generate_cuttlefish_transaction_validation_updates(),
         ));
     }
+    if let UpdateSetting::Enabled(NoSettings) = &account_getter_methods {
+        transactions.push(ProtocolUpdateTransactionDetails::flash(
+            "cuttlefish-account-getter-methods",
+            generate_cuttlefish_account_getters_extension_state_updates(store),
+        ));
+    }
     ProtocolUpdateBatch { transactions }
+}
+
+/// A quick macro for encoding and unwrapping.
+macro_rules! scrypto_encode {
+    (
+        $expr: expr
+    ) => {
+        ::radix_common::prelude::scrypto_encode($expr).unwrap()
+    };
 }
 
 fn generate_system_logic_v2_updates<S: SubstateDatabase + ?Sized>(db: &S) -> StateUpdates {
@@ -159,4 +183,165 @@ fn generate_cuttlefish_transaction_validation_updates() -> StateUpdates {
             ),
         ),
     )
+}
+
+fn generate_cuttlefish_account_getters_extension_state_updates<S: SubstateDatabase + ?Sized>(
+    db: &S,
+) -> StateUpdates {
+    let reader = SystemDatabaseReader::new(db);
+    let node_id = ACCOUNT_PACKAGE.into_node_id();
+    let blueprint_version_key = BlueprintVersionKey {
+        blueprint: ACCOUNT_BLUEPRINT.to_string(),
+        version: Default::default(),
+    };
+
+    // Creating the original code substates for extension.
+    let (code_hash, (code_substate, vm_type_substate)) = {
+        let original_code = (NativeCodeId::AccountCode3 as u64).to_be_bytes().to_vec();
+
+        let code_hash = CodeHash::from_hash(hash(&original_code));
+        let code_substate = PackageCodeOriginalCodeV1 {
+            code: original_code,
+        }
+        .into_versioned()
+        .into_locked_substate();
+        let vm_type_substate = PackageCodeVmTypeV1 {
+            vm_type: VmType::Native,
+        }
+        .into_locked_substate();
+
+        (code_hash, (code_substate, vm_type_substate))
+    };
+
+    // Creating the new schema substate with the methods added by the extension
+    let (added_functions, schema) = AccountBlueprintCuttlefishExtension::added_functions_schema();
+    let (schema_hash, schema_substate) =
+        (schema.generate_schema_hash(), schema.into_locked_substate());
+
+    // Update the auth config of the account blueprint to have these added methods and have them be
+    // public.
+    let auth_config = {
+        let mut auth_config = AccountBlueprint::get_definition().auth_config;
+        let MethodAuthTemplate::StaticRoleDefinition(StaticRoleDefinition {
+            ref mut methods, ..
+        }) = auth_config.method_auth
+        else {
+            panic!("Account doesn't have a static role definition")
+        };
+        methods.extend(
+            added_functions
+                .keys()
+                .map(ToOwned::to_owned)
+                .map(|ident| MethodKey { ident })
+                .map(|key| (key, MethodAccessibility::Public)),
+        );
+        auth_config.into_locked_substate()
+    };
+
+    // Updating the blueprint definition of the existing blueprint with the added functions.
+    let blueprint_definition_substate = {
+        let mut blueprint_definition = reader
+            .read_object_collection_entry::<_, VersionedPackageBlueprintVersionDefinition>(
+                &node_id,
+                ObjectModuleId::Main,
+                ObjectCollectionKey::KeyValue(
+                    PackageCollection::BlueprintVersionDefinitionKeyValue.collection_index(),
+                    &blueprint_version_key,
+                ),
+            )
+            .unwrap()
+            .unwrap()
+            .fully_update_and_into_latest_version();
+
+        for (function_name, added_function) in added_functions.into_iter() {
+            let TypeRef::Static(input_local_id) = added_function.input else {
+                unreachable!()
+            };
+            let TypeRef::Static(output_local_id) = added_function.output else {
+                unreachable!()
+            };
+
+            blueprint_definition.function_exports.insert(
+                function_name.clone(),
+                PackageExport {
+                    code_hash,
+                    export_name: function_name.clone(),
+                },
+            );
+            blueprint_definition.interface.functions.insert(
+                function_name,
+                FunctionSchema {
+                    receiver: added_function.receiver,
+                    input: BlueprintPayloadDef::Static(ScopedTypeId(schema_hash, input_local_id)),
+                    output: BlueprintPayloadDef::Static(ScopedTypeId(schema_hash, output_local_id)),
+                },
+            );
+        }
+
+        blueprint_definition.into_locked_substate()
+    };
+
+    // Getting the partition number of the various collections that we're updating
+    let [blueprint_version_definition_partition_number, code_vm_type_partition_number, code_original_code_partition_number, schema_partition_number, blueprint_version_auth_config_partition_number] =
+        [
+            PackageCollection::BlueprintVersionDefinitionKeyValue,
+            PackageCollection::CodeVmTypeKeyValue,
+            PackageCollection::CodeOriginalCodeKeyValue,
+            PackageCollection::SchemaKeyValue,
+            PackageCollection::BlueprintVersionAuthConfigKeyValue,
+        ]
+        .map(|package_collection| {
+            reader
+                .get_partition_of_collection(
+                    &node_id,
+                    ObjectModuleId::Main,
+                    package_collection.collection_index(),
+                )
+                .unwrap()
+        });
+
+    // Generating the state updates
+    StateUpdates {
+        by_node: indexmap!(
+            node_id => NodeStateUpdates::Delta {
+                by_partition: indexmap! {
+                    blueprint_version_definition_partition_number => PartitionStateUpdates::Delta {
+                        by_substate: indexmap! {
+                            SubstateKey::Map(scrypto_encode!(&blueprint_version_key)) => DatabaseUpdate::Set(
+                                scrypto_encode!(&blueprint_definition_substate)
+                            )
+                        }
+                    },
+                    code_vm_type_partition_number => PartitionStateUpdates::Delta {
+                        by_substate: indexmap! {
+                            SubstateKey::Map(scrypto_encode!(&code_hash)) => DatabaseUpdate::Set(
+                                scrypto_encode!(&vm_type_substate)
+                            )
+                        }
+                    },
+                    code_original_code_partition_number => PartitionStateUpdates::Delta {
+                        by_substate: indexmap! {
+                            SubstateKey::Map(scrypto_encode!(&code_hash)) => DatabaseUpdate::Set(
+                                scrypto_encode!(&code_substate)
+                            )
+                        }
+                    },
+                    schema_partition_number => PartitionStateUpdates::Delta {
+                        by_substate: indexmap! {
+                            SubstateKey::Map(scrypto_encode!(&schema_hash)) => DatabaseUpdate::Set(
+                                scrypto_encode!(&schema_substate)
+                            )
+                        }
+                    },
+                    blueprint_version_auth_config_partition_number => PartitionStateUpdates::Delta {
+                        by_substate: indexmap! {
+                            SubstateKey::Map(scrypto_encode!(&blueprint_version_key)) => DatabaseUpdate::Set(
+                                scrypto_encode!(&auth_config)
+                            )
+                        }
+                    },
+                }
+            }
+        ),
+    }
 }
