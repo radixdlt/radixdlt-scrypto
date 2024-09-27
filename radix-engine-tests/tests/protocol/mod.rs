4f96d0dc60b172883c358245332d60590fd20ba7
Omar <OmarAbdulla7@hotmail.com>
2024-09-27 09:40:31 +0300

    Fix the metadata for cuttlefish

--- radix-engine-tests/tests/protocol/mod.rs
+++ radix-engine-tests/tests/protocol/mod.rs
@@ -1,5 +1,6 @@
 // We used to use automod, but it breaks various tools
 // such as cargo fmt, so let's just list them explicitly.
 mod cuttlefish_transaction_changes;
+mod metadata;
 mod protocol_updates;
 mod transaction;
--- radix-engine/src/updates/cuttlefish.rs
+++ radix-engine/src/updates/cuttlefish.rs
@@ -1,7 +1,9 @@
 use radix_transactions::validation::*;
 
 use super::*;
+use crate::object_modules::metadata::*;
 use crate::system::system_callback::*;
+use crate::system::system_db_reader::*;
 
 #[derive(Clone)]
 pub struct CuttlefishSettings {
@@ -9,6 +11,8 @@ pub struct CuttlefishSettings {
     pub system_logic_update: UpdateSetting<NoSettings>,
     /// Add transaction validation changes
     pub transaction_validation_update: UpdateSetting<NoSettings>,
+    /// Update the metadata for cuttlefish
+    pub update_metadata: UpdateSetting<NoSettings>,
 }
 
 impl UpdateSettings for CuttlefishSettings {
@@ -22,6 +26,7 @@ impl UpdateSettings for CuttlefishSettings {
         Self {
             system_logic_update: UpdateSetting::enabled_as_default_for_network(network),
             transaction_validation_update: UpdateSetting::enabled_as_default_for_network(network),
+            update_metadata: UpdateSetting::enabled_as_default_for_network(network),
         }
     }
 
@@ -29,6 +34,7 @@ impl UpdateSettings for CuttlefishSettings {
         Self {
             system_logic_update: UpdateSetting::Disabled,
             transaction_validation_update: UpdateSetting::Disabled,
+            update_metadata: UpdateSetting::Disabled,
         }
     }
 
@@ -80,6 +86,7 @@ fn generate_principal_batch(
     CuttlefishSettings {
         system_logic_update,
         transaction_validation_update,
+        update_metadata,
     }: &CuttlefishSettings,
 ) -> ProtocolUpdateBatch {
     let mut transactions = vec![];
@@ -93,6 +100,12 @@ fn generate_principal_batch(
         transactions.push(ProtocolUpdateTransactionDetails::flash(
             "cuttlefish-transaction-validation-updates",
             generate_cuttlefish_transaction_validation_updates(),
+        ));
+    }
+    if let UpdateSetting::Enabled(NoSettings) = &update_metadata {
+        transactions.push(ProtocolUpdateTransactionDetails::flash(
+            "cuttlefish-update-metadata",
+            generate_cuttlefish_metadata_fix(store),
         ));
     }
     ProtocolUpdateBatch { transactions }
@@ -129,4 +142,87 @@ fn generate_cuttlefish_transaction_validation_updates() -> StateUpdates {
             ),
         ),
     )
+}
+
+fn generate_cuttlefish_metadata_fix<S: SubstateDatabase + ?Sized>(db: &S) -> StateUpdates {
+    struct MetadataUpdates {
+        pub name: Option<MetadataValue>,
+        pub description: Option<MetadataValue>,
+        pub icon_url: Option<MetadataValue>,
+    }
+
+    impl MetadataUpdates {
+        pub fn into_map(self) -> IndexMap<String, MetadataValue> {
+            [
+                self.name.map(|value| ("name", value)),
+                self.description.map(|value| ("description", value)),
+                self.icon_url.map(|value| ("icon_url", value)),
+            ]
+            .into_iter()
+            .flatten()
+            .map(|(k, v)| (k.to_owned(), v))
+            .collect()
+        }
+    }
+
+    let reader = SystemDatabaseReader::new(db);
+
+    // The metadata entries that we would like to update and the values that we would like to update
+    // them to be.
+    let metadata_updates = indexmap! {
+        XRD => MetadataUpdates {
+            name: None,
+            description: None,
+            icon_url: Some(MetadataValue::Url(UncheckedUrl("https://assets.radixdlt.com/icons/icon-xrd.png".into())))
+        },
+        PACKAGE_OF_DIRECT_CALLER_RESOURCE => MetadataUpdates {
+            name: Some(MetadataValue::String("Package Caller Resource".into())),
+            description: Some(MetadataValue::String("Badges generated automatically by the Radix system to represent the authority of the package for a direct caller. These badges cease to exist at the end of their transaction.".into())),
+            icon_url: Some(MetadataValue::Url(UncheckedUrl("https://assets.radixdlt.com/icons/icon-package_of_direct_caller_resource.png".into())))
+        },
+        GLOBAL_CALLER_RESOURCE => MetadataUpdates {
+            name: Some(MetadataValue::String("Global Caller Resource".into())),
+            description: Some(MetadataValue::String("Badges generated automatically by the Radix system to represent the authority of a global caller. These badges cease to exist at the end of their transaction.".into())),
+            icon_url: Some(MetadataValue::Url(UncheckedUrl("https://assets.radixdlt.com/icons/icon-global_caller_resource.png".into())))
+        },
+        SECP256K1_SIGNATURE_RESOURCE => MetadataUpdates {
+            name: Some(MetadataValue::String("ECDSA Secp256k1 Signature Resource".into())),
+            description: Some(MetadataValue::String("Badges generated automatically by the Radix system to represent ECDSA Secp256k1 signatures applied to transactions. These badges cease to exist at the end of their transaction.".into())),
+            icon_url: Some(MetadataValue::Url(UncheckedUrl("https://assets.radixdlt.com/icons/icon-ecdsa_secp256k1_signature_resource.png".into())))
+        },
+        ED25519_SIGNATURE_RESOURCE => MetadataUpdates {
+            name: Some(MetadataValue::String("EdDSA Ed25519 Resource".into())),
+            description: Some(MetadataValue::String("Badges generated automatically by the Radix system to represent EdDSA Ed25519 signatures applied to transactions. These badges cease to exist at the end of their transaction.".into())),
+            icon_url: Some(MetadataValue::Url(UncheckedUrl("https://assets.radixdlt.com/icons/icon-eddsa_ed25519_signature_resource.png".into())))
+        },
+        SYSTEM_EXECUTION_RESOURCE => MetadataUpdates {
+            name: Some(MetadataValue::String("System Execution Resource".into())),
+            description: Some(MetadataValue::String("Badges are created under this resource to represent the Radix system's authority at genesis and to affect changes to system entities during protocol updates, or to represent the Radix system's authority in the regularly occurring system transactions including round and epoch changes.".into())),
+            icon_url: Some(MetadataValue::Url(UncheckedUrl("https://assets.radixdlt.com/icons/icon-system_execution_resource.png".into())))
+        },
+    };
+
+    let mut state_updates = StateUpdates::empty();
+    for (resource_address, metadata_updates) in metadata_updates.into_iter() {
+        for (key, value) in metadata_updates.into_map().into_iter() {
+            let partition_number = reader
+                .get_partition_of_collection(
+                    resource_address.as_node_id(),
+                    ModuleId::Metadata,
+                    MetadataCollection::EntryKeyValue.collection_index(),
+                )
+                .unwrap();
+
+            state_updates = state_updates.set_substate(
+                resource_address,
+                partition_number,
+                SubstateKey::Map(
+                    scrypto_encode(&MetadataEntryKeyPayload { content: key }).unwrap(),
+                ),
+                value.into_locked_substate(),
+            );
+        }
+    }
+
+    state_updates
 }
