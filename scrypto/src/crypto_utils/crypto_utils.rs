721a8c459dc1fbffd409d81d8ce2ed68c3b53e82
Lukasz Rubaszewski <117115317+lrubasze@users.noreply.github.com>
2024-10-02 18:55:54 +0200

    Accept impl AsRef if possible in CryptoUtils

--- scrypto/src/crypto_utils/crypto_utils.rs
+++ scrypto/src/crypto_utils/crypto_utils.rs
@@ -13,16 +13,16 @@ impl CryptoUtils {
     /// Performs BLS12-381 G2 signature verification.
     /// Domain specifier tag: BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_POP_
     pub fn bls12381_v1_verify(
-        message: Vec<u8>,
-        public_key: Bls12381G1PublicKey,
-        signature: Bls12381G2Signature,
+        message: impl AsRef<[u8]>,
+        public_key: impl AsRef<Bls12381G1PublicKey>,
+        signature: impl AsRef<Bls12381G2Signature>,
     ) -> bool {
-        let public_key: Vec<u8> = scrypto_encode(&public_key).unwrap();
-        let signature: Vec<u8> = scrypto_encode(&signature).unwrap();
+        let public_key: Vec<u8> = scrypto_encode(public_key.as_ref()).unwrap();
+        let signature: Vec<u8> = scrypto_encode(signature.as_ref()).unwrap();
         unsafe {
             crypto_utils::crypto_utils_bls12381_v1_verify(
-                message.as_ptr(),
-                message.len(),
+                message.as_ref().as_ptr(),
+                message.as_ref().len(),
                 public_key.as_ptr(),
                 public_key.len(),
                 signature.as_ptr(),
@@ -36,10 +36,10 @@ impl CryptoUtils {
     /// Domain specifier tag: BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_POP_
     pub fn bls12381_v1_aggregate_verify(
         pub_keys_and_msgs: Vec<(Bls12381G1PublicKey, Vec<u8>)>,
-        signature: Bls12381G2Signature,
+        signature: impl AsRef<Bls12381G2Signature>,
     ) -> bool {
         let pub_keys_and_msgs: Vec<u8> = scrypto_encode(&pub_keys_and_msgs).unwrap();
-        let signature: Vec<u8> = scrypto_encode(&signature).unwrap();
+        let signature: Vec<u8> = scrypto_encode(signature.as_ref()).unwrap();
         unsafe {
             crypto_utils::crypto_utils_bls12381_v1_aggregate_verify(
                 pub_keys_and_msgs.as_ptr(),
@@ -54,16 +54,16 @@ impl CryptoUtils {
     /// one message signed with multiple keys.
     /// Domain specifier tag: BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_POP_
     pub fn bls12381_v1_fast_aggregate_verify(
-        message: Vec<u8>,
-        public_keys: Vec<Bls12381G1PublicKey>,
-        signature: Bls12381G2Signature,
+        message: impl AsRef<[u8]>,
+        public_keys: impl AsRef<Vec<Bls12381G1PublicKey>>,
+        signature: impl AsRef<Bls12381G2Signature>,
     ) -> bool {
-        let public_keys: Vec<u8> = scrypto_encode(&public_keys).unwrap();
-        let signature: Vec<u8> = scrypto_encode(&signature).unwrap();
+        let public_keys: Vec<u8> = scrypto_encode(public_keys.as_ref()).unwrap();
+        let signature: Vec<u8> = scrypto_encode(signature.as_ref()).unwrap();
         unsafe {
             crypto_utils::crypto_utils_bls12381_v1_fast_aggregate_verify(
-                message.as_ptr(),
-                message.len(),
+                message.as_ref().as_ptr(),
+                message.as_ref().len(),
                 public_keys.as_ptr(),
                 public_keys.len(),
                 signature.as_ptr(),
@@ -74,9 +74,9 @@ impl CryptoUtils {
 
     /// Aggregate multiple BLS12-381 G2 signatures into single one
     pub fn bls12381_g2_signature_aggregate(
-        signatures: Vec<Bls12381G2Signature>,
+        signatures: impl AsRef<Vec<Bls12381G2Signature>>,
     ) -> Bls12381G2Signature {
-        let signatures: Vec<u8> = scrypto_encode(&signatures).unwrap();
+        let signatures: Vec<u8> = scrypto_encode(signatures.as_ref()).unwrap();
         let agg_signature = copy_buffer(unsafe {
             crypto_utils::crypto_utils_bls12381_g2_signature_aggregate(
                 signatures.as_ptr(),
@@ -88,18 +88,18 @@ impl CryptoUtils {
     }
 
     /// Calculates Keccak-256 digest over given vector of bytes
-    pub fn keccak256_hash(data: Vec<u8>) -> Hash {
+    pub fn keccak256_hash(data: impl AsRef<[u8]>) -> Hash {
         let hash = copy_buffer(unsafe {
-            crypto_utils::crypto_utils_keccak256_hash(data.as_ptr(), data.len())
+            crypto_utils::crypto_utils_keccak256_hash(data.as_ref().as_ptr(), data.as_ref().len())
         });
 
         Hash(hash.try_into().unwrap())
     }
 
     /// Calculates Blake2b-256 digest over given vector of bytes
-    pub fn blake2b_256_hash(data: Vec<u8>) -> Hash {
+    pub fn blake2b_256_hash(data: impl AsRef<[u8]>) -> Hash {
         let hash = copy_buffer(unsafe {
-            crypto_utils::crypto_utils_blake2b_256_hash(data.as_ptr(), data.len())
+            crypto_utils::crypto_utils_blake2b_256_hash(data.as_ref().as_ptr(), data.as_ref().len())
         });
 
         Hash(hash.try_into().unwrap())
@@ -107,16 +107,16 @@ impl CryptoUtils {
 
     /// Performs Ed25519 signature verification.
     pub fn ed25519_verify(
-        message: Vec<u8>,
-        public_key: Ed25519PublicKey,
-        signature: Ed25519Signature,
+        message: impl AsRef<[u8]>,
+        public_key: impl AsRef<Ed25519PublicKey>,
+        signature: impl AsRef<Ed25519Signature>,
     ) -> bool {
-        let public_key: Vec<u8> = scrypto_encode(&public_key).unwrap();
-        let signature: Vec<u8> = scrypto_encode(&signature).unwrap();
+        let public_key: Vec<u8> = scrypto_encode(public_key.as_ref()).unwrap();
+        let signature: Vec<u8> = scrypto_encode(signature.as_ref()).unwrap();
         unsafe {
             crypto_utils::crypto_utils_ed25519_verify(
-                message.as_ptr(),
-                message.len(),
+                message.as_ref().as_ptr(),
+                message.as_ref().len(),
                 public_key.as_ptr(),
                 public_key.len(),
                 signature.as_ptr(),
@@ -127,16 +127,16 @@ impl CryptoUtils {
 
     /// Performs ECDSA Secp256k1 verification.
     pub fn secp256k1_ecdsa_verify(
-        message: Vec<u8>,
-        public_key: Secp256k1PublicKey,
-        signature: Secp256k1Signature,
+        message: impl AsRef<[u8]>,
+        public_key: impl AsRef<Secp256k1PublicKey>,
+        signature: impl AsRef<Secp256k1Signature>,
     ) -> bool {
-        let public_key: Vec<u8> = scrypto_encode(&public_key).unwrap();
-        let signature: Vec<u8> = scrypto_encode(&signature).unwrap();
+        let public_key: Vec<u8> = scrypto_encode(public_key.as_ref()).unwrap();
+        let signature: Vec<u8> = scrypto_encode(signature.as_ref()).unwrap();
         unsafe {
             crypto_utils::crypto_utils_secp256k1_ecdsa_verify(
-                message.as_ptr(),
-                message.len(),
+                message.as_ref().as_ptr(),
+                message.as_ref().len(),
                 public_key.as_ptr(),
                 public_key.len(),
                 signature.as_ptr(),
