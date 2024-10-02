e1f25ed9a1e64f720193477504ec46e9850f4ee3
Omar <OmarAbdulla7@hotmail.com>
2024-10-02 19:43:42 +0300

    Change the source of the metadata in the toolkit receipt

--- radix-engine-toolkit/src/receipt/receipt/runtime.rs
+++ radix-engine-toolkit/src/receipt/receipt/runtime.rs
@@ -83,50 +83,8 @@ impl TryFrom<TransactionReceiptV1> for RuntimeToolkitTransactionReceipt {
                                 .map(|value| value.into_node_id()),
                         )
                         .collect(),
-                    // We get the metadata updates from the events.
-                    metadata_updates: application_events
-                        .iter()
-                        .fold(
-                            IndexMap::<NodeId, IndexMap<String, MetadataUpdate<RuntimeTypeSelector>>>::new(),
-                            |mut acc, (EventTypeIdentifier(emitter, event_name), event_data)| {
-                                // Check if this is a metadata emitter and if this is a metadata event.
-                                match emitter {
-                                    Emitter::Method(node_id, ModuleId::Metadata) => {
-                                        match event_name.as_str() {
-                                            SetMetadataEvent::EVENT_NAME => {
-                                                let SetMetadataEvent { key, value } =
-                                                    scrypto_decode::<SetMetadataEvent>(event_data)
-                                                        .expect("Must succeed!");
-
-                                                acc.entry(*node_id)
-                                                    .or_default()
-                                                    .insert(key, MetadataUpdate::Set(value));
-                                                acc
-                                            }
-                                            RemoveMetadataEvent::EVENT_NAME => {
-                                                let RemoveMetadataEvent { key } =
-                                                    scrypto_decode::<RemoveMetadataEvent>(event_data)
-                                                        .expect("Must succeed!");
-
-                                                // If the metadata field was set and then unset in the same
-                                                // tx then we remove the entry from the map.
-                                                let map = acc.entry(*node_id).or_default();
-                                                if map.swap_remove(&key).is_none() {
-                                                    map.insert(key, MetadataUpdate::Delete);
-                                                }
-
-                                                acc
-                                            }
-                                            _ => acc,
-                                        }
-                                    }
-                                    Emitter::Method(..) | Emitter::Function(..) => acc,
-                                }
-                            },
-                        )
-                        .into_iter()
-                        .filter(|(_, metadata_updates)| !metadata_updates.is_empty())
-                        .collect(),
+                    // We get the metadata updates from the state updates.
+                    metadata_updates: get_metadata_updates(&state_updates),
                     // We get the non-fungible data updates from the state updates directly.
                     non_fungible_data_updates: state_updates
                         .by_node
@@ -151,22 +109,24 @@ impl TryFrom<TransactionReceiptV1> for RuntimeToolkitTransactionReceipt {
                                     )
                                     .unwrap();
 
-                                updates_by_partition.get(&partition_number)?
+                                updates_by_partition
+                                    .get(&partition_number)?
                                     .iter_map_entries()
                                     .filter_map(|(key, value)| match value {
                                         DatabaseUpdateRef::Set(value) => Some((key, value)),
                                         DatabaseUpdateRef::Delete => None,
                                     })
                                     .map(|(key, value)| -> Option<_> {
-                                        let key =
-                                            scrypto_decode::<NonFungibleResourceManagerDataKeyPayload>(key)
-                                                .ok()
-                                                .map(|id| {
-                                                    NonFungibleGlobalId::new(
-                                                        non_fungible_resource_address,
-                                                        id.content,
-                                                    )
-                                                })?;
+                                        let key = scrypto_decode::<
+                                            NonFungibleResourceManagerDataKeyPayload,
+                                        >(key)
+                                        .ok()
+                                        .map(|id| {
+                                            NonFungibleGlobalId::new(
+                                                non_fungible_resource_address,
+                                                id.content,
+                                            )
+                                        })?;
                                         let value = scrypto_decode::<
                                             NonFungibleResourceManagerDataEntrySubstate,
                                         >(value)
@@ -183,38 +143,49 @@ impl TryFrom<TransactionReceiptV1> for RuntimeToolkitTransactionReceipt {
                     // We get the newly minted non-fungibles from the events.
                     newly_minted_non_fungibles: application_events.iter().fold(
                         IndexSet::new(),
-                        |mut acc, (EventTypeIdentifier(emitter, event_name), event_data)| match emitter {
-                            Emitter::Method(node_id, ModuleId::Main) => {
-                                match (ResourceAddress::try_from(*node_id), event_name.as_str()) {
-                                    (Ok(resource_address), MintNonFungibleResourceEvent::EVENT_NAME) => {
-                                        let MintNonFungibleResourceEvent { ids } =
-                                            scrypto_decode(event_data).expect("Must succeed!");
-                                        acc.extend(
-                                            ids.into_iter().map(|value| {
+                        |mut acc, (EventTypeIdentifier(emitter, event_name), event_data)| {
+                            match emitter {
+                                Emitter::Method(node_id, ModuleId::Main) => {
+                                    match (ResourceAddress::try_from(*node_id), event_name.as_str())
+                                    {
+                                        (
+                                            Ok(resource_address),
+                                            MintNonFungibleResourceEvent::EVENT_NAME,
+                                        ) => {
+                                            let MintNonFungibleResourceEvent { ids } =
+                                                scrypto_decode(event_data).expect("Must succeed!");
+                                            acc.extend(ids.into_iter().map(|value| {
                                                 NonFungibleGlobalId::new(resource_address, value)
-                                            }),
-                                        );
-                                        acc
+                                            }));
+                                            acc
+                                        }
+                                        (
+                                            Ok(resource_address),
+                                            BurnNonFungibleResourceEvent::EVENT_NAME,
+                                        ) => {
+                                            let BurnNonFungibleResourceEvent { ids } =
+                                                scrypto_decode(event_data).expect("Must succeed!");
+                                            ids.into_iter()
+                                                .map(|value| {
+                                                    NonFungibleGlobalId::new(
+                                                        resource_address,
+                                                        value,
+                                                    )
+                                                })
+                                                .for_each(|global_id| {
+                                                    acc.shift_remove(&global_id);
+                                                });
+                                            acc
+                                        }
+                                        _ => acc,
                                     }
-                                    (Ok(resource_address), BurnNonFungibleResourceEvent::EVENT_NAME) => {
-                                        let BurnNonFungibleResourceEvent { ids } =
-                                            scrypto_decode(event_data).expect("Must succeed!");
-                                        ids.into_iter()
-                                            .map(|value| NonFungibleGlobalId::new(resource_address, value))
-                                            .for_each(|global_id| {
-                                                acc.shift_remove(&global_id);
-                                            });
-                                        acc
-                                    }
-                                    _ => acc,
                                 }
+                                Emitter::Method(..) | Emitter::Function(..) => acc,
                             }
-                            Emitter::Method(..) | Emitter::Function(..) => acc,
                         },
                     ),
                 },
-                worktop_changes: execution_trace
-                    .worktop_changes(),
+                worktop_changes: execution_trace.worktop_changes(),
                 fee_summary: FeeSummary {
                     execution_fees_in_xrd: fee_summary.total_execution_cost_in_xrd,
                     finalization_fees_in_xrd: fee_summary.total_finalization_cost_in_xrd,
@@ -259,4 +230,48 @@ impl TryFrom<TransactionReceiptV1> for RuntimeToolkitTransactionReceipt {
             }),
         }
     }
+}
+
+fn get_metadata_updates(
+    state_updates: &StateUpdates,
+) -> IndexMap<NodeId, IndexMap<String, MetadataUpdate<RuntimeTypeSelector>>> {
+    let desired_partition =
+        MetadataPartitionOffset::EntryKeyValue.as_partition(METADATA_BASE_PARTITION);
+    state_updates
+        .by_node
+        .iter()
+        .flat_map(|(node_id, NodeStateUpdates::Delta { by_partition })| {
+            by_partition
+                .get(&desired_partition)
+                .map(|partition_state_updates| (node_id, partition_state_updates))
+        })
+        .fold(
+            IndexMap::new(),
+            |mut acc, (node_id, partition_state_updates)| {
+                acc.entry(*node_id).or_default().extend(
+                    partition_state_updates
+                        .iter_map_entries()
+                        .map(|(key, database_update)| {
+                            let key =
+                                scrypto_decode::<MetadataEntryKeyPayload>(key).expect("Can't fail");
+                            let value = match database_update {
+                                DatabaseUpdateRef::Set(value) => {
+                                    let value = scrypto_decode::<MetadataEntryEntrySubstate>(value)
+                                        .expect("Can't fail")
+                                        .into_value();
+                                    match value {
+                                        Some(value) => MetadataUpdate::Set(
+                                            value.fully_update_and_into_latest_version(),
+                                        ),
+                                        None => MetadataUpdate::Delete,
+                                    }
+                                }
+                                DatabaseUpdateRef::Delete => MetadataUpdate::Delete,
+                            };
+                            (key.content, value)
+                        }),
+                );
+                acc
+            },
+        )
 }
