7c101c8bad81dc3cdcac81b84d9d759cf7351cb3
Joshua Primero <josh.primero@rdx.works>
2024-10-14 08:31:51 +0800

    Add more tests

--- radix-engine-tests/tests/system/assert_next_call_returns.rs
+++ radix-engine-tests/tests/system/assert_next_call_returns.rs
@@ -3,206 +3,286 @@ use scrypto_test::prelude::*;
 
 #[test]
 fn when_more_is_returned_assert_next_call_returns_include_should_succeed() {
-    run_return_two_resources_test(
-        |resource1, _resource2| {
-            ManifestResourceConstraints::new().with(
-                resource1,
-                ManifestResourceConstraint::AtLeastAmount(dec!(1)),
-            )
-        },
-        false,
-        None,
-    );
+    for use_yield in [true, false] {
+        run_return_two_resources_test(
+            |resource1, _resource2| {
+                ManifestResourceConstraints::new().with(
+                    resource1,
+                    ManifestResourceConstraint::AtLeastAmount(dec!(1)),
+                )
+            },
+            false,
+            use_yield,
+            None,
+        );
+    }
 }
 
 #[test]
 fn when_more_is_returned_assert_next_call_returns_only_should_fail() {
-    run_return_two_resources_test(
-        |resource1, _resource2| {
-            ManifestResourceConstraints::new().with(
-                resource1,
-                ManifestResourceConstraint::AtLeastAmount(dec!(1)),
-            )
-        },
-        true,
-        Some(ManifestResourceConstraintsError::UnwantedResourcesExist),
-    );
+    for use_yield in [true, false] {
+        run_return_two_resources_test(
+            |resource1, _resource2| {
+                ManifestResourceConstraints::new().with(
+                    resource1,
+                    ManifestResourceConstraint::AtLeastAmount(dec!(1)),
+                )
+            },
+            true,
+            use_yield,
+            Some(ManifestResourceConstraintsError::UnwantedResourcesExist),
+        );
+    }
 }
 
 #[test]
 fn when_exact_is_returned_assert_next_call_returns_include_should_succeed() {
-    run_return_two_resources_test(
-        |resource1, resource2| {
-            ManifestResourceConstraints::new()
-                .with(
-                    resource1,
-                    ManifestResourceConstraint::AtLeastAmount(dec!(1)),
-                )
-                .with(
-                    resource2,
-                    ManifestResourceConstraint::AtLeastAmount(dec!(1)),
-                )
-        },
-        false,
-        None,
-    );
+    for use_yield in [true, false] {
+        run_return_two_resources_test(
+            |resource1, resource2| {
+                ManifestResourceConstraints::new()
+                    .with(
+                        resource1,
+                        ManifestResourceConstraint::AtLeastAmount(dec!(1)),
+                    )
+                    .with(
+                        resource2,
+                        ManifestResourceConstraint::AtLeastAmount(dec!(1)),
+                    )
+            },
+            false,
+            use_yield,
+            None,
+        );
+    }
 }
 
 #[test]
 fn when_exact_is_returned_assert_next_call_returns_only_should_succeed() {
-    run_return_two_resources_test(
-        |resource1, resource2| {
-            ManifestResourceConstraints::new()
-                .with(
-                    resource1,
-                    ManifestResourceConstraint::AtLeastAmount(dec!(1)),
-                )
-                .with(
-                    resource2,
-                    ManifestResourceConstraint::AtLeastAmount(dec!(1)),
-                )
-        },
-        true,
-        None,
-    );
+    for use_yield in [true, false] {
+        run_return_two_resources_test(
+            |resource1, resource2| {
+                ManifestResourceConstraints::new()
+                    .with(
+                        resource1,
+                        ManifestResourceConstraint::AtLeastAmount(dec!(1)),
+                    )
+                    .with(
+                        resource2,
+                        ManifestResourceConstraint::AtLeastAmount(dec!(1)),
+                    )
+            },
+            true,
+            use_yield,
+            None,
+        );
+    }
 }
 
 #[test]
 fn when_less_is_returned_assert_next_call_returns_include_should_fail() {
-    run_return_two_resources_test(
-        |resource1, _resource2| {
-            ManifestResourceConstraints::new()
-                .with(
-                    resource1,
-                    ManifestResourceConstraint::AtLeastAmount(dec!(1)),
-                )
-                .with(XRD, ManifestResourceConstraint::AtLeastAmount(dec!(1)))
-        },
-        false,
-        Some(ManifestResourceConstraintsError::ResourceConstraint(
-            ResourceConstraintError::ExpectedAtLeastAmount {
-                expected_at_least_amount: dec!(1),
-                actual_amount: dec!(0),
+    for use_yield in [true, false] {
+        run_return_two_resources_test(
+            |resource1, _resource2| {
+                ManifestResourceConstraints::new()
+                    .with(
+                        resource1,
+                        ManifestResourceConstraint::AtLeastAmount(dec!(1)),
+                    )
+                    .with(XRD, ManifestResourceConstraint::AtLeastAmount(dec!(1)))
             },
-        )),
-    );
+            false,
+            use_yield,
+            Some(ManifestResourceConstraintsError::ResourceConstraint(
+                ResourceConstraintError::ExpectedAtLeastAmount {
+                    expected_at_least_amount: dec!(1),
+                    actual_amount: dec!(0),
+                },
+            )),
+        );
+    }
 }
 
 #[test]
 fn when_less_is_returned_assert_next_call_returns_only_should_fail() {
-    run_return_two_resources_test(
-        |resource1, _resource2| {
-            ManifestResourceConstraints::new()
-                .with(
-                    resource1,
-                    ManifestResourceConstraint::AtLeastAmount(dec!(1)),
-                )
-                .with(XRD, ManifestResourceConstraint::AtLeastAmount(dec!(1)))
-        },
-        true,
-        Some(ManifestResourceConstraintsError::ResourceConstraint(
-            ResourceConstraintError::ExpectedAtLeastAmount {
-                expected_at_least_amount: dec!(1),
-                actual_amount: dec!(0),
+    for use_yield in [true, false] {
+        run_return_two_resources_test(
+            |resource1, _resource2| {
+                ManifestResourceConstraints::new()
+                    .with(
+                        resource1,
+                        ManifestResourceConstraint::AtLeastAmount(dec!(1)),
+                    )
+                    .with(XRD, ManifestResourceConstraint::AtLeastAmount(dec!(1)))
             },
-        )),
-    );
+            true,
+            use_yield,
+            Some(ManifestResourceConstraintsError::ResourceConstraint(
+                ResourceConstraintError::ExpectedAtLeastAmount {
+                    expected_at_least_amount: dec!(1),
+                    actual_amount: dec!(0),
+                },
+            )),
+        );
+    }
 }
 
 #[test]
 fn when_empty_constraints_on_assert_next_call_returns_include_should_succeed() {
-    run_return_two_resources_test(
-        |_resource1, _resource2| ManifestResourceConstraints::new(),
-        false,
-        None,
-    );
+    for use_yield in [true, false] {
+        run_return_two_resources_test(
+            |_resource1, _resource2| ManifestResourceConstraints::new(),
+            false,
+            use_yield,
+            None,
+        );
+    }
 }
 
 #[test]
 fn when_empty_constraints_on_assert_next_call_returns_only_should_fail() {
-    run_return_two_resources_test(
-        |_resource1, _resource2| ManifestResourceConstraints::new(),
-        true,
-        Some(ManifestResourceConstraintsError::UnwantedResourcesExist),
-    );
+    for use_yield in [true, false] {
+        run_return_two_resources_test(
+            |_resource1, _resource2| ManifestResourceConstraints::new(),
+            true,
+            use_yield,
+            Some(ManifestResourceConstraintsError::UnwantedResourcesExist),
+        );
+    }
 }
 
 #[test]
 fn when_extra_zero_constraints_on_assert_next_call_returns_include_should_succeed() {
-    run_return_two_resources_test(
-        |resource1, resource2| {
-            ManifestResourceConstraints::new()
-                .with(
-                    resource1,
-                    ManifestResourceConstraint::AtLeastAmount(dec!(1)),
-                )
-                .with(
-                    resource2,
-                    ManifestResourceConstraint::AtLeastAmount(dec!(1)),
-                )
-                .with(XRD, ManifestResourceConstraint::ExactAmount(dec!(0)))
-        },
-        false,
-        None,
-    );
+    for use_yield in [true, false] {
+        run_return_two_resources_test(
+            |resource1, resource2| {
+                ManifestResourceConstraints::new()
+                    .with(
+                        resource1,
+                        ManifestResourceConstraint::AtLeastAmount(dec!(1)),
+                    )
+                    .with(
+                        resource2,
+                        ManifestResourceConstraint::AtLeastAmount(dec!(1)),
+                    )
+                    .with(XRD, ManifestResourceConstraint::ExactAmount(dec!(0)))
+            },
+            false,
+            use_yield,
+            None,
+        );
+    }
 }
 
 #[test]
 fn when_extra_zero_constraints_on_assert_next_call_returns_only_should_succeed() {
-    run_return_two_resources_test(
-        |resource1, resource2| {
-            ManifestResourceConstraints::new()
-                .with(
-                    resource1,
-                    ManifestResourceConstraint::AtLeastAmount(dec!(1)),
-                )
-                .with(
-                    resource2,
-                    ManifestResourceConstraint::AtLeastAmount(dec!(1)),
-                )
-                .with(XRD, ManifestResourceConstraint::ExactAmount(dec!(0)))
-        },
-        true,
-        None,
-    );
+    for use_yield in [true, false] {
+        run_return_two_resources_test(
+            |resource1, resource2| {
+                ManifestResourceConstraints::new()
+                    .with(
+                        resource1,
+                        ManifestResourceConstraint::AtLeastAmount(dec!(1)),
+                    )
+                    .with(
+                        resource2,
+                        ManifestResourceConstraint::AtLeastAmount(dec!(1)),
+                    )
+                    .with(XRD, ManifestResourceConstraint::ExactAmount(dec!(0)))
+            },
+            true,
+            use_yield,
+            None,
+        );
+    }
 }
 
 fn run_return_two_resources_test(
     constraints: fn(ResourceAddress, ResourceAddress) -> ManifestResourceConstraints,
     exact: bool,
+    use_yield: bool,
     result: Option<ManifestResourceConstraintsError>,
 ) {
     // Arrange
     let mut ledger = LedgerSimulatorBuilder::new().build();
     let (public_key, _, account) = ledger.new_allocated_account();
-    let (pool, lp, resource1, resource2) = create_pool(&mut ledger, account, &public_key);
 
     // Act
     let transaction = {
-        let mut builder = ManifestBuilder::new_v2()
-            .lock_fee_from_faucet()
-            .withdraw_from_account(account, lp, dec!(1))
-            .take_all_from_worktop(lp, "lp");
+        if !use_yield {
+            let (pool, lp, resource1, resource2) = create_pool(&mut ledger, account, &public_key);
+            let mut builder = ManifestBuilder::new_v2()
+                .lock_fee_from_faucet()
+                .withdraw_from_account(account, lp, dec!(1))
+                .take_all_from_worktop(lp, "lp");
 
-        builder = if exact {
-            builder.assert_next_call_returns_only(constraints(resource1, resource2))
+            builder = if exact {
+                builder.assert_next_call_returns_only(constraints(resource1, resource2))
+            } else {
+                builder.assert_next_call_returns_include(constraints(resource1, resource2))
+            };
+
+            let manifest = builder
+                .with_bucket("lp", |builder, bucket| {
+                    builder.call_method(
+                        pool,
+                        TWO_RESOURCE_POOL_REDEEM_IDENT,
+                        TwoResourcePoolRedeemManifestInput { bucket },
+                    )
+                })
+                .deposit_entire_worktop(account)
+                .build();
+            TestTransaction::new_v2_builder(ledger.next_transaction_nonce())
+                .finish_with_root_intent(manifest, [public_key.signature_proof()])
         } else {
-            builder.assert_next_call_returns_include(constraints(resource1, resource2))
-        };
+            let resource1 = ledger.create_freely_mintable_fungible_resource(
+                OwnerRole::None,
+                None,
+                DIVISIBILITY_MAXIMUM,
+                account,
+            );
+            let resource2 = ledger.create_freely_mintable_fungible_resource(
+                OwnerRole::None,
+                None,
+                DIVISIBILITY_MAXIMUM,
+                account,
+            );
 
-        builder
-            .with_bucket("lp", |builder, bucket| {
-                builder.call_method(
-                    pool,
-                    TWO_RESOURCE_POOL_REDEEM_IDENT,
-                    TwoResourcePoolRedeemManifestInput { bucket },
-                )
-            })
-            .deposit_entire_worktop(account)
-            .build()
+            let mut builder = ManifestBuilder::new_subintent_v2();
+            builder = if exact {
+                builder.assert_next_call_returns_only(constraints(resource1, resource2))
+            } else {
+                builder.assert_next_call_returns_include(constraints(resource1, resource2))
+            };
+            let child = builder
+                .yield_to_parent(())
+                .deposit_entire_worktop(account)
+                .yield_to_parent(())
+                .build();
+
+            let mut txn_builder = TestTransaction::new_v2_builder(ledger.next_transaction_nonce());
+            let child = txn_builder.add_subintent(child, [public_key.signature_proof()]);
+            txn_builder.finish_with_root_intent(
+                ManifestBuilder::new_v2()
+                    .lock_fee_from_faucet()
+                    .use_child("child", child)
+                    .yield_to_child("child", ())
+                    .mint_fungible(resource1, dec!(10))
+                    .mint_fungible(resource2, dec!(10))
+                    .take_all_from_worktop(resource1, "resource1")
+                    .take_all_from_worktop(resource2, "resource2")
+                    .with_name_lookup(|builder, lookup| {
+                        builder.yield_to_child(
+                            "child",
+                            (lookup.bucket("resource1"), lookup.bucket("resource2")),
+                        )
+                    })
+                    .build(),
+                [public_key.signature_proof()],
+            )
+        }
     };
-    let transaction = TestTransaction::new_v2_builder(ledger.next_transaction_nonce())
-        .finish_with_root_intent(transaction, [public_key.signature_proof()]);
+
     let receipt = ledger.execute_test_transaction(transaction);
 
     // Assert
