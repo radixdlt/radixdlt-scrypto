28dfe7b937374520be1a7bb72d97308a468d49ea
Lukasz Rubaszewski <117115317+lrubasze@users.noreply.github.com>
2024-10-24 09:10:45 +0200

    Comment updated

--- scrypto-compiler/tests/assets/call_indirect/c_src/call_indirect.c
+++ scrypto-compiler/tests/assets/call_indirect/c_src/call_indirect.c
@@ -11,11 +11,11 @@ int __attribute__ ((noinline)) internal_sub(int x, int y) {
 // Binary encoding of the `call_indirect` instruction changes depending on the `reference-types`.
 //
 // - reference-types disabled
-//    0x11                      - call_indirect instruction
+//    0x11                      - call_indirect binary opcode
 //    0x80 0x80 0x80 0x80 0x00  - 32-bit zero LEB encoded (function table index)
 //    0x00                      - fixed zero (reserved value)
 // - reference-types enabled
-//    0x11                      - call_indirect instruction
+//    0x11                      - call_indirect binary opcode
 //    0x80 0x80 0x80 0x80 0x00  - 32-bit zero LEB encoded (function table index)
 //    0x80 0x80 0x80 0x80 0x00  - 32-bit zero LEB encoded (filled later by linker with the final table index)
 //
