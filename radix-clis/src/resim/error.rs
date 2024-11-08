d512ff450e71a992ef3d97f85d2fcb2d6c9f7ea3
David Edey <david.edey@rdx.works>
2024-11-08 14:14:09 +0000

    fix: Remove commented out code

--- radix-clis/src/resim/error.rs
+++ radix-clis/src/resim/error.rs
@@ -87,64 +87,6 @@ impl From<Error> for String {
     }
 }
 
-// impl Debug for Error {
-//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-//         let address_encoder = AddressBech32Encoder::for_simulator();
-//         match self {
-//             Error::PackageNotFound(package_address) => {
-//                 write!(
-//                     f,
-//                     "PackageNotFound({})",
-//                     package_address.display(&address_encoder)
-//                 )
-//             }
-//             Error::SchemaNotFound(node_id, schema_hash) => {
-//                 write!(
-//                     f,
-//                     "SchemaNotFound({}, {schema_hash:?})",
-//                     node_id.display(&address_encoder)
-//                 )
-//             }
-//             Error::BlueprintNotFound(package_address, message) => {
-//                 write!(
-//                     f,
-//                     "BlueprintNotFound({}, {message})",
-//                     package_address.display(&address_encoder)
-//                 )
-//             }
-//             Error::ComponentNotFound(component_address) => {
-//                 write!(
-//                     f,
-//                     "ComponentNotFound({})",
-//                     component_address.display(&address_encoder)
-//                 )
-//             }
-//             Error::InstanceSchemaNot(component_address, index) => {
-//                 write!(
-//                     f,
-//                     "InstanceSchemaNot({}, {index})",
-//                     component_address.display(&address_encoder)
-//                 )
-//             }
-//             Error::TransactionFailed(runtime_error) => {
-//                 write!(
-//                     f,
-//                     "TransactionFailed({})",
-//                     runtime_error.display(&address_encoder)
-//                 )
-//             }
-//             Error::TransactionRejected(rejection_reason) => {
-//                 write!(
-//                     f,
-//                     "TransactionRejected({})",
-//                     rejection_reason.display(&address_encoder)
-//                 )
-//             }
-//             other => write!(f, "{:?}", other),
-//         }
-//     }
-// }
-
 impl Debug for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let address_encoder = AddressBech32Encoder::for_simulator();
