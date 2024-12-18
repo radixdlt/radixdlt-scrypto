174b2202305ac1d12499b279c025df70fc77e90e
Lukasz Rubaszewski <117115317+lrubasze@users.noreply.github.com>
2024-12-18 12:43:20 +0100

    Test execution tracing for non-fungible resources transfers

--- radix-engine-tests/assets/blueprints/execution_trace/src/execution_trace.rs
+++ radix-engine-tests/assets/blueprints/execution_trace/src/execution_trace.rs
@@ -1,5 +1,8 @@
 use scrypto::prelude::*;
 
+#[derive(ScryptoSbor, NonFungibleData)]
+pub struct DummyData {}
+
 #[blueprint]
 mod execution_trace_test {
     struct ExecutionTraceBp {
@@ -7,7 +10,7 @@ mod execution_trace_test {
     }
 
     impl ExecutionTraceBp {
-        pub fn transfer_resource_between_two_components(
+        pub fn transfer_fungible_resource_between_two_components(
             amount: u8,
             use_take_advanced: bool,
         ) -> (
@@ -45,12 +48,57 @@ mod execution_trace_test {
             (resource_address, source_component, target_component)
         }
 
+        pub fn transfer_non_fungible_resource_between_two_components(
+            amount: u8,
+        ) -> (
+            ResourceAddress,
+            Global<ExecutionTraceBp>,
+            Global<ExecutionTraceBp>,
+        ) {
+            let mut entries = vec![];
+            for i in 1..amount as u64 + 1 {
+                entries.push((i.into(), DummyData {}));
+            }
+            let bucket = ResourceBuilder::new_integer_non_fungible::<DummyData>(OwnerRole::None)
+                .mint_initial_supply(entries);
+            let resource_address = bucket.resource_address();
+
+            let source_component = ExecutionTraceBp {
+                vault: Vault::with_bucket(bucket.into()),
+            }
+            .instantiate()
+            .prepare_to_globalize(OwnerRole::None)
+            .globalize();
+
+            let target_component = ExecutionTraceBp {
+                vault: Vault::new(resource_address),
+            }
+            .instantiate()
+            .prepare_to_globalize(OwnerRole::None)
+            .globalize();
+
+            let transfer_bucket: Bucket = source_component.take_non_fungibles(amount);
+            target_component.put(transfer_bucket);
+
+            (resource_address, source_component, target_component)
+        }
+
         pub fn take(&mut self, amount: u8) -> Bucket {
             self.vault.take(amount)
         }
 
         pub fn take_advanced(&mut self, amount: u8) -> Bucket {
             self.vault.take_advanced(amount, WithdrawStrategy::Exact)
+        }
+
+        pub fn take_non_fungibles(&mut self, amount: u8) -> Bucket {
+            let amount = amount as u64;
+            let mut ids = indexset![];
+            for i in 1..amount + 1 {
+                ids.insert(NonFungibleLocalId::integer(i));
+            }
+
+            self.vault.as_non_fungible().take_non_fungibles(&ids).into()
         }
 
         pub fn put(&mut self, b: Bucket) {
@@ -70,3 +118,4 @@ mod execution_trace_test {
         }
     }
 }
+
