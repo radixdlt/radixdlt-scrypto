bad219039c5a4cf8ee19799367d6409b3beb67e6
Omar <OmarAbdulla7@hotmail.com>
2025-01-21 13:27:33 +0300

    fix tests

--- radix-engine-tests/tests/misc/typed_manifest_invocation.rs
+++ radix-engine-tests/tests/misc/typed_manifest_invocation.rs
@@ -89,13 +89,12 @@ fn test_that_all_functions_are_in_the_typed_invocation_type() {
                     // At this point we know that both types have the same set of functions. We can
                     // now iterate over the function list and compare their schemas to ensure that
                     // they're equal.
-                    let function_idents = constructed_function_type_functions_idents;
-                    for function_ident in function_idents.iter() {
+                    for function_ident in constructed_function_type_functions_idents.iter() {
                         let constructed_function_schema = constructed_function_type_functions
-                            .get(function_ident)
+                            .get(*function_ident)
                             .unwrap();
                         let canonical_function_schema = canonical_function_type_functions
-                            .get(function_ident)
+                            .get(*function_ident)
                             .unwrap();
 
                         compare_single_type_schemas(
@@ -126,207 +125,190 @@ fn test_that_all_functions_are_in_the_typed_invocation_type() {
     }
 }
 
-mod test_types {
-    use super::*;
-    use radix_rust::rust::collections::hash_map::Entry;
+#[derive(Clone, Debug, Default)]
+pub struct FunctionTable(HashMap<BlueprintName, BlueprintFunctionTable>);
 
-    #[derive(Clone, Debug, Default)]
-    pub struct FunctionTable(HashMap<BlueprintName, BlueprintFunctionTable>);
+impl IntoIterator for FunctionTable {
+    type Item = <HashMap<BlueprintName, BlueprintFunctionTable> as IntoIterator>::Item;
+    type IntoIter = <HashMap<BlueprintName, BlueprintFunctionTable> as IntoIterator>::IntoIter;
 
-    impl IntoIterator for FunctionTable {
-        type Item = <HashMap<BlueprintName, BlueprintFunctionTable> as IntoIterator>::Item;
-        type IntoIter = <HashMap<BlueprintName, BlueprintFunctionTable> as IntoIterator>::IntoIter;
+    fn into_iter(self) -> Self::IntoIter {
+        self.0.into_iter()
+    }
+}
 
-        fn into_iter(self) -> Self::IntoIter {
-            self.0.into_iter()
-        }
+impl FunctionTable {
+    pub fn get(&self, key: &BlueprintName) -> Option<&BlueprintFunctionTable> {
+        self.0.get(key)
     }
 
-    impl FunctionTable {
-        pub fn get(&self, key: &BlueprintName) -> Option<&BlueprintFunctionTable> {
-            self.0.get(key)
-        }
+    /// Constructs the function table of the [`TypedManifestNativeInvocation`] typed based on the
+    /// function table defined by the macro.
+    pub fn construct_from_typed_invocation_function_table() -> Self {
+        let mut this = Self::default();
 
-        pub fn entry(
-            &mut self,
-            key: BlueprintName,
-        ) -> Entry<'_, BlueprintName, BlueprintFunctionTable> {
-            self.0.entry(key)
-        }
+        let function_table = typed_native_invocation_function_table();
+        for (blueprint_name, functions_map) in function_table.into_iter() {
+            let blueprint_name = BlueprintName(blueprint_name.to_owned());
 
-        /// Constructs the function table of the [`TypedManifestNativeInvocation`] typed based on the
-        /// function table defined by the macro.
-        pub fn construct_from_typed_invocation_function_table() -> Self {
-            let mut this = Self::default();
+            for (function_type, functions_map) in functions_map.into_iter() {
+                let function_type = FunctionType::from_str(function_type).unwrap();
 
-            let function_table = typed_native_invocation_function_table();
-            for (blueprint_name, functions_map) in function_table.into_iter() {
-                let blueprint_name = BlueprintName(blueprint_name.to_owned());
+                for (function_ident, input_schema) in functions_map.into_iter() {
+                    let function_ident = FunctionIdent(function_ident.to_owned());
 
-                for (function_type, functions_map) in functions_map.into_iter() {
-                    let function_type = FunctionType::from_str(function_type).unwrap();
-
-                    for (function_ident, input_schema) in functions_map.into_iter() {
-                        let function_ident = FunctionIdent(function_ident.to_owned());
-
-                        this.entry(blueprint_name.clone())
-                            .or_default()
-                            .entry(function_type)
-                            .or_default()
-                            .insert(function_ident, input_schema);
-                    }
+                    this.0
+                        .entry(blueprint_name.clone())
+                        .or_default()
+                        .0
+                        .entry(function_type)
+                        .or_default()
+                        .insert(function_ident, input_schema);
                 }
             }
-
-            this
         }
 
-        pub fn construct_from_substate_database<S: SubstateDatabase + ListableSubstateDatabase>(
-            database: &S,
-        ) -> Self {
-            let mut this = Self::default();
-            let reader = SystemDatabaseReader::new(database);
+        this
+    }
 
-            // Getting all of the packages on ledger.
-            let packages = database
-                .list_partition_keys()
-                .map(|value| SpreadPrefixKeyMapper::from_db_node_key(&value.node_key))
-                .filter_map(|node_id| PackageAddress::try_from(node_id.0.as_slice()).ok())
-                .collect::<HashSet<_>>();
+    pub fn construct_from_substate_database<S: SubstateDatabase + ListableSubstateDatabase>(
+        database: &S,
+    ) -> Self {
+        let mut this = Self::default();
+        let reader = SystemDatabaseReader::new(database);
 
-            // Constructing the function table from all of the packages on-ledger
-            for package_address in packages.into_iter() {
-                for (BlueprintVersionKey { blueprint, .. }, blueprint_definition) in
-                    reader.get_package_definition(package_address).into_iter()
+        // Getting all of the packages on ledger.
+        let packages = database
+            .list_partition_keys()
+            .map(|value| SpreadPrefixKeyMapper::from_db_node_key(&value.node_key))
+            .filter_map(|node_id| PackageAddress::try_from(node_id.0.as_slice()).ok())
+            .collect::<HashSet<_>>();
+
+        // Constructing the function table from all of the packages on-ledger
+        for package_address in packages.into_iter() {
+            for (BlueprintVersionKey { blueprint, .. }, blueprint_definition) in
+                reader.get_package_definition(package_address).into_iter()
+            {
+                let blueprint_name = BlueprintName(blueprint);
+
+                for (
+                    function_ident,
+                    FunctionSchema {
+                        receiver, input, ..
+                    },
+                ) in blueprint_definition.interface.functions.into_iter()
                 {
-                    let blueprint_name = BlueprintName(blueprint);
+                    let function_type = FunctionType::from(receiver);
+                    let function_ident = FunctionIdent(function_ident);
 
-                    for (
-                        function_ident,
-                        FunctionSchema {
-                            receiver, input, ..
-                        },
-                    ) in blueprint_definition.interface.functions.into_iter()
-                    {
-                        let function_type = FunctionType::from(receiver);
-                        let function_ident = FunctionIdent(function_ident);
+                    let BlueprintPayloadDef::Static(ScopedTypeId(schema_hash, type_id)) = input
+                    else {
+                        unreachable!()
+                    };
+                    let schema = reader
+                        .get_schema(package_address.as_node_id(), &schema_hash)
+                        .unwrap()
+                        .deref()
+                        .clone();
 
-                        let BlueprintPayloadDef::Static(ScopedTypeId(schema_hash, type_id)) = input
-                        else {
-                            unreachable!()
-                        };
-                        let schema = reader
-                            .get_schema(package_address.as_node_id(), &schema_hash)
-                            .unwrap()
-                            .deref()
-                            .clone();
+                    let single_type_schema = SingleTypeSchema { schema, type_id };
 
-                        let single_type_schema = SingleTypeSchema { schema, type_id };
-
-                        this.entry(blueprint_name.clone())
-                            .or_default()
-                            .entry(function_type)
-                            .or_default()
-                            .insert(function_ident, single_type_schema);
-                    }
+                    this.0
+                        .entry(blueprint_name.clone())
+                        .or_default()
+                        .0
+                        .entry(function_type)
+                        .or_default()
+                        .insert(function_ident, single_type_schema);
                 }
             }
-
-            this
-        }
-    }
-
-    #[derive(Clone, Debug, Default)]
-    pub struct BlueprintFunctionTable(
-        HashMap<FunctionType, HashMap<FunctionIdent, SingleTypeSchema<ScryptoCustomSchema>>>,
-    );
-
-    impl BlueprintFunctionTable {
-        pub fn get(
-            &self,
-            key: &FunctionType,
-        ) -> Option<&HashMap<FunctionIdent, SingleTypeSchema<ScryptoCustomSchema>>> {
-            self.0.get(key)
         }
 
-        pub fn entry(
-            &mut self,
-            key: FunctionType,
-        ) -> Entry<'_, FunctionType, HashMap<FunctionIdent, SingleTypeSchema<ScryptoCustomSchema>>>
-        {
-            self.0.entry(key)
-        }
+        this
+    }
+}
 
-        pub fn function_idents_set(&self, function_type: &FunctionType) -> HashSet<&FunctionIdent> {
-            self.0
-                .get(function_type)
-                .into_iter()
-                .flat_map(|value| value.keys())
-                .collect()
-        }
+#[derive(Clone, Debug, Default)]
+pub struct BlueprintFunctionTable(
+    HashMap<FunctionType, HashMap<FunctionIdent, SingleTypeSchema<ScryptoCustomSchema>>>,
+);
+
+impl BlueprintFunctionTable {
+    pub fn get(
+        &self,
+        key: &FunctionType,
+    ) -> Option<&HashMap<FunctionIdent, SingleTypeSchema<ScryptoCustomSchema>>> {
+        self.0.get(key)
     }
 
-    impl IntoIterator for BlueprintFunctionTable {
-        type Item = <HashMap<
-            FunctionType,
-            HashMap<FunctionIdent, SingleTypeSchema<ScryptoCustomSchema>>,
-        > as IntoIterator>::Item;
-        type IntoIter = <HashMap<
-            FunctionType,
-            HashMap<FunctionIdent, SingleTypeSchema<ScryptoCustomSchema>>,
-        > as IntoIterator>::IntoIter;
-
-        fn into_iter(self) -> Self::IntoIter {
-            self.0.into_iter()
-        }
+    pub fn function_idents_set(&self, function_type: &FunctionType) -> HashSet<&FunctionIdent> {
+        self.0
+            .get(function_type)
+            .into_iter()
+            .flat_map(|value| value.keys())
+            .collect()
     }
+}
 
-    #[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
-    pub struct FunctionIdent(String);
+impl IntoIterator for BlueprintFunctionTable {
+    type Item = <HashMap<
+        FunctionType,
+        HashMap<FunctionIdent, SingleTypeSchema<ScryptoCustomSchema>>,
+    > as IntoIterator>::Item;
+    type IntoIter = <HashMap<
+        FunctionType,
+        HashMap<FunctionIdent, SingleTypeSchema<ScryptoCustomSchema>>,
+    > as IntoIterator>::IntoIter;
 
-    #[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
-    pub struct BlueprintName(String);
-
-    #[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
-    pub enum FunctionType {
-        Function,
-        Method,
-        DirectMethod,
+    fn into_iter(self) -> Self::IntoIter {
+        self.0.into_iter()
     }
+}
 
-    impl FunctionType {
-        pub const ALL: [Self; 3] = [Self::Function, Self::Method, Self::DirectMethod];
+#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct FunctionIdent(String);
+
+#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct BlueprintName(String);
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum FunctionType {
+    Function,
+    Method,
+    DirectMethod,
+}
+
+impl FunctionType {
+    pub const ALL: [Self; 3] = [Self::Function, Self::Method, Self::DirectMethod];
+}
+
+impl Display for FunctionType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        Debug::fmt(&self, f)
     }
+}
 
-    impl Display for FunctionType {
-        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-            Debug::fmt(&self, f)
-        }
-    }
+impl FromStr for FunctionType {
+    type Err = ();
 
-    impl FromStr for FunctionType {
-        type Err = ();
-
-        fn from_str(s: &str) -> Result<Self, Self::Err> {
-            match s {
-                "Function" => Ok(Self::Function),
-                "Method" => Ok(Self::Method),
-                "DirectMethod" => Ok(Self::DirectMethod),
-                _ => Err(()),
-            }
-        }
-    }
-
-    impl From<Option<ReceiverInfo>> for FunctionType {
-        fn from(value: Option<ReceiverInfo>) -> Self {
-            match value {
-                Some(value) if value.ref_types.contains(RefTypes::DIRECT_ACCESS) => {
-                    FunctionType::DirectMethod
-                }
-                Some(_) => FunctionType::Method,
-                None => FunctionType::Function,
-            }
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "Function" => Ok(Self::Function),
+            "Method" => Ok(Self::Method),
+            "DirectMethod" => Ok(Self::DirectMethod),
+            _ => Err(()),
         }
     }
 }
-use test_types::*;
+
+impl From<Option<ReceiverInfo>> for FunctionType {
+    fn from(value: Option<ReceiverInfo>) -> Self {
+        match value {
+            Some(value) if value.ref_types.contains(RefTypes::DIRECT_ACCESS) => {
+                FunctionType::DirectMethod
+            }
+            Some(_) => FunctionType::Method,
+            None => FunctionType::Function,
+        }
+    }
+}
